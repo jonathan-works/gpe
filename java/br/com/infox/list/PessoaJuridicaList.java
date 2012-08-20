d8861ccb882d63819a6fc170314ed6330c4526c2
erikliberal <erikliberal@069f99d3-da65-4ab0-85f1-cf03781be9e7>
2012-08-20 11:14:53 +0000

    git-svn-id: http://svn.infox.com.br/repos/infoxnovoepa/aplicacao/trunk/epa2@141 069f99d3-da65-4ab0-85f1-cf03781be9e7

---
+++ java/br/com/infox/list/PessoaJuridicaList.java
@@ -0,0 +1,50 @@
+package br.com.infox.list;
+
+import java.util.Map;
+
+import org.jboss.seam.ScopeType;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Scope;
+import org.jboss.seam.annotations.intercept.BypassInterceptors;
+
+import br.com.infox.core.action.list.EntityList;
+import br.com.infox.core.action.list.SearchCriteria;
+import br.com.infox.ibpm.entity.PessoaJuridica;
+
+@Name(PessoaJuridicaList.NAME)
+@BypassInterceptors
+@Scope(ScopeType.PAGE)
+public class PessoaJuridicaList extends EntityList<PessoaJuridica> {
+
+	public static final String NAME = "pjList";
+
+	private static final long serialVersionUID = 1L;
+	
+	private static final String DEFAULT_EJBQL = "select o from PessoaJuridica o";
+	private static final String DEFAULT_ORDER = "nome";
+	
+	@Override
+	protected void addSearchFields() {
+		addSearchField("cnpj", SearchCriteria.igual);
+		addSearchField("nome", SearchCriteria.contendo);
+		addSearchField("razaoSocial", SearchCriteria.contendo);
+		addSearchField("ativo", SearchCriteria.igual);
+	}
+	
+	@Override
+	protected String getDefaultEjbql() {
+		return DEFAULT_EJBQL;
+	}
+	
+	@Override
+	protected String getDefaultOrder() {
+		return DEFAULT_ORDER;
+	}
+	
+	@Override
+	protected Map<String, String> getCustomColumnsOrder() {
+		return null;
+	}
+	
+	
+}
