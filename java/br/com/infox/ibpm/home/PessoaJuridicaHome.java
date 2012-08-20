2bbaa2ea106b766d3440ed5328a5c136e04fb61a
erikliberal <erikliberal@069f99d3-da65-4ab0-85f1-cf03781be9e7>
2012-08-20 11:14:56 +0000

    git-svn-id: http://svn.infox.com.br/repos/infoxnovoepa/aplicacao/trunk/epa2@145 069f99d3-da65-4ab0-85f1-cf03781be9e7

---
+++ java/br/com/infox/ibpm/home/PessoaJuridicaHome.java
@@ -0,0 +1,19 @@
+package br.com.infox.ibpm.home;
+
+import org.jboss.seam.ScopeType;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Scope;
+import org.jboss.seam.annotations.intercept.BypassInterceptors;
+
+import br.com.infox.ibpm.entity.PessoaJuridica;
+import br.com.itx.component.AbstractHome;
+
+@Name(PessoaJuridicaHome.NAME)
+@BypassInterceptors
+@Scope(ScopeType.PAGE)
+public class PessoaJuridicaHome extends AbstractHome<PessoaJuridica>{
+
+	public static final String NAME = "pjHome";
+	private static final long serialVersionUID = 1L;
+	
+}
