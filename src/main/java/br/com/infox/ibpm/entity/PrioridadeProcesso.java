69d70148175a5840c3d77a138ff49f39ae6fc637
erikliberal <erikliberal@069f99d3-da65-4ab0-85f1-cf03781be9e7>
2012-08-24 14:24:58 +0000

    Atualizando da base schema
    git-svn-id: http://svn.infox.com.br/repos/infoxnovoepa/aplicacao/trunk/epa2@179 069f99d3-da65-4ab0-85f1-cf03781be9e7

---
+++ src/main/java/br/com/infox/ibpm/entity/PrioridadeProcesso.java
@@ -0,0 +1,54 @@
+package br.com.infox.ibpm.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.*;
+
+@Entity
+@Table(schema="core", name=PrioridadeProcesso.TABLE_NAME)
+public class PrioridadeProcesso implements Serializable {
+	
+	public static final String TABLE_NAME = "tb_prioridade_processo";
+	private static final long serialVersionUID = 1L;
+	
+	private Integer idPrioridade;
+	private String descricaoPrioridade;
+	private Integer peso;
+	private Boolean ativo;
+	
+	@SequenceGenerator(name="generator", sequenceName="core.sq_tb_prioridade_processo")
+	@Id
+	@GeneratedValue(generator="generator")
+	@Column(name="id_prioridade_processo")
+	public Integer getIdPrioridade() {
+		return idPrioridade;
+	}
+	public void setIdPrioridade(Integer idPrioridade) {
+		this.idPrioridade = idPrioridade;
+	}
+	
+	@Column(name="ds_prioridade_processo", nullable=false)
+	public String getDescricaoPrioridade() {
+		return descricaoPrioridade;
+	}
+	public void setDescricaoPrioridade(String descricaoPrioridade) {
+		this.descricaoPrioridade = descricaoPrioridade;
+	}
+	
+	@Column(name="nr_peso", nullable=false)
+	public Integer getPeso() {
+		return peso;
+	}
+	public void setPeso(Integer peso) {
+		this.peso = peso;
+	}
+	
+	@Column(name="in_ativo")
+	public Boolean getAtivo() {
+		return ativo;
+	}
+	public void setAtivo(Boolean ativo) {
+		this.ativo = ativo;
+	}
+	
+}
