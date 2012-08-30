eb3318b3664e92253c7c58751cf6e1f49dde0ae9
erikliberal <erikliberal@069f99d3-da65-4ab0-85f1-cf03781be9e7>
2012-08-30 14:32:24 +0000

    Processo:
    Adição das classes necessárias para a aba anexar Documentos Físicos nas abas de Fluxo
    
    git-svn-id: http://svn.infox.com.br/repos/infoxnovoepa/aplicacao/trunk/epa2@209 069f99d3-da65-4ab0-85f1-cf03781be9e7

---
+++ src/main/java/br/com/infox/ibpm/query/DocumentoFisicoQuery.java
@@ -0,0 +1,12 @@
+package br.com.infox.ibpm.query;
+
+public interface DocumentoFisicoQuery {
+
+	String QUERY_PARAM_PROCESSO = "processo";
+	
+	String LIST_BY_PROCESSO = "listDocumentoFisicoByProcesso";
+	String LIST_BY_PROCESSO_QUERY = "select o from DocumentoFisico o " +
+								 "where o.ativo = true and " +
+								 "o.processo = :" + QUERY_PARAM_PROCESSO;
+	
+}
