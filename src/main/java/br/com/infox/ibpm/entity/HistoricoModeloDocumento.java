88dcec9a9d1a3ff1db7b1e8b284e1ad2737261e2
erikliberal <erikliberal@069f99d3-da65-4ab0-85f1-cf03781be9e7>
2012-09-04 12:32:45 +0000

    HistoricoModeloDocumento: - Criação das classes Home, List e entidade
    git-svn-id: http://svn.infox.com.br/repos/infoxnovoepa/aplicacao/trunk/epa2@234 069f99d3-da65-4ab0-85f1-cf03781be9e7

---
+++ src/main/java/br/com/infox/ibpm/entity/HistoricoModeloDocumento.java
@@ -0,0 +1,98 @@
+package br.com.infox.ibpm.entity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = HistoricoModeloDocumento.TABLE_NAME, schema="core")
+public class HistoricoModeloDocumento implements java.io.Serializable {
+
+	public static final String TABLE_NAME = "tb_modelo_documento_historico";
+
+	private static final long serialVersionUID = 1L;
+	
+	private int idHistoricoModeloDocumento;
+	private ModeloDocumento modeloDocumento;
+	private Usuario usuarioAlteracao;
+	private Date dataAlteracao;
+	private String tituloModeloDocumento;
+	private String descricaoModeloDocumento;
+	private Boolean ativo;
+	
+	@SequenceGenerator(name = "generator", sequenceName = "core.sq_tb_modelo_documento_historico")
+	@Id
+	@GeneratedValue(generator = "generator")
+	@Column(name = "id_modelo_documento_historico", unique = true, nullable = false)
+	public int getIdistoricoModeloDocumento() {
+		return idHistoricoModeloDocumento;
+	}
+
+	public void setIdistoricoModeloDocumento(int idistoricoModeloDocumento) {
+		this.idHistoricoModeloDocumento = idistoricoModeloDocumento;
+	}
+
+	@ManyToOne(fetch=FetchType.LAZY)
+	@JoinColumn(name="id_modelo_documento")
+	public ModeloDocumento getModeloDocumento() {
+		return modeloDocumento;
+	}
+	public void setModeloDocumento(ModeloDocumento modeloDocumento) {
+		this.modeloDocumento = modeloDocumento;
+	}
+
+	@ManyToOne(fetch=FetchType.LAZY)
+	@JoinColumn(name="id_usuario_alteracao")
+	public Usuario getUsuarioAlteracao() {
+		return usuarioAlteracao;
+	}
+	public void setUsuarioAlteracao(Usuario usuarioAlteracao) {
+		this.usuarioAlteracao = usuarioAlteracao;
+	}
+
+	@Column(name="dt_alteracao", nullable=false)
+	public Date getDataAlteracao() {
+		return dataAlteracao;
+	}
+	public void setDataAlteracao(Date dataAlteracao) {
+		this.dataAlteracao = dataAlteracao;
+	}
+
+	@Column(name="ds_titulo_modelo_documento", nullable=false, length=60)
+	public String getTituloModeloDocumento() {
+		return tituloModeloDocumento;
+	}
+	public void setTituloModeloDocumento(String tituloModeloDocumento) {
+		this.tituloModeloDocumento = tituloModeloDocumento;
+	}
+
+	@Column(name="ds_modelo_documento", nullable=false)
+	public String getDescricaoModeloDocumento() {
+		return descricaoModeloDocumento;
+	}
+	public void setDescricaoModeloDocumento(String descricaoModeloDocumento) {
+		this.descricaoModeloDocumento = descricaoModeloDocumento;
+	}
+
+	@Column(name="in_ativo", nullable=false)
+	public Boolean getAtivo() {
+		return ativo;
+	}
+	public void setAtivo(Boolean ativo) {
+		this.ativo = ativo;
+	}
+
+	@Override
+	public String toString() {
+		return this.tituloModeloDocumento;
+	}
+
+}
