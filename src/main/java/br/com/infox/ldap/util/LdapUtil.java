4d67299056536577f40066e814f4b043c7c42734
thiagoreis <thiagoreis@069f99d3-da65-4ab0-85f1-cf03781be9e7>
2012-09-28 13:18:41 +0000

    Inclusão de novas funcionalidades:  * Acesso ao servidor de Active Directory (AD)  * Importação dos usuários do AD  * Autenticação dos usuários AD  * Mapeamento dos usuários AD para localização e papel  * Inclusão de um novo template tabbledTemplateCrudAction  * Inclusão de uma nova classe para fazer a paginação no template tabbledTemplateCrudAction
    Pequenas correções no código.
    
    git-svn-id: http://svn.infox.com.br/repos/infoxnovoepa/aplicacao/trunk/epa2@285 069f99d3-da65-4ab0-85f1-cf03781be9e7

---
+++ src/main/java/br/com/infox/ldap/util/LdapUtil.java
@@ -0,0 +1,142 @@
+package br.com.infox.ldap.util;
+
+import java.util.Hashtable;
+import java.util.StringTokenizer;
+
+import javax.naming.Context;
+import javax.naming.NamingEnumeration;
+import javax.naming.NamingException;
+import javax.naming.directory.DirContext;
+import javax.naming.directory.InitialDirContext;
+import javax.naming.directory.SearchControls;
+import javax.naming.directory.SearchResult;
+import javax.naming.ldap.InitialLdapContext;
+import javax.naming.ldap.LdapContext;
+
+import org.jboss.seam.faces.FacesMessages;
+import org.jboss.seam.international.StatusMessage.Severity;
+
+import br.com.infox.util.ParametroUtil;
+
+public final class LdapUtil {
+	
+	private LdapUtil() { }
+	
+	public static DirContext getLDAPDirContextAD() {
+		DirContext ctx = null;
+		Hashtable<String, String> env = gerarParametrosUsuarioPadraoLDAP();
+		if(env == null)
+			return ctx;
+	      
+	      try {
+	         ctx = new InitialDirContext(env);
+	      } catch (NamingException e) {
+	    	  exibirMenssagemLDAP();
+	      }
+	      return ctx;
+	}
+	
+	private static String gereParametrosAD(String domain) {
+		String nome = "";
+		StringTokenizer tokens = new StringTokenizer(domain, ".");
+		while(tokens.hasMoreTokens()) {
+			nome+= ",DC=" + tokens.nextToken();
+		}
+		return nome;
+	}
+	//InitialLdapContext
+	
+	public static LdapContext autentiqueUsuarioAD(String nome, String pw){
+		LdapContext ctx = null;
+		Hashtable<String, String> env = gerarParametrosLDAP(nome, pw);
+		if(env ==null)
+			return ctx;
+
+	      try {
+	         ctx = new InitialLdapContext(env, null);
+	      } catch (NamingException e) {
+	         //exibirMenssagemLDAP();
+	      }
+	      return ctx;
+    }
+
+	private static Hashtable<String, String> gerarParametrosUsuarioPadraoLDAP() {
+		Hashtable<String, String> env = new Hashtable<String, String>();
+	    env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
+        env.put(Context.PROVIDER_URL, "ldap://"+ ParametroUtil.getLDAPHost() + "/CN=users" + gereParametrosAD(ParametroUtil.getLDAPDomain()));
+        env.put(javax.naming.Context.SECURITY_AUTHENTICATION, "Simple");  
+        env.put(javax.naming.Context.SECURITY_PRINCIPAL, ParametroUtil.getLDAPLogin() + "@" + ParametroUtil.getLDAPDomain());
+        
+        if(ParametroUtil.getLDAPassword() == null || "".equals(ParametroUtil.getLDAPassword())) {
+        	exibirMenssagemLDAP();
+      	  return null;
+        }
+        env.put(javax.naming.Context.SECURITY_CREDENTIALS, ParametroUtil.getLDAPassword());
+		return env;
+	}
+	
+	private static Hashtable<String, String> gerarParametrosLDAP(String login, String pw) {
+		Hashtable<String, String> env = new Hashtable<String, String>();
+	    env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
+        env.put(Context.PROVIDER_URL, "ldap://"+ ParametroUtil.getLDAPHost() + "/CN=users" + gereParametrosAD(ParametroUtil.getLDAPDomain()));
+        env.put(javax.naming.Context.SECURITY_AUTHENTICATION, "Simple");  
+        env.put(javax.naming.Context.SECURITY_PRINCIPAL, login + "@" + ParametroUtil.getLDAPDomain());
+        
+        if(ParametroUtil.getLDAPassword() == null || "".equals(ParametroUtil.getLDAPassword())) {
+        	exibirMenssagemLDAP();
+      	  return null;
+        }
+        env.put(javax.naming.Context.SECURITY_CREDENTIALS, pw);
+		return env;
+	}
+
+	public static void exibirMenssagemLDAP() {
+		if(FacesMessages.instance() != null) {
+			String newLine = System.getProperty("line.separator");
+			FacesMessages.instance().add(Severity.INFO, "Erro ao acessar o servidor Active Directory. Verifique os parâmetro de acesso ao Active Directory : " + newLine +
+				  "* ldap.domain" + newLine + "* ldap.host" + newLine + "* ldap.login" + newLine + "* ldap.password" + newLine + "* ldap.authentication");
+		}
+	}
+	
+	public static boolean pesquisarUsuarioAD(String login) {
+		boolean retorno = false;
+		String pes = "(&(objectclass=person)(userprincipalname=" + login + "@" + ParametroUtil.getLDAPDomain() + ")";
+		NamingEnumeration<SearchResult> search = pesquisarLDAP(pes);
+		try {
+			retorno = search.hasMore();
+		} catch (NamingException e) {
+			exibirMenssagemLDAP();
+		}
+		return retorno;
+	}
+	
+	/**
+	 * Após pesquisar sobre o Enumeration deve-se fechar a conexão 
+	 * @return 
+	 */
+	public static NamingEnumeration<SearchResult> pesquisarUsuariosLDAP() {
+		String pes = "(objectclass=person)";
+		NamingEnumeration<SearchResult> results = pesquisarLDAP(pes);
+		return results;
+	}
+	
+	private static NamingEnumeration<SearchResult> pesquisarLDAP(String search) {
+		DirContext ctx = LdapUtil.getLDAPDirContextAD();
+		if (ctx == null)
+			return null;
+		NamingEnumeration<SearchResult> results = null;
+		try {
+			SearchControls controls = new SearchControls();
+			controls.setSearchScope(SearchControls.SUBTREE_SCOPE);
+			results = ctx.search("", search, controls);
+			ctx.close();
+			
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+		
+		return results;
+	}
+			
+
+}
