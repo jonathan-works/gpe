109aaebd68f9a668bc7f5162d481d64949f66b2a
thiagoreis <thiagoreis@069f99d3-da65-4ab0-85f1-cf03781be9e7>
2012-10-11 14:58:52 +0000

    Removendo trecho de código com warning.
    git-svn-id: http://svn.infox.com.br/repos/infoxnovoepa/aplicacao/trunk/epa2@328 069f99d3-da65-4ab0-85f1-cf03781be9e7

--- src/main/java/br/com/infox/epa/webservice/globalweather/client/service/FileDataDownload.java
+++ src/main/java/br/com/infox/epa/webservice/globalweather/client/service/FileDataDownload.java
@@ -15,14 +15,10 @@ package br.com.infox.epa.webservice.globalweather.client.service;
  Você deve ter recebido uma cópia da GNU GPL junto com este programa; se não, 
  veja em http://www.gnu.org/licenses/   
 */
-import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
@@ -32,12 +28,9 @@ public class FileDataDownload {
 
     final static int size = 1024;
 
-    private String fileAddress;
     private File fileDownload;
-    private int fileSize;
 
     public FileDataDownload(String fileAddress, File fileDownload) {
-        this.fileAddress = fileAddress;
         this.fileDownload = fileDownload;
     }
 
@@ -49,47 +42,6 @@ public class FileDataDownload {
         this.fileDownload = fileDownload;
     }
 
-    
-
-    private void FileDownload(String fileAddress, File file)
-            throws Exception {
-        OutputStream os = null;
-        URLConnection URLConn = null;
-
-        // URLConnection class represents a communication link between the
-        // application and a URL.
-
-        InputStream is = null;
-        try {
-            URL fileUrl;
-            byte[] buf;
-            int byteRead, byteWritten = 0;
-            fileUrl = new URL(fileAddress);
-            os = new BufferedOutputStream(new FileOutputStream(file));
-            //The URLConnection object is created by invoking the
-
-
-            // openConnection method on a URL.
-            URLConn = fileUrl.openConnection();
-            fileSize = URLConn.getContentLength();
-            is = URLConn.getInputStream();
-            buf = new byte[size];
-            while ((byteRead = is.read(buf)) != -1) {
-                os.write(buf, 0, byteRead);
-                byteWritten += byteRead;
-            }
-        } catch (Exception e) {
-            throw new Exception(e);
-        } finally {
-            try {
-                is.close();
-                os.close();
-            } catch (Exception e) {
-            	e.printStackTrace();
-            }
-        }
-    }
-    
     public static String getUrlAsString(String fileAddress)
 			throws Exception {
 	StringBuilder sb = new StringBuilder();
