83e25fa8a1d195c4f769922ead026972da7403af
tassio <tassio@069f99d3-da65-4ab0-85f1-cf03781be9e7>
2012-10-15 15:17:14 +0000

    Ajustes de problemas informados pelo Sonar
    git-svn-id: http://svn.infox.com.br/repos/infoxnovoepa/aplicacao/trunk/epa2@357 069f99d3-da65-4ab0-85f1-cf03781be9e7

--- src/main/java/br/com/infox/core/certificado/CertificadosCaCheckManager.java
+++ src/main/java/br/com/infox/core/certificado/CertificadosCaCheckManager.java
@@ -37,15 +37,15 @@ import br.com.itx.util.ComponentUtil;
 import br.com.itx.util.FileUtil;
 
 /**
- * Componente com escopo de aplicação responsavel pelo teste
- * de validade dos certificados confrontando a corrente de 
- * certificados com a lista das certificadoras que o ICP-Brasil 
- * fornece: http://www.iti.gov.br/twiki/bin/view/Certificacao/RepoZipCert.
- * Os arquivos ficam descompctados em uma pasta no servidor e 
- * quando a aplicação sobe os arquivos são lidos e os certificados
- * são carregados em uma lista.
+ * Componente com escopo de aplicação responsavel pelo teste de validade dos
+ * certificados confrontando a corrente de certificados com a lista das
+ * certificadoras que o ICP-Brasil fornece:
+ * http://www.iti.gov.br/twiki/bin/view/Certificacao/RepoZipCert. Os arquivos
+ * ficam descompctados em uma pasta no servidor e quando a aplicação sobe os
+ * arquivos são lidos e os certificados são carregados em uma lista.
+ * 
  * @author Rodrigo Menezes - rodrigo@infox.com.br
- *
+ * 
  */
 @Name(CertificadosCaCheckManager.NAME)
 @BypassInterceptors
@@ -53,13 +53,15 @@ import br.com.itx.util.FileUtil;
 public class CertificadosCaCheckManager {
 
 	public static final String NAME = "certificadosCaCheckManager";
-	private static final LogProvider log = Logging.getLogProvider(CertificadosCaCheckManager.class);
+	private static final LogProvider LOG = Logging
+			.getLogProvider(CertificadosCaCheckManager.class);
 	private static final String BR = System.getProperty("line.separator");
 	private List<String> acceptedCaNameList;
 	private StringBuilder acceptedCaNameSb;
 	private List<X509Certificate> listCertificadosCA;
-	
-	@Observer({"org.jboss.seam.postInitialization","org.jboss.seam.postReInitialization"})
+
+	@Observer({ "org.jboss.seam.postInitialization",
+			"org.jboss.seam.postReInitialization" })
 	public synchronized void init() {
 		MeasureTime mt = new MeasureTime(true);
 		listCertificadosCA = new ArrayList<X509Certificate>();
@@ -69,9 +71,10 @@ public class CertificadosCaCheckManager {
 			populateListMap();
 		} catch (CertificateException e) {
 			e.printStackTrace();
-			throw new RuntimeException("Erro ao iniciar " + NAME + ": " + e.getMessage(), e);
+			throw new RuntimeException("Erro ao iniciar " + NAME + ": "
+					+ e.getMessage(), e);
 		}
-		log.info(MessageFormat.format("Inicializado [{0} ms]", mt.getTime()));
+		LOG.info(MessageFormat.format("Inicializado [{0} ms]", mt.getTime()));
 	}
 
 	public List<X509Certificate> getListCertificadosCA() {
@@ -79,11 +82,13 @@ public class CertificadosCaCheckManager {
 	}
 
 	private void populateListMap() throws CertificateException {
-		CertificateFactory certFactory = CertificateFactory.getInstance("X.509");
-		
+		CertificateFactory certFactory = CertificateFactory
+				.getInstance("X.509");
+
 		List<URL> files = new ArrayList<URL>();
 		try {
-			files = getResourceListing(CertificadosCaCheckManager.class, "certificados/", "(.*crt$|.*cer$)");
+			files = getResourceListing(CertificadosCaCheckManager.class,
+					"certificados/", "(.*crt$|.*cer$)");
 		} catch (Exception e1) {
 			e1.printStackTrace();
 		}
@@ -92,9 +97,11 @@ public class CertificadosCaCheckManager {
 		for (URL fileCert : files) {
 			try {
 				is = fileCert.openStream();
-				X509Certificate x509Cert = (X509Certificate) certFactory.generateCertificate(is);
+				X509Certificate x509Cert = (X509Certificate) certFactory
+						.generateCertificate(is);
 				listCertificadosCA.add(x509Cert);
-				String cnName = Certificado.getCNValue(x509Cert.getSubjectDN().getName());
+				String cnName = Certificado.getCNValue(x509Cert.getSubjectDN()
+						.getName());
 				acceptedCaNameList.add(cnName);
 				acceptedCaNameSb.append(cnName);
 				acceptedCaNameSb.append(BR);
@@ -108,74 +115,80 @@ public class CertificadosCaCheckManager {
 		}
 
 	}
-	
-	public void verificaCertificado(String certChain) throws CertificadoException, CertificateException {
-		X509Certificate[] x509Certificates = DigitalSignatureUtils.loadCertFromBase64String(certChain);
+
+	public void verificaCertificado(String certChain)
+			throws CertificadoException, CertificateException {
+		X509Certificate[] x509Certificates = DigitalSignatureUtils
+				.loadCertFromBase64String(certChain);
 		verificaCertificado(x509Certificates);
 	}
-	  
-    public void verificaCertificado(X509Certificate[] certChain) throws CertificateException {
-    	MeasureTime mt = new MeasureTime(true);
-    	X509Certificate certificate = certChain[0];
-    	boolean valid = true;
 
-    	//primeiro valida se cada elemento da cadeia está validado pelo proximo
-    	if (certChain.length > 1) {
-    		for (int i = 1; i < certChain.length; i++) {
-    			X509Certificate trustedCert = certChain[i];
-    			try {
-    				certificate.verify(trustedCert.getPublicKey());
-    			}
-    			catch (GeneralSecurityException ex) {
-    				valid = false;
-    			}
-    			certificate = trustedCert;
-    		}
-    	}
-    	
-    	if (!valid) {
-    		String msg = "A validade da cadeia não pode ser verificada.";
-    		log.info(msg + mt.getTime());
-    		throw new CertificateException(msg);    	
-    	}
-    	
-    	X509Certificate[] aTrustedCertificates = (X509Certificate[]) listCertificadosCA.toArray(
-    			new X509Certificate[listCertificadosCA.size()]);    		
+	public void verificaCertificado(X509Certificate[] certChain)
+			throws CertificateException {
+		MeasureTime mt = new MeasureTime(true);
+		X509Certificate certificate = certChain[0];
+		boolean valid = true;
 
-    	//valida se algum elemento da cadeia foi assinado pela lista das certificadoras da certSign
-    	//se foi então o certificado é valido, pois a cadeia foi devidamente testada.
-    	for (int i = 0; i < certChain.length; i++) {
-    		X509Certificate cert = certChain[i];
-    		try {
-				DigitalSignatureUtils.verifyCertificate(cert, aTrustedCertificates);
-				log.info("Certificado Verificado com sucesso. ");
-				return;
-			} catch (GeneralSecurityException e) { }
+		// primeiro valida se cada elemento da cadeia está validado pelo proximo
+		if (certChain.length > 1) {
+			for (int i = 1; i < certChain.length; i++) {
+				X509Certificate trustedCert = certChain[i];
+				try {
+					certificate.verify(trustedCert.getPublicKey());
+				} catch (GeneralSecurityException ex) {
+					valid = false;
+				}
+				certificate = trustedCert;
+			}
 		}
-    	
+
+		if (!valid) {
+			String msg = "A validade da cadeia não pode ser verificada.";
+			LOG.info(msg + mt.getTime());
+			throw new CertificateException(msg);
+		}
+
+		X509Certificate[] aTrustedCertificates = (X509Certificate[]) listCertificadosCA
+				.toArray(new X509Certificate[listCertificadosCA.size()]);
+
+		// valida se algum elemento da cadeia foi assinado pela lista das
+		// certificadoras da certSign
+		// se foi então o certificado é valido, pois a cadeia foi devidamente
+		// testada.
+		for (int i = 0; i < certChain.length; i++) {
+			X509Certificate cert = certChain[i];
+			try {
+				DigitalSignatureUtils.verifyCertificate(cert,
+						aTrustedCertificates);
+				LOG.info("Certificado Verificado com sucesso. ");
+				return;
+			} catch (GeneralSecurityException e) {
+			}
+		}
+
 		String msg = "A validade do certificado não pode ser verificada junto ao ICP-Brasil.";
-		log.info(msg + mt.getTime());
+		LOG.info(msg + mt.getTime());
 		throw new CertificateException(msg);
-    }
-    
-    public List<String> getAcceptedCaNameList() {
+	}
+
+	public List<String> getAcceptedCaNameList() {
 		return acceptedCaNameList;
 	}
-    
-    public String getAcceptedCa() {
-    	return acceptedCaNameSb.toString();
-    }
+
+	public String getAcceptedCa() {
+		return acceptedCaNameSb.toString();
+	}
 
 	public void download() {
 		byte[] data = getAcceptedCa().getBytes();
 		FacesContext facesContext = FacesContext.getCurrentInstance();
 		HttpServletResponse response = (HttpServletResponse) facesContext
-					.getExternalContext().getResponse();
+				.getExternalContext().getResponse();
 		response.setContentType("text/plain");
 		response.setContentLength(data.length);
 		String file = "file.txt";
-		response.setHeader("Content-disposition", "inline; filename=\""
-				+ file + "\"");
+		response.setHeader("Content-disposition", "inline; filename=\"" + file
+				+ "\"");
 
 		try {
 			OutputStream out = response.getOutputStream();
@@ -186,27 +199,29 @@ public class CertificadosCaCheckManager {
 			FacesMessages.instance().add(Severity.ERROR,
 					"Error while downloading the file: " + file);
 		}
-	}    
-    
-    public static CertificadosCaCheckManager instance() {
-    	return ComponentUtil.getComponent(NAME, ScopeType.APPLICATION);
-    }
-	
-	
+	}
+
+	public static CertificadosCaCheckManager instance() {
+		return ComponentUtil.getComponent(NAME, ScopeType.APPLICATION);
+	}
+
 	/**
-	   * List directory contents for a resource folder. Not recursive.
-	   * This is basically a brute-force implementation.
-	   * Works for regular files and also JARs.
-	   * 
-	   * @author Greg Briggs
-	   * @param clazz Any java class that lives in the same place as the resources you want.
-	   * @param path Should end with "/", but not start with one.
-	   * @return Just the name of each member item, not the full paths.
-	   * @throws URISyntaxException 
-	   * @throws IOException 
-	   */
-	public static List<URL> getResourceListing(Class<? extends Object> clazz, String path,
-			String pattern) throws URISyntaxException, IOException {
+	 * List directory contents for a resource folder. Not recursive. This is
+	 * basically a brute-force implementation. Works for regular files and also
+	 * JARs.
+	 * 
+	 * @author Greg Briggs
+	 * @param clazz
+	 *            Any java class that lives in the same place as the resources
+	 *            you want.
+	 * @param path
+	 *            Should end with "/", but not start with one.
+	 * @return Just the name of each member item, not the full paths.
+	 * @throws URISyntaxException
+	 * @throws IOException
+	 */
+	public static List<URL> getResourceListing(Class<? extends Object> clazz,
+			String path, String pattern) throws URISyntaxException, IOException {
 		URL dirURL = clazz.getClassLoader().getResource(path);
 		if (dirURL.getProtocol().equals("vfsfile")) {
 			dirURL = new URL("file", dirURL.getHost(), dirURL.getFile());
@@ -223,14 +238,15 @@ public class CertificadosCaCheckManager {
 		}
 
 		/*
-		 * In case of a jar file, we can't actually find a directory. Have
-		 * to assume the same jar as clazz.
+		 * In case of a jar file, we can't actually find a directory. Have to
+		 * assume the same jar as clazz.
 		 */
 		String me = clazz.getName().replace(".", "/") + ".class";
 		dirURL = clazz.getClassLoader().getResource(me);
 
 		if (dirURL.getProtocol().equals("vfszip")) {
-			dirURL = new URL("jar", dirURL.getHost(), "file:" + dirURL.getFile().replace(".jar/", ".jar!/"));
+			dirURL = new URL("jar", dirURL.getHost(), "file:"
+					+ dirURL.getFile().replace(".jar/", ".jar!/"));
 		}
 		if (dirURL.getProtocol().equals("jar")) {
 			/* A JAR path */
