56c566ee6d4ba7c29410125f79664eddb91217be
thiagoreis <thiagoreis@069f99d3-da65-4ab0-85f1-cf03781be9e7>
2012-10-16 12:07:40 +0000

    Remoção do pequenos warnings.
    git-svn-id: http://svn.infox.com.br/repos/infoxnovoepa/aplicacao/trunk/epa2@382 069f99d3-da65-4ab0-85f1-cf03781be9e7

--- src/main/java/br/com/infox/ibpm/jbpm/importe/ImportarXPDL.java
+++ src/main/java/br/com/infox/ibpm/jbpm/importe/ImportarXPDL.java
@@ -609,21 +609,31 @@ public class ImportarXPDL implements Serializable {
 		private final String		name				= "[Temporary node - DecisionNode]";
 
 		@Override
-		public boolean equals(Object o) {
-			if (o instanceof DecisionNode) {
-				return this.getName().equals(((DecisionNode) o).getName());
+		public boolean equals(Object obj) {
+			if (this == obj) {
+				return true;
 			}
-			return false;
+			if (obj == null) {
+				return false;
+			}
+			if (!(obj instanceof ParallelNode)) {
+				return false;
+			}
+			ParallelNode other = (ParallelNode) obj;
+			return getId() == other.getId();
+		}
+
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + (int)getId();
+			return result;
 		}
 
 		@Override
 		public String toString() {
 			return name + this.getName();
-		}
-
-		@Override
-		public int hashCode() {
-			return toString().hashCode();
 		}
 	}
 
