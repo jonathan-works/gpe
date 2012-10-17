327198202b0a38f7f188ccc473cac93ea19cdf78
thiagoreis <thiagoreis@069f99d3-da65-4ab0-85f1-cf03781be9e7>
2012-10-17 16:43:59 +0000

    Inserção do @Override para evitar warnings
    git-svn-id: http://svn.infox.com.br/repos/infoxnovoepa/aplicacao/trunk/epa2@411 069f99d3-da65-4ab0-85f1-cf03781be9e7

--- src/main/java/br/com/itx/util/InstallCert.java
+++ src/main/java/br/com/itx/util/InstallCert.java
@@ -205,15 +205,18 @@ public final class InstallCert {
 			this.tm = tm;
 		}
 
+		@Override
 		public X509Certificate[] getAcceptedIssuers() {
 			throw new UnsupportedOperationException();
 		}
 
+		@Override
 		public void checkClientTrusted(X509Certificate[] chain, String authType)
 				throws CertificateException {
 			throw new UnsupportedOperationException();
 		}
 
+		@Override
 		public void checkServerTrusted(X509Certificate[] chain, String authType)
 				throws CertificateException {
 			this.chain = ArrayUtil.copyOf(chain);
