930ca0cc8b7cdf6c267a234fef3ed9e1b66c04c1
thiagoreis <thiagoreis@069f99d3-da65-4ab0-85f1-cf03781be9e7>
2012-10-22 18:27:24 +0000

    Nova Estrutura de classes do processo de importação dos arquivos XPDL.
    git-svn-id: http://svn.infox.com.br/repos/infoxnovoepa/aplicacao/trunk/epa2@436 069f99d3-da65-4ab0-85f1-cf03781be9e7

---
+++ src/main/java/br/com/infox/ibpm/jbpm/xpdl/element/ElementXPDL.java
@@ -0,0 +1,42 @@
+package br.com.infox.ibpm.jbpm.xpdl.element;
+
+import org.jdom.Element;
+
+import br.com.itx.util.XmlUtil;
+
+public class ElementXPDL {
+	
+	private Element element;
+	private String id;
+	private String name;
+	private GraphicsInfoXPDL graphics;
+	
+	public ElementXPDL(Element element2, String name) {
+		element = element2;
+		graphics = new GraphicsInfoXPDL(XmlUtil.getChildByIndex(element, "NodeGraphicsInfos", 0));
+		this.name = element2.getAttributeValue("Name");
+		if(this.name == null || this.name.isEmpty())
+			this.name = name;
+		id = element2.getAttributeValue("Id");
+	}
+	public String getId() {
+		return id;
+	}
+	public String getName() {
+		return name;
+	}
+	public GraphicsInfoXPDL getGraphics() {
+		return graphics;
+	}
+	public Element getElement() {
+		return element;
+	}
+	public void setElement(Element element) {
+		this.element = element;
+	}
+
+	@Override
+	public String toString() {
+		return "[ElementXPDL] name: " + name + graphics.toString();
+	}
+}
---
+++ src/main/java/br/com/infox/ibpm/jbpm/xpdl/element/GraphicsInfoXPDL.java
@@ -0,0 +1,52 @@
+package br.com.infox.ibpm.jbpm.xpdl.element;
+
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
+
+import org.jdom.Element;
+
+import br.com.itx.util.XmlUtil;
+
+public class GraphicsInfoXPDL {
+	
+	private Point2D position;
+	private float width;
+	private float height;
+	private Rectangle2D rectangle;
+	
+	public GraphicsInfoXPDL(Element element) {
+		completePositionInformation(element);
+	}
+	
+	private void completePositionInformation(Element graphics) {
+		Element info = XmlUtil.getChildByIndex(graphics, "NodeGraphicsInfo", 0);
+		height = Integer.parseInt(info.getAttributeValue("Height"));
+		width = Integer.parseInt(info.getAttributeValue("Width"));
+		Element coordinates = XmlUtil.getChildByIndex(info, "Coordinates", 0);
+		float x = Float.parseFloat(coordinates.getAttributeValue("XCoordinate"));
+		float y = Float.parseFloat(coordinates.getAttributeValue("YCoordinate"));
+		position = new Point2D.Float(x,y);
+		rectangle = new Rectangle2D.Float(x, y, width, height);
+	}
+
+	public Point2D getPosition() {
+		return position;
+	}
+
+	public float getWidth() {
+		return width;
+	}
+
+	public float getHeight() {
+		return height;
+	}
+
+	@Override
+	public String toString() {
+		return "[GrapghicsInfo] position (" + position.getX() + "," + position.getY() + ") width: " + width + " height: " + height;
+	}
+	
+	public Rectangle2D getRectangle() {
+		return rectangle;
+	}
+}
