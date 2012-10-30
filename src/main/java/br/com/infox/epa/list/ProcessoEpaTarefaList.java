3cc6eccd2d6822f80c72131e3c34e1a15df5457a
tassio <tassio@069f99d3-da65-4ab0-85f1-cf03781be9e7>
2012-10-30 11:48:59 +0000

    Criação do list de tarefas do processo
    git-svn-id: http://svn.infox.com.br/repos/infoxnovoepa/aplicacao/trunk/epa2@472 069f99d3-da65-4ab0-85f1-cf03781be9e7

---
+++ src/main/java/br/com/infox/epa/list/ProcessoEpaTarefaList.java
@@ -0,0 +1,58 @@
+package br.com.infox.epa.list;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.jboss.seam.annotations.Name;
+
+import br.com.infox.core.action.list.EntityList;
+import br.com.infox.core.action.list.SearchCriteria;
+import br.com.infox.epa.entity.ProcessoEpaTarefa;
+import br.com.itx.util.StringUtil;
+
+
+/**
+ * EntityList que consulta as tarefas de um determinado processo
+ * @author tassio
+ *
+ */
+@Name(ProcessoEpaTarefaList.NAME)
+public class ProcessoEpaTarefaList extends EntityList<ProcessoEpaTarefa> {
+	
+	private static final long serialVersionUID = 1L;
+	public static final String NAME = "processoEpaTarefaList";
+	
+	private static final String DEFAULT_EJBQL = "select o from ProcessoEpaTarefa o";
+	private static final String DEFAULT_ORDER = "o.idProcessoTarefa";
+
+	@Override
+	protected void addSearchFields() {
+		addSearchField("processoEpa", SearchCriteria.igual);
+	}
+	@Override
+	protected String getDefaultEjbql() {
+		return DEFAULT_EJBQL;
+	}
+	@Override
+	protected String getDefaultOrder() {
+		return DEFAULT_ORDER;
+	}
+	@Override
+	protected Map<String, String> getCustomColumnsOrder() {
+		return null;
+	}
+	
+	public String rowClasses() {
+		List<Object> classes = new ArrayList<Object>();
+		for (ProcessoEpaTarefa row: list(15)) {
+			if (row.getPorcentagem() != null && row.getPorcentagem() > 100) {
+				classes.add("red-tr");
+			} else {
+				classes.add("white-tr");
+			}
+		}
+		return StringUtil.concatList(classes, ",");
+	}
+	
+}
