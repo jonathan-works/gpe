495b13b8616fe213c81177ea6384a1e5f417a58c
tassio <tassio@069f99d3-da65-4ab0-85f1-cf03781be9e7>
2012-10-31 12:46:20 +0000

    Adição do método contemTarefaForaPrazo
    git-svn-id: http://svn.infox.com.br/repos/infoxnovoepa/aplicacao/trunk/epa2@484 069f99d3-da65-4ab0-85f1-cf03781be9e7

--- src/main/java/br/com/infox/epa/list/ProcessoEpaNaoFinalizadoList.java
+++ src/main/java/br/com/infox/epa/list/ProcessoEpaNaoFinalizadoList.java
@@ -1,6 +1,10 @@
 package br.com.infox.epa.list;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+
+import javax.persistence.Query;
 
 import org.jboss.seam.ScopeType;
 import org.jboss.seam.annotations.Name;
@@ -10,6 +14,8 @@ import br.com.infox.core.action.list.EntityList;
 import br.com.infox.core.action.list.SearchCriteria;
 import br.com.infox.epa.entity.ProcessoEpa;
 import br.com.infox.ibpm.entity.Fluxo;
+import br.com.itx.util.EntityUtil;
+import br.com.itx.util.StringUtil;
 
 /**
  * EntityList que consulta todos os processos não finalizados de um determinado fluxo
@@ -26,7 +32,7 @@ public class ProcessoEpaNaoFinalizadoList extends EntityList<ProcessoEpa> {
 	
 	private static final String DEFAULT_EJBQL = "select o from ProcessoEpa o " +
 												   "where o.dataFim is null";
-	private static final String DEFAULT_ORDER = "o.porcentagem desc";
+	private static final String DEFAULT_ORDER = "o.porcentagem asc, o.idProcesso";
 	
 	private static final String R1 = "o.naturezaCategoriaFluxo.fluxo = #{processoEpaNaoFinalizadoList.fluxo}";
 	
@@ -50,6 +56,11 @@ public class ProcessoEpaNaoFinalizadoList extends EntityList<ProcessoEpa> {
 	protected Map<String, String> getCustomColumnsOrder() {
 		return null;
 	}
+	
+	@Override
+	public void setOrderedColumn(String order) {
+		setOrder(order);
+	}
 
 	public Fluxo getFluxo() {
 		return fluxo;
@@ -57,6 +68,28 @@ public class ProcessoEpaNaoFinalizadoList extends EntityList<ProcessoEpa> {
 
 	public void setFluxo(Fluxo fluxo) {
 		this.fluxo = fluxo;
+	}
+	
+	public boolean contemTarefaForaPrazo(ProcessoEpa processoEpa) {
+		Query query = EntityUtil.createQuery("select count(o) from ProcessoEpaTarefa o " +
+											 "where o.processoEpa = :processoEpa " +
+											 "  and o.porcentagem > 100");
+		query.setParameter("processoEpa", processoEpa);
+		return (Long) query.getSingleResult() > 0;
+	}
+	
+	public String rowClasses() {
+		List<Object> classes = new ArrayList<Object>();
+		for (ProcessoEpa row: list(15)) {
+			if (row.getPorcentagem() != null && row.getPorcentagem() > 100) {
+				classes.add("red-tr");
+			} else if (contemTarefaForaPrazo(row)){
+				classes.add("yellow-tr");
+			} else {
+				classes.add("white-tr");
+			}
+		}
+		return StringUtil.concatList(classes, ",");
 	}
 
 }
