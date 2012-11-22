c51858c3d4b1f22118ee10de26966a8ad53fdafb
erikliberal <erikliberal@069f99d3-da65-4ab0-85f1-cf03781be9e7>
2012-11-22 14:10:52 +0000

    Importar XPDL. Retirado o redirecionamento e simplesmente fecha a janela assim que termina de importar.
    git-svn-id: http://svn.infox.com.br/repos/infoxnovoepa/aplicacao/trunk/epa2@549 069f99d3-da65-4ab0-85f1-cf03781be9e7

--- src/main/java/br/com/infox/ibpm/action/ImportarXPDLAction.java
+++ src/main/java/br/com/infox/ibpm/action/ImportarXPDLAction.java
@@ -3,7 +3,6 @@ package br.com.infox.ibpm.action;
 import org.jboss.seam.ScopeType;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Scope;
-import org.jboss.seam.faces.Redirect;
 import org.jboss.seam.log.Log;
 import org.jboss.seam.log.Logging;
 
@@ -22,24 +21,12 @@ public class ImportarXPDLAction {
 		try {
 			FluxoXPDL fluxoXPDL = FluxoXPDL.createInstance(bytes);
 			String xml = fluxoXPDL.toJPDL(cdFluxo);
-			redirectToProcessDefinition(cdFluxo, xml);
+			ProcessBuilder process = ProcessBuilder.instance();
+			process.setXml(xml); 
+			process.updateFluxo(cdFluxo);
 		} catch (IllegalXPDLException e) {
 			LOG.error("Erro ao importar arquivo XPDL. " + e.getMessage());
 		}
 	}
 
-	private void redirectToProcessDefinition(String cdFluxo, String xml) {
-		Redirect redirect = Redirect.instance();
-		redirect.setParameter("id", cdFluxo);
-		redirect.setParameter("tab", "Propriedades");
-		redirect.setParameter("nodeIndex", "0");
-		redirect.setConversationPropagationEnabled(true);
-		redirect.setViewId("/Fluxo/definicao/processDefinition.xhtml");
-
-		ProcessBuilder process = ProcessBuilder.instance();
-		process.setXml(xml); 
-		process.updateFluxo(cdFluxo);
-		
-		redirect.execute();
-	}
 }
