70be0cc874b62b13ad3b03cdd8a690bb7e5ecc47
tassio <tassio@069f99d3-da65-4ab0-85f1-cf03781be9e7>
2012-12-14 11:31:42 +0000

    Transformação do AssinaturaDocumentoService em um componente service
    git-svn-id: http://svn.infox.com.br/repos/infoxnovoepa/aplicacao/trunk/epa2@585 069f99d3-da65-4ab0-85f1-cf03781be9e7

--- src/main/java/br/com/infox/ibpm/service/AssinaturaDocumentoService.java
+++ src/main/java/br/com/infox/ibpm/service/AssinaturaDocumentoService.java
@@ -1,11 +1,20 @@
 package br.com.infox.ibpm.service;
 
+import org.jboss.seam.ScopeType;
+import org.jboss.seam.annotations.AutoCreate;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Scope;
 import org.jboss.seam.util.Strings;
 
 import br.com.infox.ibpm.entity.ProcessoDocumento;
 import br.com.itx.util.EntityUtil;
 
+@Name(AssinaturaDocumentoService.NAME)
+@Scope(ScopeType.CONVERSATION)
+@AutoCreate
 public class AssinaturaDocumentoService {
+	
+	public static final String NAME = "assinaturaDocumentoService";
 	
 	public Boolean isDocumentoAssinado(ProcessoDocumento processoDocumento){
 		return !Strings.isEmpty(processoDocumento.getProcessoDocumentoBin().getCertChain()) && 
