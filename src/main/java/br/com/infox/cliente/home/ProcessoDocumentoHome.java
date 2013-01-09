47c47f2a2e85c37e24a07d0d5f6a0ed1a089d717
erikliberal <erikliberal@069f99d3-da65-4ab0-85f1-cf03781be9e7>
2013-01-09 14:11:36 +0000

    Melhoria de Código: Utilizando constantes para não repetir escrita de Strings
    git-svn-id: http://svn.infox.com.br/repos/infoxnovoepa/aplicacao/trunk/epa2@603 069f99d3-da65-4ab0-85f1-cf03781be9e7

--- src/main/java/br/com/infox/cliente/home/ProcessoDocumentoHome.java
+++ src/main/java/br/com/infox/cliente/home/ProcessoDocumentoHome.java
@@ -34,7 +34,7 @@ public class ProcessoDocumentoHome
 	private static final long serialVersionUID = 1L;
 
 	public static IProcessoDocumentoHome instance() {
-		return ComponentUtil.getComponent("processoDocumentoHome");
+		return ComponentUtil.getComponent(NAME);
 	}
 	
 	@Override
