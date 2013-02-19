7fd8adaf6b4db362a163ba25fe0814dab014dc35
Gabriel Morrison Lima Dantas <gabriel@infox.com.br>
2013-02-19 08:26:45 -0300

    WIP: Tela de login funcionando, corrigindo para abrir o painel do usuário

--- src/main/webapp/js/text.js
+++
@@ -1,8 +0,0 @@
-function chkTextLength(field, max, out) {
-	if (field.value.length > max)
-		field.value = field.value.substring(0, max);
-	else { 
-		$(out).innerHTML = max - field.value.length;
-		$(out).style.visibility = 'visible';
-	}
-}
--- src/main/webapp/js/tree.js
+++
@@ -1,3 +0,0 @@
-function setZIndex(id) {
-	$(id).style.zIndex = $(id).offsetHeight > 30 ? 1 : 0;
-}
