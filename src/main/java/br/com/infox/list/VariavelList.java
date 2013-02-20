9a5298b16af77f89ad1994783b5fd84cf24d6e3d
Jonas Santos Bezerra <jonasbezerra@infox.com.br>
2013-02-20 11:06:51 -0300

    Ajuste nas página de Variável no Menu Modelo Documento

---
+++ src/main/java/br/com/infox/list/VariavelList.java
@@ -0,0 +1,50 @@
+package br.com.infox.list;
+
+import java.util.Map;
+
+import org.jboss.seam.ScopeType;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Scope;
+
+import br.com.infox.core.action.list.EntityList;
+import br.com.infox.core.action.list.SearchCriteria;
+import br.com.infox.ibpm.entity.Variavel;
+import br.com.itx.util.ComponentUtil;
+
+@Name(VariavelList.NAME)
+@Scope(ScopeType.PAGE)
+public class VariavelList extends EntityList<Variavel> {
+	
+	public static final String NAME = "variavelList";
+	private static final long serialVersionUID = 1L;
+	
+	private static final String DEFAULT_EJBQL = "select o from Variavel o";
+	private static final String DEFAULT_ORDER = "variavel";
+
+	@Override
+	protected void addSearchFields() {
+		addSearchField("variavel", SearchCriteria.contendo);
+		addSearchField("valorVariavel", SearchCriteria.contendo);
+		addSearchField("ativo", SearchCriteria.igual);
+	}
+
+	@Override
+	protected String getDefaultEjbql() {
+		return DEFAULT_EJBQL;
+	}
+
+	@Override
+	protected String getDefaultOrder() {
+		return DEFAULT_ORDER;
+	}
+
+	@Override
+	protected Map<String, String> getCustomColumnsOrder() {
+		return null;
+	}
+	
+	public static VariavelList instance() {
+		return ComponentUtil.getComponent(VariavelList.NAME);
+	}
+	
+}
