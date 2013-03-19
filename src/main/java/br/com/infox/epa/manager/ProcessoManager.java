72baba24db9312651750fb8cdd4adfd146db8b57
Jonas Santos Bezerra <jonasbezerra@infox.com.br>
2013-03-19 08:00:11 -0300

    Adicção do relacionemento entre as entidades ProcessoManager e TipoProcessoDocumentoManager

--- src/main/java/br/com/infox/epa/manager/ProcessoManager.java
+++ src/main/java/br/com/infox/epa/manager/ProcessoManager.java
@@ -6,6 +6,8 @@ import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Scope;
 
 import br.com.infox.core.manager.GenericManager;
+import br.com.infox.ibpm.jbpm.fitter.TypeFitter;
+import br.com.itx.util.ComponentUtil;
 
 @Name(ProcessoManager.NAME)
 @Scope(ScopeType.CONVERSATION)
@@ -14,5 +16,13 @@ public class ProcessoManager extends GenericManager {
 	
 	private static final long serialVersionUID = 8095772422429350875L;
 	public static final String NAME = "processoManager";
+	
+	private TipoProcessoDocumentoManager tipoProcessoDocumentoManager;
+	
+	public TipoProcessoDocumentoManager getTipoProcessoDocumentoManager(){
+		if (tipoProcessoDocumentoManager == null)
+			tipoProcessoDocumentoManager = ComponentUtil.getComponent(TipoProcessoDocumentoManager.NAME);
+		return tipoProcessoDocumentoManager;
+	}
 	
 }
