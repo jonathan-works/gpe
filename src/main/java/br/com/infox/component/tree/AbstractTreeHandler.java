40ed386b82a056819459359c98d5f86c064e877e
Gabriel Morrison Lima Dantas <gabriel@infox.com.br>
2013-04-09 09:43:37 -0300

    TreeHandler ajustado para API da tree do Richfaces

--- src/main/java/br/com/infox/component/tree/AbstractTreeHandler.java
+++ src/main/java/br/com/infox/component/tree/AbstractTreeHandler.java
@@ -12,12 +12,13 @@
  Consulte a GNU GPL para mais detalhes.
  Você deve ter recebido uma cópia da GNU GPL junto com este programa; se não, 
  veja em http://www.gnu.org/licenses/   
-*/
+ */
 package br.com.infox.component.tree;
 
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
@@ -35,30 +36,32 @@ import org.richfaces.function.RichFunction;
 
 import br.com.itx.util.EntityUtil;
 
-
 @Scope(ScopeType.CONVERSATION)
-public abstract class AbstractTreeHandler<E> implements TreeHandler<E>, Serializable {
+public abstract class AbstractTreeHandler<E> implements TreeHandler<E>,
+		Serializable {
 
-	private static final LogProvider LOG = Logging.getLogProvider(AbstractTreeHandler.class);	
-	
+	private static final LogProvider LOG = Logging
+			.getLogProvider(AbstractTreeHandler.class);
+
 	private static final long serialVersionUID = 1L;
 
 	private E selected;
-	
+
 	protected List<EntityNode<E>> rootList;
-	
+
 	protected String treeId;
-	
+
 	private String iconFolder;
-	
+
 	private String iconLeaf;
-	
+
 	private boolean folderSelectable = true;
-	
+
 	private String expression;
-	
-	private List<EntityNode<E>> selectedNodesList = new ArrayList<EntityNode<E>>(0);
-	
+
+	private List<EntityNode<E>> selectedNodesList = new ArrayList<EntityNode<E>>(
+			0);
+
 	@Override
 	public void clearTree() {
 		selectedNodesList = new ArrayList<EntityNode<E>>();
@@ -66,22 +69,22 @@ public abstract class AbstractTreeHandler<E> implements TreeHandler<E>, Serializ
 		selected = null;
 		clearUITree();
 		if (expression != null) {
-			Expressions.instance().createValueExpression(expression).setValue(null);
+			Expressions.instance().createValueExpression(expression)
+					.setValue(null);
 		}
 	}
 
 	private void clearUITree() {
 		if (treeId != null) {
-			javax.faces.component.UIComponent comp = RichFunction.findComponent(treeId);
-			
-			if (! comp.getClass().equals(UITree.class) )
+			javax.faces.component.UIComponent comp = RichFunction
+					.findComponent(treeId);
+
+			if (!comp.getClass().equals(UITree.class))
 				return;
-			
+
 			UITree tree = (UITree) comp;
-			// TODO: Verificar esses setSelected
-//			tree.setSelected();
 			tree.setRowKey(null);
-//			tree.setSelected();
+			tree.setSelection(null);
 		}
 	}
 
@@ -101,11 +104,11 @@ public abstract class AbstractTreeHandler<E> implements TreeHandler<E>, Serializ
 	protected EntityNode<E> createNode() {
 		return new EntityNode<E>(getQueryChildrenList());
 	}
-	
+
 	/**
-	 * Lista de queries que irão gerar os nós filhos
-	 * Caso haja mais de uma query, deve-se sobrescrever esse método
-	 * e retornar null no método getQueryChildren()
+	 * Lista de queries que irão gerar os nós filhos Caso haja mais de uma
+	 * query, deve-se sobrescrever esse método e retornar null no método
+	 * getQueryChildren()
 	 * 
 	 * @return
 	 */
@@ -114,7 +117,7 @@ public abstract class AbstractTreeHandler<E> implements TreeHandler<E>, Serializ
 		children[0] = getQueryChildren();
 		return children;
 	}
-	
+
 	protected EntityManager getEntityManager() {
 		return EntityUtil.getEntityManager();
 	}
@@ -127,7 +130,8 @@ public abstract class AbstractTreeHandler<E> implements TreeHandler<E>, Serializ
 		}
 		Object value = null;
 		try {
-			value = Expressions.instance().createValueExpression(expression).getValue();
+			value = Expressions.instance().createValueExpression(expression)
+					.getValue();
 		} catch (Exception ignore) {
 			ignore.printStackTrace();
 		}
@@ -139,15 +143,22 @@ public abstract class AbstractTreeHandler<E> implements TreeHandler<E>, Serializ
 		if (expression == null) {
 			this.selected = selected;
 		} else {
-			Expressions.instance().createValueExpression(expression).setValue(selected);
+			Expressions.instance().createValueExpression(expression)
+					.setValue(selected);
 		}
 	}
 
 	@Override
 	public void processTreeSelectionChange(TreeSelectionChangeEvent ev) {
+		// Considerando single selection
+		Object selectionKey = new ArrayList<Object>(ev.getNewSelection()).get(0);
 		UITree tree = (UITree) ev.getSource();
 		treeId = tree.getId();
-		EntityNode<E> en = (EntityNode<E>) tree.getData(); 
+
+		Object key = tree.getRowKey();
+		tree.setRowKey(selectionKey);
+		EntityNode<E> en = (EntityNode<E>) tree.getRowData();
+		tree.setRowKey(key);
 		setSelected(en.getEntity());
 		Events.instance().raiseEvent(getEventSelected(), getSelected());
 	}
@@ -157,7 +168,7 @@ public abstract class AbstractTreeHandler<E> implements TreeHandler<E>, Serializ
 	}
 
 	protected abstract String getQueryRoots();
-	
+
 	protected abstract String getQueryChildren();
 
 	@Override
@@ -189,7 +200,7 @@ public abstract class AbstractTreeHandler<E> implements TreeHandler<E>, Serializ
 	public void setFolderSelectable(boolean folderSelectable) {
 		this.folderSelectable = folderSelectable;
 	}
-	
+
 	public String getExpression() {
 		return expression;
 	}
@@ -200,6 +211,7 @@ public abstract class AbstractTreeHandler<E> implements TreeHandler<E>, Serializ
 
 	/**
 	 * Tratamento para que a string não fique maior que o tamanho do campo
+	 * 
 	 * @param selected
 	 * @return
 	 */
@@ -210,15 +222,15 @@ public abstract class AbstractTreeHandler<E> implements TreeHandler<E>, Serializ
 		}
 		if (selected.toString().length() > 25) {
 			selecionado = selected.toString().substring(0, 25) + "...";
-		}
-		else {
+		} else {
 			selecionado = selected.toString();
 		}
 		return selecionado;
 	}
-	
+
 	/**
 	 * Método que retorna a lista dos itens selecionados.
+	 * 
 	 * @return - Lista dos itens selecionados.
 	 */
 	public List<E> getSelectedTree() {
@@ -228,22 +240,23 @@ public abstract class AbstractTreeHandler<E> implements TreeHandler<E>, Serializ
 		}
 		return selectedList;
 	}
-	
 
 	public List<EntityNode<E>> getSelectedNodesList() {
 		return selectedNodesList;
 	}
-	
+
 	public void setSelectedNodesList(List<EntityNode<E>> selectedNodesList) {
 		this.selectedNodesList = selectedNodesList;
 	}
-	
+
 	/**
 	 * Insere o nó selecionado pela checkBox na lista dos nós selecionados.
-	 * @param node - Nó selecionado pelo usuário
+	 * 
+	 * @param node
+	 *            - Nó selecionado pelo usuário
 	 */
 	public void setSelectedNode(EntityNode<E> node) {
-		if(getSelected() == null || getSelected().toString() == null) {
+		if (getSelected() == null || getSelected().toString() == null) {
 			setSelected(node.getEntity());
 		}
 		if (selectedNodesList.contains(node)) {
@@ -253,24 +266,24 @@ public abstract class AbstractTreeHandler<E> implements TreeHandler<E>, Serializ
 			selectedNodesList.add(node);
 			selectAllChildren(node, true);
 		}
-	}	
+	}
 
 	private void selectAllChildren(EntityNode<E> selectedNode, boolean operation) {
-		for(EntityNode<E> node : selectedNode.getNodes()) {
+		for (EntityNode<E> node : selectedNode.getNodes()) {
 			selectAllChildren(node, operation);
 			node.setSelected(operation);
-			if(operation) {
+			if (operation) {
 				selectedNodesList.add(node);
 			} else {
 				selectedNodesList.remove(node);
 			}
 		}
 	}
-	
 
 	/**
 	 * Metodo que retorna a entidade que deve ser ignorada na montagem do
 	 * treeview
+	 * 
 	 * @return
 	 */
 	protected E getEntityToIgnore() {
