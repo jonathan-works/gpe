3b398386ed71ee63adfcfff2239d30e696d9055e
Jonas Santos Bezerra <jonasbezerra@infox.com.br>
2013-04-09 11:13:25 -0300

    Renomeação do Home de Histórico Parte Processo

--- src/main/java/br/com/infox/ibpm/home/HistoricoParteProcessoHome.java
+++ src/main/java/br/com/infox/ibpm/home/HistoricoParteProcessoHome.java
@@ -2,13 +2,13 @@ package br.com.infox.ibpm.home;
 
 import org.jboss.seam.annotations.Name;
 
+import br.com.infox.ibpm.entity.HistoricoParteProcesso;
 import br.com.itx.component.AbstractHome;
 
-@Name(HistoricoParteProcesso.NAME)
-public class HistoricoParteProcesso extends AbstractHome<HistoricoParteProcesso> {
+@Name(HistoricoParteProcessoHome.NAME)
+public class HistoricoParteProcessoHome extends AbstractHome<HistoricoParteProcesso> {
 
 	private static final long serialVersionUID = 1L;
 	public static final String NAME = "historicoProcessoHome";
-	
 	
 }
