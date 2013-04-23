59512ed3c23c10f5de80b96e1f741b912c62b6dd
erikliberal <erikliberal@infox.com.br>
2013-04-23 10:49:54 -0300

    Mudanças necessárias para que a aplicação suba.

--- src/main/java/br/com/infox/epp/action/BamAction.java
+++ src/main/java/br/com/infox/epp/action/BamAction.java
@@ -11,7 +11,6 @@ import org.jboss.seam.bpm.ManagedJbpmContext;
 import org.jbpm.taskmgmt.exe.TaskInstance;
 
 import br.com.infox.epp.entity.ProcessoEpa;
-import br.com.infox.epp.entity.ProcessoEpaTarefa;
 import br.com.infox.epp.manager.FluxoManager;
 import br.com.infox.epp.manager.ProcessoEpaManager;
 import br.com.infox.ibpm.entity.Fluxo;
