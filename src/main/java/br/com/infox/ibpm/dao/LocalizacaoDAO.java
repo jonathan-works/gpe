36b38811d810c3123285c5f20796001d00fd155f
Jonas Santos Bezerra <jonasbezerra@infox.com.br>
2013-05-21 08:32:24 -0300

    Migração para JSF 2: Remoção de estruturaLocalizacaoItems.component.xml e ajustes nas classes subjacentes

---
+++ src/main/java/br/com/infox/ibpm/dao/LocalizacaoDAO.java
@@ -0,0 +1,23 @@
+package br.com.infox.ibpm.dao;
+
+import java.util.List;
+
+import org.jboss.seam.annotations.AutoCreate;
+import org.jboss.seam.annotations.Name;
+
+import br.com.infox.core.dao.GenericDAO;
+import br.com.infox.ibpm.entity.Localizacao;
+
+@Name(LocalizacaoDAO.NAME)
+@AutoCreate
+public class LocalizacaoDAO extends GenericDAO {
+	
+	private static final long serialVersionUID = 1L;
+	public static final String NAME = "localizacaoDAO";
+	
+	public List<Localizacao> getLocalizacoesEstrutura(){
+		String hql = "select o from Localizacao where o.estrutura = true order by o.localizacao";
+		return entityManager.createQuery(hql).getResultList();
+	}
+
+}
