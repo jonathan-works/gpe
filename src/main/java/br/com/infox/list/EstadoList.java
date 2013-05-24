2f23bd849a8779af5e8e3b55528872b6376d2bc1
Jonas Santos Bezerra <jonasbezerra@infox.com.br>
2013-05-24 11:04:19 -0300

    Migração para JSF 2: Remoção de estadoItems.component.xml e ajustes correlatos

---
+++ src/main/java/br/com/infox/list/EstadoList.java
@@ -0,0 +1,45 @@
+package br.com.infox.list;
+
+import java.util.Map;
+
+import org.jboss.seam.ScopeType;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Scope;
+
+import br.com.infox.core.action.list.EntityList;
+import br.com.infox.core.action.list.SearchCriteria;
+import br.com.infox.ibpm.entity.Estado;
+
+@Name(EstadoList.NAME)
+@Scope(ScopeType.PAGE)
+public class EstadoList extends EntityList<Estado> {
+	
+	public static final String NAME = "estadoList";
+	private static final long serialVersionUID = 1L;
+	
+	private static final String DEFAULT_EJBQL = "select o from Estado o";
+	private static final String DEFAULT_ORDER = "estado";
+
+	@Override
+	protected void addSearchFields() {
+		addSearchField("estado", SearchCriteria.igual);
+
+	}
+
+	@Override
+	protected String getDefaultEjbql() {
+		return DEFAULT_EJBQL;
+	}
+
+	@Override
+	protected String getDefaultOrder() {
+		return DEFAULT_ORDER;
+	}
+
+	@Override
+	protected Map<String, String> getCustomColumnsOrder() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
