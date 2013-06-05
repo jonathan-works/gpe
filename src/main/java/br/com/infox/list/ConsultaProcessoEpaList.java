eb64708086a330b11d73c68788539eb8ff154d09
Jonas Santos Bezerra <jonasbezerra@infox.com.br>
2013-06-05 08:49:45 -0300

    Criação de ConsultaProcessoEpaList: futura substituta de ConsultaProcessoGrid

---
+++ src/main/java/br/com/infox/list/ConsultaProcessoEpaList.java
@@ -0,0 +1,57 @@
+package br.com.infox.list;
+
+import java.util.Map;
+
+import org.jboss.seam.ScopeType;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Scope;
+
+import br.com.infox.core.action.list.EntityList;
+import br.com.infox.core.action.list.SearchCriteria;
+import br.com.infox.epp.entity.ProcessoEpa;
+
+@Name(ConsultaProcessoEpaList.NAME)
+@Scope(ScopeType.PAGE)
+public class ConsultaProcessoEpaList extends EntityList<ProcessoEpa> {
+
+	private static final long serialVersionUID = 1L;
+	public static final String NAME = "consultaProcessoEpaList";
+	
+	private static final String DEFAULT_EJBQL = "select o from ProcessoEpa o";
+	private static final String DEFAULT_ORDER = "dataInicio";
+	
+	private static final String R1 = "o.idProcesso in (#{painelUsuarioHome.processoIdList})";
+	private static final String R2 = "o.caixa.idCaixa = #{painelUsuarioHome.idCaixa}";
+	private static final String R3 = "o.numeroProcesso = #{consultaProcessoHome.instance.numeroProcesso}";
+	private static final String R4 = "o.naturezaCategoriaFluxo.natureza = #{consultaProcessoHome.instance.natureza}";
+	private static final String R5 = "o.naturezaCategoriaFluxo.categoria = #{consultaProcessoHome.instance.categoria}";
+	//private static final String R6 = "cast(o.dataInicio as date) >= #{consultaProcessoHome.instance.dataInicio} " +
+	//									"and cast(o.dataInicio as date)<= #{consultaProcessoHome.instance.dataFim}";
+
+	@Override
+	protected void addSearchFields() {
+		addSearchField("idProcesso", SearchCriteria.igual, R1);
+		addSearchField("caixa.idCaixa", SearchCriteria.igual, R2);
+		addSearchField("numeroProcesso", SearchCriteria.igual, R3);
+		addSearchField("naturezaCategoriaFluxo.natureza", SearchCriteria.igual, R4);
+		addSearchField("naturezaCategoriaFluxo.categoria", SearchCriteria.igual, R5);
+		//addSearchField("dataInicio", SearchCriteria.igual, R6);
+	}
+
+	@Override
+	protected String getDefaultEjbql() {
+		return DEFAULT_EJBQL;
+	}
+
+	@Override
+	protected String getDefaultOrder() {
+		return DEFAULT_ORDER;
+	}
+
+	@Override
+	protected Map<String, String> getCustomColumnsOrder() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
