47277e683f6c10a1531f4bcd8cb31cb513676a13
erikliberal <erikliberal@infox.com.br>
2013-06-13 12:52:20 -0300

    Correção na inicialização do relógio em caso de não existir jQuery

--- src/main/webapp/resources/js/liveclock.js
+++ src/main/webapp/resources/js/liveclock.js
@@ -1,9 +1,10 @@
 (function() {
 	var $digital = new Date();
 	function $updateClock() {
-		var $ = window.jQuery || function() {this.html=function(){}};
-		$digital.setTime(Date.now());
-		$(".liveclock").html($digital.toLocaleTimeString())
+		if (window.jQuery) {
+			$digital.setTime(Date.now());
+			jQuery(".liveclock").html($digital.toLocaleTimeString());
+		}
 	}
 	window.clockTimer = window.clockTimer || setInterval($updateClock, 1000);
 })()
