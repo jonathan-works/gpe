32e0ebcce3b5c43ea7071ab36435f942c0722ede
erikliberal <erikliberal@infox.com.br>
2013-06-17 09:57:03 -0300

    Remoção de código específico para IExplorer que retornava erros

--- src/main/webapp/resources/js/util.js
+++ src/main/webapp/resources/js/util.js
@@ -88,38 +88,3 @@ function openPopUp(id, url, width, height) {
 	var popUp = window.open(url, id, featurePopUp);	
 	popUp.moveTo(0, 0);
 }
-
-// Corrige as imagens *.png no IE
-function correctPNG(){
-   var browserType = navigator.appName;  
-   if (browserType == "Microsoft Internet Explorer"){  
-	   for(var i = 0; i < document.images.length; i++) {
-	      var img = document.images[i];
-	      var imgName = img.src.toUpperCase();
-	      if (imgName.substring(imgName.length-3, imgName.length) == "PNG") {
-	         var imgID = (img.id) ? "id='" + img.id + "' " : "";
-	         var imgClass = (img.className) ? "class='" + img.className + "' " : "";
-	         var imgTitle = (img.title) ? "title='" + img.title + "' " : "title='" + img.alt + "' ";
-	         var imgStyle = "display:inline-block;" + img.style.cssText;
-	         if (img.align == "left") {
-	        	 imgStyle = "float:left;" + imgStyle;
-	         }
-	         if (img.align == "right") {
-	        	 imgStyle = "float:right;" + imgStyle;
-	         }
-	         if (img.parentElement.href) {
-	        	 imgStyle = "cursor:hand;" + imgStyle;
-	         }	         
-	         var strNewHTML = "<span " + imgID + imgClass + imgTitle
-	         	+ " style=\"" + "width:" + img.width + "px; height:" + img.height + "px;" + imgStyle + ";"
-	         	+ "filter:progid:DXImageTransform.Microsoft.AlphaImageLoader"
-	         	+ "(src=\'" + img.src + "\', sizingMethod='scale');\"></span>";
-	         img.outerHTML = strNewHTML;
-	         i = i - 1;
-	      }
-	   }
-   }
-}
-try{
-	window.attachEvent("onload", correctPNG);
-} catch(e) {}
