3786f10fb2d4f5ce46bb9864168a4c507cdd7fe8
Jonas Santos Bezerra <jonasbezerra@infox.com.br>
2013-07-03 12:00:41 -0300

    Criação de ParametroManager

---
+++ src/main/java/br/com/infox/ibpm/manager/ParametroManager.java
@@ -0,0 +1,28 @@
+package br.com.infox.ibpm.manager;
+
+import java.util.List;
+
+import org.jboss.seam.annotations.AutoCreate;
+import org.jboss.seam.annotations.In;
+import org.jboss.seam.annotations.Name;
+import br.com.infox.core.manager.GenericManager;
+import br.com.infox.ibpm.dao.ParametroDAO;
+import br.com.infox.ibpm.entity.Parametro;
+
+@Name(ParametroManager.NAME)
+@AutoCreate
+public class ParametroManager extends GenericManager {
+
+	private static final long serialVersionUID = 1L;
+	public static final String NAME = "parametroManager";
+	
+	@In private ParametroDAO parametroDAO;
+	
+	public String getParametro(String nome) throws IllegalArgumentException {
+		List<Parametro> resultList = parametroDAO.getParametrosByNomeVariavel(nome);
+		if (!resultList.isEmpty()) {
+			return resultList.get(0).getValorVariavel();
+		} else throw new IllegalArgumentException();
+	}
+
+}
