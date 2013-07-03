cb86ebbb03f88990ad0b4c9d34870848fbddf76b
Jonas Santos Bezerra <jonasbezerra@infox.com.br>
2013-07-03 09:27:49 -0300

    Criação de método de consulta de papel por identificador

--- src/main/java/br/com/infox/epp/dao/PapelDAO.java
+++ src/main/java/br/com/infox/epp/dao/PapelDAO.java
@@ -2,12 +2,15 @@ package br.com.infox.epp.dao;
 
 import java.util.List;
 
+import javax.persistence.Query;
+
 import org.jboss.seam.annotations.AutoCreate;
 import org.jboss.seam.annotations.Name;
 
 import br.com.infox.access.entity.Papel;
 import br.com.infox.core.dao.GenericDAO;
 import br.com.infox.ibpm.entity.TipoModeloDocumento;
+import br.com.itx.util.EntityUtil;
 
 @Name(PapelDAO.NAME)
 @AutoCreate
@@ -17,13 +20,18 @@ public class PapelDAO extends GenericDAO {
 	public static final String NAME = "papelDAO";
 	
 	@SuppressWarnings("unchecked")
-	public List<Papel> getPapeisNaoAssociadosATipoModeloDocumento(
-			TipoModeloDocumento tipoModeloDocumento) {
+	public List<Papel> getPapeisNaoAssociadosATipoModeloDocumento(TipoModeloDocumento tipoModeloDocumento) {
 		String hql = "select o from Papel o where identificador not like '/%' and o.idPapel not in ("
 				+ "select p.papel.idPapel from TipoModeloDocumentoPapel p "
 				+ "where p.tipoModeloDocumento = :tipoModeloDocumento)";
 		return (List<Papel>) entityManager.createQuery(hql).setParameter("tipoModeloDocumento",
 				tipoModeloDocumento).getResultList();
+	}
+	
+	public Papel getPapelByIndentificador(String identificador){
+		String hql = "select o from Papel o where o.identificador = :identificador";
+		Query query = entityManager.createQuery(hql).setParameter("identificador", identificador);
+		return EntityUtil.getSingleResult(query);
 	}
 
 }
