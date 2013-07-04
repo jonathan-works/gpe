9a89411b910c38c332ba5a96fc3b224f27eee6d1
Jonas Santos Bezerra <jonasbezerra@infox.com.br>
2013-07-04 08:07:34 -0300

    Adição de Método getAgrupamentosByTarefaEvento

--- src/main/java/br/com/infox/epp/dao/AgrupamentoDAO.java
+++ src/main/java/br/com/infox/epp/dao/AgrupamentoDAO.java
@@ -1,10 +1,14 @@
 package br.com.infox.epp.dao;
 
+import java.util.List;
+
 import org.jboss.seam.ScopeType;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Scope;
 
 import br.com.infox.core.dao.GenericDAO;
+import br.com.infox.ibpm.entity.Agrupamento;
+import br.com.infox.ibpm.entity.TarefaEvento;
 
 @Name(AgrupamentoDAO.NAME)
 @Scope(ScopeType.EVENT)
@@ -12,5 +16,14 @@ public class AgrupamentoDAO extends GenericDAO {
 
 	private static final long serialVersionUID = 1L;
 	public static final String NAME = "agrupamentoDao";
+	
+	@SuppressWarnings("unchecked")
+	public List<Agrupamento> getAgrupamentosByTarefaEvento(TarefaEvento tarefaEvento){
+		String hql = "select a from Agrupamento a " +
+						"inner join a.agrupamentoTarefaList at " +
+						"where at.tarefaEvento = :tarefaEvento";
+		return (List<Agrupamento>) entityManager.createQuery(hql)
+				.setParameter("tarefaEvento", tarefaEvento).getResultList();
+	}
 	
 }
