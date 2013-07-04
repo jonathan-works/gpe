fb0172423eadc9acbfc44e819e3ef68dd6d5e230
Jonas Santos Bezerra <jonasbezerra@infox.com.br>
2013-07-04 08:56:05 -0300

    Criação de Método getNextTarefaEvento

--- src/main/java/br/com/infox/epp/manager/TarefaEventoManager.java
+++ src/main/java/br/com/infox/epp/manager/TarefaEventoManager.java
@@ -1,11 +1,17 @@
 package br.com.infox.epp.manager;
 
+import java.util.List;
+
 import org.jboss.seam.annotations.AutoCreate;
 import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
+import org.jboss.seam.bpm.TaskInstance;
 
 import br.com.infox.core.manager.GenericManager;
 import br.com.infox.epp.dao.TarefaEventoDAO;
+import br.com.infox.ibpm.entity.Processo;
+import br.com.infox.ibpm.entity.TarefaEvento;
+import br.com.infox.ibpm.jbpm.JbpmUtil;
 
 @Name(TarefaEventoManager.NAME)
 @AutoCreate
@@ -15,5 +21,12 @@ public class TarefaEventoManager extends GenericManager {
 	public static final String NAME = "tarefaEventoManager";
 	
 	@In private TarefaEventoDAO tarefaEventoDAO;
+	
+	public List<TarefaEvento> getNextTarefaEvento(){
+		Processo processo = JbpmUtil.getProcesso();
+		String task = TaskInstance.instance().getTask().getName();
+		String fluxo = TaskInstance.instance().getProcessInstance().getProcessDefinition().getName();
+		return tarefaEventoDAO.getNextTarefaEvento(processo, task, fluxo);
+	}
 
 }
