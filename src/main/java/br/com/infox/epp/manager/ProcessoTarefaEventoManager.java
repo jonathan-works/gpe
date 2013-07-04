ca17ac6a6580b281ac83dd2acd9631b651ebb98a
Jonas Santos Bezerra <jonasbezerra@infox.com.br>
2013-07-04 09:49:08 -0300

    Injeção de ProcessoTarefaEventoDAO

--- src/main/java/br/com/infox/epp/manager/ProcessoTarefaEventoManager.java
+++ src/main/java/br/com/infox/epp/manager/ProcessoTarefaEventoManager.java
@@ -1,9 +1,11 @@
 package br.com.infox.epp.manager;
 
 import org.jboss.seam.annotations.AutoCreate;
+import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
 
 import br.com.infox.core.manager.GenericManager;
+import br.com.infox.epp.dao.ProcessoTarefaEventoDAO;
 
 @Name(ProcessoTarefaEventoManager.NAME)
 @AutoCreate
@@ -11,5 +13,7 @@ public class ProcessoTarefaEventoManager extends GenericManager {
 
 	private static final long serialVersionUID = 1L;
 	public static final String NAME = "processoTarefaEventoManager";
+	
+	@In private ProcessoTarefaEventoDAO processoTarefaEventoDAO;
 	
 }
