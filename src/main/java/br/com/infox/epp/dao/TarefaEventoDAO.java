917c7de6f8434e5b38bffd5f0c2b2472811b79f0
Jonas Santos Bezerra <jonasbezerra@infox.com.br>
2013-07-04 08:58:45 -0300

    Ajuste de retorno do método getNextTarefaEvento

--- src/main/java/br/com/infox/epp/dao/TarefaEventoDAO.java
+++ src/main/java/br/com/infox/epp/dao/TarefaEventoDAO.java
@@ -1,6 +1,5 @@
 package br.com.infox.epp.dao;
 
-import java.util.List;
 import org.jboss.seam.annotations.AutoCreate;
 import org.jboss.seam.annotations.Name;
 import br.com.infox.core.dao.GenericDAO;
@@ -15,14 +14,13 @@ public class TarefaEventoDAO extends GenericDAO {
 	private static final long serialVersionUID = 1L;
 	public static final String NAME = "tarefaEventoDAO";
 	
-	@SuppressWarnings("unchecked")
-	public List<TarefaEvento> getNextTarefaEvento(Processo processo, String task, String fluxo){
+	public TarefaEvento getNextTarefaEvento(Processo processo, String task, String fluxo){
 		String hql = "select et from ProcessoTarefaEvento o " +
 						"inner join o.tarefaEvento et inner join et.tarefa t " +
 						"where o.registrado = false and o.processo = :processo and " +
 						"t.tarefa = :tarefa and t.fluxo.fluxo = :fluxo " +
 						"order by et.evento";
-		return (List<TarefaEvento>) EntityUtil.createQuery(hql)
+		return (TarefaEvento) EntityUtil.createQuery(hql)
 									.setParameter("processo", processo)
 									.setParameter("tarefa", task)
 									.setParameter("fluxo", fluxo).getSingleResult();
