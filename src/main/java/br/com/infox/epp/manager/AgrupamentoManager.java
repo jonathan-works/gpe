e5c454b9e5e706a9338362e4e0c90c35e0bee00e
Jonas Santos Bezerra <jonasbezerra@infox.com.br>
2013-07-04 08:13:20 -0300

    Criado AgrupamentoManager

---
+++ src/main/java/br/com/infox/epp/manager/AgrupamentoManager.java
@@ -0,0 +1,27 @@
+package br.com.infox.epp.manager;
+
+import java.util.List;
+
+import org.jboss.seam.annotations.AutoCreate;
+import org.jboss.seam.annotations.In;
+import org.jboss.seam.annotations.Name;
+
+import br.com.infox.core.manager.GenericManager;
+import br.com.infox.epp.dao.AgrupamentoDAO;
+import br.com.infox.ibpm.entity.Agrupamento;
+import br.com.infox.ibpm.entity.TarefaEvento;
+
+@Name(AgrupamentoManager.NAME)
+@AutoCreate
+public class AgrupamentoManager extends GenericManager {
+
+	private static final long serialVersionUID = 1L;
+	public static final String NAME = "agrupamentoManager";
+	
+	@In private AgrupamentoDAO agrupamentoDAO;
+	
+	public List<Agrupamento> getAgrupamentos(TarefaEvento tarefaEvento){
+		return agrupamentoDAO.getAgrupamentosByTarefaEvento(tarefaEvento);
+	}
+
+}
