48e9f5a07f7e7b1a096dcd56145f9bfa9e0800bb
Jonas Santos Bezerra <jonasbezerra@infox.com.br>
2013-07-04 09:44:18 -0300

    Criação de método destroyProcessoTarefaEvento

--- src/main/java/br/com/infox/epp/dao/ProcessoTarefaEventoDAO.java
+++ src/main/java/br/com/infox/epp/dao/ProcessoTarefaEventoDAO.java
@@ -1,9 +1,15 @@
 package br.com.infox.epp.dao;
 
+import javax.persistence.Query;
+
 import org.jboss.seam.annotations.AutoCreate;
 import org.jboss.seam.annotations.Name;
+import org.jboss.seam.bpm.TaskInstance;
 
 import br.com.infox.core.dao.GenericDAO;
+import br.com.infox.ibpm.entity.Processo;
+import br.com.infox.ibpm.entity.TarefaEvento;
+import br.com.infox.ibpm.jbpm.JbpmUtil;
 
 @Name(ProcessoTarefaEventoDAO.NAME)
 @AutoCreate
@@ -11,5 +17,17 @@ public class ProcessoTarefaEventoDAO extends GenericDAO {
 
 	private static final long serialVersionUID = 1L;
 	public static final String NAME = "processoTarefaEventoDAO";
+
+	public void destroyProcessoTarefaEvento(Processo processo, String task, String fluxo) {
+		String hql = "delete from ProcessoTarefaEvento o " +
+						"where o.processo = :processo and exists " +
+							"(select 1 from TarefaEvento et " +
+							"inner join et.tarefa t where et = o.tarefaEvento and " +
+							"t.tarefa = :tarefa and t.fluxo.fluxo = :fluxo)";
+		entityManager.createQuery(hql).setParameter("processo", processo)
+						.setParameter("tarefa", task)
+						.setParameter("fluxo", fluxo)
+						.executeUpdate();
+	}
 
 }
