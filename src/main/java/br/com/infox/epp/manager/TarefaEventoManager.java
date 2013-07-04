c61bc0b6a59be095a9a5da3dac467eb1b65d94be
Jonas Santos Bezerra <jonasbezerra@infox.com.br>
2013-07-04 08:50:45 -0300

    Injeção de TarefaEventoDAO

--- src/main/java/br/com/infox/epp/manager/TarefaEventoManager.java
+++ src/main/java/br/com/infox/epp/manager/TarefaEventoManager.java
@@ -1,9 +1,11 @@
 package br.com.infox.epp.manager;
 
 import org.jboss.seam.annotations.AutoCreate;
+import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
 
 import br.com.infox.core.manager.GenericManager;
+import br.com.infox.epp.dao.TarefaEventoDAO;
 
 @Name(TarefaEventoManager.NAME)
 @AutoCreate
@@ -11,5 +13,7 @@ public class TarefaEventoManager extends GenericManager {
 
 	private static final long serialVersionUID = 1L;
 	public static final String NAME = "tarefaEventoManager";
+	
+	@In private TarefaEventoDAO tarefaEventoDAO;
 
 }
