08e1242685aaab8c105c5433230b925139c5b0e4
Jonas Santos Bezerra <jonasbezerra@infox.com.br>
2013-07-04 10:36:50 -0300

    Remoção de imports não utilizados

--- src/main/java/br/com/infox/epp/dao/ProcessoTarefaEventoDAO.java
+++ src/main/java/br/com/infox/epp/dao/ProcessoTarefaEventoDAO.java
@@ -1,13 +1,10 @@
 package br.com.infox.epp.dao;
 
-import javax.persistence.Query;
-
 import org.jboss.seam.annotations.AutoCreate;
 import org.jboss.seam.annotations.Name;
 import br.com.infox.core.dao.GenericDAO;
 import br.com.infox.ibpm.entity.Processo;
 import br.com.infox.ibpm.entity.TarefaEvento;
-import br.com.infox.ibpm.jbpm.JbpmUtil;
 
 @Name(ProcessoTarefaEventoDAO.NAME)
 @AutoCreate
