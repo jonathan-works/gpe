1ff7ed9a79085aac4794287a524f51e0a31e608b
Jonas Santos Bezerra <jonasbezerra@infox.com.br>
2013-07-04 08:25:24 -0300

    Remoção de trecho de código morto

--- src/main/java/br/com/infox/ibpm/bean/TarefaEventoTree.java
+++ src/main/java/br/com/infox/ibpm/bean/TarefaEventoTree.java
@@ -54,13 +54,6 @@ public class TarefaEventoTree implements Serializable {
 		if(agrupamentos == null) {
 			StringBuilder agrupamentosId = new StringBuilder();
 			currentEvent = getNextEvent();
-//			StringBuilder sb = new StringBuilder();
-//			sb.append("select a from Agrupamento a ")
-//			  .append("inner join a.agrupamentoTarefaList at ")
-//			  .append("inner join at.tarefaEvento et ")
-//			  .append("where et = :tarefaEvento");
-//			Query q = getEntityManager().createQuery(sb.toString());
-//			q.setParameter("tarefaEvento", currentEvent);
 			int i = 0;
 			List<Agrupamento> agrupamentos = agrupamentoManager.getAgrupamentosByTarefaEvento(currentEvent);
 			for(Agrupamento a : agrupamentos) {
