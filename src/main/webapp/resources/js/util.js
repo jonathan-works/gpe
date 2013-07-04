c5f7bcb7e9aa9a2923e3ae072272e7e58d609e83
erikliberal <erikliberal@infox.com.br>
2013-07-04 12:00:37 -0300

    Atualizando declaração das funções para utilizarem o padrão namespace, mas mantendo os caminhos antigos para manter compatibilidade

--- src/main/webapp/resources/js/util.js
+++ src/main/webapp/resources/js/util.js
@@ -1,81 +1,50 @@
-function defineObject(path, object, basePath) {
-  var packages = path.split(".");
-  var _namespace = basePath || window;
-  for(var i=0; i<packages.length; i++) {
-    var item = packages[i];
-    if (i==packages.length-1) {
-      _namespace = _namespace[item] = _namespace[item] || object;
-    } else {
-      _namespace = _namespace[item] = _namespace[item] || {};
-    }
-  }
-  return _namespace;
-}
-
-function existsObject(path, basePath) {
-	var packages = path.split(".");
-	var _namespace = basePath || window;
-	var result = true;
-	for(var i=0; i<packages.length; i++) {
-		var item = packages[i];
-		if (!_namespace[item]) {
-			return false;
-		}
-		_namespace = _namespace[item];
-	}
-	return true;
-}
-
 namespace("infox.util",{
 	applyCSS:function(args) {
 		setTimeout(function(){
 			$(args.selector).css(args.style);
 		},args.delay || 1);
+	},
+	openPopUp:function (id, url, width, height) {
+		var featPopUp = ["width="		,width,
+			             "height="		,height,
+			             "resizable=YES",
+			             "scrollbars=YES",
+			             "status=NO",
+			             "location=NO"];
+		var popUp = window.open(url, id, featPopUp.join(","));	
+		popUp.moveTo(0, 0);
+	},showLoading:function() {
+		RichFaces.$('modalStatus').show();	
+	},hideLoading:function() {
+		RichFaces.$('modalStatus').hide();
+	},refreshOpener:function() {
+		try {
+			opener.refreshPage();
+		} catch (e) {
+			console.error(e);
+		}
 	}
 });
-
-function showLoading() {	
-	if ($('#status')) {
-		$('#status').hide();
+namespace("infox.util",function(util) {
+	window.showLoading = util.showLoading;
+	window.hideLoading = util.hideLoading;
+	window.refreshOpener = util.refreshOpener;
+	window.openPopUp = util.openPopUp;
+	
+	if (!$.fn.clearForm) {
+		$.fn.clearForm = function() {
+			return this.each(function() {
+				var type = this.type; 
+				var tag = this.tagName.toLowerCase();
+				if (tag == 'form')
+					return $(':input',this).clearForm();
+				if (type == 'text' || type == 'password' || tag == 'textarea')
+					this.value = '';
+				else if (type == 'checkbox' || type == 'radio')
+					this.checked = false;
+				else if (tag == 'select')
+					this.selectedIndex = 0;
+			});
+		}	
 	}
-	RichFaces.$('modalStatus').show();
-}
-
-function hideLoading() {
-	RichFaces.$('modalStatus').hide();
-	if ($('#status')) {
-		$('#status').show();
-	}
-}
-
-function refreshOpener() {
-	try {
-	   opener.refreshPage();
-	} catch (e) {}
-}
-
-if (!$.fn.clearForm) {
-	$.fn.clearForm = function() {
-		return this.each(function() {
-			var type = this.type; 
-			var tag = this.tagName.toLowerCase();
-			if (tag == 'form')
-				return $(':input',this).clearForm();
-			if (type == 'text' || type == 'password' || tag == 'textarea')
-				this.value = '';
-			else if (type == 'checkbox' || type == 'radio')
-				this.checked = false;
-			else if (tag == 'select')
-				this.selectedIndex = 0;
-		});
-	}	
-}
-
-// Padronização das telas de popUps
-function openPopUp(id, url, width, height) {
-	if (!width) width = 800;
-	if (!height) height = 600;
-	var featurePopUp = "width="+width + ", height="+height+", resizable=YES, scrollbars=YES, status=NO, location=NO";
-	var popUp = window.open(url, id, featurePopUp);	
-	popUp.moveTo(0, 0);
-}
+});
