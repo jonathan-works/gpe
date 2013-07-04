0a8c65e62d6a80029a61ca1fa8f753fc792a1fe1
Jonas Santos Bezerra <jonasbezerra@infox.com.br>
2013-07-04 08:46:11 -0300

    Criação de método getNextTarefaEvento

--- src/main/java/br/com/infox/epp/dao/TarefaEventoDAO.java
+++ src/main/java/br/com/infox/epp/dao/TarefaEventoDAO.java
@@ -1,9 +1,12 @@
 package br.com.infox.epp.dao;
 
+import java.util.List;
 import org.jboss.seam.annotations.AutoCreate;
 import org.jboss.seam.annotations.Name;
 
 import br.com.infox.core.dao.GenericDAO;
+import br.com.infox.ibpm.entity.Processo;
+import br.com.infox.ibpm.entity.TarefaEvento;
 
 @Name(TarefaEventoDAO.NAME)
 @AutoCreate
@@ -11,5 +14,14 @@ public class TarefaEventoDAO extends GenericDAO {
 
 	private static final long serialVersionUID = 1L;
 	public static final String NAME = "tarefaEventoDAO";
+	
+	public List<TarefaEvento> getNextTarefaEvento(Processo processo, String tarefa, String fluxo){
+		String hql = "select et from ProcessoTarefaEvento o " +
+						"inner join o.tarefaEvento et inner join et.tarefa t " +
+						"where o.registrado = false and o.processo = :processo and " +
+						"t.tarefa = :tarefa and t.fluxo.fluxo = :fluxo " +
+						"order by et.evento";
+		return null;
+	}
 
 }
