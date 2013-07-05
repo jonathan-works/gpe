4bd54a54a200ac20249ad8cd2fc7dcbc26c24352
Jonas Santos Bezerra <jonasbezerra@infox.com.br>
2013-07-05 10:09:33 -0300

    Adicionadas notação de componentes PessoaFisicaDAO

--- src/main/java/br/com/infox/ibpm/dao/PessoaFisicaDAO.java
+++ src/main/java/br/com/infox/ibpm/dao/PessoaFisicaDAO.java
@@ -1,8 +1,13 @@
 package br.com.infox.ibpm.dao;
 
+import org.jboss.seam.annotations.AutoCreate;
+import org.jboss.seam.annotations.Name;
+
 import br.com.infox.core.dao.GenericDAO;
 import br.com.infox.ibpm.entity.PessoaFisica;
 
+@Name(PessoaFisicaDAO.NAME)
+@AutoCreate
 public class PessoaFisicaDAO extends GenericDAO {
 
 	private static final long serialVersionUID = 1L;
