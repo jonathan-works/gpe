3d2149f8e70dd884c39019427add3ec6f0739497
Jonas Santos Bezerra <jonasbezerra@infox.com.br>
2013-07-05 08:09:49 -0300

    Injetado SituacaoProcessoDAO em SituacaoProcessoManager

--- src/main/java/br/com/infox/ibpm/manager/SituacaoProcessoManager.java
+++ src/main/java/br/com/infox/ibpm/manager/SituacaoProcessoManager.java
@@ -1,9 +1,11 @@
 package br.com.infox.ibpm.manager;
 
 import org.jboss.seam.annotations.AutoCreate;
+import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
 
 import br.com.infox.core.manager.GenericManager;
+import br.com.infox.ibpm.dao.SituacaoProcessoDAO;
 
 @Name(SituacaoProcessoManager.NAME)
 @AutoCreate
@@ -11,5 +13,7 @@ public class SituacaoProcessoManager extends GenericManager {
 
 	private static final long serialVersionUID = 1L;
 	public static final String NAME = "situacaoProcessoManager";
+	
+	@In private SituacaoProcessoDAO situacaoProcessoDAO;
 
 }
