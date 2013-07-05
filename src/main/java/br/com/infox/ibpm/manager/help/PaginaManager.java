b563297df96b1ad2d40785341ad06ddd55a66db1
Jonas Santos Bezerra <jonasbezerra@infox.com.br>
2013-07-05 11:53:00 -0300

    Injeção do PaginaDAO

--- src/main/java/br/com/infox/ibpm/manager/help/PaginaManager.java
+++ src/main/java/br/com/infox/ibpm/manager/help/PaginaManager.java
@@ -1,7 +1,19 @@
 package br.com.infox.ibpm.manager.help;
 
-import br.com.infox.core.manager.GenericManager;
+import org.jboss.seam.annotations.AutoCreate;
+import org.jboss.seam.annotations.In;
+import org.jboss.seam.annotations.Name;
 
+import br.com.infox.core.manager.GenericManager;
+import br.com.infox.ibpm.dao.help.PaginaDAO;
+
+@Name(PaginaManager.NAME)
+@AutoCreate
 public class PaginaManager extends GenericManager {
+
+	private static final long serialVersionUID = 1L;
+	public static final String NAME = "paginaManager";
+	
+	@In private PaginaDAO paginaDAO;
 
 }
