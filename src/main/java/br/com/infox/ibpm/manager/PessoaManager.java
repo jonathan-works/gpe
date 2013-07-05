6806a3a0b341b63175511f03cfa720fa52af311b
Jonas Santos Bezerra <jonasbezerra@infox.com.br>
2013-07-05 10:41:59 -0300

    Criação de PessoaManager

---
+++ src/main/java/br/com/infox/ibpm/manager/PessoaManager.java
@@ -0,0 +1,30 @@
+package br.com.infox.ibpm.manager;
+
+import org.jboss.seam.annotations.AutoCreate;
+import org.jboss.seam.annotations.In;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.core.Events;
+
+import br.com.infox.core.manager.GenericManager;
+import br.com.infox.ibpm.dao.PessoaFisicaDAO;
+import br.com.infox.ibpm.dao.PessoaJuridicaDAO;
+
+@Name(PessoaManager.NAME)
+@AutoCreate
+public class PessoaManager extends GenericManager {
+
+	private static final long serialVersionUID = 1L;
+	public static final String NAME = "pessoaManager";
+	
+	@In private PessoaFisicaDAO pessoaFisicaDAO;
+	@In private PessoaJuridicaDAO pessoaJuridicaDAO;
+	
+	public void carregaPessoa(String tipoPessoa, String codigo){
+		if (tipoPessoa.equals("F") || tipoPessoa.equals("f")) {
+			Events.instance().raiseEvent("evtCarregarPessoaFisica", pessoaFisicaDAO.searchByCpf(codigo));
+		} else if (tipoPessoa.equals("J") || tipoPessoa.equals("j")){
+			Events.instance().raiseEvent("evtCarregarPessoaJuridica", pessoaJuridicaDAO.searchByCnpj(codigo));
+		} else return;
+	}
+
+}
