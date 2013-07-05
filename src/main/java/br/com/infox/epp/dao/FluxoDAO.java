72118a7fa4145731ec7af724c342140743d6ecbc
Jonas Santos Bezerra <jonasbezerra@infox.com.br>
2013-07-05 07:40:14 -0300

    Criado método getFluxoByDescricao

--- src/main/java/br/com/infox/epp/dao/FluxoDAO.java
+++ src/main/java/br/com/infox/epp/dao/FluxoDAO.java
@@ -4,6 +4,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.persistence.Query;
+
 import org.jboss.seam.ScopeType;
 import org.jboss.seam.annotations.AutoCreate;
 import org.jboss.seam.annotations.Name;
@@ -12,6 +14,7 @@ import org.jboss.seam.annotations.Scope;
 import br.com.infox.core.dao.GenericDAO;
 import br.com.infox.epp.query.FluxoQuery;
 import br.com.infox.ibpm.entity.Fluxo;
+import br.com.itx.util.EntityUtil;
 
 /**
  * Classe DAO para a entidade Fluxo
@@ -38,6 +41,12 @@ public class FluxoDAO extends GenericDAO {
 		Map<String, Object> map = new HashMap<String, Object>();
 		map.put(FluxoQuery.FLUXO_PARAM, fluxo);
 		return getNamedSingleResult(FluxoQuery.COUNT_PROCESSOS_ATRASADOS, map);
+	}
+	
+	public Fluxo getFluxoByDescricao(String descricao){
+		String hql = "select o from Fluxo o where o.fluxo like :descricao";
+		Query query = EntityUtil.createQuery(hql).setParameter("descricao", descricao);
+		return EntityUtil.getSingleResult(query);
 	}
 	
 }
