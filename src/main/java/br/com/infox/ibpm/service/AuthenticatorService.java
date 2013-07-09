4a885dd742872ac574a7b1f56a14992cacac56e5
Jonas Santos Bezerra <jonasbezerra@infox.com.br>
2013-07-09 09:59:55 -0300

    Movendo método autenticaManualmenteNoSeamSecurity do Authenticator para o AuthenticatorService

--- src/main/java/br/com/infox/ibpm/service/AuthenticatorService.java
+++ src/main/java/br/com/infox/ibpm/service/AuthenticatorService.java
@@ -1,7 +1,15 @@
 package br.com.infox.ibpm.service;
 
+import java.security.Principal;
+import java.util.List;
+
+import org.jboss.seam.Component;
 import org.jboss.seam.annotations.AutoCreate;
 import org.jboss.seam.annotations.Name;
+import org.jboss.seam.security.Credentials;
+import org.jboss.seam.security.Identity;
+import org.jboss.seam.security.management.IdentityManager;
+import org.jboss.ws.extensions.security.SimplePrincipal;
 
 import br.com.infox.core.manager.GenericManager;
 
@@ -11,5 +19,22 @@ public class AuthenticatorService extends GenericManager {
 
 	private static final long serialVersionUID = 1L;
 	public static final String NAME = "authenticatorService";
+	
+	public void autenticaManualmenteNoSeamSecurity(String login, IdentityManager identityManager) {
+		Principal principal = new SimplePrincipal(login);
+		Identity identity = Identity.instance();
+		identity.acceptExternallyAuthenticatedPrincipal(principal);
+		Credentials credentials = (Credentials) Component.getInstance(Credentials.class);
+		credentials.clear();
+		credentials.setUsername(login);
+		identity.getCredentials().clear();
+		identity.getCredentials().setUsername(login);
+		List<String> roles = identityManager.getImpliedRoles(login);
+		if (roles != null) {
+			for (String role : roles) {
+				identity.addRole(role);
+			}
+		}
+	}
 
 }
