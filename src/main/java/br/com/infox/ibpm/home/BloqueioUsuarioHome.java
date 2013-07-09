225c197e5c84709bbf04abf5a2d41759a3c982aa
Jonas Santos Bezerra <jonasbezerra@infox.com.br>
2013-07-09 07:22:15 -0300

    @Name refeito de acordo com Padrão INFOX

--- src/main/java/br/com/infox/ibpm/home/BloqueioUsuarioHome.java
+++ src/main/java/br/com/infox/ibpm/home/BloqueioUsuarioHome.java
@@ -16,17 +16,16 @@
 package br.com.infox.ibpm.home;
 
 import org.jboss.seam.annotations.Name;
-import org.jboss.seam.annotations.intercept.BypassInterceptors;
-
 import br.com.infox.ibpm.entity.BloqueioUsuario;
 
 
-@Name("bloqueioUsuarioHome")
-@BypassInterceptors
+@Name(BloqueioUsuarioHome.NAME)
 public class BloqueioUsuarioHome
 		extends
 			AbstractBloqueioUsuarioHome<BloqueioUsuario> {
 
+	public static final String NAME = "bloqueioUsuarioHome";
+	
 	private static final long serialVersionUID = 1L;
 
 }
