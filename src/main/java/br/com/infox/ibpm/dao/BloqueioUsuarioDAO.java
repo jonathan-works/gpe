91f5967a665991642d903d5bee8f121dd71ec556
Jonas Santos Bezerra <jonasbezerra@infox.com.br>
2013-07-09 07:57:28 -0300

    Criação do método getBloqueioUsuarioMaisRecente

--- src/main/java/br/com/infox/ibpm/dao/BloqueioUsuarioDAO.java
+++ src/main/java/br/com/infox/ibpm/dao/BloqueioUsuarioDAO.java
@@ -1,9 +1,14 @@
 package br.com.infox.ibpm.dao;
 
+import javax.persistence.Query;
+
 import org.jboss.seam.annotations.AutoCreate;
 import org.jboss.seam.annotations.Name;
 
+import br.com.infox.access.entity.UsuarioLogin;
 import br.com.infox.core.dao.GenericDAO;
+import br.com.infox.ibpm.entity.BloqueioUsuario;
+import br.com.itx.util.EntityUtil;
 
 @Name(BloqueioUsuarioDAO.NAME)
 @AutoCreate
@@ -11,5 +16,12 @@ public class BloqueioUsuarioDAO extends GenericDAO {
 
 	private static final long serialVersionUID = 1L;
 	public static final String NAME = "bloqueioUsuarioDAO";
+	
+	public BloqueioUsuario getBloqueioUsuarioMaisRecente(UsuarioLogin usuarioLogin){
+		String hql = "select o from BloqueioUsuario o where o.dataBloqueio = " +
+						"(select max(b.dataBloqueio) from BloqueioUsuario b where b.usuario = :usuario)";
+		Query query = EntityUtil.createQuery(hql).setParameter("usuario", usuarioLogin);
+		return EntityUtil.getSingleResult(query);
+	}
 
 }
