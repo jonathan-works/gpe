eae7a9a100838cf10aa11f99c36106671c7be05d
Jonas Santos Bezerra <jonasbezerra@infox.com.br>
2013-07-09 10:11:12 -0300

    Injeção de UsuarioLoginManager e BloqueioUsuarioManager

--- src/main/java/br/com/infox/ibpm/service/AuthenticatorService.java
+++ src/main/java/br/com/infox/ibpm/service/AuthenticatorService.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.jboss.seam.Component;
 import org.jboss.seam.annotations.AutoCreate;
+import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.security.Credentials;
 import org.jboss.seam.security.Identity;
@@ -12,6 +13,8 @@ import org.jboss.seam.security.management.IdentityManager;
 import org.jboss.ws.extensions.security.SimplePrincipal;
 
 import br.com.infox.core.manager.GenericManager;
+import br.com.infox.ibpm.manager.BloqueioUsuarioManager;
+import br.com.infox.ibpm.manager.UsuarioLoginManager;
 
 @Name(AuthenticatorService.NAME)
 @AutoCreate
@@ -19,6 +22,9 @@ public class AuthenticatorService extends GenericManager {
 
 	private static final long serialVersionUID = 1L;
 	public static final String NAME = "authenticatorService";
+	
+	@In private UsuarioLoginManager usuarioLoginManager;
+	@In private BloqueioUsuarioManager bloqueioUsuarioManager;
 	
 	public void autenticaManualmenteNoSeamSecurity(String login, IdentityManager identityManager) {
 		Principal principal = new SimplePrincipal(login);
