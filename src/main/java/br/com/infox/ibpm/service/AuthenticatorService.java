ff1cb7e925f8a9ac9d849508c7cb98e7f4cc5e67
Jonas Santos Bezerra <jonasbezerra@infox.com.br>
2013-07-10 10:29:49 -0300

    Transferência de método setUsuarioLogadoSessao de Authenticator para AuthenticatorService

--- src/main/java/br/com/infox/ibpm/service/AuthenticatorService.java
+++ src/main/java/br/com/infox/ibpm/service/AuthenticatorService.java
@@ -1,6 +1,8 @@
 package br.com.infox.ibpm.service;
 
 import java.security.Principal;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import javax.security.auth.login.LoginException;
@@ -9,6 +11,8 @@ import org.jboss.seam.Component;
 import org.jboss.seam.annotations.AutoCreate;
 import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
+import org.jboss.seam.contexts.Contexts;
+import org.jboss.seam.core.Events;
 import org.jboss.seam.security.Credentials;
 import org.jboss.seam.security.Identity;
 import org.jboss.seam.security.management.IdentityManager;
@@ -16,6 +20,8 @@ import org.jboss.ws.extensions.security.SimplePrincipal;
 
 import br.com.infox.access.entity.UsuarioLogin;
 import br.com.infox.core.manager.GenericManager;
+import br.com.infox.ibpm.entity.UsuarioLocalizacao;
+import br.com.infox.ibpm.home.UsuarioLocalizacaoComparator;
 import br.com.infox.ibpm.manager.BloqueioUsuarioManager;
 import br.com.infox.ibpm.manager.LocalizacaoManager;
 import br.com.infox.ibpm.manager.UsuarioLoginManager;
@@ -30,6 +36,11 @@ public class AuthenticatorService extends GenericManager {
 	@In private UsuarioLoginManager usuarioLoginManager;
 	@In private BloqueioUsuarioManager bloqueioUsuarioManager;
 	@In private LocalizacaoManager localizacaoManager;
+	
+	private static final UsuarioLocalizacaoComparator USUARIO_LOCALIZACAO_COMPARATOR = new UsuarioLocalizacaoComparator();
+	public static final String USUARIO_LOGADO = "usuarioLogado";
+	public static final String USUARIO_LOCALIZACAO_LIST = "usuarioLocalizacaoList";
+	public static final String SET_USUARIO_LOCALIZACAO_LIST_EVENT = "authenticator.setUsuarioLocalizacaoListEvent";
 	
 	public void autenticaManualmenteNoSeamSecurity(String login, IdentityManager identityManager) {
 		Principal principal = new SimplePrincipal(login);
@@ -46,6 +57,19 @@ public class AuthenticatorService extends GenericManager {
 				identity.addRole(role);
 			}
 		}
+	}
+	
+	/**
+	 * Metodo que coloca o usuario logado na sessão
+	 * @param usuario
+	 */
+	public void setUsuarioLogadoSessao(UsuarioLogin usuario) {
+		Contexts.getSessionContext().set(USUARIO_LOGADO, usuario);
+		List<UsuarioLocalizacao> usuarioLocalizacaoList = new ArrayList<UsuarioLocalizacao>(
+				usuario.getUsuarioLocalizacaoList());
+		Collections.sort(usuarioLocalizacaoList, USUARIO_LOCALIZACAO_COMPARATOR);
+		Contexts.getSessionContext().set(USUARIO_LOCALIZACAO_LIST, usuarioLocalizacaoList);
+		Events.instance().raiseEvent(SET_USUARIO_LOCALIZACAO_LIST_EVENT, usuarioLocalizacaoList);
 	}
 	
 	public void validarUsuario(UsuarioLogin usuario) throws LoginException {
