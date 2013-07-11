fb02926b0531de1b2ab30b0627e7f0c184743eb9
Jonas Santos Bezerra <jonasbezerra@infox.com.br>
2013-07-11 11:17:51 -0300

    Criação de método atualizarTarefasModificadas

--- src/main/java/br/com/infox/jbpm/dao/JbpmTaskDAO.java
+++ src/main/java/br/com/infox/jbpm/dao/JbpmTaskDAO.java
@@ -1,9 +1,15 @@
 package br.com.infox.jbpm.dao;
 
+import java.math.BigInteger;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.hibernate.Query;
 import org.jboss.seam.annotations.AutoCreate;
 import org.jboss.seam.annotations.Name;
 
 import br.com.infox.core.dao.GenericDAO;
+import br.com.infox.ibpm.jbpm.JbpmUtil;
 
 @Name(JbpmTaskDAO.NAME)
 @AutoCreate
@@ -11,5 +17,18 @@ public class JbpmTaskDAO extends GenericDAO {
 
 	private static final long serialVersionUID = 1L;
 	public static final String NAME = "jbpmTaskDAO";
+	
+	public void atualizarTarefasModificadas(Map<BigInteger, String> modifiedTasks){
+		if (modifiedTasks.size() > 0) {
+			String update = "update jbpm_task set name_ = :taskName where id_ = :taskId";
+			Query q = JbpmUtil.getJbpmSession().createSQLQuery(update);
+			for (Entry<BigInteger, String> e : modifiedTasks.entrySet()) {
+				q.setParameter("taskName", e.getValue());
+				q.setParameter("taskId", e.getKey());
+				q.executeUpdate();
+			}
+		}
+		JbpmUtil.getJbpmSession().flush();
+	}
 
 }
