c230a1c997495a5f8594db894a6d62f819c98b9b
Jonas Santos Bezerra <jonasbezerra@infox.com.br>
2013-07-12 07:45:47 -0300

    Criação de JbpmNodeManager

---
+++ src/main/java/br/com/infox/jbpm/manager/JbpmNodeManager.java
@@ -0,0 +1,25 @@
+package br.com.infox.jbpm.manager;
+
+import java.math.BigInteger;
+
+import org.jboss.seam.annotations.AutoCreate;
+import org.jboss.seam.annotations.In;
+import org.jboss.seam.annotations.Name;
+
+import br.com.infox.core.manager.GenericManager;
+import br.com.infox.jbpm.dao.JbpmNodeDAO;
+
+@Name(JbpmNodeManager.NAME)
+@AutoCreate
+public class JbpmNodeManager extends GenericManager {
+
+	private static final long serialVersionUID = 1L;
+	public static final String NAME = "jbpmNodeManager";
+	
+	@In private JbpmNodeDAO jbpmNodeDAO;
+	
+	public BigInteger findNodeIdByIdProcessDefinitionAndName(BigInteger idProcessDefinition, String taskName){
+		return jbpmNodeDAO.findNodeIdByIdProcessDefinitionAndName(idProcessDefinition, taskName);
+	}
+
+}
