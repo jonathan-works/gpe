d9e989f42d5468d1fb9a08a9af515ae451e37448
Jonas Santos Bezerra <jonasbezerra@infox.com.br>
2013-07-12 07:45:13 -0300

    Criação do método findNodeIdByIdProcessDefinitionAndName

--- src/main/java/br/com/infox/jbpm/dao/JbpmNodeDAO.java
+++ src/main/java/br/com/infox/jbpm/dao/JbpmNodeDAO.java
@@ -1,9 +1,11 @@
 package br.com.infox.jbpm.dao;
 
+import java.math.BigInteger;
 import org.jboss.seam.annotations.AutoCreate;
 import org.jboss.seam.annotations.Name;
 
 import br.com.infox.core.dao.GenericDAO;
+import br.com.infox.ibpm.jbpm.JbpmUtil;
 
 @Name(JbpmNodeDAO.NAME)
 @AutoCreate
@@ -11,5 +13,12 @@ public class JbpmNodeDAO extends GenericDAO {
 
 	private static final long serialVersionUID = 1L;
 	public static final String NAME = "jbpmNodeDAO";
+	
+	public BigInteger findNodeIdByIdProcessDefinitionAndName(BigInteger idProcessDefinition, String taskName){
+		String hql = "select max(id_) from jbpm_node where processdefinition_ = :idProcessDefinition and name_ = :nodeName";
+		return (BigInteger) JbpmUtil.getJbpmSession().createSQLQuery(hql)
+									.setParameter("idProcessDefinition",idProcessDefinition)
+									.setParameter("nodeName", taskName).uniqueResult();
+	}
 
 }
