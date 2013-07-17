cfd91b45cf02b84cdbe1251d6c6291bbdc6de141
Jonas Santos Bezerra <jonasbezerra@infox.com.br>
2013-07-17 12:02:20 -0300

    Remoção de import não utilizado

--- src/main/java/br/com/infox/ibpm/manager/ParametroManager.java
+++ src/main/java/br/com/infox/ibpm/manager/ParametroManager.java
@@ -1,7 +1,5 @@
 package br.com.infox.ibpm.manager;
 
-import java.util.List;
-
 import org.jboss.seam.annotations.AutoCreate;
 import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
