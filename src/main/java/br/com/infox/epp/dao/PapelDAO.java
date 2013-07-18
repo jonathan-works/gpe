fb9f56481be7bff81d1037091856263f54f60db4
Jonas Santos Bezerra <jonasbezerra@infox.com.br>
2013-07-18 12:30:04 -0300

    Criação do método getPapeisNaoAssociadosATipoProcessoDocumento

--- src/main/java/br/com/infox/epp/dao/PapelDAO.java
+++ src/main/java/br/com/infox/epp/dao/PapelDAO.java
@@ -10,6 +10,7 @@ import org.jboss.seam.annotations.Name;
 import br.com.infox.access.entity.Papel;
 import br.com.infox.core.dao.GenericDAO;
 import br.com.infox.ibpm.entity.TipoModeloDocumento;
+import br.com.infox.ibpm.entity.TipoProcessoDocumento;
 import br.com.itx.util.EntityUtil;
 
 @Name(PapelDAO.NAME)
@@ -26,6 +27,15 @@ public class PapelDAO extends GenericDAO {
 				+ "where p.tipoModeloDocumento = :tipoModeloDocumento)";
 		return (List<Papel>) entityManager.createQuery(hql).setParameter("tipoModeloDocumento",
 				tipoModeloDocumento).getResultList();
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<Papel> getPapeisNaoAssociadosATipoProcessoDocumento(TipoProcessoDocumento tipoProcessoDocumento){
+		String hql = "select o from Papel o where identificador not like '/%' and o not in " +
+						"(select p.papel from TipoProcessoDocumentoPapel p " +
+						"where p.tipoProcessoDocumento = :tipoProcessoDocumento)";
+		return (List<Papel>) entityManager.createQuery(hql).setParameter("tipoProcessoDocumento", 
+				tipoProcessoDocumento).getResultList();
 	}
 	
 	public Papel getPapelByIndentificador(String identificador){
