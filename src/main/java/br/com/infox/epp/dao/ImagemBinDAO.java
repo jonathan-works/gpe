987e439f79356ebb837612cf453841e50c21d283
Jonas Santos Bezerra <jonasbezerra@infox.com.br>
2013-07-18 10:06:07 -0300

    Criação do método getTodasAsImagens

--- src/main/java/br/com/infox/epp/dao/ImagemBinDAO.java
+++ src/main/java/br/com/infox/epp/dao/ImagemBinDAO.java
@@ -1,9 +1,12 @@
 package br.com.infox.epp.dao;
 
+import java.util.List;
+
 import org.jboss.seam.annotations.AutoCreate;
 import org.jboss.seam.annotations.Name;
 
 import br.com.infox.core.dao.GenericDAO;
+import br.com.infox.epp.entity.ImagemBin;
 
 @Name(ImagemBinDAO.NAME)
 @AutoCreate
@@ -11,5 +14,11 @@ public class ImagemBinDAO extends GenericDAO {
 
 	private static final long serialVersionUID = 1L;
 	public static final String NAME = "imagemBinDAO";
+	
+	@SuppressWarnings("unchecked")
+	public List<ImagemBin> getTodasAsImagens(){
+		String hql = "select o from ImagemBin o";
+		return (List<ImagemBin>) entityManager.createQuery(hql).getResultList();
+	}
 
 }
