ace52b1d61da056a532715f828b70579c264aa75
Jonas Santos Bezerra <jonasbezerra@infox.com.br>
2013-07-18 10:08:47 -0300

    Criação do método getTodasAsImagens()

--- src/main/java/br/com/infox/epp/manager/ImagemBinManager.java
+++ src/main/java/br/com/infox/epp/manager/ImagemBinManager.java
@@ -1,11 +1,14 @@
 package br.com.infox.epp.manager;
 
+import java.util.List;
+
 import org.jboss.seam.annotations.AutoCreate;
 import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
 
 import br.com.infox.core.manager.GenericManager;
 import br.com.infox.epp.dao.ImagemBinDAO;
+import br.com.infox.epp.entity.ImagemBin;
 
 @Name(ImagemBinManager.NAME)
 @AutoCreate
@@ -16,4 +19,8 @@ public class ImagemBinManager extends GenericManager {
 	
 	@In private ImagemBinDAO imagemBinDAO;
 
+	public List<ImagemBin> getTodasAsImagens(){
+		return imagemBinDAO.getTodasAsImagens();
+	}
+	
 }
