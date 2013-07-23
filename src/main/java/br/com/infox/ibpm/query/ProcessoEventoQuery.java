c299076ee155c6df4223066f978199f2848d370f
Jonas Santos Bezerra <jonasbezerra@infox.com.br>
2013-07-23 09:20:28 -0300

    Ajustes de internacionalização e remoção de resquícios de TarefaEvento

--- src/main/java/br/com/infox/ibpm/query/ProcessoEventoQuery.java
+++
@@ -1,9 +0,0 @@
-package br.com.infox.ibpm.query;
-
-public interface ProcessoEventoQuery {
-
-	String LIST_EVENTO_NAO_PROCESSADO = "listEventoNaoProcessado";
-	String LIST_EVENTO_NAO_PROCESSADO_QUERY = "select o from ProcessoEvento o where o.processado = false and " +
-											  "o.verificadoProcessado = false";
-	
-}
