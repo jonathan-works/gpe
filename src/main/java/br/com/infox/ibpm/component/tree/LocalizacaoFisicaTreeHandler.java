d4f53d13a415ec215fbe74ad464a45fd630dc5a8
Jonas Santos Bezerra <jonasbezerra@infox.com.br>
2013-07-25 11:52:49 -0300

    Removida anotação de bypassinterceptors e imports não utilizados

--- src/main/java/br/com/infox/ibpm/component/tree/LocalizacaoFisicaTreeHandler.java
+++ src/main/java/br/com/infox/ibpm/component/tree/LocalizacaoFisicaTreeHandler.java
@@ -1,16 +1,12 @@
 package br.com.infox.ibpm.component.tree;
 
 import org.jboss.seam.annotations.Name;
-import org.jboss.seam.annotations.intercept.BypassInterceptors;
-
-import br.com.infox.annotations.Recursive;
 import br.com.infox.component.tree.AbstractTreeHandler;
 import br.com.infox.component.tree.EntityNode;
 import br.com.infox.ibpm.entity.LocalizacaoFisica;
 import br.com.itx.util.ComponentUtil;
 
 @Name(LocalizacaoFisicaTreeHandler.NAME)
-@BypassInterceptors
 public class LocalizacaoFisicaTreeHandler extends AbstractTreeHandler<LocalizacaoFisica> {
 	
 	public static final String NAME = "localizacaoFisicaTree";
