895ca15d9c99e8d3239cb50139d2e66be07b3994
Jonas Santos Bezerra <jonasbezerra@infox.com.br>
2013-07-25 12:18:51 -0300

    Adicionada anotação de suppresswarnings

--- src/main/java/br/com/infox/ibpm/dao/LocalizacaoDAO.java
+++ src/main/java/br/com/infox/ibpm/dao/LocalizacaoDAO.java
@@ -15,6 +15,7 @@ public class LocalizacaoDAO extends GenericDAO {
 	private static final long serialVersionUID = 1L;
 	public static final String NAME = "localizacaoDAO";
 	
+	@SuppressWarnings("unchecked")
 	public List<Localizacao> getLocalizacoesEstrutura(){
 		String hql = "select o from Localizacao where o.estrutura = true order by o.localizacao";
 		return entityManager.createQuery(hql).getResultList();
