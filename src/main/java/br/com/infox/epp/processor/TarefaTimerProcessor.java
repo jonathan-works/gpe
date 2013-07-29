9a63a04c614bc536c150f36ddc574bb2b0648bab
erikliberal <erikliberal@infox.com.br>
2013-07-29 09:06:36 -0300

    Formatação e adição de Logger para tratamento de exceções

--- src/main/java/br/com/infox/epp/processor/TarefaTimerProcessor.java
+++ src/main/java/br/com/infox/epp/processor/TarefaTimerProcessor.java
@@ -8,6 +8,8 @@ import org.jboss.seam.annotations.Transactional;
 import org.jboss.seam.annotations.async.Asynchronous;
 import org.jboss.seam.annotations.async.IntervalCron;
 import org.jboss.seam.async.QuartzTriggerHandle;
+import org.jboss.seam.log.LogProvider;
+import org.jboss.seam.log.Logging;
 import org.quartz.SchedulerException;
 import org.quartz.Trigger;
 
@@ -17,48 +19,54 @@ import br.com.infox.ibpm.type.PrazoEnum;
 import br.com.infox.timer.TimerUtil;
 
 /**
- * Processor que irá incrementar os tempos decorridos
- * para cada tarefa aberta no sistema, verificando a 
- * partir da localização do processo seus respectivos 
- * turnos, calculando somente o horário útil gasto para 
- * cada tarefa em execução do sistema.
+ * Processor que irá incrementar os tempos decorridos para cada tarefa aberta no
+ * sistema, verificando a partir da localização do processo seus respectivos
+ * turnos, calculando somente o horário útil gasto para cada tarefa em execução
+ * do sistema.
+ * 
  * @author Daniel
- *
  */
 @Name(TarefaTimerProcessor.NAME)
 @AutoCreate
 public class TarefaTimerProcessor {
 
+	private static final LogProvider LOG = Logging
+			.getLogProvider(TarefaTimerProcessor.class);
 	public static final String NAME = "tarefaTimerProcessor";
-	
+
 	@In
 	private ProcessoEpaTarefaManager processoEpaTarefaManager;
-	
+
 	public static TarefaTimerProcessor instance() {
 		return (TarefaTimerProcessor) Component.getInstance(NAME);
-	}	
-	
+	}
+
 	/**
-	 * Incrementa o tempo de cada tarefa, verificando se está dentro do turno da sua localização.
-	 * @param cron - que está em execução
+	 * Incrementa o tempo de cada tarefa, verificando se está dentro do turno da
+	 * sua localização.
+	 * 
+	 * @param cron
+	 *            - que está em execução
 	 * @return null
 	 */
 	@Asynchronous
 	@Transactional
 	public QuartzTriggerHandle increaseTaskTimeSpent(@IntervalCron String cron) {
 		String idTaskTimer = null;
-		idTaskTimer = TimerUtil.getParametro(TarefaTimerStarter.ID_INICIAR_TASK_TIMER_PARAMETER);
+		idTaskTimer = TimerUtil
+				.getParametro(TarefaTimerStarter.ID_INICIAR_TASK_TIMER_PARAMETER);
 		QuartzTriggerHandle handle = new QuartzTriggerHandle(idTaskTimer);
 		Trigger trigger = null;
 		try {
 			trigger = handle.getTrigger();
 		} catch (SchedulerException e) {
-			e.printStackTrace();
+			LOG.error("TarefaTimerProcessor.increaseTaskTimeSpent()", e);
 		}
 		if (trigger != null) {
-			processoEpaTarefaManager.updateTarefasNaoFinalizadas(trigger.getPreviousFireTime(), PrazoEnum.H);
+			processoEpaTarefaManager.updateTarefasNaoFinalizadas(
+					trigger.getPreviousFireTime(), PrazoEnum.H);
 		}
 		return null;
 	}
-	
+
 }
