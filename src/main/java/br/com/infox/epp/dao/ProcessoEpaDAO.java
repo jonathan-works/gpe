38d95fb8280cca3d2d04559740e0a1d28e865435
erikliberal <erikliberal@infox.com.br>
2013-07-29 09:07:43 -0300

    Adição de função que executa a busca adicionada para cálculo de tempo gasto

--- src/main/java/br/com/infox/epp/dao/ProcessoEpaDAO.java
+++ src/main/java/br/com/infox/epp/dao/ProcessoEpaDAO.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.persistence.EntityManager;
 import javax.persistence.Query;
 
 import org.jboss.seam.ScopeType;
@@ -110,6 +111,12 @@ public class ProcessoEpaDAO extends GenericDAO {
 	public Item getItemDoProcesso(int idProcesso){
 		String query = "select o.itemDoProcesso from ProcessoEpa o where o.idProcesso =:idProcesso";
 		return (Item) entityManager.createQuery(query).setParameter("idProcesso", idProcesso).getSingleResult();
+	}
+	
+	public Map<String, Object> getTempoGasto(ProcessoEpa processoEpa) {
+		Query q = entityManager.createQuery(ProcessoEpaQuery.TEMPO_GASTO_PROCESSO_EPP_QUERY)
+				.setParameter("idProcesso", processoEpa.getIdProcesso());
+		return (Map<String, Object>)q.getSingleResult();
 	}
 	
 }
