4bfcf91c216a566dcba2f4c44dff739aea817f34
erikliberal <erikliberal@infox.com.br>
2013-07-29 09:04:36 -0300

    Formatação e substituição de strings muito usadas por constatnes

--- src/main/java/br/com/infox/component/quartz/QuartzJobsInfo.java
+++ src/main/java/br/com/infox/component/quartz/QuartzJobsInfo.java
@@ -36,53 +36,58 @@ import br.com.infox.ibpm.entity.Parametro;
 import br.com.itx.util.ComponentUtil;
 import br.com.itx.util.EntityUtil;
 
-@Name("quartzJobsInfo")
+@Name(QuartzJobsInfo.NAME)
 @Scope(ScopeType.APPLICATION)
 @BypassInterceptors
-@Startup(depends="org.jboss.seam.async.dispatcher")
-@Install(dependencies={"org.jboss.seam.async.dispatcher"})
+@Startup(depends = QuartzConstant.JBOSS_SEAM_ASYNC_DISPATCHER)
+@Install(dependencies = { QuartzConstant.JBOSS_SEAM_ASYNC_DISPATCHER })
 public class QuartzJobsInfo implements Serializable {
-	
+
 	private static final long serialVersionUID = 1L;
-	
-	private static Pattern patternExpr = Pattern.compile("^AsynchronousInvocation\\((.*)\\)$");
-	
+	public static final String NAME = "quartzJobsInfo";
+
+	private static Pattern patternExpr = Pattern
+			.compile("^AsynchronousInvocation\\((.*)\\)$");
+
 	public static Scheduler getScheduler() {
 		return QuartzDispatcher.instance().getScheduler();
 	}
-	
+
 	public List<Map<String, Object>> getDetailJobsInfo() {
-		List<Map<String, Object>> maps = new ArrayList<Map<String,Object>>();
+		List<Map<String, Object>> maps = new ArrayList<Map<String, Object>>();
 		try {
 			Scheduler scheduler = getScheduler();
 			String[] jobGroupNames = scheduler.getJobGroupNames();
 			for (String groupName : jobGroupNames) {
-				List<Map<String,Object>> mapInfoGroup = getListMapInfoGroupFromJobs(groupName);
+				List<Map<String, Object>> mapInfoGroup = getListMapInfoGroupFromJobs(groupName);
 				maps.addAll(mapInfoGroup);
 			}
 		} catch (SchedulerException e) {
-			FacesMessages.instance().add(Severity.ERROR, "Erro ao obter os detalhes dos jobs do quartz.", e);
+			FacesMessages.instance().add(Severity.ERROR,
+					"Erro ao obter os detalhes dos jobs do quartz.", e);
 		}
 		return maps;
 	}
 
-	private List<Map<String, Object>> getListMapInfoGroupFromJobs(String groupName)
-			throws SchedulerException {
+	private List<Map<String, Object>> getListMapInfoGroupFromJobs(
+			String groupName) throws SchedulerException {
 		Scheduler scheduler = getScheduler();
 		String[] jobNames = scheduler.getJobNames(groupName);
-		List<Map<String, Object>> maps = new ArrayList<Map<String,Object>>();
+		List<Map<String, Object>> maps = new ArrayList<Map<String, Object>>();
 		for (String jobName : jobNames) {
 			JobDetail jobDetail = scheduler.getJobDetail(jobName, groupName);
-			Trigger[] triggersOfJob = scheduler.getTriggersOfJob(jobName, groupName);
+			Trigger[] triggersOfJob = scheduler.getTriggersOfJob(jobName,
+					groupName);
 			for (Trigger trigger : triggersOfJob) {
 				maps.add(getTrigerDetailMap(jobDetail, trigger));
 			}
 		}
 		return maps;
-	}	
-	
-	private Map<String,Object> getTrigerDetailMap(JobDetail jobDetail, Trigger trigger) {
-		Map<String,Object> map = new HashMap<String, Object>();
+	}
+
+	private Map<String, Object> getTrigerDetailMap(JobDetail jobDetail,
+			Trigger trigger) {
+		Map<String, Object> map = new HashMap<String, Object>();
 		String jobName = trigger.getJobName();
 		JobDataMap jobDataMap = jobDetail.getJobDataMap();
 		map.put("triggerName", trigger.getName());
@@ -96,10 +101,10 @@ public class QuartzJobsInfo implements Serializable {
 		if (trigger instanceof CronTrigger) {
 			CronTrigger cronTrigger = (CronTrigger) trigger;
 			map.put("cronExpression", cronTrigger.getCronExpression());
-		}		
+		}
 		return map;
 	}
-	
+
 	public static String getJobExpression(JobDataMap dataMap) {
 		Collection<?> values = dataMap.values();
 		if (values != null && !values.isEmpty()) {
@@ -107,15 +112,15 @@ public class QuartzJobsInfo implements Serializable {
 			Matcher matcher = patternExpr.matcher(dataJobDetail);
 			if (matcher.find()) {
 				return matcher.group(1);
-			} 
+			}
 			return dataJobDetail;
 		}
 		return null;
 	}
-	
-	
+
 	/**
 	 * Test se o a expressão do job é valida.
+	 * 
 	 * @param jobExpression
 	 * @return
 	 */
@@ -132,39 +137,44 @@ public class QuartzJobsInfo implements Serializable {
 		String medothName = split[1].replaceAll("[()]", "");
 		return isMethodValid(component, medothName);
 	}
-	
+
 	private boolean isMethodValid(Object component, String medothName) {
 		try {
-			component.getClass().getDeclaredMethod(medothName, Date.class, String.class);
+			component.getClass().getDeclaredMethod(medothName, Date.class,
+					String.class);
 			return true;
-		} catch (Exception e) { /* not found */ }
+		} catch (Exception e) { /* not found */}
 		try {
 			component.getClass().getDeclaredMethod(medothName, String.class);
 			return true;
-		} catch (Exception e) { /* not found */ }	
+		} catch (Exception e) { /* not found */}
 		return false;
 	}
 
 	public void triggerJob(String jobName, String groupName) {
 		try {
 			getScheduler().triggerJob(jobName, groupName);
-			FacesMessages.instance().add(Severity.INFO, "Job executado com sucesso: " + jobName);
+			FacesMessages.instance().add(Severity.INFO,
+					"Job executado com sucesso: " + jobName);
 		} catch (SchedulerException e) {
-			FacesMessages.instance().add(Severity.ERROR, "Erro ao executar job " + jobName, e);
+			FacesMessages.instance().add(Severity.ERROR,
+					"Erro ao executar job " + jobName, e);
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void deleteJob(String jobName, String groupName) {
 		try {
 			getScheduler().deleteJob(jobName, groupName);
-			FacesMessages.instance().add(Severity.INFO, "Job removido com sucesso: " + jobName);
+			FacesMessages.instance().add(Severity.INFO,
+					"Job removido com sucesso: " + jobName);
 		} catch (SchedulerException e) {
-			FacesMessages.instance().add(Severity.ERROR, "Erro ao remover job " + jobName, e);
+			FacesMessages.instance().add(Severity.ERROR,
+					"Erro ao remover job " + jobName, e);
 			e.printStackTrace();
 		}
-	}	
-	
+	}
+
 	@Create
 	public void addGlobalTriggerListener() throws SchedulerException {
 		Scheduler scheduler = QuartzJobsInfo.getScheduler();
@@ -172,48 +182,55 @@ public class QuartzJobsInfo implements Serializable {
 			scheduler.addGlobalTriggerListener(new TriggerListenerLog());
 		}
 	}
-	
+
 	public void apagarJobs() {
-		String sql = "delete from qrtz_cron_triggers; " +
-			"delete from qrtz_fired_triggers; " +
-			"delete from qrtz_triggers; " +
-			"delete from qrtz_job_details; " +
-			"delete from core.tb_parametro " + 
-			"where vl_variavel like '________:___________:_____';";
+		String sql = "delete from qrtz_cron_triggers; "
+				+ "delete from qrtz_fired_triggers; "
+				+ "delete from qrtz_triggers; "
+				+ "delete from qrtz_job_details; "
+				+ "delete from core.tb_parametro "
+				+ "where vl_variavel like '________:___________:_____';";
 		Query query = EntityUtil.getEntityManager().createNativeQuery(sql);
 		query.executeUpdate();
-		FacesMessages.instance().add(Severity.INFO, "Jobs apagados com sucesso. Reinicie o servidor para que os Jobs sejam refeitos.");
+		FacesMessages
+				.instance()
+				.add(Severity.INFO,
+						"Jobs apagados com sucesso. Reinicie o servidor para que os Jobs sejam refeitos.");
 	}
-	
+
 	public void apagarHistoricoEstatisticaEventoProcesso() {
 		String hql = "delete from HistoricoEstatisticaEventoProcesso o where cast(o.dtUltimaAtualizacao as date) = current_date";
-		FacesMessages.instance().add(Severity.INFO, "HistoricoEstatisticaEventoProcesso para o dia atual foi apagado com sucesso.");
+		FacesMessages
+				.instance()
+				.add(Severity.INFO,
+						"HistoricoEstatisticaEventoProcesso para o dia atual foi apagado com sucesso.");
 		EntityUtil.createQuery(hql).executeUpdate();
 	}
 
-	public List<Map<String, Object>> getMapParametroTriggers() throws SchedulerException {
+	public List<Map<String, Object>> getMapParametroTriggers()
+			throws SchedulerException {
 		List<String> triggersNames = getTriggersNames();
 		if (triggersNames.isEmpty()) {
 			return Collections.emptyList();
 		}
-		String hql = "select new map(o.nomeVariavel as nomeVariavel, " +
-				"o.descricaoVariavel as descricaoVariavel, " +
-				"o.valorVariavel as valorVariavel, " +
-				"o.idParametro as idParametro," +
-				"case when o.valorVariavel in (:triggersNames) then true else false end as valido) " +
-				"from Parametro o where o.valorVariavel like '________:___________:_____'";
+		String hql = "select new map(o.nomeVariavel as nomeVariavel, "
+				+ "o.descricaoVariavel as descricaoVariavel, "
+				+ "o.valorVariavel as valorVariavel, "
+				+ "o.idParametro as idParametro,"
+				+ "case when o.valorVariavel in (:triggersNames) then true else false end as valido) "
+				+ "from Parametro o where o.valorVariavel like '________:___________:_____'";
 		Query query = EntityUtil.createQuery(hql);
 		query.setParameter("triggersNames", triggersNames);
 		return query.getResultList();
 	}
-	
+
 	public void removeParametro(int idParametro) {
 		EntityManager em = EntityUtil.getEntityManager();
 		Parametro parametro = em.find(Parametro.class, idParametro);
 		em.remove(parametro);
 		em.flush();
 	}
-	
+
 	private List<String> getTriggersNames() throws SchedulerException {
 		Scheduler scheduler = getScheduler();
 		String[] groupNames = scheduler.getTriggerGroupNames();
@@ -224,5 +241,5 @@ public class QuartzJobsInfo implements Serializable {
 		}
 		return list;
 	}
-	
+
 }
