3f93cc11d3ba6db102b92b1ff78dfde5f0ce48cf
Jonas Santos Bezerra <jonasbezerra@infox.com.br>
2013-07-31 11:08:18 -0300

    Adiciona constante DESCRICAO_MD5

--- src/main/java/br/com/infox/util/constants/LengthConstants.java
+++ src/main/java/br/com/infox/util/constants/LengthConstants.java
@@ -4,6 +4,7 @@ public interface LengthConstants {
     
     int DESCRICAO_MINIMA = 15;
     int DESCRICAO_PEQUENA = 30;
+    int DESCRICAO_MD5 = 32;
     int DESCRICAO_PADRAO = 100;
     int DESCRICAO_GRANDE = 255;
 
