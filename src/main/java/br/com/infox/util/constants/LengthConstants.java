bec6eba3f82fc749e704bdf4f3f975201b2035dd
Jonas Santos Bezerra <jonasbezerra@infox.com.br>
2013-07-31 11:28:39 -0300

    Adiciona constante DESCRICAO_PACOTE

--- src/main/java/br/com/infox/util/constants/LengthConstants.java
+++ src/main/java/br/com/infox/util/constants/LengthConstants.java
@@ -2,12 +2,17 @@ package br.com.infox.util.constants;
 
 public interface LengthConstants {
     
+    /*
+     * Favor, utilizar nomes intuitivos para as constantes mesmo que os valores se repitam
+     * */
+    
     //Tamanhos das Descrições
     int DESCRICAO_MINIMA = 15;
     int DESCRICAO_PEQUENA = 30;
     int DESCRICAO_MD5 = 32;
     int DESCRICAO_ENTIDADE = 50;
     int DESCRICAO_PADRAO = 100;
+    int DESCRICAO_PACOTE = 150;
     int DESCRICAO_GRANDE = 255;
     int DESCRICAO_NOME_ARQUIVO = 300;
     
