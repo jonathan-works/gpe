aeb7ae165d6b81dab20896e40e1e74063c267b27
Jonas Santos Bezerra <jonasbezerra@infox.com.br>
2013-07-31 11:20:13 -0300

    Adicionada constante ID_PAGINA

--- src/main/java/br/com/infox/util/constants/LengthConstants.java
+++ src/main/java/br/com/infox/util/constants/LengthConstants.java
@@ -2,11 +2,15 @@ package br.com.infox.util.constants;
 
 public interface LengthConstants {
     
+    //Tamanhos das Descrições
     int DESCRICAO_MINIMA = 15;
     int DESCRICAO_PEQUENA = 30;
     int DESCRICAO_MD5 = 32;
     int DESCRICAO_PADRAO = 100;
     int DESCRICAO_GRANDE = 255;
     int DESCRICAO_NOME_ARQUIVO = 300;
+    
+    //Tamanhos de ids
+    int ID_PAGINA = 200;
 
 }
