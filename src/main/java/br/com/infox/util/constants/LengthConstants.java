73fd7eff86d8778c778a19f5c8d331d7c7102101
Jonas Santos Bezerra <jonasbezerra@infox.com.br>
2013-07-31 11:04:04 -0300

    Adiciona constante DESCRICAO_MINIMA

--- src/main/java/br/com/infox/util/constants/LengthConstants.java
+++ src/main/java/br/com/infox/util/constants/LengthConstants.java
@@ -2,6 +2,7 @@ package br.com.infox.util.constants;
 
 public interface LengthConstants {
     
+    int DESCRICAO_MINIMA = 15;
     int DESCRICAO_PEQUENA = 30;
     int DESCRICAO_PADRAO = 100;
     int DESCRICAO_GRANDE = 255;
