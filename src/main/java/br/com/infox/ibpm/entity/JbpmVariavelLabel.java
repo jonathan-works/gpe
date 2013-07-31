ee76f8c2620adf64fd6e82fe6947d9a802c08e08
Jonas Santos Bezerra <jonasbezerra@infox.com.br>
2013-07-31 12:42:45 -0300

    Remoção de imports não utilizados de JbpmVariavelLabel

--- src/main/java/br/com/infox/ibpm/entity/JbpmVariavelLabel.java
+++ src/main/java/br/com/infox/ibpm/entity/JbpmVariavelLabel.java
@@ -22,7 +22,6 @@ import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
-import javax.validation.constraints.Size;
 import javax.validation.constraints.NotNull;
 
 import br.com.infox.util.constants.LengthConstants;
