41edd2ddc802b3a42be35f3b8d438324c1ab5fd7
erikliberal <erikliberal@infox.com.br>
2013-08-02 11:39:04 -0300

    Remoção das antigas funções de manipulação do tinyMCE e adição de evento click a ser usado para adicionar imagens ao editor ativo

--- src/main/webapp/resources/js/imageUpload.js
+++ src/main/webapp/resources/js/imageUpload.js
@@ -1,23 +1,7 @@
-var img;
-function uploadHandler(e, path) {
-	src = path + "/" + e.memo.entry.fileName;
-	img="<img src=\"" + src + "\" />";
-}
-
-function insert() {
-	setTimeout("tinymce.EditorManager.activeEditor.execCommand('mceInsertContent', false,'" +  img + "')",500);
-}
-
-function configureDragDrop() {
-	tinymce.dom.Event.add(tinymce.EditorManager.activeEditor.getDoc(), 'dragdrop', function(e) {
-		ed = tinymce.EditorManager.activeEditor;
-		texto = ed.getContent().replace(/img style="max-width: 50px;"/gi, 'img');
-		texto = texto.replace(/title="Arraste para o documento"/gi, '');
-		ed.setContent(texto)
-	});
-	tinyMCE.activeEditor.onClick.add(function(ed, e) { 
-		texto = ed.getContent().replace('max-width: 50px;', '');
-		ed.setContent(texto)
-	});
-}
-
+namespace("infox.imageUpload",{
+	click:function(evt, editorId) {
+		if (editorId) {
+			infox.editor[editorId].insertImage(evt.src);
+		}
+	}
+});
