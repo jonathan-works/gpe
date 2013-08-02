41d57fbe711803173447886e0092b8f0361cd1f7
Jonas Santos Bezerra <jonasbezerra@infox.com.br>
2013-08-02 08:10:26 -0300

    Resolucao de Issue : if/else/for/while/do statements should always use curly braces

--- src/main/java/br/com/infox/ibpm/home/LocalizacaoFisicaHome.java
+++ src/main/java/br/com/infox/ibpm/home/LocalizacaoFisicaHome.java
@@ -47,8 +47,9 @@ public class LocalizacaoFisicaHome extends AbstractHome<LocalizacaoFisica>{
 	
 	@Override
 	protected boolean beforePersistOrUpdate() {
-		if (instance.getLocalizacaoFisicaPai() != null && !instance.getLocalizacaoFisicaPai().getAtivo())
-			instance.setAtivo(false);
+		if (instance.getLocalizacaoFisicaPai() != null && !instance.getLocalizacaoFisicaPai().getAtivo()){
+		    instance.setAtivo(false);
+		}
 		return true;
 	}
 	
