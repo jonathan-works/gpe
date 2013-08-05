1836eb104dd53b71fc613a46c58c1bc8b57e6a92
Jonas Santos Bezerra <jonasbezerra@infox.com.br>
2013-08-05 09:16:04 -0300

    Remoção de código morto : método print utilizado para testes

--- src/main/java/br/com/itx/component/MeasureTime.java
+++ src/main/java/br/com/itx/component/MeasureTime.java
@@ -44,10 +44,6 @@ public class MeasureTime {
 		return watch.getTime();
 	}
 	
-	public void print(String msg) {
-		System.out.println(msg + ". Tempo decorrido: " + watch.getTime());
-	}
-	
 	public void reset() {
 		watch.reset();
 	}
