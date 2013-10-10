76d8c9da923237444164db847a2345a977b3faa0
erikliberal <erikliberal@infox.com.br>
2013-10-10 08:01:32 -0300

    Atualização da biblioteca javascript namespace

--- src/main/webapp/resources/js/namespace.js
+++ src/main/webapp/resources/js/namespace.js
@@ -1,108 +1,175 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 (function() {
-	if (window.namespace) {
-		return false;
-	} else {
-		window.namespace = $namespace;
-		window.loadScript = $loadDependency;
-		window.addEventListener("load", $afterLoadPage, false);
-		$namespace.whenReady = $execute;
-	}
+  if(window.namespace) {
+    throw"Object window.namespace is already declared";
+  }
+  var $library = {};
+  var $loaded = false;
+  var $execQueue = [];
 
-	var $_library = {};
-	var $_hasLoaded = false;
-
-	function $afterLoadPage($ev) {
-		$_hasLoaded = true;
-	}
-
-	function $execute(func) {
-		if (typeof func === "function") {
-			if ($_hasLoaded) {
-				func();
-			} else {
-				window.addEventListener("load", func, false);
-			}
-		}
-	}
-
-	function $createNamespace(namespace, object) {
-		var $path, $first, $_namespace, $item;
-		if (typeof namespace === "string") {
-			$path = namespace.split(".");
-			$_namespace = $_library
-			for ( var i = 0; i < $path.length; i++) {
-				$item = $path[i];
-				if (i == $path.length - 1) {
-					if ($_namespace[$item]) {
-						$_namespace = $_namespace[$item];
-						if (!Object.isFrozen($_namespace)) {
-							for ( var attr in object) {
-								if (!$_namespace[attr]) {
-									$_namespace[attr] = object[attr];
-								}
-							}
-						}
-					} else {
-						$_namespace = $_namespace[$item] = object;
-					}
-				} else {
-					$_namespace = $_namespace[$item] = $_namespace[$item] || {};
-				}
-				if (i == 0) {
-					$namespace[$item] = $_namespace;
-				}
-			}
-		} else {
-			$_namespace = object;
-		}
-		return $_namespace;
-	}
-
-	function $loadDependency(source) {
-		$execute(function() {
-			var script = document.createElement("script");
-			script.setAttribute("src", source);
-			script.setAttribute("type", "text/javascript");
-			document.head.appendChild(script);
-		});
-	}
-
-	function emptyFunc() {
-	}
-
-	function $namespace(namespace, object) {
-		var $obj;
-		var $params = [];
-		var isObjectFunc;
-		if (typeof namespace === "string") {
-			isObjectFunc = typeof object === "function";
-			if (isObjectFunc && object.name === "") {
-				$obj = $createNamespace(namespace, {});
-			} else {
-				$obj = $createNamespace(namespace, object);	
-			}
-			if ($obj !== object) {
-				if (isObjectFunc) {
-					$execute(function() {
-						object($obj);
-					});
-				} else if (typeof $obj === "function") {
-					$execute(function() {
-						$obj(object);
-					});
-				}
-			}
-		} else if (typeof namespace === "object"
-				&& namespace.constructor === [].constructor) {
-			for ( var i = 0; i < namespace.length; i++) {
-				$params.push($createNamespace(namespace[i], {}));
-			}
-			for ( var i = 0; i < object.length - namespace.length; i++) {
-				$params.push(emptyFunc);
-			}
-			$execute(function() {
-				object.apply(object, $params);
-			});
-		}
-	}
-})()
+  window.loadScript = window.loadScript || function loadScript(_path, _callback) {
+    if(typeof _path !== "string") {
+      throw"";
+    }
+    if(typeof _callback !== "function") {
+      throw"";
+    }
+    $invoke([], function loadScript() {
+      var script = document.createElement("script");
+      script.addEventListener("load", _callback);
+      script.src = _path;
+      script.type = "text/javascript";
+      document.head.appendChild(script);
+    }, []);
+  };
+  
+  function $getObjectFromPath(path) {
+	  var res = {
+	    path:"",
+	    object:{},
+	    get next() {
+	    	return this.object[this.path];
+	    },
+	    set next(obj) {
+	    	this.object[this.path] = obj;
+	    }
+	  };
+	  var $namespace = $library;
+	  for(var i=0,l=path.length;i<l;i++) {
+		  res.path = path[i];
+		  res.object = $namespace;
+		  if (i<l-1) {
+			  $namespace = res.next = res.next || {};
+		  }
+	  }
+	  return res;
+  }
+  
+  function $create(_path, _object, _options) {
+    var $nm = $getObjectFromPath(_path.split("."));
+    _options = _options || {};
+    _options.mergeType = _options.mergeType || "merge";
+    
+    if($nm.next !== _object) {
+      if (_options.mergeType === "override") {
+    	  $nm.next = _object;
+      } else if (_options.mergeType === "merge") {
+    	  $nm.next = $merge($nm.next, _object);  
+      } else {
+    	  $nm.next = $nm.next || _object;
+      }
+    }
+    $nm = $nm.next;
+    if(typeof $nm === "function") {
+      $nm.toString = function toString() {
+        return _path;
+      };
+    }
+    return $nm;
+  }
+  function $flushQueue() {
+    var exec = $execQueue.shift();
+    try {
+      exec.func.apply(exec.func, $merge(exec.args, $loadDependencies(exec.dependencies)));
+    }catch(e) {
+      console.error(e);
+    }
+    if($execQueue.length > 0) {
+      setTimeout($flushQueue, 1);
+    }else {
+      $loaded = true;
+    }
+  }
+  function $mergeWeight(object) {
+    var type = typeof object;
+    if(type === "function") {
+      return 4;
+    }else {
+      if(type === "object" && [].constructor === object.constructor) {
+        return 2;
+      }else {
+        return 1;
+      }
+    }
+  }
+  function $processMerge(obj1, obj2) {
+    for(var index in obj2) {
+      if(obj2.hasOwnProperty(index) && !obj1.hasOwnProperty(index)) {
+        obj1[index] = obj2[index];
+      }
+    }
+    return obj1;
+  }
+  function $merge(obj1, obj2) {
+    var w1 = $mergeWeight(obj1);
+    var w2 = $mergeWeight(obj2);
+    return w1 > w2 ? $processMerge(obj1, obj2) : $processMerge(obj2, obj1);
+  }
+  function $extend(_object, _path) {
+	var nm = $getObjectFromPath(_path.split("."));
+    if(!nm.next) {
+      throw"Parent object not found in path " + _path;
+    }
+    _object.uber = nm.next;
+    return _object;
+  }
+  function $loadDependencies(dependencies) {
+    var loaded = [];
+    for(var i = 0, arrlength = dependencies.length;i < arrlength;i++) {
+      if(typeof dependencies[i] !== "string") {
+        throw"Dependencies array contains an invalid type, must contain strings only. Contains " + typeof dependencies[i];
+      }
+      loaded.push($getObjectFromPath(dependencies[i].split(".")).next);
+    }
+    return loaded;
+  }
+  function $invoke(dependencies, callback, params) {
+    if(typeof dependencies !== "object" || dependencies.constructor !== params.constructor) {
+      throw"Dependencies argument is of the wrong type, must be an array. Is " + typeof dependencies;
+    }
+    if(typeof callback !== "function") {
+      throw"Callback argument is of the wrong type, must be a function. Is " + typeof callback;
+    }
+    if($loaded) {
+      callback.apply(callback, $merge(params, $loadDependencies(dependencies)));
+    }else {
+      $execQueue.push({func:callback, args:params, dependencies:dependencies});
+    }
+  }
+  window.invoke = window.invoke || function invoke(dependencies, callback) {
+    $invoke(dependencies, callback, []);
+  };
+  window.namespace = window.namespace || function namespace(path, obj, args) {
+    var params = [];
+    if(typeof path !== "string") {
+      throw"Path argument is of the wrong type, must be string. Is " + typeof path;
+    }
+    if(typeof obj !== "function" && typeof obj !== "object") {
+      throw"Object argument is of the wrong type, must be object or function. Is " + typeof obj;
+    }
+    if(args) {
+      if(args.callback) {
+        $invoke(args.required||[], function populate() {
+          for(var i = 0, l = arguments.length;i < l;i++) {
+            params.push(arguments[i]);
+          }
+        }, []);
+        params.push(obj);
+        $invoke([], $create, [path, obj, args]);
+        $invoke([], args.callback, params);
+        return 0;
+      }
+    }
+    $invoke([], $create, [path, obj, args]);
+    return 0;
+  };
+  loadScript.toString = invoke.toString = namespace.toString = function toString() {
+    return this.name;
+  };
+  Object.freeze(window.namespace);
+  Object.freeze(window.invoke);
+  Object.freeze(window.loadScript);
+  window.addEventListener("load", $flushQueue);
+})();
