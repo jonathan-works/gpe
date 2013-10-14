8b03dcc9fc936d987a6587d442643932ad2badb6
Gabriel Morrison Lima Dantas <gabriel@infox.com.br>
2013-10-14 12:08:43 -0300

    Implementação do EmailValidator

--- src/main/java/br/com/infox/validator/EmailValidator.java
+++ src/main/java/br/com/infox/validator/EmailValidator.java
@@ -15,24 +15,39 @@
 */
 package br.com.infox.validator;
 
+import java.util.regex.Matcher;
+
 import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
+import javax.faces.validator.FacesValidator;
 import javax.faces.validator.Validator;
 import javax.faces.validator.ValidatorException;
 
-import org.jboss.seam.annotations.Name;
-import org.jboss.seam.annotations.intercept.BypassInterceptors;
 
-
-@org.jboss.seam.annotations.faces.Validator(id="emailValidator")
-@Name("emailValidator")
-@BypassInterceptors
+@FacesValidator("emailValidator")
 public class EmailValidator implements Validator {
 
+	private static String ATOM = "[a-z0-9!#$%&'*+/=?^_`{|}~-]";
+	private static String DOMAIN = "(" + ATOM + "+(\\." + ATOM + "+)*";
+	private static String IP_DOMAIN = "\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\]";
+
+	private java.util.regex.Pattern pattern = java.util.regex.Pattern.compile(
+			"^" + ATOM + "+(\\." + ATOM + "+)*@"
+					+ DOMAIN
+					+ "|"
+					+ IP_DOMAIN
+					+ ")$",
+			java.util.regex.Pattern.CASE_INSENSITIVE
+	);
+	
 	@Override
 	public void validate(FacesContext context, UIComponent component, Object value) {
-	    //TODO implementar, o antigo dava erro
-		throw new ValidatorException(new FacesMessage(FacesMessage.SEVERITY_FATAL, "Validador de email não implementado", null));
+		if (value != null && !value.toString().isEmpty()) {
+			Matcher matcher = pattern.matcher(value.toString());
+			if (!matcher.matches()) {
+				throw new ValidatorException(new FacesMessage("Email inválido"));
+			}
+		}
 	}
 }
