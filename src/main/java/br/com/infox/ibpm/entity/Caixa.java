030020bb6dc55c8905f11b1146ba480617925b62
Gabriel Morrison Lima Dantas <gabriel@infox.com.br>
2013-10-24 10:55:44 -0300

    Remoção de warnings

--- src/main/java/br/com/infox/ibpm/entity/Caixa.java
+++ src/main/java/br/com/infox/ibpm/entity/Caixa.java
@@ -19,7 +19,6 @@ import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
 import javax.validation.constraints.Size;
 
 import br.com.infox.util.constants.LengthConstants;
