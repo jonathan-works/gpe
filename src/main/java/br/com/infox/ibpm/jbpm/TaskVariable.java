67893f081a2fce3e2a891acb7ebce12886a1cbc0
Jonas Santos Bezerra <jonasbezerra@infox.com.br>
2013-10-28 11:01:12 -0300

    Criação de método hasVariable()

--- src/main/java/br/com/infox/ibpm/jbpm/TaskVariable.java
+++ src/main/java/br/com/infox/ibpm/jbpm/TaskVariable.java
@@ -47,5 +47,9 @@ final class TaskVariable {
     public void setVariable(Object variable) {
         this.variable = variable;
     }
+    
+    public boolean hasVariable(){
+        return variable != null;
+    }
 
 }
