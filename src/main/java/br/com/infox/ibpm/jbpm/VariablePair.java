7fc3ce157a4a14b180d809ae95addb3caf9c88eb
Jonas Santos Bezerra <jonasbezerra@infox.com.br>
2013-10-28 11:33:33 -0300

    Criação de classe VariablePair

---
+++ src/main/java/br/com/infox/ibpm/jbpm/VariablePair.java
@@ -0,0 +1,13 @@
+package br.com.infox.ibpm.jbpm;
+
+final class VariablePair {
+    
+    public String name;
+    public Object variable;
+    
+    public VariablePair(String name, Object variable) {
+        this.name = name;
+        this.variable = variable;
+    }
+
+}
