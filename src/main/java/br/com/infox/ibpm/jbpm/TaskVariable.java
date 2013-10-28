351c8640f1f53483b96ff7ae075fefed9d7baebb
Jonas Santos Bezerra <jonasbezerra@infox.com.br>
2013-10-28 10:28:57 -0300

    Criação dos atributos e getters básicos

--- src/main/java/br/com/infox/ibpm/jbpm/TaskVariable.java
+++ src/main/java/br/com/infox/ibpm/jbpm/TaskVariable.java
@@ -1,5 +1,23 @@
 package br.com.infox.ibpm.jbpm;
 
+import org.jbpm.context.def.VariableAccess;
+
 final class TaskVariable {
+    
+    private String name;
+    private String type;
+    
+    public TaskVariable (VariableAccess var){
+        this.type = var.getMappedName().split(":")[0];
+        this.name = var.getMappedName().split(":")[1];
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
 
 }
