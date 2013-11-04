c5bdc9f5c8e4c82a9862878ec1eaa32eaab496b2
Jonas Santos Bezerra <jonasbezerra@infox.com.br>
2013-11-04 14:20:36 -0300

    refExportarXLS : Pessoa Juridica

--- src/main/java/br/com/infox/list/PessoaJuridicaList.java
+++ src/main/java/br/com/infox/list/PessoaJuridicaList.java
@@ -5,21 +5,21 @@ import java.util.Map;
 import org.jboss.seam.ScopeType;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Scope;
-import org.jboss.seam.annotations.intercept.BypassInterceptors;
-
 import br.com.infox.core.action.list.EntityList;
 import br.com.infox.core.action.list.SearchCriteria;
 import br.com.infox.ibpm.entity.PessoaJuridica;
 import br.com.itx.util.ComponentUtil;
 
 @Name(PessoaJuridicaList.NAME)
-@BypassInterceptors
 @Scope(ScopeType.PAGE)
 public class PessoaJuridicaList extends EntityList<PessoaJuridica> {
 
 	public static final String NAME = "pessoaJuridicaList";
 
 	private static final long serialVersionUID = 1L;
+	
+	private static final String TEMPLATE = "/PessoaJuridica/pessoaJuridicaTemplate.xls";
+    private static final String DOWNLOAD_XLS_NAME = "PessoaJuridica.xls";
 	
 	private static final String DEFAULT_EJBQL = "select o from PessoaJuridica o";
 	private static final String DEFAULT_ORDER = "nome";
@@ -51,5 +51,18 @@ public class PessoaJuridicaList extends EntityList<PessoaJuridica> {
 		return ComponentUtil.getComponent(PessoaJuridicaList.NAME);
 	}
 	
-	
+	@Override
+    public EntityList<PessoaJuridica> getBeanList() {
+        return PessoaJuridicaList.instance();
+    }
+    
+    @Override
+    public String getTemplate() {
+        return TEMPLATE;
+    }
+    
+    @Override
+    public String getDownloadXlsName() {
+        return DOWNLOAD_XLS_NAME;
+    }
 }
