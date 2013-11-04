21b9f3369ae9234ac57d81dbf1f086c3a2ed806b
Jonas Santos Bezerra <jonasbezerra@infox.com.br>
2013-11-04 14:14:02 -0300

    refExportarXLS : CNAE

--- src/main/java/br/com/infox/list/CnaeList.java
+++ src/main/java/br/com/infox/list/CnaeList.java
@@ -5,21 +5,21 @@ import java.util.Map;
 import org.jboss.seam.ScopeType;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Scope;
-import org.jboss.seam.annotations.intercept.BypassInterceptors;
-
 import br.com.infox.core.action.list.EntityList;
 import br.com.infox.core.action.list.SearchCriteria;
 import br.com.infox.ibpm.entity.Cnae;
 import br.com.itx.util.ComponentUtil;
 
 @Name(CnaeList.NAME)
-@BypassInterceptors
 @Scope(ScopeType.PAGE)
 public class CnaeList extends EntityList<Cnae> {
 
 	public static final String NAME = "cnaeList";
 
 	private static final long serialVersionUID = 1L;
+	
+	private static final String TEMPLATE = "/CNAE/CnaeTemplate.xls";
+    private static final String DOWNLOAD_XLS_NAME = "Cnae.xls";
 	
 	private static final String DEFAULT_EJBQL = "select o from Cnae o";
 	private static final String DEFAULT_ORDER = "codCnae";
@@ -49,4 +49,19 @@ public class CnaeList extends EntityList<Cnae> {
 	public static CnaeList instance(){
 		return ComponentUtil.getComponent(CnaeList.NAME);
 	}
+	
+    @Override
+    public EntityList<Cnae> getBeanList() {
+        return CnaeList.instance();
+    }
+    
+    @Override
+    public String getTemplate() {
+        return TEMPLATE;
+    }
+    
+    @Override
+    public String getDownloadXlsName() {
+        return DOWNLOAD_XLS_NAME;
+    }
 }
