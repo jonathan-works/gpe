ff0d3c134c540acc7c2234d87e4e21fb173197ab
Jonas Santos Bezerra <jonasbezerra@infox.com.br>
2013-11-05 10:42:28 -0300

    Movido ItemHome para br.com.infox.epp.fluxo.home

--- src/main/java/br/com/infox/epp/fluxo/home/ItemHome.java
+++ src/main/java/br/com/infox/epp/fluxo/home/ItemHome.java
@@ -13,7 +13,7 @@
  Você deve ter recebido uma cópia da GNU GPL junto com este programa; se não, 
  veja em http://www.gnu.org/licenses/   
 */
-package br.com.infox.ibpm.home;
+package br.com.infox.epp.fluxo.home;
 
 import org.jboss.seam.annotations.Name;
 import br.com.infox.annotations.manager.RecursiveManager;
