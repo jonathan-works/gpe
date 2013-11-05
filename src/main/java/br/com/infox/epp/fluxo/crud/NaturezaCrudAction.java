d10528e4aa24c3774dafe5e3f664ba4e1ef9524c
Jonas Santos Bezerra <jonasbezerra@infox.com.br>
2013-11-05 12:07:22 -0300

    Criação de NaturezaCrudAction

---
+++ src/main/java/br/com/infox/epp/fluxo/crud/NaturezaCrudAction.java
@@ -0,0 +1,16 @@
+package br.com.infox.epp.fluxo.crud;
+
+import org.jboss.seam.ScopeType;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Scope;
+
+import br.com.infox.core.action.crud.AbstractCrudAction;
+import br.com.infox.epp.fluxo.entity.Natureza;
+
+@Name(NaturezaCrudAction.NAME)
+@Scope(ScopeType.PAGE)
+public class NaturezaCrudAction extends AbstractCrudAction<Natureza> {
+    
+    public static final String NAME = "naturezaCrudAction";
+
+}
