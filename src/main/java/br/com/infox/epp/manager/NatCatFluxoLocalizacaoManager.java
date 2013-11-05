205055652b28305a1bc8ac3f4270cbaf1f924d95
Jonas Santos Bezerra <jonasbezerra@infox.com.br>
2013-11-05 14:57:03 -0300

    Movido NatCatFluxoLocalizacaoDAO para br.com.infox.epp.fluxo.dao

--- src/main/java/br/com/infox/epp/manager/NatCatFluxoLocalizacaoManager.java
+++ src/main/java/br/com/infox/epp/manager/NatCatFluxoLocalizacaoManager.java
@@ -10,7 +10,7 @@ import org.jboss.seam.annotations.Scope;
 
 import br.com.infox.access.entity.Papel;
 import br.com.infox.core.manager.GenericManager;
-import br.com.infox.epp.dao.NatCatFluxoLocalizacaoDAO;
+import br.com.infox.epp.fluxo.dao.NatCatFluxoLocalizacaoDAO;
 import br.com.infox.epp.fluxo.entity.NatCatFluxoLocalizacao;
 import br.com.infox.epp.fluxo.entity.NaturezaCategoriaFluxo;
 import br.com.infox.ibpm.entity.Localizacao;
