bd461102b8d03c7aec8b8a77a03ed3b7ae476c94
Jonas Santos Bezerra <jonasbezerra@infox.com.br>
2013-11-06 14:41:02 -0300

    Utilizando LocalizacaoFisicaCrudAction ao invés de LocalizaçãoFisicaHome

---
+++ src/main/java/br/com/infox/epp/documento/crud/LocalizacaoFisicaCrudAction.java
@@ -0,0 +1,32 @@
+package br.com.infox.epp.documento.crud;
+
+import org.jboss.seam.annotations.Name;
+
+import br.com.infox.core.action.crud.AbstractCrudAction;
+import br.com.infox.epp.documento.entity.LocalizacaoFisica;
+
+@Name(LocalizacaoFisicaCrudAction.NAME)
+public class LocalizacaoFisicaCrudAction extends AbstractCrudAction<LocalizacaoFisica> {
+    
+    public static final String NAME = "localizacaoFisicaCrudAction";
+    
+    public String inactive(LocalizacaoFisica localizacaoFisica) {
+        return inactiveRecursive(localizacaoFisica);
+    }
+    
+    protected boolean beforeSave() {
+        if (getInstance().getLocalizacaoFisicaPai() != null && !getInstance().getLocalizacaoFisicaPai().getAtivo()){
+            getInstance().setAtivo(false);
+        }
+        return true;
+    }
+    
+    @Override
+    public String save() {
+        if (!getInstance().getAtivo()){
+            inactiveRecursive(getInstance());
+        }
+        return super.save();
+    }
+    
+}
