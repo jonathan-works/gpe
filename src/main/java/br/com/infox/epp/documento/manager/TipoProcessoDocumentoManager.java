2b2c10c82a8719c1f07e5e97801dc107a9675c17
Jonas Santos Bezerra <jonasbezerra@infox.com.br>
2013-11-06 08:08:20 -0300

    Movido TipoProcessoDocumentoDAO para br.com.infox.epp.documento.dao

--- src/main/java/br/com/infox/epp/documento/manager/TipoProcessoDocumentoManager.java
+++ src/main/java/br/com/infox/epp/documento/manager/TipoProcessoDocumentoManager.java
@@ -9,8 +9,8 @@ import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Scope;
 
 import br.com.infox.core.manager.GenericManager;
+import br.com.infox.epp.documento.dao.TipoProcessoDocumentoDAO;
 import br.com.infox.epp.documento.entity.TipoProcessoDocumento;
-import br.com.infox.ibpm.dao.TipoProcessoDocumentoDAO;
 
 @Name(TipoProcessoDocumentoManager.NAME)
 @Scope(ScopeType.CONVERSATION)
