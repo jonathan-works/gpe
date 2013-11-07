129d17bf1bf478c6343035d3a9b035d4e20ca1bc
Jonas Santos Bezerra <jonasbezerra@infox.com.br>
2013-11-07 14:20:26 -0300

    Validação no método Save()

--- src/main/java/br/com/infox/epp/documento/crud/ClassificacaoDocumentoCrudAction.java
+++ src/main/java/br/com/infox/epp/documento/crud/ClassificacaoDocumentoCrudAction.java
@@ -1,10 +1,21 @@
 package br.com.infox.epp.documento.crud;
 
+import org.jboss.seam.annotations.Name;
+
 import br.com.infox.core.action.crud.AbstractCrudAction;
 import br.com.infox.epp.documento.entity.TipoProcessoDocumento;
 
+@Name(ClassificacaoDocumentoCrudAction.NAME)
 public class ClassificacaoDocumentoCrudAction extends AbstractCrudAction<TipoProcessoDocumento> {
     
     public static final String NAME = "classificacaoDocumentoCrudAction";
+    
+    @Override
+    public String save() {
+        if (!getInstance().getNumera()){
+            getInstance().setTipoNumeracao(null);
+        }
+        return super.save();
+    }
 
 }
