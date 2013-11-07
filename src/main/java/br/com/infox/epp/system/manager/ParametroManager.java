dd7332585fd9aeecf157f880a66f1828a1acab70
Jonas Santos Bezerra <jonasbezerra@infox.com.br>
2013-11-07 15:29:07 -0300

    Movido ParametroDAO para br.com.infox.epp.system.dao

--- src/main/java/br/com/infox/epp/system/manager/ParametroManager.java
+++ src/main/java/br/com/infox/epp/system/manager/ParametroManager.java
@@ -4,8 +4,8 @@ import org.jboss.seam.annotations.AutoCreate;
 import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
 import br.com.infox.core.manager.GenericManager;
+import br.com.infox.epp.system.dao.ParametroDAO;
 import br.com.infox.epp.system.entity.Parametro;
-import br.com.infox.ibpm.dao.ParametroDAO;
 
 @Name(ParametroManager.NAME)
 @AutoCreate
