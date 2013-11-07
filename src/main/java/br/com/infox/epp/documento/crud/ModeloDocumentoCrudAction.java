7292efb96313a535fdc9539ed23de5ebd42fd04b
Jonas Santos Bezerra <jonasbezerra@infox.com.br>
2013-11-07 11:03:12 -0300

    Criação de ModeloDocumentoCrudAction

---
+++ src/main/java/br/com/infox/epp/documento/crud/ModeloDocumentoCrudAction.java
@@ -0,0 +1,96 @@
+package br.com.infox.epp.documento.crud;
+
+import java.util.Date;
+
+import org.jboss.seam.ScopeType;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Scope;
+import org.jboss.seam.log.LogProvider;
+import org.jboss.seam.log.Logging;
+
+import br.com.infox.access.entity.UsuarioLogin;
+import br.com.infox.core.action.crud.AbstractCrudAction;
+import br.com.infox.epp.documento.entity.HistoricoModeloDocumento;
+import br.com.infox.epp.documento.entity.ModeloDocumento;
+import br.com.infox.ibpm.home.Authenticator;
+import br.com.itx.util.ComponentUtil;
+import br.com.itx.util.EntityUtil;
+
+@Name(ModeloDocumentoCrudAction.NAME)
+@Scope(ScopeType.CONVERSATION)
+public class ModeloDocumentoCrudAction extends AbstractCrudAction<ModeloDocumento> {
+    
+    public static final String NAME = "modeloDocumentoCrudAction";
+    
+    private static final LogProvider LOG = Logging.getLogProvider(ModeloDocumentoCrudAction.class);
+    
+    private ModeloDocumento modeloDocumentoAnterior;
+    
+    @Override
+    public void newInstance() {
+        modeloDocumentoAnterior = null;
+        super.newInstance();
+    }
+    
+    @Override
+    public void setId(Object id) {
+        super.setId(id);
+        updateOldInstance();
+    }
+
+    private void updateOldInstance() {
+        try {
+            modeloDocumentoAnterior = (ModeloDocumento) EntityUtil.cloneObject(getInstance(), false);
+        } catch (InstantiationException | IllegalAccessException e) {
+            LOG.error(".updateOldInstance()", e);
+        }
+    }
+    
+    @Override
+    protected void afterSave() {
+        updateOldInstance();
+        super.afterSave();
+    }
+    
+    @Override
+    public String save() {
+        gravarHistorico();
+        return super.save();
+    }
+    
+    private void gravarHistorico() {
+        if (haModificacoesNoModelo()){
+            HistoricoModeloDocumento historico = new HistoricoModeloDocumento();
+            historico.setTituloModeloDocumento(modeloDocumentoAnterior.getTituloModeloDocumento());
+            historico.setDescricaoModeloDocumento(modeloDocumentoAnterior.getModeloDocumento());
+            historico.setAtivo(modeloDocumentoAnterior.getAtivo());
+            historico.setDataAlteracao(new Date());
+            historico.setModeloDocumento(getInstance());
+            historico.setUsuarioAlteracao((UsuarioLogin) ComponentUtil.getComponent(Authenticator.USUARIO_LOGADO));
+            persist(historico);
+        }
+        
+    }
+
+    private boolean haModificacoesNoModelo() {
+        return modeloDocumentoAnterior != null && getInstance().hasChanges(modeloDocumentoAnterior);
+    }
+    
+    public void restaurar(HistoricoModeloDocumento historicoModeloDocumento){
+        if (historicoModeloDocumento == null) {
+            return;
+        }
+        updateOldInstance();
+        restaurarAtributos(historicoModeloDocumento);
+        save();
+    }
+
+    private void restaurarAtributos(
+            HistoricoModeloDocumento historicoModeloDocumento) {
+        getInstance().setAtivo(historicoModeloDocumento.getAtivo());
+        getInstance().setModeloDocumento(historicoModeloDocumento.getDescricaoModeloDocumento());
+        getInstance().setTipoModeloDocumento(historicoModeloDocumento.getModeloDocumento().getTipoModeloDocumento());
+        getInstance().setTituloModeloDocumento(historicoModeloDocumento.getTituloModeloDocumento());
+    }
+
+}
