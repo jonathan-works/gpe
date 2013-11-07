a657941cd91c3ece2d787b83dbbe2cc37c015823
Jonas Santos Bezerra <jonasbezerra@infox.com.br>
2013-11-07 12:51:37 -0300

    Criado ClassificacaoDocumentoCrudAction

---
+++ src/main/java/br/com/infox/epp/documento/crud/ClassificacaoDocumentoCrudAction.java
@@ -0,0 +1,10 @@
+package br.com.infox.epp.documento.crud;
+
+import br.com.infox.core.action.crud.AbstractCrudAction;
+import br.com.infox.epp.documento.entity.TipoProcessoDocumento;
+
+public class ClassificacaoDocumentoCrudAction extends AbstractCrudAction<TipoProcessoDocumento> {
+    
+    public static final String NAME = "classificacaoDocumentoCrudAction";
+
+}
