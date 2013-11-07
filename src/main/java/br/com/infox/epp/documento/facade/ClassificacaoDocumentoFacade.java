f2b14e234b3d781af4ad1a6db60ec3e379461a03
Jonas Santos Bezerra <jonasbezerra@infox.com.br>
2013-11-07 15:07:27 -0300

    Criação de método getTipoProcessoDocumentoInterno em ClassificacaoDocumentoFacade

--- src/main/java/br/com/infox/epp/documento/facade/ClassificacaoDocumentoFacade.java
+++ src/main/java/br/com/infox/epp/documento/facade/ClassificacaoDocumentoFacade.java
@@ -1,9 +1,14 @@
 package br.com.infox.epp.documento.facade;
 
+import java.util.List;
+
 import org.jboss.seam.ScopeType;
+import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Scope;
 
+import br.com.infox.epp.documento.entity.TipoProcessoDocumento;
+import br.com.infox.epp.documento.manager.TipoProcessoDocumentoManager;
 import br.com.infox.epp.documento.type.TipoDocumentoEnum;
 import br.com.infox.epp.documento.type.TipoNumeracaoEnum;
 import br.com.infox.epp.documento.type.VisibilidadeEnum;
@@ -11,6 +16,8 @@ import br.com.infox.epp.documento.type.VisibilidadeEnum;
 @Name(ClassificacaoDocumentoFacade.NAME)
 @Scope(ScopeType.CONVERSATION)
 public class ClassificacaoDocumentoFacade {
+    
+    @In TipoProcessoDocumentoManager tipoProcessoDocumentoManager;
     
     public static final String NAME = "classificacaoDocumentoFacade";
     
@@ -24,6 +31,10 @@ public class ClassificacaoDocumentoFacade {
     
     public VisibilidadeEnum[] getVisibilidadeEnumValues(){
         return VisibilidadeEnum.values();
+    }
+    
+    public List<TipoProcessoDocumento> getTipoProcessoDocumentoInterno(boolean isModelo){
+        return tipoProcessoDocumentoManager.getTipoProcessoDocumentoInterno(isModelo);
     }
 
 }
