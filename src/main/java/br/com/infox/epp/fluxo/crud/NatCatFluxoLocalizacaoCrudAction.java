dd973ae3c9f3cd561df09755a10d63b8677e67b6
Jonas Santos Bezerra <jonasbezerra@infox.com.br>
2013-11-08 13:19:42 -0300

    Adição de método clearTree e sobrescrição de save()

--- src/main/java/br/com/infox/epp/fluxo/crud/NatCatFluxoLocalizacaoCrudAction.java
+++ src/main/java/br/com/infox/epp/fluxo/crud/NatCatFluxoLocalizacaoCrudAction.java
@@ -1,10 +1,14 @@
 package br.com.infox.epp.fluxo.crud;
 
+import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
 
 import br.com.infox.core.action.crud.AbstractCrudAction;
 import br.com.infox.epp.fluxo.entity.NatCatFluxoLocalizacao;
 import br.com.infox.epp.fluxo.entity.NaturezaCategoriaFluxo;
+import br.com.infox.epp.fluxo.manager.NatCatFluxoLocalizacaoManager;
+import br.com.infox.ibpm.component.tree.LocalizacaoTreeHandler;
+import br.com.itx.util.ComponentUtil;
 
 @Name(NatCatFluxoLocalizacaoCrudAction.NAME)
 public class NatCatFluxoLocalizacaoCrudAction extends AbstractCrudAction<NatCatFluxoLocalizacao> {
@@ -12,6 +16,7 @@ public class NatCatFluxoLocalizacaoCrudAction extends AbstractCrudAction<NatCatF
     public static final String NAME = "natCatFluxoLocalizacaoCrudAction";
     
     private NaturezaCategoriaFluxo naturezaCategoriaFluxoCorrente;
+    @In private NatCatFluxoLocalizacaoManager natCatFluxoLocalizacaoManager;
     
     public void setNaturezaCategoriaFluxo(NaturezaCategoriaFluxo naturezaCategoriaFluxo){
         getInstance().setNaturezaCategoriaFluxo(naturezaCategoriaFluxo);
@@ -25,9 +30,23 @@ public class NatCatFluxoLocalizacaoCrudAction extends AbstractCrudAction<NatCatF
     }
     
     @Override
+    public String save() {
+        if (getInstance().getHeranca()) {
+            natCatFluxoLocalizacaoManager.persistWithChildren(getInstance());
+        }
+        return super.save();
+    }
+    
+    @Override
     protected void afterSave() {
         super.afterSave();
         newInstance();
+        clearTree();
+    }
+
+    private void clearTree() {
+        LocalizacaoTreeHandler treeHandler = ComponentUtil.getComponent("localizacaoTree");
+        treeHandler.clearTree();
     }
     
 }
