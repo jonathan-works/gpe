bfeaf9754a19358b73cb6f1e9740255913d20691
Jonas Santos Bezerra <jonasbezerra@infox.com.br>
2013-11-11 08:02:13 -0300

    Movido LogEventListener para br.com.infox.epp.system.listener

--- src/main/java/br/com/infox/epp/system/listener/LogEventListener.java
+++ src/main/java/br/com/infox/epp/system/listener/LogEventListener.java
@@ -13,7 +13,7 @@
  Você deve ter recebido uma cópia da GNU GPL junto com este programa; se não, 
  veja em http://www.gnu.org/licenses/   
 */
-package br.com.infox.listener;
+package br.com.infox.epp.system.listener;
 
 import org.hibernate.event.spi.PostDeleteEvent;
 import org.hibernate.event.spi.PostDeleteEventListener;
