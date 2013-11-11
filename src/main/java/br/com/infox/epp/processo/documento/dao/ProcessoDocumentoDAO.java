2d6d6f66552d63a9aa30629dc75f555ac4740d30
Jonas Santos Bezerra <jonasbezerra@infox.com.br>
2013-11-11 15:50:51 -0300

    Movido ProcessoDocumentoDAO para br.com.infox.epp.processo.documento.dao

--- src/main/java/br/com/infox/epp/processo/documento/dao/ProcessoDocumentoDAO.java
+++ src/main/java/br/com/infox/epp/processo/documento/dao/ProcessoDocumentoDAO.java
@@ -1,4 +1,4 @@
-package br.com.infox.ibpm.dao;
+package br.com.infox.epp.processo.documento.dao;
 
 import java.util.List;
 
--- src/main/java/br/com/infox/epp/processo/documento/manager/ProcessoDocumentoManager.java
+++ src/main/java/br/com/infox/epp/processo/documento/manager/ProcessoDocumentoManager.java
@@ -7,8 +7,8 @@ import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
 
 import br.com.infox.core.manager.GenericManager;
+import br.com.infox.epp.processo.documento.dao.ProcessoDocumentoDAO;
 import br.com.infox.epp.processo.entity.Processo;
-import br.com.infox.ibpm.dao.ProcessoDocumentoDAO;
 
 @Name(ProcessoDocumentoManager.NAME)
 @AutoCreate
