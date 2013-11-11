ddf21ac54bb8434e9087da82ad3481874e7a416d
Jonas Santos Bezerra <jonasbezerra@infox.com.br>
2013-11-11 15:04:14 -0300

    Movido SituacaoProcessoDAO para br.com.infox.epp.processo.situacao.dao

--- src/main/java/br/com/infox/ibpm/manager/SituacaoProcessoManager.java
+++ src/main/java/br/com/infox/ibpm/manager/SituacaoProcessoManager.java
@@ -8,7 +8,7 @@ import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
 
 import br.com.infox.core.manager.GenericManager;
-import br.com.infox.ibpm.dao.SituacaoProcessoDAO;
+import br.com.infox.epp.processo.situacao.dao.SituacaoProcessoDAO;
 
 @Name(SituacaoProcessoManager.NAME)
 @AutoCreate
