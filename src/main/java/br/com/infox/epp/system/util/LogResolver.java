3ae7dec6133dbaf4986a136ba4bcad8f3b2ee0cc
Jonas Santos Bezerra <jonasbezerra@infox.com.br>
2013-11-11 08:10:27 -0300

    Movido LogResolver para br.com.infox.epp.system.util

--- src/main/java/br/com/infox/epp/system/util/LogResolver.java
+++ src/main/java/br/com/infox/epp/system/util/LogResolver.java
@@ -13,7 +13,7 @@
  Você deve ter recebido uma cópia da GNU GPL junto com este programa; se não, 
  veja em http://www.gnu.org/licenses/   
 */
-package br.com.infox.epp.system.resolver;
+package br.com.infox.epp.system.util;
 
 import org.apache.log4j.ConsoleAppender;
 import org.apache.log4j.Level;
