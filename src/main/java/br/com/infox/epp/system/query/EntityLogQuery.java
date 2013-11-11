2737096ae08d04882b1919d243e8c09406edf36a
Jonas Santos Bezerra <jonasbezerra@infox.com.br>
2013-11-11 07:49:53 -0300

    Movido EntityLogQuery para br.com.infox.epp.system.query

--- src/main/java/br/com/infox/epp/system/query/EntityLogQuery.java
+++ src/main/java/br/com/infox/epp/system/query/EntityLogQuery.java
@@ -13,7 +13,7 @@
  Você deve ter recebido uma cópia da GNU GPL junto com este programa; se não, 
  veja em http://www.gnu.org/licenses/   
 */
-package br.com.infox.ibpm.entity.log;
+package br.com.infox.epp.system.query;
 
 import java.util.List;
 
