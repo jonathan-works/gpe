2d45fc87328090e726d254826e4bb01ac16c349a
erikliberal <erikliberal@infox.com.br>
2013-11-12 09:33:38 -0300

    [CategoriaItemManager] adição de mensagens e ajuste na chamada de persistência de adição de novos itens

--- src/main/java/br/com/infox/epp/manager/CategoriaItemManager.java
+++ src/main/java/br/com/infox/epp/manager/CategoriaItemManager.java
@@ -1,5 +1,7 @@
 package br.com.infox.epp.manager;
 
+import static org.jboss.seam.faces.FacesMessages.instance;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
@@ -7,12 +9,16 @@ import java.util.Set;
 import org.jboss.seam.annotations.AutoCreate;
 import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
+import org.jboss.seam.faces.FacesMessages;
+import org.jboss.seam.international.StatusMessage.Severity;
+
 import br.com.infox.core.manager.GenericManager;
 import br.com.infox.epp.dao.CategoriaItemDAO;
 import br.com.infox.epp.entity.Categoria;
 import br.com.infox.epp.entity.CategoriaItem;
 import br.com.infox.ibpm.entity.Item;
 import br.com.infox.ibpm.home.CategoriaHome;
+import br.com.itx.component.Util;
 import br.com.itx.util.ComponentUtil;
 
 @Name(CategoriaItemManager.NAME)
@@ -48,13 +54,38 @@ public class CategoriaItemManager extends GenericManager{
         if (itens != null) {
             for (Item item : itens) {
                 if (item.getAtivo()) {
-                    CategoriaItem ci = new CategoriaItem(categoria, item);
-                    persist(ci);
-                    categoriaItemList.add(ci);
+                    persistCategoriaItem(categoria, item, categoriaItemList);
                 }
             }
         }
+        FacesMessages fm = instance();
+        fm.clear();
+        if (categoriaItemList.size() > 0) {
+            fm.add("#{messages['entity_created']}");
+        } else {
+            fm.add(Severity.ERROR,"Falha ao inserir");
+        }
         return categoriaItemList;
 	}
+	
+	/**
+	 * @param categoria Categoria a ser associada ao Item
+	 * @param item Item a ser associado à categoria
+	 * @param categoriaItemList Lista resultante
+	 * 
+	 * Este método deve iniciar a transação antes de persistir e finalizar após persistir
+	 * devido à necessidade de serem inserções atômicas, caso múltiplas entradas fossem
+	 * permitidas em uma única transação e uma destas causasse uma exceção, então
+	 * todas as outras falhariam
+	 */
+    private void persistCategoriaItem(Categoria categoria, Item item,
+            List<CategoriaItem> categoriaItemList) {
+        Util.beginTransaction();
+        CategoriaItem ci = persist(new CategoriaItem(categoria, item));
+        Util.commitTransction();
+        if (ci!=null) {
+            categoriaItemList.add(ci);
+        }
+    }
 	
 }
