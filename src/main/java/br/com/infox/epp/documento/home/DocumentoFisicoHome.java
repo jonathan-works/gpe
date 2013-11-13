31d7cf8616678d0876fd0cfda4b61a3eecb9b042
Jonas Santos Bezerra <jonasbezerra@infox.com.br>
2013-11-13 09:10:43 -0300

    Utilizando DocumentoFisicoCrudAction ao invés de DocumentoFisicoHome

--- src/main/java/br/com/infox/epp/documento/home/DocumentoFisicoHome.java
+++ src/main/java/br/com/infox/epp/documento/home/DocumentoFisicoHome.java
@@ -1,14 +1,11 @@
 package br.com.infox.epp.documento.home;
 
-import java.util.List;
-
 import org.jboss.seam.ScopeType;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Scope;
 
 import br.com.infox.epp.documento.entity.DocumentoFisico;
 import br.com.itx.component.AbstractHome;
-import br.com.itx.util.EntityUtil;
 
 /**
  * 
@@ -22,23 +19,5 @@ public class DocumentoFisicoHome extends AbstractHome<DocumentoFisico> {
 
 	private static final long serialVersionUID = 1L;
 	public static final String NAME = "documentoFisicoHome";
-	
-	private List<DocumentoFisico> documentoFisicoList; 
-	
-	@Override
-	public void create() {
-		super.create();
-		
-		setDocumentoFisicoList(EntityUtil.getEntityList(DocumentoFisico.class));
-	}
-
-	public List<DocumentoFisico> getDocumentoFisicoList() {
-		return documentoFisicoList;
-	}
-	public void setDocumentoFisicoList(List<DocumentoFisico> documentoFisicoList) {
-		this.documentoFisicoList = documentoFisicoList;
-	}
-	
-	
 	
 }
