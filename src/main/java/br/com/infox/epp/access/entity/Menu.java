9b688ee2c806ccb5fa4df6afb75f01adfe19c48e
Jonas Santos Bezerra <jonasbezerra@infox.com.br>
2013-11-14 11:39:05 -0300

    Movido MenuItem para br.com.infox.epp.access.entity

--- src/main/java/br/com/infox/epp/access/entity/Menu.java
+++ src/main/java/br/com/infox/epp/access/entity/Menu.java
@@ -20,7 +20,6 @@ import org.jboss.seam.security.RunAsOperation;
 import org.jboss.seam.security.management.IdentityManager;
 import org.richfaces.event.DropEvent;
 
-import br.com.infox.access.MenuItem;
 import br.com.infox.epp.access.util.SecurityUtil;
 import br.com.itx.component.Util;
 import br.com.itx.util.EntityUtil;
