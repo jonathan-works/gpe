1c99b3b4e5317668caf180bc8bd226d7c8fdcd81
Jonas Santos Bezerra <jonasbezerra@infox.com.br>
2013-11-14 15:35:19 -0300

    Movido CustomResourceResolver para br.com.infox.epp.access.service

--- src/main/java/br/com/infox/epp/access/service/CustomResourceResolver.java
+++ src/main/java/br/com/infox/epp/access/service/CustomResourceResolver.java
@@ -13,7 +13,7 @@
  Você deve ter recebido uma cópia da GNU GPL junto com este programa; se não, 
  veja em http://www.gnu.org/licenses/   
 */
-package br.com.infox.component;
+package br.com.infox.epp.access.service;
 
 import java.net.URL;
 
