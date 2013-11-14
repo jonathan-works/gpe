0dc8b055344a243d43efde7f6ec9b9cb5f758345
Jonas Santos Bezerra <jonasbezerra@infox.com.br>
2013-11-14 11:11:41 -0300

    Movido CepSuggestBean para br.com.infox.epp.endereco.component.suggest

--- src/main/java/br/com/infox/epp/endereco/component/suggest/CepSuggestBean.java
+++ src/main/java/br/com/infox/epp/endereco/component/suggest/CepSuggestBean.java
@@ -13,7 +13,7 @@
  Você deve ter recebido uma cópia da GNU GPL junto com este programa; se não, 
  veja em http://www.gnu.org/licenses/   
 */
-package br.com.infox.ibpm.component.suggest;
+package br.com.infox.epp.endereco.component.suggest;
 
 import org.jboss.seam.ScopeType;
 import org.jboss.seam.annotations.Name;
