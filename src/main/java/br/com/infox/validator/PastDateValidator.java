4745edb2d7606b9d105bc5bdbd5ffd60a952ef03
erikliberal <erikliberal@infox.com.br>
2013-11-14 07:31:35 -0300

    Adição de mensagem do arquivo de internacionalização para o validador de datas passadas

--- src/main/java/br/com/infox/validator/PastDateValidator.java
+++ src/main/java/br/com/infox/validator/PastDateValidator.java
@@ -11,17 +11,19 @@ import javax.faces.validator.ValidatorException;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.intercept.BypassInterceptors;
 
-@org.jboss.seam.annotations.faces.Validator(id="pastDateValidator")
-@Name("pastDateValidator")
+@org.jboss.seam.annotations.faces.Validator(id=PastDateValidator.NAME)
+@Name(PastDateValidator.NAME)
 @BypassInterceptors
 public class PastDateValidator implements Validator {
 
-	public void validate(FacesContext context, UIComponent component, Object value) {
+	public static final String NAME = "pastDateValidator";
+
+    public void validate(FacesContext context, UIComponent component, Object value) {
 		
 		Date date = (Date) value;
 		Date now = new Date();
 		if (date != null && date.after(now)) {
-			throw new ValidatorException(new FacesMessage("A data deve ser menor ou IGUAL que a atual."));
+			throw new ValidatorException(new FacesMessage("#{messages['validator.Date.PAST']}"));
 		}
 		
 	}
