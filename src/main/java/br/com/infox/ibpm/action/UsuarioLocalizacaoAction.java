0ef46f3c50217b0c1ee942521a699661995f8214
Jonas Santos Bezerra <jonasbezerra@infox.com.br>
2013-11-14 10:03:46 -0300

    Movido UsuarioLocalizaçãoList para br.com.infox.epp.access.list

--- src/main/java/br/com/infox/ibpm/action/UsuarioLocalizacaoAction.java
+++ src/main/java/br/com/infox/ibpm/action/UsuarioLocalizacaoAction.java
@@ -10,10 +10,10 @@ import org.jboss.seam.annotations.Scope;
 import br.com.infox.component.tree.AbstractTreeHandler;
 import br.com.infox.core.manager.GenericManager;
 import br.com.infox.epp.access.entity.UsuarioLocalizacao;
+import br.com.infox.epp.access.list.UsuarioLocalizacaoList;
 import br.com.infox.ibpm.component.tree.LocalizacaoEstruturaTreeHandler;
 import br.com.infox.ibpm.component.tree.PapelTreeHandler;
 import br.com.infox.ibpm.home.UsuarioHome;
-import br.com.infox.list.UsuarioLocalizacaoList;
 
 @Name(UsuarioLocalizacaoAction.NAME)
 @Scope(ScopeType.PAGE)
