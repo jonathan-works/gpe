a526edc502d9cfff0fd2cd622e5a9a9672d448a7
Jonas Santos Bezerra <jonasbezerra@infox.com.br>
2013-11-14 12:05:31 -0300

    Movido pacote validator de br.com.infox para br.com.infox.pessoa por só conter validadores de CPF e CNPJ

--- src/main/java/br/com/infox/epp/pessoa/validator/CnpjValidator.java
+++ src/main/java/br/com/infox/epp/pessoa/validator/CnpjValidator.java
@@ -8,12 +8,12 @@
  Este programa é distribuído na expectativa de que seja útil, porém, SEM 
  NENHUMA GARANTIA; nem mesmo a garantia implícita de COMERCIABILIDADE OU 
  ADEQUAÇÃO A UMA FINALIDADE ESPECÍFICA.
- 
+
  Consulte a GNU GPL para mais detalhes.
  Você deve ter recebido uma cópia da GNU GPL junto com este programa; se não, 
  veja em http://www.gnu.org/licenses/   
-*/
-package br.com.infox.validator;
+ */
+package br.com.infox.epp.pessoa.validator;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -27,58 +27,64 @@ import javax.faces.validator.ValidatorException;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.intercept.BypassInterceptors;
 
-
-@org.jboss.seam.annotations.faces.Validator(id=CpfValidator.NAME)
-@Name(CpfValidator.NAME)
+@org.jboss.seam.annotations.faces.Validator(id = "cnpjValidator")
+@Name("cnpjValidator")
 @BypassInterceptors
-public class CpfValidator implements Validator {
-	public static final String NAME = "cpfValidator";
+public class CnpjValidator implements Validator {
 
-	public void validate(FacesContext context, UIComponent component, Object value) {
-		try {
-			String cpfValue = (String) value;
-			cpfValue = cpfValue.replaceAll("\\.", "");
-			cpfValue = cpfValue.replaceAll("-", "");
-			cpfValue = cpfValue.replaceAll("_", "");
-			Pattern p = Pattern.compile("^[0-9]{11}$");
-			Matcher m = p.matcher(cpfValue);
-			if ( !m.matches() || !validarCpf(cpfValue) ) {
-				throw new ValidatorException(new FacesMessage("CPF inválido"));
+	public void validate(FacesContext context, UIComponent component,
+			Object value) {
+		try {		
+			String cnpjValue = (String) value;
+			cnpjValue = cnpjValue.replaceAll("\\.", "");
+			cnpjValue = cnpjValue.replaceAll("-", "");
+			cnpjValue = cnpjValue.replaceAll("_", "");
+			cnpjValue = cnpjValue.replaceAll("/", "");
+			Pattern p = Pattern.compile("^[0-9]{14}$");
+			Matcher m = p.matcher(cnpjValue);
+			if ( !m.matches() || !validarCnpj(cnpjValue) ) {
+				throw new ValidatorException(new FacesMessage("CNPJ inválido."));
 			}
 		} catch (Exception e) {
-			throw new ValidatorException(new FacesMessage("CPF inválido"), e);
-		}		
+			throw new ValidatorException(new FacesMessage("CNPJ inválido."), e);
+		}
 	}
 
-	private boolean validarCpf(String cpf) {	
+	private boolean validarCnpj(String cnpj) {
 		int soma = 0;
-		for (int i = 0; i < 9; i++) { 
-			soma = soma + parseInt(cpf.charAt(i)) * (10 - i);
+		for (int i = 0; i < 4; i++) {
+			soma = soma + parseInt(cnpj.charAt(i)) * (5 - i);
+		}
+		for (int i = 4; i < 12; i++) {
+			soma = soma + parseInt(cnpj.charAt(i)) * (13 - i);
 		}
 		int dv1 = 11 - (soma % 11);
-		if (dv1 == 10 || dv1 == 11) {
+		if (dv1 >= 10) {
 			dv1 = 0;
 		}
-		if (dv1 != parseInt(cpf.charAt(9))) {
+		if (dv1 != parseInt(cnpj.charAt(12))) {
 			return false;
 		}
 		soma = 0;
-		for (int i = 0; i < 9; i++) {
-			soma = soma + parseInt(cpf.charAt(i)) * (11 - i);
+		for (int i = 0; i < 5; i++) {
+			soma = soma + parseInt(cnpj.charAt(i)) * (6 - i);
+		}
+		for (int i = 5; i < 12; i++) {
+			soma = soma + parseInt(cnpj.charAt(i)) * (14 - i);
 		}
 		soma = soma + dv1 * 2;
 		int dv2 = 11 - (soma % 11);
-		if (dv2 == 10 || dv2 == 11) {
+		if (dv2 >= 10) {
 			dv2 = 0;
 		}
-		if (dv2 != parseInt(cpf.charAt(10))) {
+		if (dv2 != parseInt(cnpj.charAt(13))) {
 			return false;
 		}
 		return true;
 	}
-	
+
 	private int parseInt(Character c) {
 		return Integer.parseInt(c.toString());
 	}
-	
+
 }
