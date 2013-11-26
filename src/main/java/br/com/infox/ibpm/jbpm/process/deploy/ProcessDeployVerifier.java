b5c48243d79e9159532890b353c79e6f46870ced
Jonas Santos Bezerra <jonasbezerra@infox.com.br>
2013-11-26 15:41:57 -0300

    Movido ProcessDeployVerifier para br.com.infox.ibpm.jbpm.process.deploy

--- src/main/java/br/com/infox/ibpm/jbpm/process/deploy/ProcessDeployVerifier.java
+++ src/main/java/br/com/infox/ibpm/jbpm/process/deploy/ProcessDeployVerifier.java
@@ -13,7 +13,7 @@
  Você deve ter recebido uma cópia da GNU GPL junto com este programa; se não, 
  veja em http://www.gnu.org/licenses/   
 */
-package br.com.infox.jbpm;
+package br.com.infox.ibpm.jbpm.process.deploy;
 
 import java.io.StringReader;
 import java.text.MessageFormat;
