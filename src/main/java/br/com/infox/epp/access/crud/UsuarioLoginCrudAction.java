006a3e19e092ca084d14639b114680c63c6c42c1
Jonas Santos Bezerra <jonasbezerra@infox.com.br>
2013-11-26 08:17:14 -0300

    Utilizando PasswordService ao invés de métodos próprios

--- src/main/java/br/com/infox/epp/access/crud/UsuarioLoginCrudAction.java
+++ src/main/java/br/com/infox/epp/access/crud/UsuarioLoginCrudAction.java
@@ -2,16 +2,12 @@ package br.com.infox.epp.access.crud;
 
 import java.util.Date;
 
+import javax.security.auth.login.LoginException;
+
 import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
-import org.jboss.seam.faces.FacesMessages;
-import org.jboss.seam.international.StatusMessage;
 import org.jboss.seam.log.LogProvider;
 import org.jboss.seam.log.Logging;
-import org.jboss.seam.security.RunAsOperation;
-import org.jboss.seam.security.management.IdentityManager;
-import org.jboss.seam.util.RandomStringUtils;
-
 import br.com.infox.core.crud.AbstractCrudAction;
 import br.com.infox.core.persistence.DAOException;
 import br.com.infox.epp.access.entity.BloqueioUsuario;
@@ -19,15 +15,9 @@ import br.com.infox.epp.access.entity.UsuarioLogin;
 import br.com.infox.epp.access.manager.BloqueioUsuarioManager;
 import br.com.infox.epp.access.manager.UsuarioLoginManager;
 import br.com.infox.epp.access.service.PasswordService;
-import br.com.infox.epp.documento.action.ModeloDocumentoAction;
-import br.com.infox.epp.documento.entity.ModeloDocumento;
-import br.com.infox.epp.documento.manager.ModeloDocumentoManager;
-import br.com.infox.epp.mail.command.SendmailCommand;
-import br.com.infox.epp.mail.entity.EMailData;
 import br.com.infox.epp.pessoa.entity.PessoaFisica;
 import br.com.infox.epp.pessoa.manager.PessoaManager;
 import br.com.infox.epp.system.util.ParametroUtil;
-import br.com.itx.util.ComponentUtil;
 import br.com.itx.util.EntityUtil;
 
 @Name(UsuarioLoginCrudAction.NAME)
@@ -36,13 +26,10 @@ public class UsuarioLoginCrudAction extends AbstractCrudAction<UsuarioLogin> {
     public static final String NAME = "usuarioLoginCrudAction";
     private static final LogProvider LOG = Logging.getLogProvider(UsuarioLoginCrudAction.class);
     
-    private static final int PASSWORD_LENGTH = 8;
-    
     private BloqueioUsuario novoBloqueio;
     
     @In private UsuarioLoginManager usuarioLoginManager;
     @In private PessoaManager pessoaManager;
-    @In private ModeloDocumentoManager modeloDocumentoManager;
     @In private BloqueioUsuarioManager bloqueioUsuarioManager;
     
     @In private PasswordService passwordService;
@@ -115,7 +102,11 @@ public class UsuarioLoginCrudAction extends AbstractCrudAction<UsuarioLogin> {
     @Override
     protected void afterSave(String ret) {
         if (getInstance().getSenha() == null || ParametroUtil.LOGIN_USUARIO_EXTERNO.equals(getInstance().getLogin())) {
-            gerarNovaSenha();
+            try {
+                passwordService.requisitarNovaSenha(getInstance().getEmail(), "");
+            } catch (LoginException e) {
+                LOG.error("afterSave()", e);
+            }
         }
         if (getInstance() instanceof PessoaFisica){
             try {
@@ -126,105 +117,6 @@ public class UsuarioLoginCrudAction extends AbstractCrudAction<UsuarioLogin> {
             getInstance().loadDataFromPessoaFisica(getInstance());
         };
         super.afterSave(ret);
-    }
-    
-    public void gerarNovaSenha() {
-        gerarNovaSenha("email");
-    }
-    
-    public void gerarNovaSenha(String parametro) {
-        if (ParametroUtil.LOGIN_USUARIO_EXTERNO.equals(getInstance().getLogin())) {
-            getInstance().setSenha("");
-        } else {
-            setPassword(RandomStringUtils.randomAlphabetic(PASSWORD_LENGTH));
-            getInstance().setSenha(getPassword());
-        }
-        new RunAsOperation(true) {
-            @Override
-            public void execute() {
-                IdentityManager.instance().changePassword(getInstance().getLogin(), getInstance().getSenha());
-            }
-        }.run();
-        
-        EntityUtil.getEntityManager().flush();
-        
-        iniciarRequisicao(parametro);
-    }
-    
-    /**
-     * Inicia o processo de requisição de senha
-     * 
-     * Requisita nova senha baseada na informação fornecida pelo usuário e tenta
-     * enviar com base na informação recuperada
-     * {@link #enviarModeloPorNome(String)}.
-     * 
-     * TODO:melhorar nome do método
-     * 
-     * @param parametro
-     *            Tipo da requisição de senha
-     */
-    private void iniciarRequisicao(String parametro) {
-        String nomeParam = null;
-        if ("login".equals(parametro)) {
-            nomeParam = "tituloModeloEmailMudancaSenha";
-        } else if ("email".equals(parametro)) {
-            nomeParam = "tituloModeloEmailMudancaSenhaComLogin";
-        }
-
-        String nomeModelo = ParametroUtil.getParametroOrFalse(nomeParam);
-
-        if (!enviarModeloPorNome(nomeModelo)) {
-            FacesMessages.instance().add(
-                    StatusMessage.Severity.ERROR,
-                    "Erro no envio do e-mail. O parâmetro de sistema '"
-                            + nomeParam
-                            + "' não foi definido ou possui um valor inválido");
-        }
-    }
-    
-    /**
-     * Método que recupera um modelo de documento pelo seu nome e envia
-     * {@link #enviarEmailModelo(ModeloDocumento)}
-     * 
-     * @param nomeModeloDocumento
-     *            Nome do Modelo de Documento a enviar por e-mail
-     * @return true se o e-mail for enviado e false se falhar
-     */
-    private boolean enviarModeloPorNome(String nomeModeloDocumento) {
-        if (nomeModeloDocumento == null || "false".equals(nomeModeloDocumento)) {
-            return false;
-        }
-        ModeloDocumento modelo = modeloDocumentoManager.getModeloDocumentoByTitulo(nomeModeloDocumento);
-        if (modelo == null) {
-            return false;
-        } else {
-            enviarEmailModelo(modelo);
-        }
-
-        return true;
-    }
-    
-    /**
-     * Envia e-mail baseado em um Modelo de Documento
-     * 
-     * @param modelo
-     *            Modelo do e-mail a ser enviado
-     */
-    private void enviarEmailModelo(ModeloDocumento modelo) {
-         if (modelo == null) {
-            return;
-        }
-        pessoaFisicaCadastrada = false;
-        String conteudo = ModeloDocumentoAction.instance().getConteudo(modelo);
-
-        EMailData data = ComponentUtil.getComponent(EMailData.NAME);
-        data.setUseHtmlBody(true);
-        data.setBody(conteudo);
-        data.getRecipientList().clear();
-        data.getRecipientList().add(getInstance());
-        data.setSubject("Senha do Sistema");
-        FacesMessages.instance().add("Senha gerada com sucesso.");
-        new SendmailCommand().execute("/WEB-INF/email/emailTemplate.xhtml");
     }
     
     @Override
@@ -256,8 +148,6 @@ public class UsuarioLoginCrudAction extends AbstractCrudAction<UsuarioLogin> {
             pessoaFisicaCadastrada = false;
         }
     }
-    
-// ------------------- Getters and Setters ---------------------\\
     
     public BloqueioUsuario getNovoBloqueio() {
         return novoBloqueio;
