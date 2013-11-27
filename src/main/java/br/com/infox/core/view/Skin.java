691693265da03f447fe3d49918bc6b67794e2f88
Jonas Santos Bezerra <jonasbezerra@infox.com.br>
2013-11-27 15:07:32 -0300

    Movido Skin de br.com.itx.jsf para br.com.infox.core.view

--- src/main/java/br/com/infox/core/view/Skin.java
+++ src/main/java/br/com/infox/core/view/Skin.java
@@ -13,7 +13,7 @@
  Você deve ter recebido uma cópia da GNU GPL junto com este programa; se não, 
  veja em http://www.gnu.org/licenses/   
 */
-package br.com.itx.jsf;
+package br.com.infox.core.view;
 
 import java.util.ArrayList;
 import java.util.List;
