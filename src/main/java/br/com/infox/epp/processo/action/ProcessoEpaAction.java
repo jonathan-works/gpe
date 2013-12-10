ce748e64f3e7526f93c130cfa9dfea92e610eda7
erikliberal <erikliberal@infox.com.br>
2013-12-10 10:57:44 -0300

    Remoção de método que não é mais necessário

--- src/main/java/br/com/infox/epp/processo/action/ProcessoEpaAction.java
+++ src/main/java/br/com/infox/epp/processo/action/ProcessoEpaAction.java
@@ -4,10 +4,7 @@ import org.jboss.seam.ScopeType;
 import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Scope;
-import org.jboss.seam.faces.FacesMessages;
-import org.jboss.seam.log.Log;
-import org.jboss.seam.log.Logging;
-import br.com.infox.core.persistence.DAOException;
+
 import br.com.infox.core.action.AbstractAction;
 import br.com.infox.epp.processo.entity.ProcessoEpa;
 import br.com.infox.epp.tarefa.manager.ProcessoEpaTarefaManager;
@@ -16,21 +13,11 @@ import br.com.infox.epp.tarefa.manager.ProcessoEpaTarefaManager;
 @Scope(ScopeType.CONVERSATION)
 public class ProcessoEpaAction extends AbstractAction<ProcessoEpa> {
 	public static final String NAME = "processoEpaAction";
-	private static final Log LOG = Logging.getLog(ProcessoEpaAction.class);
 	
 	@In private ProcessoEpaTarefaManager processoEpaTarefaManager;
 	
 	public boolean alternarContabilizar(ProcessoEpa processoEpa) {
-		return AbstractAction.UPDATED.equals(update(processoEpa));
-	}
-	
-	public void atualizarFinalizadas() {
-		try {
-			processoEpaTarefaManager.updateTarefasFinalizadas();
-		} catch (DAOException e) {
-			LOG.error(".atualizarFinalizadas()", e);
-			FacesMessages.instance().add(e.getLocalizedMessage());
-		}
+		return UPDATED.equals(update(processoEpa));
 	}
 	
 }
