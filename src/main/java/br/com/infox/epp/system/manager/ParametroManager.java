f11f6fee9a1550aa9f07d0896f58dc6b053084a5
erikliberal <erikliberal@infox.com.br>
2013-12-13 11:29:25 -0300

    Adição de método para salvar novos Parâmetros rapidamente

--- src/main/java/br/com/infox/epp/system/manager/ParametroManager.java
+++ src/main/java/br/com/infox/epp/system/manager/ParametroManager.java
@@ -1,9 +1,13 @@
 package br.com.infox.epp.system.manager;
 
+import java.util.Date;
+
 import org.jboss.seam.annotations.AutoCreate;
 import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
+
 import br.com.infox.core.manager.GenericManager;
+import br.com.infox.core.persistence.DAOException;
 import br.com.infox.epp.system.dao.ParametroDAO;
 import br.com.infox.epp.system.entity.Parametro;
 
@@ -20,4 +24,15 @@ public class ParametroManager extends GenericManager {
 		return parametroDAO.getParametroByNomeVariavel(nome);
 	}
 
+	public void saveSystemParameter(String nomeVariavel,String valorVariavel, String descricaoVariavel) throws DAOException {
+        Parametro p = new Parametro();
+        p.setNomeVariavel(nomeVariavel);
+        p.setValorVariavel(valorVariavel);
+        p.setDescricaoVariavel(descricaoVariavel);
+        p.setDataAtualizacao(new Date());
+        p.setSistema(true);
+        p.setAtivo(true);
+        persist(p);
+    }
+	
 }
