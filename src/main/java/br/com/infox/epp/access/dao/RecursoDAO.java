7320e0c2cce178f1cc04de4010435841af55e3a1
jonasbezerra <jonasbezerra@infox.com.br>
2013-12-13 10:20:45 -0300

    Criação de RecursoDAO e RecursoManager

--- src/main/java/br/com/infox/epp/access/dao/RecursoDAO.java
+++ src/main/java/br/com/infox/epp/access/dao/RecursoDAO.java
@@ -1,11 +1,16 @@
 package br.com.infox.epp.access.dao;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Query;
 
 import org.jboss.seam.annotations.AutoCreate;
 import org.jboss.seam.annotations.Name;
 
 import br.com.infox.core.dao.GenericDAO;
+import br.com.infox.epp.access.entity.Permissao;
+import br.com.infox.epp.access.entity.Recurso;
 
 @Name(RecursoDAO.NAME)
 @AutoCreate
@@ -18,6 +23,28 @@ public class RecursoDAO extends GenericDAO {
         String hql = "select count(o) from Recurso o where o.identificador = :identificador";
         Query query = getEntityManager().createQuery(hql).setParameter("identificador", identificador);
         return ((Long) query.getSingleResult()) > 0;
+    }
+    
+    public List<Recurso> getRecursosFromPermissoes(List<Permissao> permissoes){
+        List<String> identificadores = getListaIdentificadoresFromPermissoes(permissoes);
+        String hql = "select distinct o from Recurso o where o.identificador in (:identificadores)";
+        return getEntityManager().createQuery(hql, Recurso.class)
+                .setParameter("identificadores", identificadores).getResultList();
+    }
+    
+    public List<Recurso> getRecursosWithoutPermissoes(List<Permissao> permissoes){
+        List<String> identificadores = getListaIdentificadoresFromPermissoes(permissoes);
+        String hql = "select distinct o from Recurso o where o.identificador not in (:identificadores)";
+        return getEntityManager().createQuery(hql, Recurso.class)
+                .setParameter("identificadores", identificadores).getResultList();
+    }
+
+    private List<String> getListaIdentificadoresFromPermissoes(List<Permissao> permissoes) {
+        List<String> identificadores = new ArrayList<>();
+        for (Permissao permissao : permissoes) {
+            identificadores.add(permissao.getAlvo());
+        }
+        return identificadores;
     }
 
 }
--- src/main/java/br/com/infox/epp/access/manager/RecursoManager.java
+++ src/main/java/br/com/infox/epp/access/manager/RecursoManager.java
@@ -1,11 +1,15 @@
 package br.com.infox.epp.access.manager;
 
+import java.util.List;
+
 import org.jboss.seam.annotations.AutoCreate;
 import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
 
 import br.com.infox.core.manager.GenericManager;
 import br.com.infox.epp.access.dao.RecursoDAO;
+import br.com.infox.epp.access.entity.Permissao;
+import br.com.infox.epp.access.entity.Recurso;
 
 @Name(RecursoManager.NAME)
 @AutoCreate
@@ -18,6 +22,14 @@ public class RecursoManager extends GenericManager {
     
     public boolean existsRecurso(String identificador){
         return recursoDAO.existsRecurso(identificador);
+    }
+    
+    public List<Recurso> getRecursosFromPermissoes(List<Permissao> permissoes){
+        return recursoDAO.getRecursosFromPermissoes(permissoes);
+    }
+    
+    public List<Recurso> getRecursosWithoutPermissoes(List<Permissao> permissoes){
+        return recursoDAO.getRecursosWithoutPermissoes(permissoes);
     }
 
 }
