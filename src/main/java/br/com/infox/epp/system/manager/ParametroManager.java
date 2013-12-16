0138009ae6c68aa52f5fec9af634f3b4dabcbfeb
erikliberal <erikliberal@infox.com.br>
2013-12-16 10:43:09 -0300

    Remoção de método que existe agora em BamTimerManager

--- src/main/java/br/com/infox/epp/system/manager/ParametroManager.java
+++ src/main/java/br/com/infox/epp/system/manager/ParametroManager.java
@@ -1,13 +1,10 @@
 package br.com.infox.epp.system.manager;
 
-import java.util.Date;
-
 import org.jboss.seam.annotations.AutoCreate;
 import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
 
 import br.com.infox.core.manager.GenericManager;
-import br.com.infox.core.persistence.DAOException;
 import br.com.infox.epp.system.dao.ParametroDAO;
 import br.com.infox.epp.system.entity.Parametro;
 
@@ -23,16 +20,5 @@ public class ParametroManager extends GenericManager {
 	public Parametro getParametro(String nome) {
 		return parametroDAO.getParametroByNomeVariavel(nome);
 	}
-
-	public void saveSystemParameter(String nomeVariavel,String valorVariavel, String descricaoVariavel) throws DAOException {
-        Parametro p = new Parametro();
-        p.setNomeVariavel(nomeVariavel);
-        p.setValorVariavel(valorVariavel);
-        p.setDescricaoVariavel(descricaoVariavel);
-        p.setDataAtualizacao(new Date());
-        p.setSistema(true);
-        p.setAtivo(true);
-        persist(p);
-    }
 	
 }
