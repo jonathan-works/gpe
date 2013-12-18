5357d6103236dd29f4d67a4444864fa6e600275a
erikliberal <erikliberal@infox.com.br>
2013-12-18 10:47:50 -0300

    Adição de novos testes para ClassificacaoDocumentoCrudTest

--- src/test/java/br/com/infox/epp/test/documento/crud/ClassificacaoDocumentoCrudTest.java
+++ src/test/java/br/com/infox/epp/test/documento/crud/ClassificacaoDocumentoCrudTest.java
@@ -1,12 +1,14 @@
 package br.com.infox.epp.test.documento.crud;
 
+import java.util.List;
+
 import junit.framework.Assert;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import br.com.infox.epp.documento.crud.ClassificacaoDocumentoCrudAction;
+import br.com.infox.core.constants.LengthConstants;
 import br.com.infox.epp.documento.entity.TipoProcessoDocumento;
 import br.com.infox.epp.documento.type.TipoDocumentoEnum;
 import br.com.infox.epp.documento.type.TipoNumeracaoEnum;
@@ -14,69 +16,23 @@ import br.com.infox.epp.documento.type.VisibilidadeEnum;
 import br.com.itx.util.EntityUtil;
 
 public class ClassificacaoDocumentoCrudTest {
+    private static int id=1;
+    private MockClassificacaoDocumentoCrud classificacaoDocumentoCrudAction;
 
-    private ClassificacaoDocumentoCrudAction classificacaoDocumentoCrudAction;
-    //private SessionFactory sessionFactory;
-    //private Session session;
-
-//    @SuppressWarnings("deprecation")
-//    private SessionFactory createSessionFactory() {
-//        AnnotationConfiguration configuration = new AnnotationConfiguration();
-//        configuration.addAnnotatedClass(TipoProcessoDocumento.class).addAnnotatedClass(ProcessoDocumento.class).addAnnotatedClass(ProcessoDocumentoBin.class).addAnnotatedClass(Localizacao.class).addAnnotatedClass(Papel.class).addAnnotatedClass(UsuarioLogin.class).addAnnotatedClass(Caixa.class).addAnnotatedClass(ItemTipoDocumento.class).addAnnotatedClass(GrupoModeloDocumento.class).addAnnotatedClass(TipoModeloDocumento.class).addAnnotatedClass(ModeloDocumento.class).addAnnotatedClass(VariavelTipoModelo.class).addAnnotatedClass(Variavel.class).addAnnotatedClass(TarefaJbpm.class).addAnnotatedClass(LocalizacaoTurno.class).addAnnotatedClass(EntityLog.class).addAnnotatedClass(BloqueioUsuario.class).addAnnotatedClass(EntityLogDetail.class).addAnnotatedClass(UsuarioLocalizacao.class).addAnnotatedClass(Tarefa.class).addAnnotatedClass(Fluxo.class).addAnnotatedClass(FluxoPapel.class).addAnnotatedClass(PessoaFisica.class).addAnnotatedClass(Processo.class).addAnnotatedClass(Estatistica.class);
-//        configuration.setProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
-//        configuration.setProperty("hibernate.connection.driver_class", "org.h2.Driver");
-//        configuration.setProperty("hibernate.connection.url", "jdbc:h2:mem");
-//        configuration.setProperty("hibernate.hbm2ddl.auto", "create");
-//        return configuration.buildSessionFactory();
-//    }
+    @After
+    public void afterTest() {
+//        session.close();
+//        sessionFactory.close();
+    }
 
     @Before
     public void beforeTest() {
-//        sessionFactory = createSessionFactory();
-//        session = sessionFactory.openSession();
         classificacaoDocumentoCrudAction = new MockClassificacaoDocumentoCrud();
     }
 
-    private String getFilled(String string, int length) {
-        StringBuilder sb = new StringBuilder();
-        if (string.length() < 1) {
-            string = " ";
-        }
-        for (int i = 0, l = 1 + (length / string.length()); i < l; i++) {
-            sb.append(string);
-        }
-
-        return sb.substring(0, length);
-    }
-
-    @Test
-    public void testSave() throws InstantiationException, IllegalAccessException {
-        int id=1;
-        String codigo = getFilled("codigoDocumento", 15);
-        String descricao = getFilled("descricao", 9);
-        TipoDocumentoEnum tipoDocumento = TipoDocumentoEnum.T;
-        VisibilidadeEnum visibilidade = VisibilidadeEnum.A;
-        Boolean numeravel = Boolean.TRUE;
-        TipoNumeracaoEnum tipoNumeracao = TipoNumeracaoEnum.S;
-        Boolean sistema = Boolean.FALSE;
-        Boolean publico = Boolean.FALSE;
-        Boolean ativo = Boolean.TRUE;
-        String tipoProcessoDocumentoObservacao = getFilled("tipoProcessoDocumentoObservacao", 10);
-        
-        TipoProcessoDocumento entity = createInstance(id++, codigo, descricao, tipoDocumento, visibilidade, numeravel, tipoNumeracao, sistema, publico, ativo, tipoProcessoDocumentoObservacao);
-        TipoProcessoDocumento cloneEntity = EntityUtil.cloneEntity(entity, sistema);
-        cloneEntity.setIdTipoProcessoDocumento(entity.getIdTipoProcessoDocumento());
-        
-        persistEntity(entity);
-        Assert.assertNotNull(classificacaoDocumentoCrudAction.getId());
-        TipoProcessoDocumento currentInstance = classificacaoDocumentoCrudAction.getInstance();
-        Assert.assertNotNull(currentInstance);
-        assertEquals(cloneEntity, currentInstance);
-    }
-    
     @Test
     public void testInactivate() {
-        int entityId = 1;
+        int entityId = id++;
         TipoProcessoDocumento entity = createInstance(entityId, "", "", TipoDocumentoEnum.T, VisibilidadeEnum.A, Boolean.TRUE, TipoNumeracaoEnum.S, Boolean.FALSE, Boolean.TRUE, Boolean.TRUE, "");
         classificacaoDocumentoCrudAction.newInstance();
         persistEntity(entity);
@@ -87,8 +43,65 @@ public class ClassificacaoDocumentoCrudTest {
     }
     
     @Test
+    public void testMultipleInserts() {
+        for (int i = 0; i < 25; i++) {
+            persistEntity(createInstance(id++, i+"", i+"", TipoDocumentoEnum.T, VisibilidadeEnum.A, Boolean.TRUE, TipoNumeracaoEnum.S, Boolean.FALSE, Boolean.TRUE, Boolean.TRUE, i+""));
+            classificacaoDocumentoCrudAction.newInstance();
+        }
+        List<TipoProcessoDocumento> list = classificacaoDocumentoCrudAction.getAll();
+        Assert.assertTrue(list.size() >= 25);
+    }
+
+    @Test
+    public void testRemove() {
+        for (int i = 1; i <= 25; i++) {
+            persistEntity(createInstance(id++, i+"", i+"", TipoDocumentoEnum.T, VisibilidadeEnum.A, Boolean.TRUE, TipoNumeracaoEnum.S, Boolean.FALSE, Boolean.TRUE, Boolean.TRUE, i+""));
+            classificacaoDocumentoCrudAction.newInstance();    
+        }
+        
+        final List<TipoProcessoDocumento> list = classificacaoDocumentoCrudAction.getAll();
+        for (TipoProcessoDocumento tipoProcessoDocumento : list) {
+            assertRemoveTrue(tipoProcessoDocumento);
+        }
+    }
+    
+    @Test
+    public void testRestricoesCodigoDocumento() {
+        assertInsertTrue(createInstance(id++, fillStr("codigoDocumento", LengthConstants.CODIGO_DOCUMENTO), fillStr("descricao", 9), TipoDocumentoEnum.T, VisibilidadeEnum.A, Boolean.TRUE, TipoNumeracaoEnum.S, Boolean.FALSE, Boolean.FALSE, Boolean.TRUE, fillStr("tipoProcessoDocumentoObservacao", 10)));
+        
+        assertInsertTrue(createInstance(id++, fillStr("codigoDocumento", 0), fillStr("descricao", 9), TipoDocumentoEnum.T, VisibilidadeEnum.A, Boolean.TRUE, TipoNumeracaoEnum.S, Boolean.FALSE, Boolean.FALSE, Boolean.TRUE, fillStr("tipoProcessoDocumentoObservacao", 10)));
+        
+        assertInsertFalse(createInstance(id++, fillStr("codigoDocumento", LengthConstants.CODIGO_DOCUMENTO+1), fillStr("descricao", 9), TipoDocumentoEnum.T, VisibilidadeEnum.A, Boolean.TRUE, TipoNumeracaoEnum.S, Boolean.FALSE, Boolean.FALSE, Boolean.TRUE, fillStr("tipoProcessoDocumentoObservacao", 10)));
+    }
+    
+    @Test
+    public void testRestricoesTipoProcessoDocumento() {
+        assertInsertTrue(createInstance(id++, fillStr("codigoDocumento", 12), fillStr("descricao", LengthConstants.DESCRICAO_PADRAO), TipoDocumentoEnum.T, VisibilidadeEnum.A, Boolean.TRUE, TipoNumeracaoEnum.S, Boolean.FALSE, Boolean.FALSE, Boolean.TRUE, fillStr("tipoProcessoDocumentoObservacao", 10)));
+
+        assertInsertTrue(createInstance(id++, fillStr("codigoDocumento", 12), fillStr("descricao", 0), TipoDocumentoEnum.T, VisibilidadeEnum.A, Boolean.TRUE, TipoNumeracaoEnum.S, Boolean.FALSE, Boolean.FALSE, Boolean.TRUE, fillStr("tipoProcessoDocumentoObservacao", 10)));
+
+        assertInsertFalse(createInstance(id++, fillStr("codigoDocumento", 12), fillStr("descricao", LengthConstants.DESCRICAO_PADRAO+1), TipoDocumentoEnum.T, VisibilidadeEnum.A, Boolean.TRUE, TipoNumeracaoEnum.S, Boolean.FALSE, Boolean.FALSE, Boolean.TRUE, fillStr("tipoProcessoDocumentoObservacao", 10)));
+        
+        assertInsertFalse(createInstance(id++, fillStr("codigoDocumento", 12), null, TipoDocumentoEnum.T, VisibilidadeEnum.A, Boolean.TRUE, TipoNumeracaoEnum.S, Boolean.FALSE, Boolean.FALSE, Boolean.TRUE, fillStr("tipoProcessoDocumentoObservacao", 10)));
+    }
+    
+    @Test
+    public void testSave() throws InstantiationException, IllegalAccessException {
+        TipoProcessoDocumento entity = createInstance(id++, fillStr("codigoDocumento", 15), fillStr("descricao", 9), TipoDocumentoEnum.T, VisibilidadeEnum.A, Boolean.TRUE, TipoNumeracaoEnum.S, Boolean.FALSE, Boolean.FALSE, Boolean.TRUE, fillStr("tipoProcessoDocumentoObservacao", 10));
+        TipoProcessoDocumento cloneEntity = EntityUtil.cloneEntity(entity, false);
+        
+        persistEntity(entity);
+        
+        cloneEntity.setIdTipoProcessoDocumento(entity.getIdTipoProcessoDocumento());
+        Assert.assertNotNull(classificacaoDocumentoCrudAction.getId());
+        TipoProcessoDocumento currentInstance = classificacaoDocumentoCrudAction.getInstance();
+        Assert.assertNotNull(currentInstance);
+        assertEquals(cloneEntity, currentInstance);
+    }
+    
+    @Test
     public void testUpdate() {
-        int entityId = 1;
+        int entityId = id++;
         TipoProcessoDocumento entity = createInstance(entityId, "", "", TipoDocumentoEnum.T, VisibilidadeEnum.A, Boolean.TRUE, TipoNumeracaoEnum.S, Boolean.FALSE, Boolean.TRUE, Boolean.TRUE, "");
         classificacaoDocumentoCrudAction.newInstance();
         persistEntity(entity);
@@ -102,43 +115,55 @@ public class ClassificacaoDocumentoCrudTest {
         Assert.assertEquals("novoCodigo", entity.getCodigoDocumento());
     }
 
-    private void persistEntity(TipoProcessoDocumento entity) {
-        Assert.assertFalse(classificacaoDocumentoCrudAction.isManaged());
-        classificacaoDocumentoCrudAction.setInstance(entity);
-        classificacaoDocumentoCrudAction.save();
-        Assert.assertTrue(classificacaoDocumentoCrudAction.isManaged());
-    }
-    
-    @Test
-    public void testRemove() {
-        int id=1;
-        for (int i = 1; i <= 25; i++) {
-            persistEntity(createInstance(id++, i+"", i+"", TipoDocumentoEnum.T, VisibilidadeEnum.A, Boolean.TRUE, TipoNumeracaoEnum.S, Boolean.FALSE, Boolean.TRUE, Boolean.TRUE, i+""));
-            classificacaoDocumentoCrudAction.newInstance();    
+    private void assertEquals(TipoProcessoDocumento expected,
+            TipoProcessoDocumento actual) {
+        Assert.assertEquals(expected.getIdTipoProcessoDocumento(), actual.getIdTipoProcessoDocumento());
+        Assert.assertEquals(expected.getCodigoDocumento(), actual.getCodigoDocumento());
+        Assert.assertEquals(expected.getTipoProcessoDocumento(), actual.getTipoProcessoDocumento());
+        Assert.assertEquals(expected.getInTipoDocumento(), actual.getInTipoDocumento());
+        Assert.assertEquals(expected.getVisibilidade(), actual.getVisibilidade());
+        Assert.assertEquals(expected.getNumera(), actual.getNumera());
+        
+        if (expected.getNumera()) {
+            Assert.assertEquals(expected.getTipoNumeracao(), actual.getTipoNumeracao());   
+        } else {
+            Assert.assertNull(actual.getTipoNumeracao());
         }
         
-        classificacaoDocumentoCrudAction.setId(3);
-        TipoProcessoDocumento tipoProcessoDocumento = classificacaoDocumentoCrudAction.getInstance();
-        classificacaoDocumentoCrudAction.remove();
-        classificacaoDocumentoCrudAction.newInstance();
-
-        persistEntity(tipoProcessoDocumento);
+        Assert.assertEquals(expected.getSistema(), actual.getSistema());
+        Assert.assertEquals(expected.getPublico(), actual.getPublico());
+        Assert.assertEquals(expected.getAtivo(), actual.getAtivo());
+        Assert.assertEquals(expected.getTipoProcessoDocumentoObservacao(), actual.getTipoProcessoDocumentoObservacao());
     }
     
-    @Test
-    public void testMultipleInserts() {
-        int id=1;
-        for (int i = 0; i < 25; i++) {
-            persistEntity(createInstance(id++, i+"", i+"", TipoDocumentoEnum.T, VisibilidadeEnum.A, Boolean.TRUE, TipoNumeracaoEnum.S, Boolean.FALSE, Boolean.TRUE, Boolean.TRUE, i+""));
-            classificacaoDocumentoCrudAction.newInstance();    
-        }
-        TipoProcessoDocumento anterior = null;
-        for (int i = 1; i < id; i++) {
-            classificacaoDocumentoCrudAction.setId(i);
-            Assert.assertNotNull(classificacaoDocumentoCrudAction.getInstance());
-            Assert.assertFalse(classificacaoDocumentoCrudAction.getInstance().equals(anterior));
-            anterior = classificacaoDocumentoCrudAction.getInstance();
-        }
+    private void assertRemoveTrue(TipoProcessoDocumento entity) {
+        int size = classificacaoDocumentoCrudAction.getAll().size();
+        classificacaoDocumentoCrudAction.remove(entity);
+        int afterSize = classificacaoDocumentoCrudAction.getAll().size();
+        Assert.assertTrue(afterSize < size);
+    }
+    
+    private void assertRemoveFalse(TipoProcessoDocumento entity) {
+        int size = classificacaoDocumentoCrudAction.getAll().size();
+        classificacaoDocumentoCrudAction.remove(entity);
+        int afterSize = classificacaoDocumentoCrudAction.getAll().size();
+        Assert.assertTrue(afterSize == size);
+    }
+    
+    private void assertInsertFalse(TipoProcessoDocumento entity) {
+        int size = classificacaoDocumentoCrudAction.getAll().size();
+        persistEntity(entity);
+        classificacaoDocumentoCrudAction.newInstance();
+        int afterSize = classificacaoDocumentoCrudAction.getAll().size();
+        Assert.assertTrue(size==afterSize);
+    }
+    
+    private void assertInsertTrue(TipoProcessoDocumento entity) {
+        int size = classificacaoDocumentoCrudAction.getAll().size();
+        persistEntity(entity);
+        classificacaoDocumentoCrudAction.newInstance();
+        int afterSize = classificacaoDocumentoCrudAction.getAll().size();
+        Assert.assertTrue(size<afterSize);
     }
 
     private TipoProcessoDocumento createInstance(int idTipoProcessoDocumento,
@@ -162,31 +187,26 @@ public class ClassificacaoDocumentoCrudTest {
         return instance;
     }
     
-    private void assertEquals(TipoProcessoDocumento expected,
-            TipoProcessoDocumento actual) {
-        Assert.assertEquals(expected.getIdTipoProcessoDocumento(), actual.getIdTipoProcessoDocumento());
-        Assert.assertEquals(expected.getCodigoDocumento(), actual.getCodigoDocumento());
-        Assert.assertEquals(expected.getTipoProcessoDocumento(), actual.getTipoProcessoDocumento());
-        Assert.assertEquals(expected.getInTipoDocumento(), actual.getInTipoDocumento());
-        Assert.assertEquals(expected.getVisibilidade(), actual.getVisibilidade());
-        Assert.assertEquals(expected.getNumera(), actual.getNumera());
-        
-        if (expected.getNumera()) {
-            Assert.assertEquals(expected.getTipoNumeracao(), actual.getTipoNumeracao());   
-        } else {
-            Assert.assertNull(actual.getTipoNumeracao());
+    private String fillStr(String string, int topLength) {
+        if (string == null || string.length() < 1) {
+            string = "-";
         }
         
-        Assert.assertEquals(expected.getSistema(), actual.getSistema());
-        Assert.assertEquals(expected.getPublico(), actual.getPublico());
-        Assert.assertEquals(expected.getAtivo(), actual.getAtivo());
-        Assert.assertEquals(expected.getTipoProcessoDocumentoObservacao(), actual.getTipoProcessoDocumentoObservacao());
+        StringBuilder sb = new StringBuilder(string);
+        int length = string.length();
+        if (length < topLength) {
+            for (int i = 0, l = topLength-length; i < l; i++) {
+                sb.append(" ");
+            }
+        }
+        return sb.substring(0, topLength);
     }
 
-    @After
-    public void afterTest() {
-//        session.close();
-//        sessionFactory.close();
+    private void persistEntity(TipoProcessoDocumento entity) {
+        Assert.assertFalse(classificacaoDocumentoCrudAction.isManaged());
+        classificacaoDocumentoCrudAction.setInstance(entity);
+        classificacaoDocumentoCrudAction.save();
+        Assert.assertTrue(classificacaoDocumentoCrudAction.isManaged());
     }
 
 }
