02053ee1dfcb3c87718c0e29769d3b7ff0579fde
jonasbezerra <jonasbezerra@infox.com.br>
2013-12-18 15:10:45 -0300

    Utilizando getNamedSingleResult do GenericDAO

--- src/main/java/br/com/infox/epp/access/dao/UsuarioLoginDAO.java
+++ src/main/java/br/com/infox/epp/access/dao/UsuarioLoginDAO.java
@@ -10,14 +10,11 @@ import static br.com.infox.epp.access.query.UsuarioLoginQuery.USUARIO_LOGIN_NAME
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.persistence.Query;
-
 import org.jboss.seam.annotations.AutoCreate;
 import org.jboss.seam.annotations.Name;
 
 import br.com.infox.core.dao.GenericDAO;
 import br.com.infox.epp.access.entity.UsuarioLogin;
-import br.com.itx.util.EntityUtil;
 
 @Name(UsuarioLoginDAO.NAME)
 @AutoCreate
@@ -27,9 +24,9 @@ public class UsuarioLoginDAO extends GenericDAO {
 	public static final String NAME = "usuarioLoginDAO";
 	
 	public UsuarioLogin getUsuarioLoginByEmail(String email){
-	    Query query  = getEntityManager().createNamedQuery(USUARIO_BY_EMAIL);
-	    query.setParameter(PARAM_EMAIL, email);
-		return EntityUtil.getSingleResult(query);
+	    Map<String, Object> parameters = new HashMap<>();
+	    parameters.put(PARAM_EMAIL, email);
+	    return getNamedSingleResult(USUARIO_BY_EMAIL, parameters);
 	}
 	
 	public UsuarioLogin getUsuarioByLoginTaskInstance(Long idTaskInstance, String actorId) {
