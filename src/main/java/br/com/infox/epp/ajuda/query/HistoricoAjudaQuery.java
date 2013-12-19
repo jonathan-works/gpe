96cf65c0de555a646d2b4f6089c0c5d5313fdeb1
jonasbezerra <jonasbezerra@infox.com.br>
2013-12-19 14:37:08 -0300

    Criação de HistoricoAjudaQuery

---
+++ src/main/java/br/com/infox/epp/ajuda/query/HistoricoAjudaQuery.java
@@ -0,0 +1,13 @@
+package br.com.infox.epp.ajuda.query;
+
+public interface HistoricoAjudaQuery {
+    
+    String TABLE_HISTORICO_AJUDA  = "tb_historico_ajuda";
+    String SEQUENCE_HISTORICO_AJUDA = "public.sq_tb_historico_ajuda";
+    String ID_HISTORICO_AJUDA = "id_historico_ajuda";
+    String DATA_REGISTRO = "dt_registro";
+    String TEXTO = "ds_texto";
+    String PAGINA = "id_pagina";
+    String USUARIO = "id_usuario";
+
+}
