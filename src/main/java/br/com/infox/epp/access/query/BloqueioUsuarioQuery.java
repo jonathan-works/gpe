5bbc5072b6fcc06e6819c87e8aec65e85cb9cfd0
jonasbezerra <jonasbezerra@infox.com.br>
2013-12-19 08:50:03 -0300

    Criação de BloqueioUsuarioLoginQuery

---
+++ src/main/java/br/com/infox/epp/access/query/BloqueioUsuarioQuery.java
@@ -0,0 +1,21 @@
+package br.com.infox.epp.access.query;
+
+import java.io.Serializable;
+
+public interface BloqueioUsuarioQuery extends Serializable {
+    
+    String TABLE_BLOQUEIO_USUARIO = "tb_bloqueio_usuario";
+    String SEQUENCE_BLOQUEIO_USUARIO = "public.sq_tb_bloqueio_usuario";
+    String ID_BLOQUEIO_USUARIO = "id_bloqueio_usuario";
+    String ID_USUARIO = "id_usuario";
+    String DATA_BLOQUEIO = "dt_bloqueio";
+    String DATA_PREVISAO_DESBLOQUEIO = "dt_previsao_desbloqueio";
+    String MOTIVO_BLOQUEIO = "ds_motivo_bloqueio";
+    String DATA_DESBLOQUEIO = "dt_desbloqueio";
+    
+    String PARAM_USUARIO = "usuario";
+    String BLOQUEIO_MAIS_RECENTE = "bloqueioMaisRecente";
+    String BLOQUEIO_MAIS_RECENTE_QUERY = "select o from BloqueioUsuario o where o.idBloqueioUsuario = " +
+            "(select max(b.idBloqueioUsuario) from BloqueioUsuario b where b.usuario = :" + PARAM_USUARIO + ")";
+
+}
