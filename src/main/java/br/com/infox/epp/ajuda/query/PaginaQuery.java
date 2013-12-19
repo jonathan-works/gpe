a8e508805e60efd5ad8994da9f6e50e8a310dca9
jonasbezerra <jonasbezerra@infox.com.br>
2013-12-19 14:47:35 -0300

    Criação de PaginaQuery

---
+++ src/main/java/br/com/infox/epp/ajuda/query/PaginaQuery.java
@@ -0,0 +1,11 @@
+package br.com.infox.epp.ajuda.query;
+
+public interface PaginaQuery {
+    
+    String TABLE_PAGINA = "tb_pagina";
+    String SEQUENCE_PAGINA = "public.sq_tb_pagina";
+    String ID_PAGINA = "id_pagina";
+    String DESCRICAO = "ds_descricao";
+    String URL = "ds_url";
+
+}
