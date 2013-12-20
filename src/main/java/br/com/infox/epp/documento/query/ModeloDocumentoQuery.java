10761e469e59a4b4b985298d66631bd0673809ae
jonasbezerra <jonasbezerra@infox.com.br>
2013-12-20 10:14:34 -0300

    Utilizando contantes de mapeamento OR definidas em ModeloDOcumentoQUery

--- src/main/java/br/com/infox/epp/documento/query/ModeloDocumentoQuery.java
+++ src/main/java/br/com/infox/epp/documento/query/ModeloDocumentoQuery.java
@@ -2,12 +2,20 @@ package br.com.infox.epp.documento.query;
 
 /**
  * Interface com as queries da entidade ModeloDocumento
+ * 
  * @author erikliberal
  */
 public interface ModeloDocumentoQuery {
 
-	String LIST_ATIVOS = "listModeloDocumentoAtivo";
-	String LIST_ATIVOS_QUERY = "select o from ModeloDocumento o " +
-								"where o.ativo = true";
-	
+    String TABLE_MODELO_DOCUMENTO = "tb_modelo_documento";
+    String SEQUENCE_MODELO_DOCUMENTO = "public.sq_tb_modelo_documento";
+    String ID_MODELO_DOCUMENTO = "id_modelo_documento";
+    String ID_TIPO_MODELO_DOCUMENTO = "id_tipo_modelo_documento";
+    String TITULO_MODELO_DOCUMENTO = "ds_titulo_modelo_documento";
+    String CONTEUDO_MODELO_DOCUMENTO = "ds_modelo_documento";
+
+    String LIST_ATIVOS = "listModeloDocumentoAtivo";
+    String LIST_ATIVOS_QUERY = "select o from ModeloDocumento o "
+            + "where o.ativo = true";
+
 }
