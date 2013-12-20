4582db85f162f97e26cc6eee2c872ceee4506ffd
erikliberal <erikliberal@infox.com.br>
2013-12-20 08:43:18 -0300

    Atualização do Mock de classificação de documento para que utilize a nova interface em conjunção do AbstractCrudAction padrão

--- src/test/java/br/com/infox/epp/test/documento/crud/MockClassificacaoDocumentoCrud.java
+++ src/test/java/br/com/infox/epp/test/documento/crud/MockClassificacaoDocumentoCrud.java
@@ -2,14 +2,25 @@ package br.com.infox.epp.test.documento.crud;
 
 import java.util.List;
 
+import br.com.infox.epp.documento.crud.ClassificacaoDocumentoCrudAction;
 import br.com.infox.epp.documento.entity.TipoProcessoDocumento;
-import br.com.infox.epp.test.crud.AbstractMockCrudAction;
+import br.com.infox.epp.test.core.messages.MockMessagesHandler;
+import br.com.infox.epp.test.crud.MockCrudAction;
+import br.com.infox.epp.test.infra.MockGenericManager;
 
-public class MockClassificacaoDocumentoCrud extends AbstractMockCrudAction<TipoProcessoDocumento> {
+public class MockClassificacaoDocumentoCrud extends ClassificacaoDocumentoCrudAction implements MockCrudAction<TipoProcessoDocumento> {
     
-    @Override
+    public MockClassificacaoDocumentoCrud() {
+        super();
+        setGenericManager(new MockGenericManager());
+    }
+    
     public List<TipoProcessoDocumento> getAll() {
         return getGenericManager().findAll(TipoProcessoDocumento.class);
     }
     
+    @Override
+    protected final MockMessagesHandler getMessagesHandler() {
+        return MockMessagesHandler.instance();
+    }
 }
