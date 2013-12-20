8c1308deceae2bcb5894fdf04d39bf4a11f3a20b
erikliberal <erikliberal@infox.com.br>
2013-12-20 08:05:21 -0300

    Atualização do Mock de ClassificacaoDocumento para o novo teste de CRUD

--- src/test/java/br/com/infox/epp/test/documento/crud/MockClassificacaoDocumentoCrud.java
+++ src/test/java/br/com/infox/epp/test/documento/crud/MockClassificacaoDocumentoCrud.java
@@ -2,25 +2,14 @@ package br.com.infox.epp.test.documento.crud;
 
 import java.util.List;
 
-import br.com.infox.epp.documento.crud.ClassificacaoDocumentoCrudAction;
 import br.com.infox.epp.documento.entity.TipoProcessoDocumento;
-import br.com.infox.epp.test.core.messages.MockMessagesHandler;
-import br.com.infox.epp.test.infra.MockGenericManager;
+import br.com.infox.epp.test.crud.AbstractMockCrudAction;
 
-public class MockClassificacaoDocumentoCrud extends ClassificacaoDocumentoCrudAction {
-
-    public MockClassificacaoDocumentoCrud() {
-        super();
-        setGenericManager(new MockGenericManager());
-    }
-    
-    public List<TipoProcessoDocumento> getAll() {
-        return getGenericManager().findAll(TipoProcessoDocumento.class);
-    }
+public class MockClassificacaoDocumentoCrud extends AbstractMockCrudAction<TipoProcessoDocumento> {
     
     @Override
-    protected MockMessagesHandler getMessagesHandler() {
-        return MockMessagesHandler.instance();
+    public List<TipoProcessoDocumento> getAll() {
+        return getGenericManager().findAll(TipoProcessoDocumento.class);
     }
     
 }
