4a33b4ff612af2c68f1a317666dfe711d957db48
jonasbezerra <jonasbezerra@infox.com.br>
2013-12-20 08:02:35 -0300

    Definindo atributos de mapeamento OR e nomes para as NamedQuery

--- src/main/java/br/com/infox/epp/documento/query/HistoricoModeloDocumentoQuery.java
+++ src/main/java/br/com/infox/epp/documento/query/HistoricoModeloDocumentoQuery.java
@@ -1,9 +1,22 @@
 package br.com.infox.epp.documento.query;
 
 public interface HistoricoModeloDocumentoQuery {
-	
-	String LIST_MODELO_QUERY = "select distinct o.modeloDocumento from HistoricoModeloDocumento o";
-	String LIST_USUARIO_PARAM_MODELO = "modeloDocumento";
-	String LIST_USUARIO_QUERY = "select distinct o.usuarioAlteracao from HistoricoModeloDocumento o where o.modeloDocumento=:"+LIST_USUARIO_PARAM_MODELO;
+
+    String TABLE_HISTORICO_MODELO_DOCUMENTO = "tb_modelo_documento_historico";
+    String SEQUENCE_HISTORICO_MODELO_DOCUMENTO = "sq_tb_modelo_documento_historico";
+    String ID_HISTORICO_MODELO_DOCUMENTO = "id_modelo_documento_historico";
+    String ID_MODELO_DOCUMENTO = "id_modelo_documento";
+    String ID_USUARIO_ALTERACAO = "id_usuario_alteracao";
+    String DATA_ALTERACAO = "dt_alteracao";
+    String TITULO_MODELO_DOCUMENTO = "ds_titulo_modelo_documento";
+    String CONTEUDO_MODELO_DOCUMENTO = "ds_modelo_documento";
+    
+    String LIST_MODELO = "listModeloDocumento";
+    String LIST_MODELO_QUERY = "select distinct o.modeloDocumento from HistoricoModeloDocumento o";
+    
+    String LIST_USUARIO = "listUsarioAlteracao";
+    String LIST_USUARIO_PARAM_MODELO = "modeloDocumento";
+    String LIST_USUARIO_QUERY = "select distinct o.usuarioAlteracao from HistoricoModeloDocumento o where o.modeloDocumento=:"
+            + LIST_USUARIO_PARAM_MODELO;
 
 }
