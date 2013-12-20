752cc68357bcfb81ac3984903a1fe6ab9bcacfb5
jonasbezerra <jonasbezerra@infox.com.br>
2013-12-20 09:50:07 -0300

    Criação de ItempoTipoDocumentoQuery

---
+++ src/main/java/br/com/infox/epp/documento/query/ItemTipoDocumentoQuery.java
@@ -0,0 +1,11 @@
+package br.com.infox.epp.documento.query;
+
+public interface ItemTipoDocumentoQuery {
+    
+    String TABLE_ITEM_TIPO_DOCUMENTO = "tb_item_tipo_documento";
+    String SEQUENCE_ITEM_TIPO_DOCUMENTO = "public.sq_tb_item_tipo_documento";
+    String ID_ITEM_TIPO_DOCUMENTO = "id_item_tipo_documento";
+    String ID_LOCALIZACAO = "id_localizacao";
+    String ID_GRUPO_MODELO_DOCUMENTO = "id_grupo_modelo_documento";
+
+}
