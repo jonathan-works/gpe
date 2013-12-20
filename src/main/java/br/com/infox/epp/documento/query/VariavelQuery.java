b4850a400da94b30b16a8ed2af2990f2ad0e2d1b
jonasbezerra <jonasbezerra@infox.com.br>
2013-12-20 15:31:50 -0300

    Criação de VariavelQuery

---
+++ src/main/java/br/com/infox/epp/documento/query/VariavelQuery.java
@@ -0,0 +1,12 @@
+package br.com.infox.epp.documento.query;
+
+public interface VariavelQuery {
+    
+    String TABLE_VARIAVEL = "tb_variavel";
+    String SEQUENCE_VARIAVEL = "public.sq_tb_variavel";
+    String ID_VARIAVEL = "id_variavel";
+    String DESCRICAO_VARIAVEL = "ds_variavel";
+    String VALOR_VARIAVEL = "vl_variavel";
+    String VARIAVEL_ATTRIBUTE = "variavel";
+
+}
