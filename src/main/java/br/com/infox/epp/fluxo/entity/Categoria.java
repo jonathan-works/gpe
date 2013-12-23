f4c4618e8195edb999d15c8cb759c7ca51206e5e
jonasbezerra <jonasbezerra@infox.com.br>
2013-12-23 09:20:43 -0300

    Criadas contantes pra mapeamento OR de Categoria

--- src/main/java/br/com/infox/epp/fluxo/entity/Categoria.java
+++ src/main/java/br/com/infox/epp/fluxo/entity/Categoria.java
@@ -1,5 +1,7 @@
 package br.com.infox.epp.fluxo.entity;
 
+import static br.com.infox.core.persistence.ORConstants.*;
+import static br.com.infox.epp.fluxo.query.CategoriaQuery.*;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
@@ -19,19 +21,15 @@ import javax.validation.constraints.Size;
 import javax.validation.constraints.NotNull;
 
 import br.com.infox.core.constants.LengthConstants;
-import br.com.infox.epp.fluxo.query.CategoriaQuery;
 import br.com.itx.util.HibernateUtil;
 
 @Entity
-@Table(name=Categoria.TABLE_NAME, schema="public")
+@Table(name=TABLE_CATEGORIA, schema=PUBLIC)
 @NamedQueries(value={
-				@NamedQuery(name=CategoriaQuery.LIST_PROCESSO_EPP_BY_CATEGORIA,
-						    query=CategoriaQuery.LIST_PROCESSO_EPA_BY_CATEGORIA_QUERY)
-			  })
+    @NamedQuery(name=LIST_PROCESSO_EPP_BY_CATEGORIA, query=LIST_PROCESSO_EPA_BY_CATEGORIA_QUERY)
+})
 public class Categoria implements Serializable{
 	private static final long serialVersionUID = 1L;
-
-	public static final String TABLE_NAME = "tb_categoria";
 
 	private int idCategoria;
 	private String categoria;
@@ -40,10 +38,10 @@ public class Categoria implements Serializable{
 	private List<NaturezaCategoriaFluxo> naturezaCategoriaFluxoList = new ArrayList<NaturezaCategoriaFluxo>(0);
 	private List<CategoriaItem> categoriaItemList = new ArrayList<CategoriaItem>();
 	
-	@SequenceGenerator(name = "generator", sequenceName = "public.sq_tb_categoria")
+	@SequenceGenerator(name = GENERATOR, sequenceName = SEQUENCE_CATEGORIA)
 	@Id
-	@GeneratedValue(generator = "generator")
-	@Column(name = "id_categoria", unique = true, nullable = false)
+	@GeneratedValue(generator = GENERATOR)
+	@Column(name = ID_CATEGORIA, unique = true, nullable = false)
 	public int getIdCategoria() {
 		return idCategoria;
 	}
@@ -51,7 +49,7 @@ public class Categoria implements Serializable{
 		this.idCategoria = idCategoria;
 	}
 	
-	@Column(name="ds_categoria", length=LengthConstants.DESCRICAO_PEQUENA, nullable=false)
+	@Column(name=DESCRICAO_CATEGORIA, length=LengthConstants.DESCRICAO_PEQUENA, nullable=false)
 	@Size(max=LengthConstants.DESCRICAO_PEQUENA)
 	@NotNull
 	public String getCategoria() {
@@ -62,7 +60,7 @@ public class Categoria implements Serializable{
 		this.categoria = categoria;
 	}
 	
-	@Column(name="in_ativo", nullable=false)
+	@Column(name=ATIVO, nullable=false)
 	@NotNull
 	public Boolean getAtivo() {
 		return ativo;
@@ -77,7 +75,7 @@ public class Categoria implements Serializable{
 		return categoria;
 	}
 
-	@OneToMany(fetch = FetchType.EAGER, mappedBy = "categoria", cascade = CascadeType.ALL)
+	@OneToMany(fetch = FetchType.EAGER, mappedBy = CATEGORIA_ATTRIBUTE, cascade = CascadeType.ALL)
 	public List<CategoriaItem> getCategoriaItemList() {
 		return categoriaItemList;
 	}
@@ -90,7 +88,7 @@ public class Categoria implements Serializable{
 		this.naturezaCategoriaFluxoList = naturezaCategoriaFluxoList;
 	}
 	
-	@OneToMany(fetch=FetchType.LAZY, mappedBy="categoria")
+	@OneToMany(fetch=FetchType.LAZY, mappedBy=CATEGORIA_ATTRIBUTE)
 	public List<NaturezaCategoriaFluxo> getNaturezaCategoriaFluxoList() {
 		return naturezaCategoriaFluxoList;
 	}
