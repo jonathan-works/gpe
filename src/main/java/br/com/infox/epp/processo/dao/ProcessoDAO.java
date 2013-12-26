f3b4ed8804b8967781a74eab2d5c1868b8680a62
jonasbezerra <jonasbezerra@infox.com.br>
2013-12-26 14:50:51 -0300

    Utilizando NamedNativeQuery definida em ProcessoQuery

--- src/main/java/br/com/infox/epp/processo/dao/ProcessoDAO.java
+++ src/main/java/br/com/infox/epp/processo/dao/ProcessoDAO.java
@@ -5,7 +5,7 @@ import static br.com.infox.epp.processo.query.ProcessoQuery.APAGA_ACTOR_ID_DO_PR
 import static br.com.infox.epp.processo.query.ProcessoQuery.LIST_PROCESSOS_BY_ID_PROCESSO_AND_ACTOR_ID;
 import static br.com.infox.epp.processo.query.ProcessoQuery.PARAM_ACTOR_ID;
 import static br.com.infox.epp.processo.query.ProcessoQuery.PARAM_ID_PROCESSO;
-import static br.com.infox.epp.processo.query.ProcessoQuery.REMOVE_PROCESSO_DA_CAIXA_ATUAL;
+import static br.com.infox.epp.processo.query.ProcessoQuery.*;
 
 import java.util.HashMap;
 import java.util.List;
@@ -78,36 +78,8 @@ public class ProcessoDAO extends GenericDAO {
         return getNamedResultList(LIST_PROCESSOS_BY_ID_PROCESSO_AND_ACTOR_ID, parameters);
 	}
 	
-	public void atualizarProcessos(){
-		String sql = "update jbpm_processinstance pi set processdefinition_ = " +
-				"(select max(id_) from jbpm_processdefinition pd " +
-				"where name_ = (select name_ from jbpm_processdefinition " +
-				"where id_ = pi.processdefinition_));\n" +
-
-				"update jbpm_token t set node_ = "+
-				"(select max(n.id_) from jbpm_node n "+
-				"inner join jbpm_processdefinition pd on pd.id_ = n.processdefinition_ "+
-				"where n.name_ = (select name_ from jbpm_node where id_ = t.node_) "+
-				"and pd.name_ = (select procdef.name_ from jbpm_processinstance procinst "+
-				"inner join jbpm_processdefinition procdef on procdef.id_ = procinst.processdefinition_ "+
-				"where procinst.id_ = t.processinstance_) "+
-				"and n.class_ = (select class_ from jbpm_node where id_ = t.node_));\n" +
-				
-				"update jbpm_taskinstance ti set task_ = "+ 
-				"(select max(t.id_) from jbpm_task t "+
-				"inner join jbpm_processdefinition pd on pd.id_ = t.processdefinition_ "+
-				"where t.name_ = (select name_ from jbpm_task where id_ = ti.task_) and "+
-				"pd.name_ = (select procdef.name_ from jbpm_processinstance procinst "+
-				"inner join jbpm_processdefinition procdef on procdef.id_ = procinst.processdefinition_ "+
-				"where procinst.id_ = ti.procinst_)) "+
-				"where end_ is null;\n" +
-				
-				"update public.tb_processo_localizacao_ibpm pl set id_task_jbpm =" +
-				"(select max(id_) from jbpm_task t where exists" +
-				"(select * from jbpm_task where name_ = t.name_ " +
-				"and id_ = pl.id_task_jbpm))";
-		
-		JbpmUtil.getJbpmSession().createSQLQuery(sql).executeUpdate();
-	}
+    public void atualizarProcessos() {
+        JbpmUtil.getJbpmSession().createSQLQuery(ATUALIZAR_PROCESSOS_QUERY).executeUpdate();
+    }
 
 }
