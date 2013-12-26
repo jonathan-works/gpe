a583f62e0d9af9ccbae3d9c6a6a143f4160a96eb
jonasbezerra <jonasbezerra@infox.com.br>
2013-12-26 15:59:01 -0300

    Formatação

--- src/main/java/br/com/infox/epp/processo/dao/ProcessoEpaDAO.java
+++ src/main/java/br/com/infox/epp/processo/dao/ProcessoEpaDAO.java
@@ -31,16 +31,17 @@ import br.com.itx.util.HibernateUtil;
 
 /**
  * Classe DAO para a entidade ProcessoEpa
+ * 
  * @author Daniel
- *
+ * 
  */
 @Name(ProcessoEpaDAO.NAME)
 @AutoCreate
 public class ProcessoEpaDAO extends GenericDAO {
 
-	private static final long serialVersionUID = 8899227886410190168L;
-	private static final LogProvider LOG = Logging.getLogProvider(ProcessoEpaDAO.class);
-	public static final String NAME = "processoEpaDAO";
+    private static final long serialVersionUID = 8899227886410190168L;
+    private static final LogProvider LOG = Logging.getLogProvider(ProcessoEpaDAO.class);
+    public static final String NAME = "processoEpaDAO";
 
     public List<ProcessoEpa> listAllNotEnded() {
         return getNamedResultList(LIST_ALL_NOT_ENDED);
@@ -51,55 +52,54 @@ public class ProcessoEpaDAO extends GenericDAO {
         parameters.put(PARAM_FLUXO, fluxo);
         return getNamedResultList(LIST_NOT_ENDED_BY_FLUXO, parameters);
     }
-	
-	public ProcessoEpa getProcessoEpaByProcesso(Processo processo){
-		return find(ProcessoEpa.class, processo.getIdProcesso());
-	}
-	
+
+    public ProcessoEpa getProcessoEpaByProcesso(Processo processo) {
+        return find(ProcessoEpa.class, processo.getIdProcesso());
+    }
+
     private ProcessoEpa getProcessoEpaByIdJbpm(Long idJbpm) {
         Map<String, Object> parameters = new HashMap<>();
         parameters.put(PARAM_ID_JBPM, idJbpm);
         return getNamedSingleResult(PROCESSO_EPA_BY_ID_JBPM, parameters);
     }
-	
-	public List<PessoaFisica> getPessoaFisicaList(){
-		ProcessoEpa pe = getProcessoEpaByIdJbpm(ProcessInstance.instance().getId());
-		List<PessoaFisica> pessoaFisicaList = new ArrayList<PessoaFisica>();
-		for (ParteProcesso parte : pe.getPartes()){
-			if (parte.getPessoa().getTipoPessoa().equals(TipoPessoaEnum.F)){
-				pessoaFisicaList.add((PessoaFisica) HibernateUtil.removeProxy(parte.getPessoa()));
-			}
-		}
-		return pessoaFisicaList;
-	}
-	
-	public List<PessoaJuridica> getPessoaJuridicaList(){
-	    ProcessoEpa pe = getProcessoEpaByIdJbpm(ProcessInstance.instance().getId());
-		List<PessoaJuridica> pessoaJuridicaList = new ArrayList<PessoaJuridica>();
-		for (ParteProcesso parte : pe.getPartes()){
-			if (parte.getPessoa().getTipoPessoa().equals(TipoPessoaEnum.J)){
-				pessoaJuridicaList.add((PessoaJuridica) HibernateUtil.removeProxy(parte.getPessoa()));
-			}
-		}
-		return pessoaJuridicaList;
-	}
-	
-	public boolean hasPartes(Processo processo){
-		return hasPartes(processo.getIdJbpm());
-	}
-	
-	public boolean hasPartes(Long idJbpm){
-	    ProcessoEpa pe = getProcessoEpaByIdJbpm(ProcessInstance.instance().getId());
-		return (pe != null) && (pe.hasPartes());
-	}
-	
-	
-	/**
-	 * Quando um processo necessita de partes, não é permitido inativar todas
-	 * as partes do processo de uma vez.
-	 * Esse método retorna falso (não há permissão de inativar) se o processo
-	 * possuir uma única parte ativa no momento.
-	 * */
+
+    public List<PessoaFisica> getPessoaFisicaList() {
+        ProcessoEpa pe = getProcessoEpaByIdJbpm(ProcessInstance.instance().getId());
+        List<PessoaFisica> pessoaFisicaList = new ArrayList<PessoaFisica>();
+        for (ParteProcesso parte : pe.getPartes()) {
+            if (parte.getPessoa().getTipoPessoa().equals(TipoPessoaEnum.F)) {
+                pessoaFisicaList.add((PessoaFisica) HibernateUtil.removeProxy(parte.getPessoa()));
+            }
+        }
+        return pessoaFisicaList;
+    }
+
+    public List<PessoaJuridica> getPessoaJuridicaList() {
+        ProcessoEpa pe = getProcessoEpaByIdJbpm(ProcessInstance.instance().getId());
+        List<PessoaJuridica> pessoaJuridicaList = new ArrayList<PessoaJuridica>();
+        for (ParteProcesso parte : pe.getPartes()) {
+            if (parte.getPessoa().getTipoPessoa().equals(TipoPessoaEnum.J)) {
+                pessoaJuridicaList.add((PessoaJuridica) HibernateUtil.removeProxy(parte.getPessoa()));
+            }
+        }
+        return pessoaJuridicaList;
+    }
+
+    public boolean hasPartes(Processo processo) {
+        return hasPartes(processo.getIdJbpm());
+    }
+
+    public boolean hasPartes(Long idJbpm) {
+        ProcessoEpa pe = getProcessoEpaByIdJbpm(ProcessInstance.instance().getId());
+        return (pe != null) && (pe.hasPartes());
+    }
+
+    /**
+     * Quando um processo necessita de partes, não é permitido inativar todas as
+     * partes do processo de uma vez. Esse método retorna falso (não há
+     * permissão de inativar) se o processo possuir uma única parte ativa no
+     * momento.
+     * */
     public Boolean podeInativarPartes(ProcessoEpa processoEpa) {
         Map<String, Object> parameters = new HashMap<>();
         parameters.put(QUERY_PARAM_PROCESSO_EPA, processoEpa);
@@ -112,20 +112,19 @@ public class ProcessoEpaDAO extends GenericDAO {
         parameters.put(PARAM_ID_PROCESSO, idProcesso);
         return getNamedSingleResult(ITEM_DO_PROCESSO, parameters);
     }
-	
-	@SuppressWarnings(UNCHECKED)
+
+    @SuppressWarnings(UNCHECKED)
     public Map<String, Object> getTempoGasto(ProcessoEpa processoEpa) {
-		Query q = getEntityManager().createQuery(TEMPO_GASTO_PROCESSO_EPP_QUERY)
-				.setParameter("idProcesso", processoEpa.getIdProcesso());
-		Map<String,Object> result = null;
-		try {
-			result = (Map<String, Object>) q.getSingleResult();			
-		} catch(NoResultException e) {
-		    LOG.info(".getTempoGasto()", e);
-		}
-		return result;
-	}
-	
+        Query q = getEntityManager().createQuery(TEMPO_GASTO_PROCESSO_EPP_QUERY).setParameter("idProcesso", processoEpa.getIdProcesso());
+        Map<String, Object> result = null;
+        try {
+            result = (Map<String, Object>) q.getSingleResult();
+        } catch (NoResultException e) {
+            LOG.info(".getTempoGasto()", e);
+        }
+        return result;
+    }
+
     public Date getDataInicioPrimeiraTarefa(ProcessoEpa processoEpa) {
         Map<String, Object> parameters = new HashMap<>();
         parameters.put(QUERY_PARAM_PROCESSO_EPA, processoEpa);
