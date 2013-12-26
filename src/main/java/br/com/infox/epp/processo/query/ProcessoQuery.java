62357b8e3f800dd7bcb92de316e57eda57c3dc18
jonasbezerra <jonasbezerra@infox.com.br>
2013-12-26 14:48:47 -0300

    Criação de NamedNativeQuery ATUALIZAR_PROCESSOS

--- src/main/java/br/com/infox/epp/processo/query/ProcessoQuery.java
+++ src/main/java/br/com/infox/epp/processo/query/ProcessoQuery.java
@@ -36,5 +36,34 @@ public interface ProcessoQuery {
     String LIST_PROCESSOS_BY_ID_PROCESSO_AND_ACTOR_ID = "listProcessosByIdProcessoAndActorId";
     String LIST_PROCESSOS_BY_ID_PROCESSO_AND_ACTOR_ID_QUERY = "select o from Processo o where o.idProcesso = :" 
             + PARAM_ID_PROCESSO + " and o.actorId like :" + PARAM_ACTOR_ID;
+    
+    String ATUALIZAR_PROCESSOS = "atualizarProcessos";
+    String ATUALIZAR_PROCESSOS_QUERY = "update jbpm_processinstance pi set processdefinition_ = " +
+            "(select max(id_) from jbpm_processdefinition pd " +
+            "where name_ = (select name_ from jbpm_processdefinition " +
+            "where id_ = pi.processdefinition_));\n" +
+
+            "update jbpm_token t set node_ = "+
+            "(select max(n.id_) from jbpm_node n "+
+            "inner join jbpm_processdefinition pd on pd.id_ = n.processdefinition_ "+
+            "where n.name_ = (select name_ from jbpm_node where id_ = t.node_) "+
+            "and pd.name_ = (select procdef.name_ from jbpm_processinstance procinst "+
+            "inner join jbpm_processdefinition procdef on procdef.id_ = procinst.processdefinition_ "+
+            "where procinst.id_ = t.processinstance_) "+
+            "and n.class_ = (select class_ from jbpm_node where id_ = t.node_));\n" +
+            
+            "update jbpm_taskinstance ti set task_ = "+ 
+            "(select max(t.id_) from jbpm_task t "+
+            "inner join jbpm_processdefinition pd on pd.id_ = t.processdefinition_ "+
+            "where t.name_ = (select name_ from jbpm_task where id_ = ti.task_) and "+
+            "pd.name_ = (select procdef.name_ from jbpm_processinstance procinst "+
+            "inner join jbpm_processdefinition procdef on procdef.id_ = procinst.processdefinition_ "+
+            "where procinst.id_ = ti.procinst_)) "+
+            "where end_ is null;\n" +
+            
+            "update public.tb_processo_localizacao_ibpm pl set id_task_jbpm =" +
+            "(select max(id_) from jbpm_task t where exists" +
+            "(select * from jbpm_task where name_ = t.name_ " +
+            "and id_ = pl.id_task_jbpm))";
 
 }
