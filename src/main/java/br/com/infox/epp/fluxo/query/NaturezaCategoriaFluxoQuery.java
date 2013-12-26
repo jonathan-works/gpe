66f85f435baf843998d98329a3d4f10677a04f1d
jonasbezerra <jonasbezerra@infox.com.br>
2013-12-26 08:12:40 -0300

    Criação de NamedQuery LIST_BY_RELATIONSHIP

--- src/main/java/br/com/infox/epp/fluxo/query/NaturezaCategoriaFluxoQuery.java
+++ src/main/java/br/com/infox/epp/fluxo/query/NaturezaCategoriaFluxoQuery.java
@@ -18,12 +18,15 @@ public interface NaturezaCategoriaFluxoQuery {
     String LIST_BY_NATUREZA_QUERY = "select o from NaturezaCategoriaFluxo o "
             + "where o.natureza = :" + PARAM_NATUREZA;
 
+    String LIST_CATEGORIA_ATIVO = "listByCategoriaAtiva";
     String LIST_CATEGORIA_ATIVO_QUERY = "select o from Categoria o"
             + " where o.ativo = true";
 
+    String LIST_FLUXO_ATIVO = "listByFluxoAtivo";
     String LIST_FLUXO_ATIVO_QUERY = "select o from Fluxo o"
             + " where o.ativo = true";
 
+    String LIST_BY_RELATIONSHIP = "listByNaturezaCategoriaAndFluxo";
     String BY_RELATIONSHIP_QUERY = "select o from NaturezaCategoriaFluxo o where o.natureza = :"
             + PARAM_NATUREZA
             + " and o.categoria = :"
