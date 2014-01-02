3b2f3fd075edb2ecaf2eab4b9cc0d332a272c38c
Gabriel Morrison Lima Dantas <gabriel@infox.com.br>
2014-01-02 11:01:40 -0300

    Alterações para gerenciar melhor a replicação do fluxo

--- src/main/java/br/com/infox/epp/fluxo/crud/FluxoCrudAction.java
+++ src/main/java/br/com/infox/epp/fluxo/crud/FluxoCrudAction.java
@@ -4,36 +4,73 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
 import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.faces.FacesMessages;
 import org.jboss.seam.international.StatusMessage;
 import org.jboss.seam.international.StatusMessage.Severity;
+import org.jboss.seam.international.StatusMessages;
 import org.jboss.seam.log.LogProvider;
 import org.jboss.seam.log.Logging;
 
 import br.com.infox.core.crud.AbstractCrudAction;
+import br.com.infox.core.persistence.PostgreSQLErrorCode;
 import br.com.infox.epp.access.entity.UsuarioLogin;
 import br.com.infox.epp.fluxo.entity.Fluxo;
 import br.com.infox.epp.fluxo.manager.FluxoManager;
+import br.com.infox.epp.fluxo.query.FluxoQuery;
 import br.com.itx.util.EntityUtil;
-import br.com.itx.util.HibernateUtil;
 
 @Name(FluxoCrudAction.NAME)
 public class FluxoCrudAction extends AbstractCrudAction<Fluxo> {
-    
-    public static final String NAME = "fluxoCrudAction";
-    
+
+	public static final String NAME = "fluxoCrudAction";
+    private static final String DESCRICAO_FLUXO_COMPONENT_ID = "defaultTabPanel:fluxoForm:descricaoFluxoDecoration:descricaoFluxo";
+	private static final String COD_FLUXO_COMPONENT_ID = "defaultTabPanel:fluxoForm:codFluxoDecoration:codFluxo";
     private static final LogProvider LOG = Logging.getLogProvider(FluxoCrudAction.class);
     
     @In private FluxoManager fluxoManager;
     
+    private boolean isReplica = false;
+    
     public String criarReplica() {
+    	this.isReplica = true;
         Fluxo fluxo = getInstance();
-        HibernateUtil.getSession().evict(fluxo);
+        fluxoManager.detach(fluxo);
         fluxo.setIdFluxo(null);
-        return persist();
+        setId(null);
+        String ret = save();
+        
+        if (PostgreSQLErrorCode.UNIQUE_VIOLATION.toString().equals(ret)) {
+        	handleReplicaUniqueViolation();
+        } else if (PERSISTED.equals(ret)) {
+        	this.isReplica = false;
+        }
+        return ret;
     }
+
+	private void handleReplicaUniqueViolation() {
+		String sqlMessage = getDaoException().getSqlException().getMessage();
+		FacesMessage message = null;
+		String componentId = null;
+		StatusMessages messages = getMessagesHandler();
+		
+		if (sqlMessage.contains(FluxoQuery.CODIGO_FLUXO)) {
+			message = FacesMessages.createFacesMessage(FacesMessage.SEVERITY_ERROR, "#{messages['fluxo.codigoDuplicado']}");
+			componentId = COD_FLUXO_COMPONENT_ID;
+		} else if (sqlMessage.contains(FluxoQuery.DESCRICAO_FLUXO)) {
+			message = FacesMessages.createFacesMessage(FacesMessage.SEVERITY_ERROR, "#{messages['fluxo.descricaoDuplicada']}");
+			componentId = DESCRICAO_FLUXO_COMPONENT_ID;
+		}
+		
+		if (message != null && componentId != null) {
+			messages.clear();
+			FacesContext.getCurrentInstance().addMessage(componentId, message);
+		}
+	}
     
     @Override
     protected boolean beforeSave() {
@@ -96,4 +133,8 @@ public class FluxoCrudAction extends AbstractCrudAction<Fluxo> {
         }
         
     }
+    
+    public boolean isReplica() {
+		return this.isReplica;
+	}
 }
