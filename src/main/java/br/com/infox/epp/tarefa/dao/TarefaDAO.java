541cad40a3273e4be155ed3767d0aef08004709f
jonasbezerra <jonasbezerra@infox.com.br>
2014-01-02 09:01:34 -0300

    Utilizando NamedQuery NOVAS_TAREFAS

--- src/main/java/br/com/infox/epp/tarefa/dao/TarefaDAO.java
+++ src/main/java/br/com/infox/epp/tarefa/dao/TarefaDAO.java
@@ -1,5 +1,6 @@
 package br.com.infox.epp.tarefa.dao;
 
+import static br.com.infox.epp.tarefa.query.TarefaQuery.*;
 import static br.com.infox.core.constants.WarningConstants.*;
 
 import java.util.ArrayList;
@@ -13,7 +14,6 @@ import org.jboss.seam.annotations.Name;
 
 import br.com.infox.core.dao.GenericDAO;
 import br.com.infox.epp.tarefa.entity.Tarefa;
-import br.com.itx.util.EntityUtil;
 
 @Name(TarefaDAO.NAME)
 @AutoCreate
@@ -44,20 +44,12 @@ public class TarefaDAO extends GenericDAO {
 		return previousTasksItems;
 	}
 	
-	/**
-	 * Popula a tabela tb_tarefa com todas as tarefas de todos os fluxos, 
-	 * considerando como chave o nome da tarefa task.name_
-	 */
-	public void encontrarNovasTarefas() {
-		String hql = "insert into public.tb_tarefa (id_fluxo, ds_tarefa) " +
-						"select f.id_fluxo, t.name_ from jbpm_task t " +
-							"inner join jbpm_processdefinition pd on (pd.id_ = t.processdefinition_) " +
-							"inner join public.tb_fluxo f on (f.ds_fluxo = pd.name_) " +
-							"inner join jbpm_node jn on (t.tasknode_ = jn.id_ and jn.class_ = 'K') " +
-							"where pd.id_ = t.processdefinition_ and not exists " +
-								"(select 1 from public.tb_tarefa where ds_tarefa = t.name_ and id_fluxo = f.id_fluxo) " +
-							"group by f.id_fluxo, t.name_";
-		EntityUtil.getEntityManager().createNativeQuery(hql).executeUpdate();
-	}
-	
+    /**
+     * Popula a tabela tb_tarefa com todas as tarefas de todos os fluxos,
+     * considerando como chave o nome da tarefa task.name_
+     */
+    public void encontrarNovasTarefas() {
+        executeNamedQueryUpdate(NOVAS_TAREFAS);
+    }
+
 }
