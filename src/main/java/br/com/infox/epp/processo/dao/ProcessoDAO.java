7c77b55c8b2373de5ad3e2c81c2dff20944154a4
jonasbezerra <jonasbezerra@infox.com.br>
2014-01-03 11:38:12 -0300

    Utilizando Método executeNamedQueryUpdate() em anularTodosActorId()

--- src/main/java/br/com/infox/epp/processo/dao/ProcessoDAO.java
+++ src/main/java/br/com/infox/epp/processo/dao/ProcessoDAO.java
@@ -1,13 +1,16 @@
 package br.com.infox.epp.processo.dao;
 
 import static br.com.infox.epp.processo.query.ProcessoQuery.ANULA_ACTOR_ID;
-import static br.com.infox.epp.processo.query.ProcessoQuery.ANULA_TODOS_OS_ACTOR_IDS_QUERY;
+import static br.com.infox.epp.processo.query.ProcessoQuery.ANULA_TODOS_OS_ACTOR_IDS;
 import static br.com.infox.epp.processo.query.ProcessoQuery.APAGA_ACTOR_ID_DO_PROCESSO;
 import static br.com.infox.epp.processo.query.ProcessoQuery.ATUALIZAR_PROCESSOS_QUERY;
+import static br.com.infox.epp.processo.query.ProcessoQuery.CAIXA_PARAM;
+import static br.com.infox.epp.processo.query.ProcessoQuery.ID_LIST_PROCESSO_PARAM;
 import static br.com.infox.epp.processo.query.ProcessoQuery.LIST_PROCESSOS_BY_ID_PROCESSO_AND_ACTOR_ID;
+import static br.com.infox.epp.processo.query.ProcessoQuery.MOVER_PROCESSOS_PARA_CAIXA;
 import static br.com.infox.epp.processo.query.ProcessoQuery.PARAM_ACTOR_ID;
 import static br.com.infox.epp.processo.query.ProcessoQuery.PARAM_ID_PROCESSO;
-import static br.com.infox.epp.processo.query.ProcessoQuery.*;
+import static br.com.infox.epp.processo.query.ProcessoQuery.REMOVE_PROCESSO_DA_CAIXA_ATUAL;
 
 import java.util.HashMap;
 import java.util.List;
@@ -15,13 +18,14 @@ import java.util.Map;
 
 import org.jboss.seam.annotations.AutoCreate;
 import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.TransactionPropagationType;
+import org.jboss.seam.annotations.Transactional;
 
 import br.com.infox.core.dao.GenericDAO;
 import br.com.infox.epp.painel.caixa.Caixa;
 import br.com.infox.epp.processo.entity.Processo;
 import br.com.infox.ibpm.util.JbpmUtil;
 import br.com.itx.util.EntityUtil;
-import br.com.itx.util.HibernateUtil;
 
 @Name(ProcessoDAO.NAME)
 @AutoCreate
@@ -42,9 +46,10 @@ public class ProcessoDAO extends GenericDAO {
         executeNamedQueryUpdate(APAGA_ACTOR_ID_DO_PROCESSO, parameters);
 	}
 	
-	public void anularTodosActorId() {
-		HibernateUtil.getSession().createSQLQuery(ANULA_TODOS_OS_ACTOR_IDS_QUERY).executeUpdate();
-	}
+    @Transactional(TransactionPropagationType.REQUIRED)
+    public void anularTodosActorId() {
+        executeNamedQueryUpdate(ANULA_TODOS_OS_ACTOR_IDS);
+    }
 
     // TODO esse flush() é realmente necessário?
     public void moverProcessosParaCaixa(List<Integer> idList, Caixa caixa) {
