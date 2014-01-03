76a4b32964a824697da750853cff2623496c44ff
erikliberal <erikliberal@infox.com.br>
2014-01-03 12:21:48 -0300

    Atualização de PessoaJuridicaDAO para que utilize métodos do GenericDAO em lugar de invocar do EntityUtil

--- src/main/java/br/com/infox/epp/pessoa/dao/PessoaJuridicaDAO.java
+++ src/main/java/br/com/infox/epp/pessoa/dao/PessoaJuridicaDAO.java
@@ -1,13 +1,12 @@
 package br.com.infox.epp.pessoa.dao;
 
-import javax.persistence.Query;
+import java.util.HashMap;
 
 import org.jboss.seam.annotations.AutoCreate;
 import org.jboss.seam.annotations.Name;
 
 import br.com.infox.core.dao.GenericDAO;
 import br.com.infox.epp.pessoa.entity.PessoaJuridica;
-import br.com.itx.util.EntityUtil;
 
 @Name(PessoaJuridicaDAO.NAME)
 @AutoCreate
@@ -16,10 +15,11 @@ public class PessoaJuridicaDAO extends GenericDAO {
 	private static final long serialVersionUID = 1L;
 	public static final String NAME = "pessoaJuridicaDAO";
 	
-	public PessoaJuridica searchByCnpj(String cnpj){
-		String hql = "select o from PessoaJuridica o where o.cnpj = :cnpj";
-		Query query = EntityUtil.createQuery(hql).setParameter("cnpj", cnpj);
-		return EntityUtil.getSingleResult(query);
+	public PessoaJuridica searchByCnpj(final String cnpj){
+		final String hql = "select o from PessoaJuridica o where o.cnpj = :cnpj";
+		final HashMap<String,Object> parameters = new HashMap<>();
+		parameters.put("cnpj", cnpj);
+		return getSingleResult(hql, parameters);
 	}
 
 }
