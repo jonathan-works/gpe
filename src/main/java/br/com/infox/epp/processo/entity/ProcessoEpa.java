ca9c0d9ee9eb44c5e626211f2788ac8dbafefd95
erikliberal <erikliberal@infox.com.br>
2014-01-07 11:09:03 -0300

    Adição de novo construtor para inserção de múltiplos valores durante a instanciação

--- src/main/java/br/com/infox/epp/processo/entity/ProcessoEpa.java
+++ src/main/java/br/com/infox/epp/processo/entity/ProcessoEpa.java
@@ -1,6 +1,7 @@
 package br.com.infox.epp.processo.entity;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import javax.persistence.CascadeType;
@@ -19,6 +20,7 @@ import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
 import br.com.infox.epp.access.entity.Localizacao;
+import br.com.infox.epp.access.entity.UsuarioLogin;
 import br.com.infox.epp.estatistica.type.SituacaoPrazoEnum;
 import br.com.infox.epp.fluxo.entity.Item;
 import br.com.infox.epp.fluxo.entity.NaturezaCategoriaFluxo;
@@ -52,6 +54,23 @@ public class ProcessoEpa extends Processo {
 	
 	private List<ProcessoEpaTarefa> processoEpaTarefaList = new ArrayList<ProcessoEpaTarefa>(0);
 	private List<ParteProcesso> partes = new ArrayList<ParteProcesso>(0);
+	
+	public ProcessoEpa() {
+	    super();
+    }
+	
+	public ProcessoEpa(final SituacaoPrazoEnum situacaoPrazo, final Date dataInicio, 
+	        final String numeroProcesso, final UsuarioLogin usuarioLogado, 
+	        final NaturezaCategoriaFluxo naturezaCategoriaFluxo,
+	        final Localizacao localizacao, final Item itemDoProcesso) {
+	    this.setSituacaoPrazo(situacaoPrazo);
+        this.setDataInicio(dataInicio);
+        this.setNumeroProcesso(numeroProcesso);
+        this.setUsuarioCadastroProcesso(usuarioLogado);
+        this.setNaturezaCategoriaFluxo(naturezaCategoriaFluxo);
+        this.setLocalizacao(localizacao);
+        this.setItemDoProcesso(itemDoProcesso);
+	}
 	
 	public void setNaturezaCategoriaFluxo(NaturezaCategoriaFluxo naturezaCategoriaFluxo) {
 		this.naturezaCategoriaFluxo = naturezaCategoriaFluxo;
