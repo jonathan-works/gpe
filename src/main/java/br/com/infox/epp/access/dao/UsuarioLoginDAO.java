6b7cd684a75784ca803d80501203b61eec2e9bbb
jonasbezerra <jonasbezerra@infox.com.br>
2014-01-10 09:48:54 -0300

    [32147] Formatação

--- src/main/java/br/com/infox/epp/access/dao/UsuarioLoginDAO.java
+++ src/main/java/br/com/infox/epp/access/dao/UsuarioLoginDAO.java
@@ -22,32 +22,32 @@ import br.com.infox.epp.access.entity.UsuarioLogin;
 @AutoCreate
 public class UsuarioLoginDAO extends GenericDAO {
 
-	private static final long serialVersionUID = 1L;
-	public static final String NAME = "usuarioLoginDAO";
-	
-	public UsuarioLogin getUsuarioLoginByEmail(String email){
-	    Map<String, Object> parameters = new HashMap<>();
-	    parameters.put(PARAM_EMAIL, email);
-	    return getNamedSingleResult(USUARIO_BY_EMAIL, parameters);
-	}
-	
-	public UsuarioLogin getUsuarioByLoginTaskInstance(Long idTaskInstance, String actorId) {
-		Map<String,Object> parameters = new HashMap<String,Object>();
-		parameters.put(PARAM_LOGIN, actorId);
-		parameters.put(PARAM_ID_TASK_INSTANCE, idTaskInstance);
-		return getNamedSingleResult(USUARIO_BY_LOGIN_TASK_INSTANCE, parameters);
-	}
-	
-	public void inativarUsuario(UsuarioLogin usuario) {
-		Map<String, Object> parameters = new HashMap<>();
+    private static final long serialVersionUID = 1L;
+    public static final String NAME = "usuarioLoginDAO";
+
+    public UsuarioLogin getUsuarioLoginByEmail(String email) {
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put(PARAM_EMAIL, email);
+        return getNamedSingleResult(USUARIO_BY_EMAIL, parameters);
+    }
+
+    public UsuarioLogin getUsuarioByLoginTaskInstance(Long idTaskInstance, String actorId) {
+        Map<String, Object> parameters = new HashMap<String, Object>();
+        parameters.put(PARAM_LOGIN, actorId);
+        parameters.put(PARAM_ID_TASK_INSTANCE, idTaskInstance);
+        return getNamedSingleResult(USUARIO_BY_LOGIN_TASK_INSTANCE, parameters);
+    }
+
+    public void inativarUsuario(UsuarioLogin usuario) {
+        Map<String, Object> parameters = new HashMap<>();
         parameters.put(PARAM_ID, usuario.getIdUsuarioLogin());
         executeNamedQueryUpdate(INATIVAR_USUARIO, parameters);
-	}
-	
-	public UsuarioLogin getUsuarioLoginByLogin(String login) {
-	    Map<String, Object> parameters = new HashMap<>();
-	    parameters.put(PARAM_LOGIN, login);
-	    return getNamedSingleResult(USUARIO_LOGIN_NAME, parameters);
     }
-	
+
+    public UsuarioLogin getUsuarioLoginByLogin(String login) {
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put(PARAM_LOGIN, login);
+        return getNamedSingleResult(USUARIO_LOGIN_NAME, parameters);
+    }
+
 }
