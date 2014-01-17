640b622dc739f7256d16f828d8aa5d5facf5c677
erikliberal <erikliberal@infox.com.br>
2014-01-17 12:52:07 -0300

    Ajuste em UsuarioLocalizacaoAction para que o mesmo seja filho de AbstractCrudAction

--- src/main/java/br/com/infox/epp/access/action/UsuarioLocalizacaoAction.java
+++ src/main/java/br/com/infox/epp/access/action/UsuarioLocalizacaoAction.java
@@ -1,90 +1,102 @@
 package br.com.infox.epp.access.action;
 
 
-import static br.com.infox.core.constants.WarningConstants.RAWTYPES;
+import static br.com.infox.core.constants.WarningConstants.UNCHECKED;
+import static br.com.infox.epp.access.query.UsuarioLocalizacaoQuery.ESTRUTURA_CONDITION;
+import static br.com.infox.epp.access.query.UsuarioLocalizacaoQuery.ESTRUTURA_NULL_CONDITION;
+import static br.com.infox.epp.access.query.UsuarioLocalizacaoQuery.EXISTE_USUARIO_LOCALIZACAO_QUERY;
+import static br.com.infox.epp.access.query.UsuarioLocalizacaoQuery.PARAM_ESTRUTURA;
+import static br.com.infox.epp.access.query.UsuarioLocalizacaoQuery.PARAM_LOCALIZACAO;
+import static br.com.infox.epp.access.query.UsuarioLocalizacaoQuery.PARAM_PAPEL;
+import static br.com.infox.epp.access.query.UsuarioLocalizacaoQuery.PARAM_USUARIO;
+
+import java.util.HashMap;
 
 import org.jboss.seam.Component;
 import org.jboss.seam.ScopeType;
 import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Scope;
-import org.jboss.seam.faces.FacesMessages;
-import org.jboss.seam.log.Log;
-import org.jboss.seam.log.Logging;
 
-import br.com.infox.core.persistence.DAOException;
+import br.com.infox.core.crud.AbstractCrudAction;
 import br.com.infox.core.tree.AbstractTreeHandler;
 import br.com.infox.epp.access.component.tree.LocalizacaoEstruturaTreeHandler;
 import br.com.infox.epp.access.component.tree.PapelTreeHandler;
+import br.com.infox.epp.access.entity.Localizacao;
+import br.com.infox.epp.access.entity.Papel;
 import br.com.infox.epp.access.entity.UsuarioLocalizacao;
 import br.com.infox.epp.access.entity.UsuarioLogin;
 import br.com.infox.epp.access.manager.UsuarioLocalizacaoManager;
 
 @Name(UsuarioLocalizacaoAction.NAME)
-@Scope(ScopeType.PAGE)
-public class UsuarioLocalizacaoAction {
+@Scope(ScopeType.CONVERSATION)
+public class UsuarioLocalizacaoAction extends AbstractCrudAction<UsuarioLocalizacao>{
 	public static final String NAME = "usuarioLocalizacaoAction";
-	private static final Log LOG = Logging.getLog(UsuarioLocalizacaoAction.class);
-	
-	private UsuarioLocalizacao instance;
 	
 	@In
 	private UsuarioLocalizacaoManager usuarioLocalizacaoManager;
 	
 	private UsuarioLogin usuarioGerenciado;
 	
-	public UsuarioLocalizacao getInstance() {
-		return instance;
-	}
-	
-	public void setInstance(final UsuarioLocalizacao instance) {
-		this.instance = instance;
-	}
-	
-	@SuppressWarnings(RAWTYPES)
+	@SuppressWarnings(UNCHECKED)
     private void limparArvores() {
-        AbstractTreeHandler tree = (AbstractTreeHandler)Component.getInstance(LocalizacaoEstruturaTreeHandler.NAME);
-        tree.clearTree();
-        tree = (AbstractTreeHandler)Component.getInstance(PapelTreeHandler.NAME);
-        tree.clearTree();
+	    clearTree((AbstractTreeHandler<Localizacao>) Component.getInstance(LocalizacaoEstruturaTreeHandler.NAME));
+        clearTree((AbstractTreeHandler<Papel>) Component.getInstance(PapelTreeHandler.NAME));
 	}
 	
+	private <T> void clearTree(final AbstractTreeHandler<T> handler) {
+	    if (handler != null) {
+	        handler.clearTree();
+	    }
+	}
+	
+	@Override
 	public void newInstance() {
-		this.instance = new UsuarioLocalizacao();
-		this.instance.setResponsavelLocalizacao(Boolean.FALSE);
-		this.instance.setUsuario(usuarioGerenciado);
+	    super.newInstance();
+		final UsuarioLocalizacao instance = getInstance();
+		instance.setResponsavelLocalizacao(Boolean.FALSE);
+		instance.setUsuario(usuarioGerenciado);
 		limparArvores();
 	}
 	
-	public void persist() {
-        try {
-        	usuarioLocalizacaoManager.persist(instance);
-			newInstance();
-		} catch (DAOException e) {
-            processDAOException(e, ".persist()");
-		}
+	public boolean existeUsuarioLocalizacao(final UsuarioLocalizacao usuarioLocalizacao) {
+        final StringBuilder hql = new StringBuilder(EXISTE_USUARIO_LOCALIZACAO_QUERY);
+        if (usuarioLocalizacao.getEstrutura() != null) {
+            hql.append(ESTRUTURA_CONDITION);
+        } else {
+            hql.append(ESTRUTURA_NULL_CONDITION);
+        }
+        
+        final HashMap<String, Object> params = new HashMap<>();
+        params.put(PARAM_USUARIO, usuarioLocalizacao.getUsuario());
+        params.put(PARAM_PAPEL, usuarioLocalizacao.getPapel());
+        params.put(PARAM_LOCALIZACAO, usuarioLocalizacao.getLocalizacao());
+        if (usuarioLocalizacao.getEstrutura() != null) {
+            params.put(PARAM_ESTRUTURA, usuarioLocalizacao.getEstrutura());
+        }
+        
+        return (Long) getGenericManager().getSingleResult(hql.toString(), params) > 0;
+    }
+	
+	@Override
+	protected boolean beforeSave() {
+	    return !existeUsuarioLocalizacao(getInstance());
 	}
 	
-	public void remove(final UsuarioLocalizacao usuarioLocalizacao) {
-		setInstance(usuarioLocalizacao);
-		try {
-			usuarioLocalizacaoManager.remove(instance);
-			newInstance();
-		} catch (DAOException e) {
-            processDAOException(e, ".remove()");
-		}
+	@Override
+	protected void afterSave(String ret) {
+	    if (PERSISTED.equals(ret)) {
+	        newInstance();
+	    }
 	}
-
-    private void processDAOException(final DAOException e, final String message) {
-        LOG.error(message, e);
-        FacesMessages facesMessages = FacesMessages.instance();
-        facesMessages.clear();
-        if (e.getLocalizedMessage() != null) {
-        	facesMessages.add(e.getLocalizedMessage());
-        } else {
-        	facesMessages.add(e.getMessage());
-        }
-    }
+	
+	@Override
+	public String remove(final UsuarioLocalizacao usuarioLocalizacao) {
+        setInstance(usuarioLocalizacao);
+	    final String ret = super.remove(getInstance());
+		newInstance();
+		return ret;
+	}
 
     public UsuarioLogin getUsuarioGerenciado() {
         return usuarioGerenciado;
@@ -92,6 +104,6 @@ public class UsuarioLocalizacaoAction {
 
     public void setUsuarioGerenciado(final UsuarioLogin usuarioGerenciado) {
         this.usuarioGerenciado = usuarioGerenciado;
-        this.instance.setUsuario(usuarioGerenciado);
+        getInstance().setUsuario(usuarioGerenciado);
     }
 }
