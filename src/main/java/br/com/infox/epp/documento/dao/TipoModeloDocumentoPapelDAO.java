04ccc748b379c96c000991ecc0fef9294d8e1cf3
jonasbezerra <jonasbezerra@infox.com.br>
2014-01-17 10:18:27 -0300

    [34748] Criação de NamedQuery tiposDeModeloDcumentoPermitidos

--- src/main/java/br/com/infox/epp/documento/dao/TipoModeloDocumentoPapelDAO.java
+++ src/main/java/br/com/infox/epp/documento/dao/TipoModeloDocumentoPapelDAO.java
@@ -1,8 +1,11 @@
 package br.com.infox.epp.documento.dao;
 
-import static br.com.infox.core.constants.WarningConstants.*;
+import static br.com.infox.epp.documento.query.TipoModeloDocumentoPapelQuery.PAPEL_PARAM;
+import static br.com.infox.epp.documento.query.TipoModeloDocumentoPapelQuery.TIPOS_MODELO_DOCUMENTO_PERMITIDOS;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.jboss.seam.annotations.AutoCreate;
 import org.jboss.seam.annotations.Name;
@@ -14,23 +17,18 @@ import br.com.infox.epp.documento.entity.TipoModeloDocumentoPapel;
 @Name(TipoModeloDocumentoPapelDAO.NAME)
 @AutoCreate
 public class TipoModeloDocumentoPapelDAO extends GenericDAO {
-	
-	private static final long serialVersionUID = 1L;
-	public static final String NAME = "tipoModeloDocumentoPapelDAO";
-	
-	/**
-	 * Retorna uma lista com os tipos de modelo de documento que o perfil (localização+papel)
-	 * do usuário logado possue permissão para acessar.
-	 * */
-	@SuppressWarnings(UNCHECKED)
-	public List<TipoModeloDocumentoPapel> getTiposModeloDocumentoPermitidos(){
-		String hql = "select t from TipoModeloDocumentoPapel tmdp " +
-				"join tmdp.tipoModeloDocumento t " +
-				"where tmdp.papel = :papel " +
-				"order by t.tipoModeloDocumento";
-		return getEntityManager().createQuery(hql)
-			.setParameter("papel", Authenticator.getUsuarioLocalizacaoAtual()
-			.getPapel()).getResultList();
-	}
+
+    private static final long serialVersionUID = 1L;
+    public static final String NAME = "tipoModeloDocumentoPapelDAO";
+
+    /**
+     * Retorna uma lista com os tipos de modelo de documento que o perfil
+     * (localização+papel) do usuário logado possue permissão para acessar.
+     * */
+    public List<TipoModeloDocumentoPapel> getTiposModeloDocumentoPermitidos() {
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put(PAPEL_PARAM, Authenticator.getUsuarioLocalizacaoAtual().getPapel());
+        return getNamedResultList(TIPOS_MODELO_DOCUMENTO_PERMITIDOS, parameters);
+    }
 
 }
