a427a9532808cb38a5838f6081dd7067152d58a3
jonasbezerra <jonasbezerra@infox.com.br>
2014-01-20 07:38:14 -0300

    [35083] Criação de AJudaView

---
+++ src/main/java/br/com/infox/epp/ajuda/view/AjudaView.java
@@ -0,0 +1,13 @@
+package br.com.infox.epp.ajuda.view;
+
+import org.jboss.seam.ScopeType;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Scope;
+
+@Name(AjudaView.NAME)
+@Scope(ScopeType.PAGE)
+public class AjudaView <Ajuda>{
+    
+    public static final String NAME = "ajudaView";
+
+}
