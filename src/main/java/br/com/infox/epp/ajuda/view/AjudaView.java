b8dc77b3ab6f69d813b1a20b251aaced81215579
jonasbezerra <jonasbezerra@infox.com.br>
2014-01-20 10:48:57 -0300

    [32147] Formatação

--- src/main/java/br/com/infox/epp/ajuda/view/AjudaView.java
+++ src/main/java/br/com/infox/epp/ajuda/view/AjudaView.java
@@ -9,8 +9,6 @@ import org.jboss.seam.ScopeType;
 import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Scope;
-import org.jboss.seam.log.LogProvider;
-import org.jboss.seam.log.Logging;
 
 import br.com.infox.epp.ajuda.entity.Ajuda;
 import br.com.infox.epp.ajuda.entity.Pagina;
@@ -23,7 +21,6 @@ import br.com.infox.epp.search.SearchService;
 public class AjudaView {
     
     public static final String NAME = "ajudaView";
-    private static final LogProvider LOG = Logging.getLogProvider(AjudaView.class);
     
     private Ajuda instance;
 //    private Ajuda oldInstance;
