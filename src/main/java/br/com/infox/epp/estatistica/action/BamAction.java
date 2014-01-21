23d3591a5b37bececda92733ba2ef79cebc0bdc6
jonasbezerra <jonasbezerra@infox.com.br>
2014-01-21 07:52:05 -0300

    [32147] Formatação

--- src/main/java/br/com/infox/epp/estatistica/action/BamAction.java
+++ src/main/java/br/com/infox/epp/estatistica/action/BamAction.java
@@ -25,79 +25,81 @@ import br.com.infox.epp.tarefa.type.PrazoEnum;
 
 /**
  * Classe Action para o BAM
+ * 
  * @author tassio
  */
 @Name(BamAction.NAME)
 @Scope(ScopeType.CONVERSATION)
 public class BamAction {
 
-	public static final String NAME = "bamAction";
-	
-	@In
-	private FluxoManager fluxoManager;
-	@In
-	private ProcessoEpaManager processoEpaManager;
-	@In
-	private ProcessoEpaTarefaManager processoEpaTarefaManager;
-	
-	private List<Fluxo> fluxoList;
-	
-	/**
-	 * Retorna todos os fluxos ativos no sistema
-	 * @return lista com todos os fluxos ativos
-	 */
-	public List<Fluxo> getFluxoList() {
-		if (fluxoList == null) {
-			fluxoList = fluxoManager.getFluxoList(); 
-		}
-		return fluxoList;
-	}
-	
-	public List<Fluxo> getFluxoSuggest(Object suggest) {
-		String prefix = (String) suggest;
-		List<Fluxo> fluxoList = getFluxoList();
-		List<Fluxo> result = new ArrayList<Fluxo>();
-		for (Fluxo fluxo: fluxoList) {
-			if (fluxo.getFluxo().startsWith(prefix)) {
-				result.add(fluxo);
-			}
-		}
-		return result;
-	}
-	
-	public Boolean contemProcessoAtrasado(Fluxo fluxo) {
-		return fluxoManager.contemProcessoAtrasado(fluxo);
-	}
-	
-	/**
-	 * Retorna os processo não finalizados de um determinado fluxo
-	 */
-	public List<ProcessoEpa> getProcessosNaoFinalizados(Fluxo fluxo) {
-		return processoEpaManager.listNotEnded(fluxo);
-	}
+    public static final String NAME = "bamAction";
 
-	public TaskInstance getTaskInstance(Long idTaskInstance) {
-		return ManagedJbpmContext.instance().getTaskInstance(idTaskInstance);
-	}
-    
-	public void forceUpdateTarefasFinalizadas() {
-	    for (ProcessoEpaTarefa pt : processoEpaTarefaManager.getTarefaEnded()) {
+    @In
+    private FluxoManager fluxoManager;
+    @In
+    private ProcessoEpaManager processoEpaManager;
+    @In
+    private ProcessoEpaTarefaManager processoEpaTarefaManager;
+
+    private List<Fluxo> fluxoList;
+
+    /**
+     * Retorna todos os fluxos ativos no sistema
+     * 
+     * @return lista com todos os fluxos ativos
+     */
+    public List<Fluxo> getFluxoList() {
+        if (fluxoList == null) {
+            fluxoList = fluxoManager.getFluxoList();
+        }
+        return fluxoList;
+    }
+
+    public List<Fluxo> getFluxoSuggest(Object suggest) {
+        String prefix = (String) suggest;
+        List<Fluxo> fluxoList = getFluxoList();
+        List<Fluxo> result = new ArrayList<Fluxo>();
+        for (Fluxo fluxo : fluxoList) {
+            if (fluxo.getFluxo().startsWith(prefix)) {
+                result.add(fluxo);
+            }
+        }
+        return result;
+    }
+
+    public Boolean contemProcessoAtrasado(Fluxo fluxo) {
+        return fluxoManager.contemProcessoAtrasado(fluxo);
+    }
+
+    /**
+     * Retorna os processo não finalizados de um determinado fluxo
+     */
+    public List<ProcessoEpa> getProcessosNaoFinalizados(Fluxo fluxo) {
+        return processoEpaManager.listNotEnded(fluxo);
+    }
+
+    public TaskInstance getTaskInstance(Long idTaskInstance) {
+        return ManagedJbpmContext.instance().getTaskInstance(idTaskInstance);
+    }
+
+    public void forceUpdateTarefasFinalizadas() {
+        for (ProcessoEpaTarefa pt : processoEpaTarefaManager.getTarefaEnded()) {
             pt.setUltimoDisparo(pt.getDataInicio());
             pt.setTempoGasto(0);
             pt.setPorcentagem(0);
             try {
                 processoEpaTarefaManager.updateTempoGasto(pt.getDataFim(), pt);
             } catch (DAOException e) {
-                instance().add(Severity.ERROR,"forceUpdateFinalizadas()",e);
+                instance().add(Severity.ERROR, "forceUpdateFinalizadas()", e);
             }
         }
         try {
             processoEpaManager.updateTempoGastoProcessoEpa();
         } catch (DAOException e) {
-            instance().add(Severity.ERROR,"forceUpdateFinalizadasProcesso()",e);
+            instance().add(Severity.ERROR, "forceUpdateFinalizadasProcesso()", e);
         }
-	}
-	
+    }
+
     public void forceUpdateTarefasNaoFinalizadas() {
         Date fireTime = new Date();
         for (ProcessoEpaTarefa pt : processoEpaTarefaManager.getTarefaNotEnded(PrazoEnum.H)) {
@@ -107,7 +109,7 @@ public class BamAction {
             try {
                 processoEpaTarefaManager.updateTempoGasto(fireTime, pt);
             } catch (DAOException e) {
-                instance().add(Severity.ERROR,"forceUpdate(H)",e);
+                instance().add(Severity.ERROR, "forceUpdate(H)", e);
             }
         }
         for (ProcessoEpaTarefa pt : processoEpaTarefaManager.getTarefaNotEnded(PrazoEnum.D)) {
@@ -115,13 +117,13 @@ public class BamAction {
             try {
                 processoEpaTarefaManager.updateTempoGasto(fireTime, pt);
             } catch (DAOException e) {
-                instance().add(Severity.ERROR,"forceUpdate(D)",e);
+                instance().add(Severity.ERROR, "forceUpdate(D)", e);
             }
         }
         try {
             processoEpaManager.updateTempoGastoProcessoEpa();
         } catch (DAOException e) {
-            instance().add(Severity.ERROR,"forceUpdateProcesso()",e);
+            instance().add(Severity.ERROR, "forceUpdateProcesso()", e);
         }
     }
 }
