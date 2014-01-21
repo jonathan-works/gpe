d130208957f3ccadba4756d509e1d4e2c825d6f3
jonasbezerra <jonasbezerra@infox.com.br>
2014-01-21 08:42:40 -0300

    [32147] Formatação

--- src/main/java/br/com/infox/epp/painel/caixa/CaixaHome.java
+++ src/main/java/br/com/infox/epp/painel/caixa/CaixaHome.java
@@ -24,82 +24,84 @@ import br.com.itx.util.ComponentUtil;
 public class CaixaHome extends AbstractHome<Caixa> {
     private static final long serialVersionUID = 1L;
     private static final LogProvider LOG = Logging.getLogProvider(CaixaHome.class);
-    
-	public static final String NAME = "caixaHome";
-	public static final String ADD_CAIXA_EVENT = "addCaixaEvent";
-	
-	@In private TarefaManager tarefaManager;
-	@In private CaixaManager caixaManager;
-	
-	public List<SelectItem> getPreviousNodes() {
-		return getPreviousNodes(instance.getTarefa());
-	}
-	
-	public List<SelectItem> getPreviousNodes(Integer idTarefa) {
-		return tarefaManager.getPreviousNodes(tarefaManager.find(Tarefa.class, idTarefa));
-	}
-	
-    public List<SelectItem> getPreviousNodes(Tarefa tarefa) {
-		return tarefaManager.getPreviousNodes(tarefa);
-	}
 
-	@Override
-	protected boolean beforePersistOrUpdate() {
-	    if (instance.getTarefa() == null) {
-	        return false;
-	    }
-	    return true;
-	}
-	
-	public void addCaixa(int idTarefa) {
-		instance.setTarefa(caixaManager.find(Tarefa.class, idTarefa));
-		instance.setNomeIndice(MessageFormat.format("{0}-{1}", instance.getNomeCaixa(), idTarefa));
-		persist();
-		
-		newInstance();
-	}
-	
-	@Override
-	protected String afterPersistOrUpdate(String ret) {
-	    if (AbstractHome.PERSISTED.equals(ret)) {
-	        JbpmEventsHandler.updatePostDeploy();
-	        TarefasTreeHandler.clearActiveTree();
-	    }
-	    return super.afterPersistOrUpdate(ret);
-	}
-	
-	@Override
-	public String update() {
-		String ret = super.update();
-		try {
-			if (PostgreSQLErrorCode.valueOf(ret) == PostgreSQLErrorCode.UNIQUE_VIOLATION) {
-		    	FacesMessages.instance().clear();
-		    	FacesMessages.instance().add(Severity.ERROR, "Já existe uma caixa na mesma tarefa com o nó anterior especificado.");
-		    }
-		} catch (IllegalArgumentException e) {
-			LOG.warn(".update()", e);
-			// Retorno do update não pertence ao enum, nada a fazer
-		}
-		return ret;
-	}
-	
-	public static CaixaHome instance() {
-		return ComponentUtil.getComponent(NAME);
-	}
-	
+    public static final String NAME = "caixaHome";
+    public static final String ADD_CAIXA_EVENT = "addCaixaEvent";
+
+    @In
+    private TarefaManager tarefaManager;
+    @In
+    private CaixaManager caixaManager;
+
+    public List<SelectItem> getPreviousNodes() {
+        return getPreviousNodes(instance.getTarefa());
+    }
+
+    public List<SelectItem> getPreviousNodes(Integer idTarefa) {
+        return tarefaManager.getPreviousNodes(tarefaManager.find(Tarefa.class, idTarefa));
+    }
+
+    public List<SelectItem> getPreviousNodes(Tarefa tarefa) {
+        return tarefaManager.getPreviousNodes(tarefa);
+    }
+
+    @Override
+    protected boolean beforePersistOrUpdate() {
+        if (instance.getTarefa() == null) {
+            return false;
+        }
+        return true;
+    }
+
+    public void addCaixa(int idTarefa) {
+        instance.setTarefa(caixaManager.find(Tarefa.class, idTarefa));
+        instance.setNomeIndice(MessageFormat.format("{0}-{1}", instance.getNomeCaixa(), idTarefa));
+        persist();
+
+        newInstance();
+    }
+
+    @Override
+    protected String afterPersistOrUpdate(String ret) {
+        if (AbstractHome.PERSISTED.equals(ret)) {
+            JbpmEventsHandler.updatePostDeploy();
+            TarefasTreeHandler.clearActiveTree();
+        }
+        return super.afterPersistOrUpdate(ret);
+    }
+
+    @Override
+    public String update() {
+        String ret = super.update();
+        try {
+            if (PostgreSQLErrorCode.valueOf(ret) == PostgreSQLErrorCode.UNIQUE_VIOLATION) {
+                FacesMessages.instance().clear();
+                FacesMessages.instance().add(Severity.ERROR, "Já existe uma caixa na mesma tarefa com o nó anterior especificado.");
+            }
+        } catch (IllegalArgumentException e) {
+            LOG.warn(".update()", e);
+            // Retorno do update não pertence ao enum, nada a fazer
+        }
+        return ret;
+    }
+
+    public static CaixaHome instance() {
+        return ComponentUtil.getComponent(NAME);
+    }
+
     @Override
     protected Caixa createInstance() {
         return new Caixa();
     }
-    
+
     @Override
     public String remove() {
         caixaManager.removeCaixaByIdCaixa(instance.getIdCaixa());
-    	String ret = super.remove();
-    	TarefasTreeHandler.clearActiveTree();
-    	return ret;
+        String ret = super.remove();
+        TarefasTreeHandler.clearActiveTree();
+        return ret;
     }
-    
+
     public void setCaixaIdCaixa(Integer id) {
         setId(id);
     }
@@ -107,18 +109,18 @@ public class CaixaHome extends AbstractHome<Caixa> {
     public Integer getCaixaIdCaixa() {
         return (Integer) getId();
     }
-    
+
     public void removeCaixa(int idCaixa) {
-        if(idCaixa == 0) {
+        if (idCaixa == 0) {
             return;
         }
         instance = caixaManager.find(Caixa.class, idCaixa);
-        if(instance != null){
+        if (instance != null) {
             remove();
-        }else{
+        } else {
             FacesMessages.instance().add(Severity.ERROR, "Por favor, selecione a caixa que deseja excluir!");
         }
         TarefasTreeHandler tree = ComponentUtil.getComponent("tarefasTree");
-        tree.clearTree();   
+        tree.clearTree();
     }
 }
