19bfc235cba7ce58ea9f0c604325b1601d34f0ec
jonasbezerra <jonasbezerra@infox.com.br>
2014-01-22 15:27:42 -0300

    [32147] Formatação

--- src/main/java/br/com/infox/epp/access/util/SecurityUtil.java
+++ src/main/java/br/com/infox/epp/access/util/SecurityUtil.java
@@ -21,23 +21,22 @@ import br.com.itx.util.ComponentUtil;
 @BypassInterceptors
 public class SecurityUtil {
 
-	public static final String PAGES_PREFIX = "/pages";
-	private static final LogProvider LOG = Logging.getLogProvider(SecurityUtil.class);
+    public static final String PAGES_PREFIX = "/pages";
+    private static final LogProvider LOG = Logging.getLogProvider(SecurityUtil.class);
 
-	public boolean checkPage() {
-		HttpServletRequest request = ServletContexts.instance().getRequest();
-		String servletPath = request.getServletPath();
-		boolean hasPermission = Identity.instance().hasPermission(PAGES_PREFIX + servletPath, "access");
-		if (!hasPermission) {
-			LOG.info(MessageFormat.format(
-					"Bloqueado o acesso do perfil ''{0}'' para página ''{1}''.",
-					Contexts.getSessionContext().get("identificadorPapelAtual"), servletPath));
-		}
-		return hasPermission;
-	}
-	
-	public static SecurityUtil instance() {
-		return ComponentUtil.getComponent("security");
-	}
-	
+    public boolean checkPage() {
+        HttpServletRequest request = ServletContexts.instance().getRequest();
+        String servletPath = request.getServletPath();
+        boolean hasPermission = Identity.instance().hasPermission(PAGES_PREFIX
+                + servletPath, "access");
+        if (!hasPermission) {
+            LOG.info(MessageFormat.format("Bloqueado o acesso do perfil ''{0}'' para página ''{1}''.", Contexts.getSessionContext().get("identificadorPapelAtual"), servletPath));
+        }
+        return hasPermission;
+    }
+
+    public static SecurityUtil instance() {
+        return ComponentUtil.getComponent("security");
+    }
+
 }
