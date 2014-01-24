3e88d914349fd928d61b828491574e5742eebe6b
Gabriel Morrison Lima Dantas <gabriel@infox.com.br>
2014-01-24 11:59:37 -0300

    Refatoração de métodos

--- src/main/java/br/com/infox/core/crud/Crudable.java
+++ src/main/java/br/com/infox/core/crud/Crudable.java
@@ -12,45 +12,6 @@ import br.com.infox.core.controller.Controller;
 public interface Crudable<T> extends Controller {
 
     /**
-     * Id na página para a tab de pesquisa no tabPanel.
-     */
-    String TAB_SEARCH = "search";
-
-    /**
-     * Id na página para a tab de formulário no tabPanel.
-     */
-    String TAB_FORM = "form";
-
-    /**
-     * Método que retorna o Id da instância gerênciada.
-     * 
-     * @return Id da instância gerênciada.
-     */
-    Object getId();
-
-    /**
-     * Define informando o id como buscar o registro referente a ele no banco e
-     * atribuí-lo à instância.
-     * 
-     * @param id Chave primária do registro que deve ser buscado.
-     */
-    void setId(Object id);
-
-    /**
-     * Informa a tab corrente da página, Search ou Form
-     * 
-     * @return
-     */
-    String getTab();
-
-    /**
-     * Define a tab que será exibida na página.
-     * 
-     * @param tab Será definida como a aba atual.
-     */
-    void setTab(String tab);
-
-    /**
      * Informa se a instância parametrizada do Bean está gerenciavel ou não.
      * 
      * @return true se estiver gerenciavel.
