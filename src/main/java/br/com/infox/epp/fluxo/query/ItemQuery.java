263c149ba5758a61860e71869f97c7afcdd2d4af
erikliberal <erikliberal@infox.com.br>
2014-01-28 09:05:46 -0300

    Adição de query que busca todas as folhas abaixo de um item passado como parâmetro

--- src/main/java/br/com/infox/epp/fluxo/query/ItemQuery.java
+++ src/main/java/br/com/infox/epp/fluxo/query/ItemQuery.java
@@ -1,7 +1,7 @@
 package br.com.infox.epp.fluxo.query;
 
 public interface ItemQuery {
-    
+
     String TABLE_ITEM = "tb_item";
     String SEQUENCE_ITEM = "public.sq_tb_item";
     String ID_ITEM = "id_item";
@@ -10,5 +10,12 @@ public interface ItemQuery {
     String DESCRICAO_ITEM = "ds_item";
     String CAMINHO_COMPLETO = "ds_caminho_completo";
     String ITEM_PAI_ATTRIBUTE = "itemPai";
+
+    String GET_FOLHAS = "getFolhas";
+    String GET_FOLHAS_QUERY = "select i from Item i "
+            + "where i.caminhoCompleto like concat(:" + CAMINHO_COMPLETO
+            + ",'%') and not i.idItem in ("
+            + "select distinct it.itemPai.idItem from Item it "
+            + "where not it.itemPai is null)";
 
 }
