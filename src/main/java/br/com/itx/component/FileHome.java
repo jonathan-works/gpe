8b20629c03b6ec4a7542b522456f5ec3a41b9c0f
jonasbezerra <jonasbezerra@infox.com.br>
2014-01-29 10:26:37 -0300

    [32147] Formatação

--- src/main/java/br/com/itx/component/FileHome.java
+++ src/main/java/br/com/itx/component/FileHome.java
@@ -1,18 +1,17 @@
 /*
- IBPM - Ferramenta de produtividade Java
- Copyright (c) 1986-2009 Infox Tecnologia da Informação Ltda.
-
- Este programa é software livre; você pode redistribuí-lo e/ou modificá-lo 
- sob os termos da GNU GENERAL PUBLIC LICENSE (GPL) conforme publicada pela 
- Free Software Foundation; versão 2 da Licença.
- Este programa é distribuído na expectativa de que seja útil, porém, SEM 
- NENHUMA GARANTIA; nem mesmo a garantia implícita de COMERCIABILIDADE OU 
- ADEQUAÇÃO A UMA FINALIDADE ESPECÍFICA.
- 
- Consulte a GNU GPL para mais detalhes.
- Você deve ter recebido uma cópia da GNU GPL junto com este programa; se não, 
- veja em http://www.gnu.org/licenses/   
-*/
+ * IBPM - Ferramenta de produtividade Java Copyright (c) 1986-2009 Infox
+ * Tecnologia da Informação Ltda.
+ * 
+ * Este programa é software livre; você pode redistribuí-lo e/ou modificá-lo sob
+ * os termos da GNU GENERAL PUBLIC LICENSE (GPL) conforme publicada pela Free
+ * Software Foundation; versão 2 da Licença. Este programa é distribuído na
+ * expectativa de que seja útil, porém, SEM NENHUMA GARANTIA; nem mesmo a
+ * garantia implícita de COMERCIABILIDADE OU ADEQUAÇÃO A UMA FINALIDADE
+ * ESPECÍFICA.
+ * 
+ * Consulte a GNU GPL para mais detalhes. Você deve ter recebido uma cópia da
+ * GNU GPL junto com este programa; se não, veja em http://www.gnu.org/licenses/
+ */
 package br.com.itx.component;
 
 import java.io.IOException;
@@ -42,104 +41,106 @@ import br.com.itx.util.Crypto;
 @Scope(ScopeType.CONVERSATION)
 @BypassInterceptors
 public class FileHome implements Serializable, FileUploadListener {
-	
-	private static final LogProvider LOG = Logging.getLogProvider(FileHome.class);
 
-	private static final long serialVersionUID = 1L;
-	public static final String NAME = "fileHome";
+    private static final LogProvider LOG = Logging.getLogProvider(FileHome.class);
 
-	private byte[] data;
-	private String fileName;
-	private Integer size;
-	private String contentType;
+    private static final long serialVersionUID = 1L;
+    public static final String NAME = "fileHome";
 
-	public byte[] getData() {
-		return data;
-	}
-	
-	public void setData(byte[] data) {
-		this.data = ArrayUtil.copyOf(data);
-	}
+    private byte[] data;
+    private String fileName;
+    private Integer size;
+    private String contentType;
 
-	public String getFileName() {
-		return fileName;
-	}
+    public byte[] getData() {
+        return data;
+    }
 
-	public void setFileName(String fileName) {
-		this.fileName = fileName;
-	}
-	
-	public String update() {
-		return null;
-	}
+    public void setData(byte[] data) {
+        this.data = ArrayUtil.copyOf(data);
+    }
 
-	public Integer getSize() {
-		return size;
-	}
+    public String getFileName() {
+        return fileName;
+    }
 
-	public void setSize(Integer size) {
-		this.size = size;
-	}
-	
-	public String getContentType() {
-		return contentType;
-	}
-	
-	public void setContentType(String contentType) {
-		this.contentType = contentType;
-	}
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
 
-	public String getFileType() {
-		String ret = "";
-		if (fileName != null) {
-			ret = fileName.substring(fileName.lastIndexOf('.')+1);
-		}
-		return ret;
-	}
-	
-	public static FileHome instance() {
-		return ComponentUtil.getComponent(NAME);
-	}
-	
-	public void clear() {
-		this.data = null;
-		this.fileName = null;
-		this.size = null;
-		this.contentType = null;
-	}
-	
-	public String getMD5() {
-		return Crypto.encodeMD5(data);
-	}
-	
-	@Override
-	public void processFileUpload(final FileUploadEvent ue) {
-		final UploadedFile ui = ue.getUploadedFile();
-		this.data = ui.getData();
-		this.fileName = ui.getName();
-		this.size = Long.valueOf(ui.getSize()).intValue();
-		this.contentType = ui.getContentType();
-		FacesMessages.instance().add(Messages.instance().get("processoDocumento.doneLabel"));
-	}
+    public String update() {
+        return null;
+    }
 
-	public void download() {
-		if (data == null) {
-			return;
-		}
-		FacesContext facesContext = FacesContext.getCurrentInstance();
-		HttpServletResponse response = (HttpServletResponse) facesContext.getExternalContext().getResponse();
-		response.setContentType(contentType);
-		response.setContentLength(data.length);
-		response.setHeader("Content-disposition", "attachment; filename=\""	+ getFileName() + "\"");
-		try {
-			OutputStream out = response.getOutputStream();
-			out.write(data);
-			out.flush();
-			facesContext.responseComplete();
-		} catch (IOException ex) {
-			LOG.error(".download()", ex);
-			FacesMessages.instance().add("Erro ao descarregar o arquivo: " + fileName);
-		}
-	}
+    public Integer getSize() {
+        return size;
+    }
+
+    public void setSize(Integer size) {
+        this.size = size;
+    }
+
+    public String getContentType() {
+        return contentType;
+    }
+
+    public void setContentType(String contentType) {
+        this.contentType = contentType;
+    }
+
+    public String getFileType() {
+        String ret = "";
+        if (fileName != null) {
+            ret = fileName.substring(fileName.lastIndexOf('.') + 1);
+        }
+        return ret;
+    }
+
+    public static FileHome instance() {
+        return ComponentUtil.getComponent(NAME);
+    }
+
+    public void clear() {
+        this.data = null;
+        this.fileName = null;
+        this.size = null;
+        this.contentType = null;
+    }
+
+    public String getMD5() {
+        return Crypto.encodeMD5(data);
+    }
+
+    @Override
+    public void processFileUpload(final FileUploadEvent ue) {
+        final UploadedFile ui = ue.getUploadedFile();
+        this.data = ui.getData();
+        this.fileName = ui.getName();
+        this.size = Long.valueOf(ui.getSize()).intValue();
+        this.contentType = ui.getContentType();
+        FacesMessages.instance().add(Messages.instance().get("processoDocumento.doneLabel"));
+    }
+
+    public void download() {
+        if (data == null) {
+            return;
+        }
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        HttpServletResponse response = (HttpServletResponse) facesContext.getExternalContext().getResponse();
+        response.setContentType(contentType);
+        response.setContentLength(data.length);
+        response.setHeader("Content-disposition", "attachment; filename=\""
+                + getFileName() + "\"");
+        try {
+            OutputStream out = response.getOutputStream();
+            out.write(data);
+            out.flush();
+            facesContext.responseComplete();
+        } catch (IOException ex) {
+            LOG.error(".download()", ex);
+            FacesMessages.instance().add("Erro ao descarregar o arquivo: "
+                    + fileName);
+        }
+    }
 
 }
