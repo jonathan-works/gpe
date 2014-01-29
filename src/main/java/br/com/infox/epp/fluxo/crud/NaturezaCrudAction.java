7c33f82e3522e2cd785a86746e9f40c0a0e3c32f
erikliberal <erikliberal@infox.com.br>
2014-01-29 08:48:07 -0300

    Adição de interface Serializable

--- src/main/java/br/com/infox/epp/fluxo/crud/NaturezaCrudAction.java
+++ src/main/java/br/com/infox/epp/fluxo/crud/NaturezaCrudAction.java
@@ -1,13 +1,16 @@
 package br.com.infox.epp.fluxo.crud;
 
+import java.io.Serializable;
+
 import org.jboss.seam.annotations.Name;
 
 import br.com.infox.core.crud.AbstractCrudAction;
 import br.com.infox.epp.fluxo.entity.Natureza;
 
 @Name(NaturezaCrudAction.NAME)
-public class NaturezaCrudAction extends AbstractCrudAction<Natureza> {
+public class NaturezaCrudAction extends AbstractCrudAction<Natureza> implements Serializable {
     
+    private static final long serialVersionUID = 1L;
     public static final String NAME = "naturezaCrudAction";
 
 }
