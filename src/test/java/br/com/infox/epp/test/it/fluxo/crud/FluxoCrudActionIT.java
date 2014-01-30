10d66b2e588f72e2e72146cee47c78f23ff2d9ad
erikliberal <erikliberal@infox.com.br>
2014-01-30 09:54:05 -0300

    Adição de teste de integração para FluxoCrudActionIT

--- src/test/java/br/com/infox/epp/test/it/fluxo/crud/FluxoCrudActionIT.java
+++ src/test/java/br/com/infox/epp/test/it/fluxo/crud/FluxoCrudActionIT.java
@@ -5,7 +5,11 @@ import static br.com.infox.core.constants.LengthConstants.DESCRICAO_PEQUENA;
 import static java.lang.Boolean.FALSE;
 import static java.lang.Boolean.TRUE;
 import static java.text.MessageFormat.format;
+import static org.junit.Assert.*;
+import static br.com.infox.core.action.AbstractAction.*;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
 
@@ -26,6 +30,13 @@ import br.com.infox.epp.test.infra.ArquillianSeamTestSetup;
 @RunWith(Arquillian.class)
 public class FluxoCrudActionIT extends AbstractGenericCrudTest<Fluxo> {
     
+    private static final String FIELD_ATIVO = "ativo";
+    private static final String FIELD_PUBLICADO = "publicado";
+    private static final String FIELD_DT_FIM = "dataFimPublicacao";
+    private static final String FIELD_DT_INICIO = "dataInicioPublicacao";
+    private static final String FIELD_PRAZO = "qtPrazo";
+    private static final String FIELD_DESC = "fluxo";
+    private static final String FIELD_CODIGO = "codFluxo";
     private static final Boolean[] booleans = {TRUE, FALSE};
     private static final Boolean[] allBooleans = {TRUE, FALSE, null};
     
@@ -43,14 +54,14 @@ public class FluxoCrudActionIT extends AbstractGenericCrudTest<Fluxo> {
     }
 
     @Override
-    protected void initEntity(Fluxo entity, ICrudActions<Fluxo> crudActions) {
-        crudActions.setEntityValue("codFluxo", entity.getCodFluxo());//* validator
-        crudActions.setEntityValue("fluxo", entity.getFluxo()); //*
-        crudActions.setEntityValue("qtPrazo", entity.getQtPrazo()); //*
-        crudActions.setEntityValue("dataInicioPublicacao", entity.getDataInicioPublicacao());//*
-        crudActions.setEntityValue("dataFimPublicacao", entity.getDataFimPublicacao());
-        crudActions.setEntityValue("publicado", entity.getPublicado());//*
-        crudActions.setEntityValue("ativo", entity.getAtivo());//*
+    protected void initEntity(final Fluxo entity, final ICrudActions<Fluxo> crudActions) {
+        crudActions.setEntityValue(FIELD_CODIGO, entity.getCodFluxo());//* validator
+        crudActions.setEntityValue(FIELD_DESC, entity.getFluxo()); //*
+        crudActions.setEntityValue(FIELD_PRAZO, entity.getQtPrazo()); //*
+        crudActions.setEntityValue(FIELD_DT_INICIO, entity.getDataInicioPublicacao());//*
+        crudActions.setEntityValue(FIELD_DT_FIM, entity.getDataFimPublicacao());
+        crudActions.setEntityValue(FIELD_PUBLICADO, entity.getPublicado());//*
+        crudActions.setEntityValue(FIELD_ATIVO, entity.getAtivo());//*
     }
     
     private static int id=0;
@@ -64,9 +75,9 @@ public class FluxoCrudActionIT extends AbstractGenericCrudTest<Fluxo> {
         final GregorianCalendar currentDate = new GregorianCalendar();
         final Date dataInicio = currentDate.getTime();
         final Date[] datasFim = {null, getIncrementedDate(currentDate,GregorianCalendar.DAY_OF_YEAR,20)};
-        for(Date dataFim : datasFim) {
-            for(Boolean publicado: allBooleans) {
-                for (Boolean ativo:booleans) {
+        for(final Date dataFim : datasFim) {
+            for(final Boolean publicado: allBooleans) {
+                for (final Boolean ativo:booleans) {
                     final String codigo = generateName("persistSuccessFluxo");
                     persistSuccess.runTest(new Fluxo(codigo,codigo.replace('.', ' '), 5, dataInicio, dataFim, publicado, ativo));
                 }
@@ -88,8 +99,8 @@ public class FluxoCrudActionIT extends AbstractGenericCrudTest<Fluxo> {
         final GregorianCalendar currentDate = new GregorianCalendar();
         final Date dataInicio = currentDate.getTime();
         final Date[] datasFim = {null, getIncrementedDate(currentDate,GregorianCalendar.DAY_OF_YEAR,20)};
-        for(Date dataFim : datasFim) {
-            for(Boolean publicado : allBooleans) {
+        for(final Date dataFim : datasFim) {
+            for(final Boolean publicado : allBooleans) {
                 final String codigo = generateName("persistFailFluxo");
                 persistFail.runTest(new Fluxo(codigo,codigo.replace('.', ' '), 20, dataInicio, dataFim, publicado, null));
             }
@@ -99,8 +110,8 @@ public class FluxoCrudActionIT extends AbstractGenericCrudTest<Fluxo> {
     private void persistFailDataFimTest() throws Exception {
         final GregorianCalendar currentDate = new GregorianCalendar();
         final Date dataInicio = currentDate.getTime();
-        for(Boolean publicado : allBooleans) {
-            for (Boolean ativo : booleans) {
+        for(final Boolean publicado : allBooleans) {
+            for (final Boolean ativo : booleans) {
                 final String codigo = generateName("persistFailFluxo");
                 persistFail.runTest(new Fluxo(codigo,codigo.replace('.', ' '), 20, dataInicio, getIncrementedDate(currentDate,GregorianCalendar.DAY_OF_YEAR,-20), publicado, ativo));
             }
@@ -111,9 +122,9 @@ public class FluxoCrudActionIT extends AbstractGenericCrudTest<Fluxo> {
         final GregorianCalendar currentDate = new GregorianCalendar();
         final Date dataInicio = currentDate.getTime();
         final Date[] datasFim = {null, getIncrementedDate(currentDate,GregorianCalendar.DAY_OF_YEAR,20)};
-        for(Date dataFim : datasFim) {
-            for(Boolean publicado : allBooleans) {
-                for (Boolean ativo : booleans) {
+        for(final Date dataFim : datasFim) {
+            for(final Boolean publicado : allBooleans) {
+                for (final Boolean ativo : booleans) {
                     final String codigo = generateName("persistFailFluxo");
                     persistFail.runTest(new Fluxo(codigo,codigo.replace('.', ' '), null, dataInicio, dataFim, publicado, ativo));
                 }
@@ -122,9 +133,9 @@ public class FluxoCrudActionIT extends AbstractGenericCrudTest<Fluxo> {
     }
 
     private void persistFailDataInicioTest() throws Exception {
-        for(Date dataFim : new Date[]{null, getIncrementedDate(new GregorianCalendar(), GregorianCalendar.DAY_OF_YEAR, 20)}) {
-            for(Boolean publicado: allBooleans) {
-                for (Boolean ativo : booleans) {
+        for(final Date dataFim : new Date[]{null, getIncrementedDate(new GregorianCalendar(), GregorianCalendar.DAY_OF_YEAR, 20)}) {
+            for(final Boolean publicado: allBooleans) {
+                for (final Boolean ativo : booleans) {
                     final String codigo = generateName("persistFailFluxo");
                     persistFail.runTest(new Fluxo(codigo,codigo.replace('.', ' '), 5, null, dataFim, publicado, ativo));
                 }
@@ -133,13 +144,13 @@ public class FluxoCrudActionIT extends AbstractGenericCrudTest<Fluxo> {
     }
 
     private void persistFailFluxoTest() throws Exception {
-        for(String fluxo : new String[]{null, "", fillStr("codigo",DESCRICAO_PADRAO+1)}) {
+        for(final String fluxo : new String[]{null, "", fillStr("codigo",DESCRICAO_PADRAO+1)}) {
             final GregorianCalendar currentDate = new GregorianCalendar();
             final Date dataInicio = currentDate.getTime();
             final Date[] datasFim = {null, getIncrementedDate(currentDate,GregorianCalendar.DAY_OF_YEAR, 20)};
-            for(Date dataFim : datasFim) {
-                for(Boolean publicado: allBooleans) {
-                    for (Boolean ativo:booleans) {
+            for(final Date dataFim : datasFim) {
+                for(final Boolean publicado: allBooleans) {
+                    for (final Boolean ativo:booleans) {
                         persistFail.runTest(new Fluxo(generateName("persistFailFluxo"), fluxo, 5, dataInicio, dataFim, publicado, ativo));
                     }
                 }
@@ -148,13 +159,13 @@ public class FluxoCrudActionIT extends AbstractGenericCrudTest<Fluxo> {
     }
 
     private void persistFailCodigoTest() throws Exception {
-        for(String codigo : new String[]{null, "", fillStr(generateName("persistFailFluxo"), DESCRICAO_PEQUENA+1)}) {
+        for(final String codigo : new String[]{null, "", fillStr(generateName("persistFailFluxo"), DESCRICAO_PEQUENA+1)}) {
             final GregorianCalendar currentDate = new GregorianCalendar();
             final Date dataInicio = currentDate.getTime();
             final Date[] datasFim = {null, getIncrementedDate(currentDate,GregorianCalendar.DAY_OF_YEAR, 20)};
-            for(Date dataFim : datasFim) {
-                for(Boolean publicado: allBooleans) {
-                    for (Boolean ativo:booleans) {
+            for(final Date dataFim : datasFim) {
+                for(final Boolean publicado: allBooleans) {
+                    for (final Boolean ativo:booleans) {
                         persistFail.runTest(new Fluxo(codigo,generateName("persistFailFluxo"), 5, dataInicio, dataFim, publicado, ativo));
                     }
                 }
@@ -162,6 +173,16 @@ public class FluxoCrudActionIT extends AbstractGenericCrudTest<Fluxo> {
         }
     }
 
+    private Date getIncrementedDate(final Date currentDate, final int field, final int ammount) {
+        final GregorianCalendar calendar = new GregorianCalendar();
+        if (currentDate == null) {
+            calendar.setTime(new Date());
+        } else {
+            calendar.setTime(currentDate);
+        }
+        return getIncrementedDate(calendar, field, ammount);
+    }
+    
     private Date getIncrementedDate(final GregorianCalendar currentDate, final int field, final int ammount) {
         currentDate.add(field, ammount);
         final Date dataFim = currentDate.getTime();
@@ -173,14 +194,137 @@ public class FluxoCrudActionIT extends AbstractGenericCrudTest<Fluxo> {
         final GregorianCalendar currentDate = new GregorianCalendar();
         final Date dataInicio = currentDate.getTime();
         final Date[] datasFim = {null, getIncrementedDate(currentDate,GregorianCalendar.DAY_OF_YEAR,20)};
-        for(Date dataFim : datasFim) {
-            for(Boolean publicado: allBooleans) {
-                for (Boolean ativo:booleans) {
+        for(final Date dataFim : datasFim) {
+            for(final Boolean publicado: allBooleans) {
+                for (final Boolean ativo:booleans) {
                     final String codigo = generateName("persistSuccessFluxo");
                     inactivateSuccess.runTest(new Fluxo(codigo,codigo.replace('.', ' '), 5, dataInicio, dataFim, publicado, ativo));
                 }
             }
         }
+    }
+    
+    /*
+     * TODO: TODO: construir teste de falha para inativação de fluxo
+     * após construir testes envolvendo naturezacategoriafluxo
+     * e inicialização de processos
+     */
+    public void inactivateFailTest() throws Exception {}
+    
+    private boolean compareObjects(final Object obj1, final Object obj2) {
+        return (obj1 == obj2) || ((obj1!=null) && obj1.equals(obj2));
+    }
+    
+    @Override
+    protected boolean compareEntityValues(final Fluxo entity,final ICrudActions<Fluxo> crudActions) {
+        final SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
+        final String dataHoje = formato.format(new Date());
+        final String dataInicio = formato.format(crudActions.getEntityValue(FIELD_DT_INICIO));
+        if (dataHoje.equals(dataInicio)){
+            entity.setPublicado(TRUE);
+        }
+        
+        return compareObjects(entity.getCodFluxo(), crudActions.getEntityValue(FIELD_CODIGO))
+                && compareObjects(entity.getFluxo(), crudActions.getEntityValue(FIELD_DESC))
+                && compareObjects(entity.getQtPrazo(), crudActions.getEntityValue(FIELD_PRAZO))
+                && compareObjects(entity.getDataInicioPublicacao(), crudActions.getEntityValue(FIELD_DT_INICIO))
+                && compareObjects(entity.getDataFimPublicacao(), crudActions.getEntityValue(FIELD_DT_FIM))
+                && compareObjects(entity.getPublicado(), crudActions.getEntityValue(FIELD_PUBLICADO))
+                && compareObjects(entity.getAtivo(), crudActions.getEntityValue(FIELD_ATIVO));
+    }
+    
+    @Test
+    public void updateSuccessTest() throws Exception {
+        final EntityActionContainer<Fluxo> actionContainer = new EntityActionContainer<Fluxo>() {
+            @Override
+            public void execute(final ICrudActions<Fluxo> crudActions) {
+                final Fluxo baseEntity = getEntity();
+                final Integer id = crudActions.getId();
+                crudActions.resetInstance(id);
+                baseEntity.setCodFluxo(updateStringField(crudActions, baseEntity, id, FIELD_CODIGO));
+                baseEntity.setFluxo(updateStringField(crudActions, baseEntity, id, FIELD_DESC));
+                baseEntity.setQtPrazo(updateIntegerField(crudActions, baseEntity, id, FIELD_PRAZO));
+                baseEntity.setDataInicioPublicacao(updateDateField(crudActions, baseEntity, id, FIELD_DT_INICIO, Calendar.DAY_OF_YEAR, -5));
+                baseEntity.setDataFimPublicacao(updateDateField(crudActions, baseEntity, id, FIELD_DT_FIM, Calendar.DAY_OF_YEAR, 5));
+                baseEntity.setPublicado(updateBooleanField(crudActions, baseEntity, id, FIELD_PUBLICADO));
+                baseEntity.setAtivo(updateBooleanField(crudActions, baseEntity, id, FIELD_ATIVO));
+            }
+
+            private Boolean updateBooleanField(final ICrudActions<Fluxo> crudActions,
+                    final Fluxo baseEntity, final Integer id, final String fieldName) {
+                assertTrue("entidade igual", compareEntityValues(baseEntity, crudActions));
+                final Boolean newValue = !((Boolean)crudActions.getEntityValue(fieldName));
+                crudActions.setEntityValue(fieldName, newValue);
+                assertEquals(UPDATED, UPDATED, crudActions.save());
+                crudActions.resetInstance(id);
+                assertFalse("entidade diferente", compareEntityValues(baseEntity, crudActions));
+                final Boolean entityValue = crudActions.getEntityValue(fieldName);
+                assertEquals("boolean are equal", entityValue, newValue);
+                return newValue;
+            }
+            
+            private Date updateDateField(final ICrudActions<Fluxo> crudActions,
+                    final Fluxo baseEntity, final Integer id, final String fieldName,
+                    final int dateField, final int increment) {
+                assertTrue("entidade igual", compareEntityValues(baseEntity, crudActions));
+                final Date entityValue = crudActions.getEntityValue(fieldName);
+                final Date newEntityValue = getIncrementedDate(entityValue, dateField, increment);
+                crudActions.setEntityValue(fieldName, newEntityValue);
+                assertEquals(UPDATED, UPDATED, crudActions.save());
+                crudActions.resetInstance(id);
+                assertFalse("entidade igual", compareEntityValues(baseEntity, crudActions));
+                assertEquals("valor de data correto", crudActions.getEntityValue(fieldName), newEntityValue);
+                return newEntityValue;
+            }
+            
+            private Integer updateIntegerField(final ICrudActions<Fluxo> crudActions,
+                    final Fluxo baseEntity, final Integer id, final String fieldName) {
+                assertTrue("entidade igual", compareEntityValues(baseEntity, crudActions));
+                final Integer novoPrazo = ((Integer)crudActions.getEntityValue(fieldName))+5;
+                crudActions.setEntityValue(fieldName, novoPrazo);
+                assertEquals(UPDATED, UPDATED, crudActions.save());
+                crudActions.resetInstance(id);
+                assertFalse("entidade diferente", compareEntityValues(baseEntity, crudActions));
+                assertEquals("valor de prazo correto", crudActions.getEntityValue(fieldName), novoPrazo);
+                return novoPrazo;
+            }
+
+            private String updateStringField(final ICrudActions<Fluxo> crudActions,
+                    final Fluxo baseEntity, final Integer id,
+                    final String fieldName) {
+                assertTrue("entidade igual", compareEntityValues(baseEntity, crudActions));
+                final String suffix = ".changed";
+                crudActions.setEntityValue(fieldName, crudActions.getEntityValue(fieldName)+suffix);
+                assertEquals(UPDATED, UPDATED, crudActions.save());
+                crudActions.resetInstance(id);
+                assertFalse("entidade diferente", compareEntityValues(baseEntity, crudActions));
+                final String entityValue = crudActions.getEntityValue(fieldName);
+                assertTrue("entityValue ends with "+ suffix, entityValue.endsWith(suffix));
+                return entityValue;
+            }
+        };
+        final GregorianCalendar currentDate = new GregorianCalendar();
+        final Date dataInicio = currentDate.getTime();
+        final Date[] datasFim = {null, getIncrementedDate(currentDate,GregorianCalendar.DAY_OF_YEAR,20)};
+        for(final Date dataFim : datasFim) {
+            for(final Boolean publicado: allBooleans) {
+                for (final Boolean ativo:booleans) {
+                    final String codigo = generateName("updateSuccessFluxo");
+                    persistSuccess.runTest(actionContainer,new Fluxo(codigo,codigo.replace('.', ' '), 5, dataInicio, dataFim, publicado, ativo));
+                }
+            }
+        }
+    }
+    
+    @Test
+    public void updateFailTest() throws Exception {
+        final EntityActionContainer<Fluxo> actionContainer = new EntityActionContainer<Fluxo>() {
+            @Override
+            public void execute(final ICrudActions<Fluxo> crudActions) {
+                // TODO Auto-generated method stub
+                
+            }
+        };
     }
     
 }
