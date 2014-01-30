862f7fe18f2f72fa263072496fbf95d1214c300e
jonasbezerra <jonasbezerra@infox.com.br>
2014-01-30 11:41:39 -0300

    [32147] Formatação

--- src/main/java/br/com/infox/core/dao/GenericDAO.java
+++ src/main/java/br/com/infox/core/dao/GenericDAO.java
@@ -7,47 +7,41 @@ import org.jboss.seam.annotations.AutoCreate;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Scope;
 
-/**
- * DAO generico para consultas, persistencia entre outros.
- * 
- * @author Daniel
- * 
- */
 @Name(GenericDAO.NAME)
 @Scope(ScopeType.EVENT)
 @AutoCreate
 public class GenericDAO extends DAO<Object, Object> {
 
-	public static final String NAME = "genericDAO";
+    public static final String NAME = "genericDAO";
     private static final long serialVersionUID = 2513102779632819212L;
     private static final String MSG_INVALID_METHOD = "Operação inválida no GenericDAO";
-    
+
     @Override
     public Object find(Object id) {
-    	throw new UnsupportedOperationException(MSG_INVALID_METHOD);
+        throw new UnsupportedOperationException(MSG_INVALID_METHOD);
     }
-    
+
     @Override
     public List<Object> findAll() {
-    	throw new UnsupportedOperationException(MSG_INVALID_METHOD);
+        throw new UnsupportedOperationException(MSG_INVALID_METHOD);
     }
-    
+
     @Override
     public Object getReference(Object primaryKey) {
-    	throw new UnsupportedOperationException(MSG_INVALID_METHOD);
+        throw new UnsupportedOperationException(MSG_INVALID_METHOD);
     }
-    
+
     public <T> T find(Class<T> entityClass, Object id) {
-    	return getEntityManager().find(entityClass, id);
+        return getEntityManager().find(entityClass, id);
     }
-    
+
     public <T> List<T> findAll(Class<T> entityClass) {
-    	final StringBuilder sb = new StringBuilder();
-		sb.append("select o from ").append(entityClass.getName()).append(" o");
-		return getEntityManager().createQuery(sb.toString(), entityClass).getResultList();
+        final StringBuilder sb = new StringBuilder();
+        sb.append("select o from ").append(entityClass.getName()).append(" o");
+        return getEntityManager().createQuery(sb.toString(), entityClass).getResultList();
     }
-    
+
     public <T> T getReference(Class<T> entityClass, Object id) {
-    	return getEntityManager().getReference(entityClass, id);
+        return getEntityManager().getReference(entityClass, id);
     }
 }
