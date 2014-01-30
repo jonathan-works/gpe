bc0b1ae0a407cfbd06d887907b24fcf8abeeb256
jonasbezerra <jonasbezerra@infox.com.br>
2014-01-30 09:46:22 -0300

    [35083] Início da criação de um componente específico para uploade de documentos do processo - ProcessoDocumentoUploader

---
+++ src/main/java/br/com/infox/epp/processo/documento/component/ProcessoDocumentoUploader.java
@@ -0,0 +1,22 @@
+package br.com.infox.epp.processo.documento.component;
+
+import org.jboss.seam.annotations.Name;
+
+import br.com.infox.epp.processo.documento.entity.ProcessoDocumento;
+
+@Name(ProcessoDocumentoUploader.NAME)
+public class ProcessoDocumentoUploader {
+
+    public static final String NAME = "processoDocumentoUploader";
+    
+    private ProcessoDocumento processoDocumento;
+
+    public ProcessoDocumento getProcessoDocumento() {
+        return processoDocumento;
+    }
+
+    public void setProcessoDocumento(ProcessoDocumento processoDocumento) {
+        this.processoDocumento = processoDocumento;
+    }
+    
+}
