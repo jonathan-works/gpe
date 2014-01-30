c5f5295b3076dbac57b153db38e4d7d6e5f92881
jonasbezerra <jonasbezerra@infox.com.br>
2014-01-30 10:38:26 -0300

    [00000] Testando notificações do sonar

--- src/main/java/br/com/infox/epp/processo/documento/component/ProcessoDocumentoUploader.java
+++ src/main/java/br/com/infox/epp/processo/documento/component/ProcessoDocumentoUploader.java
@@ -9,14 +9,14 @@ public class ProcessoDocumentoUploader {
 
     public static final String NAME = "processoDocumentoUploader";
     
-    private ProcessoDocumento processoDocumento;
-
-    public ProcessoDocumento getProcessoDocumento() {
-        return processoDocumento;
-    }
-
-    public void setProcessoDocumento(ProcessoDocumento processoDocumento) {
-        this.processoDocumento = processoDocumento;
-    }
+//    private ProcessoDocumento processoDocumento;
+//
+//    public ProcessoDocumento getProcessoDocumento() {
+//        return processoDocumento;
+//    }
+//
+//    public void setProcessoDocumento(ProcessoDocumento processoDocumento) {
+//        this.processoDocumento = processoDocumento;
+//    }
     
 }
