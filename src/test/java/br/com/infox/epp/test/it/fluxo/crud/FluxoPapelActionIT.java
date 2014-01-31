a0e572648801cc9bf12b73ea320b521985117b58
erikliberal <erikliberal@infox.com.br>
2014-01-31 12:44:37 -0300

    Ajustes organizacionais e nos imports do FluxoPapelActionIT

--- src/test/java/br/com/infox/epp/test/it/fluxo/crud/FluxoPapelActionIT.java
+++ src/test/java/br/com/infox/epp/test/it/fluxo/crud/FluxoPapelActionIT.java
@@ -5,11 +5,11 @@ import static br.com.infox.core.action.AbstractAction.REMOVED;
 import static java.lang.Boolean.FALSE;
 import static java.lang.Boolean.TRUE;
 import static java.text.MessageFormat.format;
-import static junit.framework.Assert.assertEquals;
-import static junit.framework.Assert.assertFalse;
-import static junit.framework.Assert.assertNotNull;
-import static junit.framework.Assert.assertNull;
-import static junit.framework.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
 import java.util.Date;
