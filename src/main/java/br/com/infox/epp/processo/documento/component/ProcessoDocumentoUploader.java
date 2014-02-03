35dc7e1c6008caab7f256b804e58d34a80e02f01
jonasbezerra <jonasbezerra@infox.com.br>
2014-02-03 11:18:20 -0300

    [35083] Prosseguindo com a reimplementação da anexação de documentos

--- src/main/java/br/com/infox/epp/processo/documento/component/ProcessoDocumentoUploader.java
+++ src/main/java/br/com/infox/epp/processo/documento/component/ProcessoDocumentoUploader.java
@@ -1,15 +1,24 @@
 package br.com.infox.epp.processo.documento.component;
 
+import java.util.List;
+
+import org.jboss.seam.ScopeType;
 import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Scope;
+import org.richfaces.event.FileUploadEvent;
+import org.richfaces.event.FileUploadListener;
 
 import br.com.infox.epp.processo.documento.entity.ProcessoDocumento;
+import br.com.infox.epp.processo.documento.entity.ProcessoDocumentoBin;
 
 @Name(ProcessoDocumentoUploader.NAME)
-public class ProcessoDocumentoUploader {
+@Scope(ScopeType.CONVERSATION)
+public class ProcessoDocumentoUploader implements FileUploadListener{
 
     public static final String NAME = "processoDocumentoUploader";
     
     private ProcessoDocumento processoDocumento;
+    private List<ProcessoDocumento> processoDocumentosDaSessao;
 
     public ProcessoDocumento getProcessoDocumento() {
         return processoDocumento;
@@ -18,5 +27,35 @@ public class ProcessoDocumentoUploader {
     public void setProcessoDocumento(ProcessoDocumento processoDocumento) {
         this.processoDocumento = processoDocumento;
     }
+
+    public List<ProcessoDocumento> getProcessoDocumentosDaSessao() {
+        return processoDocumentosDaSessao;
+    }
+
+    public void setProcessoDocumentosDaSessao(
+            List<ProcessoDocumento> processoDocumentosDaSessao) {
+        this.processoDocumentosDaSessao = processoDocumentosDaSessao;
+    }
+
+    @Override
+    public void processFileUpload(FileUploadEvent arg0) {
+        // TODO Auto-generated method stub
+        
+    }
     
+    
+    public void onClickTabAnexar(){
+        setProcessoDocumento(new ProcessoDocumento());
+        getProcessoDocumento().setProcessoDocumentoBin(new ProcessoDocumentoBin());
+        
+    }
+    
+    public void persist(){
+        System.out.println(processoDocumento);
+        System.out.println(processoDocumento.getProcessoDocumentoBin());
+        System.out.println(processoDocumento.getProcessoDocumentoBin().getModeloDocumento());
+        System.out.println("--------------------");
+        System.out.println("persisted");
+        
+    }
 }
