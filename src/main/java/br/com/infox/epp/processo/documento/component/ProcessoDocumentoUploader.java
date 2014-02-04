b5d6023f80b376572d7ace31494cbce407048c1e
jonasbezerra <jonasbezerra@infox.com.br>
2014-02-04 11:55:45 -0300

    [35083] Colocando atributos necessários em ProcessoDocumentoUploader

--- src/main/java/br/com/infox/epp/processo/documento/component/ProcessoDocumentoUploader.java
+++ src/main/java/br/com/infox/epp/processo/documento/component/ProcessoDocumentoUploader.java
@@ -9,20 +9,29 @@ import org.richfaces.event.FileUploadEvent;
 import org.richfaces.event.FileUploadListener;
 import org.richfaces.model.UploadedFile;
 
+import br.com.infox.epp.processo.documento.entity.ProcessoDocumento;
 import br.com.infox.epp.processo.entity.Processo;
 
 @Name(ProcessoDocumentoUploader.NAME)
 @Scope(ScopeType.CONVERSATION)
 public class ProcessoDocumentoUploader implements FileUploadListener {
-    
+
     public static final String NAME = "processoDocumentoUploader";
-    
+
     private byte[] data;
     private String fileName;
     private Integer size;
     private String contentType;
     private Processo processo;
-    
+    private ProcessoDocumento processoDocumento;
+
+    public ProcessoDocumento getProcessoDocumento() {
+        return processoDocumento;
+    }
+
+    public void setProcessoDocumento(ProcessoDocumento processoDocumento) {
+        this.processoDocumento = processoDocumento;
+    }
 
     @Override
     public void processFileUpload(FileUploadEvent fileUploadEvent) {
