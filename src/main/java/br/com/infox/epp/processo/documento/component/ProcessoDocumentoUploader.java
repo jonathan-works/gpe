5ec4bde6e5841da8de502ea77fc3bfdc8bd26f03
jonasbezerra <jonasbezerra@infox.com.br>
2014-02-04 11:36:49 -0300

    [35083] Copiados atributos necessário com o objeivo de substituir o FileHome

--- src/main/java/br/com/infox/epp/processo/documento/component/ProcessoDocumentoUploader.java
+++ src/main/java/br/com/infox/epp/processo/documento/component/ProcessoDocumentoUploader.java
@@ -3,19 +3,35 @@ package br.com.infox.epp.processo.documento.component;
 import org.jboss.seam.ScopeType;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Scope;
+import org.jboss.seam.faces.FacesMessages;
+import org.jboss.seam.international.Messages;
 import org.richfaces.event.FileUploadEvent;
 import org.richfaces.event.FileUploadListener;
+import org.richfaces.model.UploadedFile;
+
+import br.com.infox.epp.processo.entity.Processo;
 
 @Name(ProcessoDocumentoUploader.NAME)
 @Scope(ScopeType.CONVERSATION)
 public class ProcessoDocumentoUploader implements FileUploadListener {
     
     public static final String NAME = "processoDocumentoUploader";
+    
+    private byte[] data;
+    private String fileName;
+    private Integer size;
+    private String contentType;
+    private Processo processo;
+    
 
     @Override
-    public void processFileUpload(FileUploadEvent arg0) {
-        // TODO Auto-generated method stub
-
+    public void processFileUpload(FileUploadEvent fileUploadEvent) {
+        final UploadedFile ui = fileUploadEvent.getUploadedFile();
+        this.data = ui.getData();
+        this.fileName = ui.getName();
+        this.size = Long.valueOf(ui.getSize()).intValue();
+        this.contentType = ui.getContentType();
+        FacesMessages.instance().add(Messages.instance().get("processoDocumento.doneLabel"));
     }
 
 }
