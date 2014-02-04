ccd38930385efc7f0a168331fc0d22fd4fda72c0
jonasbezerra <jonasbezerra@infox.com.br>
2014-02-04 09:39:39 -0300

    [35083] Criação de AnexoController

---
+++ src/main/java/br/com/infox/epp/processo/documento/AnexoController.java
@@ -0,0 +1,42 @@
+package br.com.infox.epp.processo.documento;
+
+import java.util.List;
+
+import org.jboss.seam.annotations.Name;
+
+import br.com.infox.core.controller.AbstractController;
+import br.com.infox.epp.processo.documento.entity.ProcessoDocumento;
+import br.com.infox.epp.processo.entity.Processo;
+
+@Name(AnexoController.NAME)
+public class AnexoController extends AbstractController {
+
+    private static final long serialVersionUID = 1L;
+    public static final String NAME = "anexoController";
+    
+    private Processo processo;
+    private List<ProcessoDocumento> processoDocumentosDaSessao;
+    
+    public Processo getProcesso() {
+        return processo;
+    }
+
+    public void setProcesso(Processo processo) {
+        this.processo = processo;
+    }
+
+    public List<ProcessoDocumento> getProcessoDocumentosDaSessao() {
+        return processoDocumentosDaSessao;
+    }
+
+    public void setProcessoDocumentosDaSessao(
+            List<ProcessoDocumento> processoDocumentosDaSessao) {
+        this.processoDocumentosDaSessao = processoDocumentosDaSessao;
+    }
+
+    @Override
+    public void onClickFormTab() {
+        super.onClickFormTab();
+    }
+
+}
