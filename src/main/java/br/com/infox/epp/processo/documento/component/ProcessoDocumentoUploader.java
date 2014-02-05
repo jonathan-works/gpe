7125f1f72766030b7ff63f9a65f220909d561c33
jonasbezerra <jonasbezerra@infox.com.br>
2014-02-05 09:36:30 -0300

    [32147] Ajuste de import

--- src/main/java/br/com/infox/epp/processo/documento/component/ProcessoDocumentoUploader.java
+++ src/main/java/br/com/infox/epp/processo/documento/component/ProcessoDocumentoUploader.java
@@ -7,7 +7,6 @@ import org.jboss.seam.ScopeType;
 import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Scope;
-import org.jboss.seam.annotations.Transactional;
 import org.jboss.seam.faces.FacesMessages;
 import org.jboss.seam.international.Messages;
 import org.richfaces.event.FileUploadEvent;
