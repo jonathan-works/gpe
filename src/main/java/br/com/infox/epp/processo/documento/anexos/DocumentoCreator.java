555746bbb2e1eee7e93ea1656afa2d5e240d9ec4
jonasbezerra <jonasbezerra@infox.com.br>
2014-02-05 11:21:37 -0300

    [35083] Criação de classe abstrata DocumentoCreator

---
+++ src/main/java/br/com/infox/epp/processo/documento/anexos/DocumentoCreator.java
@@ -0,0 +1,35 @@
+package br.com.infox.epp.processo.documento.anexos;
+
+import br.com.infox.epp.processo.documento.entity.ProcessoDocumento;
+import br.com.infox.epp.processo.documento.entity.ProcessoDocumentoBin;
+import br.com.infox.epp.processo.entity.Processo;
+
+abstract class DocumentoCreator {
+    
+    private Processo processo;
+    private ProcessoDocumento processoDocumento;
+    
+    public Processo getProcesso() {
+        return processo;
+    }
+
+    public void setProcesso(Processo processo) {
+        this.processo = processo;
+    }
+
+    public ProcessoDocumento getProcessoDocumento() {
+        return processoDocumento;
+    }
+
+    public void setProcessoDocumento(ProcessoDocumento processoDocumento) {
+        this.processoDocumento = processoDocumento;
+    }
+    
+    public void clear() {
+        setProcessoDocumento(new ProcessoDocumento());
+        getProcessoDocumento().setProcessoDocumentoBin(new ProcessoDocumentoBin());
+    }
+    
+    public abstract ProcessoDocumento notificarCriacaoDeDocumento();
+
+}
