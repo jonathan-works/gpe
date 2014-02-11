36574f466e0d8e1c7af4dd22f091d51716972fed
jonasbezerra <jonasbezerra@infox.com.br>
2014-02-11 16:07:43 -0300

    [35083] Remoção de métodos de validação que foram movidos para naturezaCrudAction

--- src/main/java/br/com/infox/epp/fluxo/dao/NaturezaDAO.java
+++ src/main/java/br/com/infox/epp/fluxo/dao/NaturezaDAO.java
@@ -4,7 +4,6 @@ import org.jboss.seam.annotations.AutoCreate;
 import org.jboss.seam.annotations.Name;
 
 import br.com.infox.core.dao.DAO;
-import br.com.infox.core.persistence.DAOException;
 import br.com.infox.epp.fluxo.entity.Natureza;
 
 @Name(NaturezaDAO.NAME)
@@ -13,25 +12,5 @@ public class NaturezaDAO extends DAO<Natureza> {
 
     private static final long serialVersionUID = -7175831474709085125L;
     public static final String NAME = "naturezaDAO";
-
-    @Override
-    public Natureza persist(Natureza object) throws DAOException {
-        validarNecessidadeDePartes(object);
-        return super.persist(object);
-    }
-
-    @Override
-    public Natureza update(Natureza object) throws DAOException {
-        validarNecessidadeDePartes(object);
-        return super.update(object);
-    }
-
-    private void validarNecessidadeDePartes(Natureza natureza) throws DAOException {
-        if (natureza.getHasPartes()
-                && (natureza.getTipoPartes() == null || natureza.getTipoPartes() == null)) {
-            throw new DAOException("Se a natureza possui partes então nem o tipo das partes "
-                    + "nem a quantidade podem ser nulos");
-        }
-    }
 
 }
