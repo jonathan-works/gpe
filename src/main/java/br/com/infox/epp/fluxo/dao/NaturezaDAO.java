74a4c4dba70048719280cb4889069a9d68374d9f
jonasbezerra <jonasbezerra@infox.com.br>
2014-02-11 09:38:40 -0300

    [35083] Validação de persit/update em NaturezaDAO

--- src/main/java/br/com/infox/epp/fluxo/dao/NaturezaDAO.java
+++ src/main/java/br/com/infox/epp/fluxo/dao/NaturezaDAO.java
@@ -4,6 +4,7 @@ import org.jboss.seam.annotations.AutoCreate;
 import org.jboss.seam.annotations.Name;
 
 import br.com.infox.core.dao.DAO;
+import br.com.infox.core.persistence.DAOException;
 import br.com.infox.epp.fluxo.entity.Natureza;
 
 @Name(NaturezaDAO.NAME)
@@ -12,5 +13,25 @@ public class NaturezaDAO extends DAO<Natureza> {
 
     private static final long serialVersionUID = -7175831474709085125L;
     public static final String NAME = "naturezaDAO";
+
+    @Override
+    public Natureza persist(Natureza object) throws DAOException {
+        validarNecessidadeDePartes(object);
+        return super.persist(object);
+    }
+
+    @Override
+    public Natureza update(Natureza object) throws DAOException {
+        validarNecessidadeDePartes(object);
+        return super.update(object);
+    }
+
+    private void validarNecessidadeDePartes(Natureza natureza) throws DAOException {
+        if (natureza.getHasPartes()
+                && (natureza.getTipoPartes() == null || natureza.getTipoPartes() == null)) {
+            throw new DAOException("Se a natureza possui partes então nem o tipo das partes "
+                    + "nem a quantidade podem ser nulos");
+        }
+    }
 
 }
