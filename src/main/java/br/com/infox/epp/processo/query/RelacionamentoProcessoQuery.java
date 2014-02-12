181bf30dec10a77e21dfe3f4001414549e7cbf2b
erikliberal <erikliberal@infox.com.br>
2014-02-12 12:49:30 -0300

    Adição de interfaces Query contendo constantes para as novas entidades para relacionamento de processo

---
+++ src/main/java/br/com/infox/epp/processo/query/RelacionamentoProcessoQuery.java
@@ -0,0 +1,10 @@
+package br.com.infox.epp.processo.query;
+
+public interface RelacionamentoProcessoQuery {
+    String TABLE_NAME="tb_relacionamento_processo";
+    String NOME_USUARIO = "nm_usuario";
+    String MOTIVO = "ds_motivo";
+    String DATA_RELACIONAMENTO = "dt_relacionamento";
+    String ID_RELACIONAMENTO_PROCESSO = "id_relacionamento_processo";
+    String SEQUENCE_NAME = "public.sq_tb_relacionamento_processo";
+}
---
+++ src/main/java/br/com/infox/epp/processo/query/TipoRelacionamentoProcessoQuery.java
@@ -0,0 +1,10 @@
+package br.com.infox.epp.processo.query;
+
+public interface TipoRelacionamentoProcessoQuery {
+
+    String TIPO_RELACIONAMENTO = "ds_tipo_relacionamento_processo";
+    String SEQUENCE_NAME = "public.sq_tb_tipo_relacionamento_processo";
+    String ID_TIPO_RELACIONAMENTO_PROCESSO = "id_tipo_relacionamento_processo";
+    String TABLE_NAME = "tb_tipo_relacionamento_processo";
+
+}
