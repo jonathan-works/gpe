6c134d50cb4dbfa92f5690518c221be52e755674
erikliberal <erikliberal@infox.com.br>
2014-02-13 12:54:39 -0300

    Adição de busca que retorna um Relacionamento baseado em seu processo

--- src/main/java/br/com/infox/epp/processo/query/RelacionamentoProcessoQuery.java
+++ src/main/java/br/com/infox/epp/processo/query/RelacionamentoProcessoQuery.java
@@ -1,5 +1,7 @@
 package br.com.infox.epp.processo.query;
 
+import static br.com.infox.epp.processo.query.ProcessoQuery.ID_PROCESSO;
+
 public interface RelacionamentoProcessoQuery {
     String TABLE_NAME="tb_relacionamento_processo";
     String NOME_USUARIO = "nm_usuario";
@@ -7,4 +9,8 @@ public interface RelacionamentoProcessoQuery {
     String DATA_RELACIONAMENTO = "dt_relacionamento";
     String ID_RELACIONAMENTO_PROCESSO = "id_relacionamento_processo";
     String SEQUENCE_NAME = "public.sq_tb_relacionamento_processo";
+    
+    String RELACIONAMENTO_BY_PROCESSO = "relacionamentoByProcesso";
+    String RELACIONAMENTO_BY_PROCESSO_QUERY = "select r from RelacionamentoProcesso rp inner join rp.relacionamento r where rp.processo=:"+ID_PROCESSO;
+    String NUMERO_PROCESSO = "nr_processo";
 }
