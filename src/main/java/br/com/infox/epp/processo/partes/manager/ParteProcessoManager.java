e7089ca25e50f8140fc7b837fcc94e9785e77ec6
jonasbezerra <jonasbezerra@infox.com.br>
2014-02-14 09:57:38 -0300

    [35083] Refatoração do método de inclusão de partes no processo

--- src/main/java/br/com/infox/epp/processo/partes/manager/ParteProcessoManager.java
+++ src/main/java/br/com/infox/epp/processo/partes/manager/ParteProcessoManager.java
@@ -2,24 +2,16 @@ package br.com.infox.epp.processo.partes.manager;
 
 import javax.xml.bind.ValidationException;
 
-import org.jboss.seam.Component;
 import org.jboss.seam.annotations.AutoCreate;
 import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
-import org.jboss.seam.faces.FacesMessages;
-import org.jboss.seam.international.StatusMessage.Severity;
 
 import br.com.infox.core.manager.Manager;
 import br.com.infox.core.persistence.DAOException;
 import br.com.infox.epp.pessoa.dao.PessoaFisicaDAO;
 import br.com.infox.epp.pessoa.dao.PessoaJuridicaDAO;
-import br.com.infox.epp.pessoa.entity.PessoaFisica;
-import br.com.infox.epp.pessoa.entity.PessoaJuridica;
-import br.com.infox.epp.pessoa.home.PessoaFisicaHome;
-import br.com.infox.epp.pessoa.home.PessoaJuridicaHome;
-import br.com.infox.epp.pessoa.type.TipoPessoaEnum;
+import br.com.infox.epp.pessoa.entity.Pessoa;
 import br.com.infox.epp.processo.dao.ProcessoEpaDAO;
-import br.com.infox.epp.processo.entity.Processo;
 import br.com.infox.epp.processo.entity.ProcessoEpa;
 import br.com.infox.epp.processo.partes.dao.HistoricoParteProcessoDAO;
 import br.com.infox.epp.processo.partes.dao.ParteProcessoDAO;
@@ -49,42 +41,9 @@ public class ParteProcessoManager extends Manager<ParteProcessoDAO, ParteProcess
         update(parteProcesso);
         historicoParteProcessoDAO.persist(hpp);
     }
-
-    public void incluir(Processo processo, String tipoPessoa) throws DAOException {
-        ProcessoEpa processoEpa = processoEpaDAO.getProcessoEpaByProcesso(processo);
-        if ("F".equals(tipoPessoa) || "f".equals(tipoPessoa)) {
-            PessoaFisicaHome pf = (PessoaFisicaHome) Component.getInstance("pessoaFisicaHome");
-            PessoaFisica p = pf.getInstance();
-            if (p.getAtivo() == null) {
-                p.setAtivo(true);
-                p.setTipoPessoa(TipoPessoaEnum.F);
-                pessoaFisicaDAO.persist(p);
-                flush();
-            }
-            if (processoEpa.getPartes().contains(p)) {
-                FacesMessages.instance().add(Severity.ERROR, "Parte já cadastrada no processo");
-            } else {
-                persist(new ParteProcesso(processoEpa, p));
-            }
-            pf.setInstance(null);
-        } else if ("J".equals(tipoPessoa) || "j".equals(tipoPessoa)) {
-            PessoaJuridicaHome pj = (PessoaJuridicaHome) Component.getInstance("pessoaJuridicaHome");
-            PessoaJuridica p = pj.getInstance();
-            if (p.getAtivo() == null) {
-                p.setAtivo(true);
-                p.setTipoPessoa(TipoPessoaEnum.J);
-                pessoaJuridicaDAO.persist(p);
-                flush();
-            }
-            if (processoEpa.getPartes().contains(p)) {
-                FacesMessages.instance().add(Severity.ERROR, "Parte já cadastrada no processo");
-            } else {
-                persist(new ParteProcesso(processoEpa, p));
-            }
-            pj.setInstance(null);
-        } else {
-            return;
-        }
+    
+    public void incluir(ProcessoEpa processoEpa, Pessoa pessoa) throws DAOException {
+        persist(new ParteProcesso(processoEpa, pessoa));
     }
 
     public HistoricoParteProcesso restaurarParteProcesso(
