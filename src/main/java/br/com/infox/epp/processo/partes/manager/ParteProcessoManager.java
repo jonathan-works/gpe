daa2eed80be35983da5c991faa9c0d6dcaa43fc9
jonasbezerra <jonasbezerra@infox.com.br>
2014-02-14 15:34:43 -0300

    [35083] Remoção de injeção de managers que não mais utilizado

--- src/main/java/br/com/infox/epp/processo/partes/manager/ParteProcessoManager.java
+++ src/main/java/br/com/infox/epp/processo/partes/manager/ParteProcessoManager.java
@@ -1,21 +1,13 @@
 package br.com.infox.epp.processo.partes.manager;
 
-import javax.xml.bind.ValidationException;
-
 import org.jboss.seam.annotations.AutoCreate;
-import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
 
 import br.com.infox.core.manager.Manager;
 import br.com.infox.core.persistence.DAOException;
-import br.com.infox.epp.pessoa.dao.PessoaFisicaDAO;
-import br.com.infox.epp.pessoa.dao.PessoaJuridicaDAO;
 import br.com.infox.epp.pessoa.entity.Pessoa;
-import br.com.infox.epp.processo.dao.ProcessoEpaDAO;
 import br.com.infox.epp.processo.entity.ProcessoEpa;
-import br.com.infox.epp.processo.partes.dao.HistoricoParteProcessoDAO;
 import br.com.infox.epp.processo.partes.dao.ParteProcessoDAO;
-import br.com.infox.epp.processo.partes.entity.HistoricoParteProcesso;
 import br.com.infox.epp.processo.partes.entity.ParteProcesso;
 
 @Name(ParteProcessoManager.NAME)
@@ -24,15 +16,6 @@ public class ParteProcessoManager extends Manager<ParteProcessoDAO, ParteProcess
 
     public static final String NAME = "parteProcessoManager";
     private static final long serialVersionUID = 1L;
-
-    @In
-    private ProcessoEpaDAO processoEpaDAO;
-    @In
-    private PessoaFisicaDAO pessoaFisicaDAO;
-    @In
-    private PessoaJuridicaDAO pessoaJuridicaDAO;
-    @In
-    private HistoricoParteProcessoDAO historicoParteProcessoDAO;
 
     public void incluir(ProcessoEpa processoEpa, Pessoa pessoa) throws DAOException {
         persist(new ParteProcesso(processoEpa, pessoa));
