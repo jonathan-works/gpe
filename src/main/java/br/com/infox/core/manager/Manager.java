acff41e07a9691efedf27f37176247c1e4a78111
jonasbezerra <jonasbezerra@infox.com.br>
2014-02-14 14:36:12 -0300

    [32147] Formatação

--- src/main/java/br/com/infox/core/manager/Manager.java
+++ src/main/java/br/com/infox/core/manager/Manager.java
@@ -16,72 +16,73 @@ import br.com.infox.core.persistence.DAOException;
 
 public abstract class Manager<D extends DAO<T>, T> implements Serializable {
 
-	private static final long serialVersionUID = 1L;
-	private D dao;
-	
-	@SuppressWarnings(UNCHECKED)
-	@Create
-	public void init() {
-		this.dao = (D) Component.getInstance(getDaoName());
-	}
+    private static final long serialVersionUID = 1L;
+    private D dao;
 
-	public T persist(T o) throws DAOException {
-		return (T) dao.persist(o);
-	}
+    @SuppressWarnings(UNCHECKED)
+    @Create
+    public void init() {
+        this.dao = (D) Component.getInstance(getDaoName());
+    }
 
-	public T update(T o) throws DAOException {
-		return (T) dao.update(o);
-	}
+    public T persist(T o) throws DAOException {
+        return (T) dao.persist(o);
+    }
 
-	public T remove(T o) throws DAOException {
-		return (T) dao.remove(o);
-	}
+    public T update(T o) throws DAOException {
+        return (T) dao.update(o);
+    }
 
-	public T find(Object id) {
-		return dao.find(id);
-	}
+    public T remove(T o) throws DAOException {
+        return (T) dao.remove(o);
+    }
 
-	public List<T> findAll() {
-		return dao.findAll();
-	}
+    public T find(Object id) {
+        return dao.find(id);
+    }
 
-	public boolean contains(Object o) {
-		return dao.contains(o);
-	}
+    public List<T> findAll() {
+        return dao.findAll();
+    }
 
-	public T merge(T o) throws DAOException {
-		return dao.merge(o);
-	}
+    public boolean contains(Object o) {
+        return dao.contains(o);
+    }
 
-	public void detach(T o) {
-		dao.detach(o);
-	}
+    public T merge(T o) throws DAOException {
+        return dao.merge(o);
+    }
 
-	public void clear() {
-		dao.clear();
-	}
+    public void detach(T o) {
+        dao.detach(o);
+    }
 
-	public void flush() {
-	    dao.flush();
-	}
+    public void clear() {
+        dao.clear();
+    }
 
-	public void refresh(T o) {
-	    dao.refresh(o);
-	}
+    public void flush() {
+        dao.flush();
+    }
 
-	public Long getSingleResult(final String query, final Map<String, Object> params) {
-	    return (Long) dao.getSingleResult(query, params);
-	}
+    public void refresh(T o) {
+        dao.refresh(o);
+    }
 
-	protected D getDao() {
-		return this.dao;
-	}
+    public Long getSingleResult(final String query,
+            final Map<String, Object> params) {
+        return (Long) dao.getSingleResult(query, params);
+    }
 
-	@SuppressWarnings(UNCHECKED)
-	protected String getDaoName() {
-		ParameterizedType superType = (ParameterizedType) getClass().getGenericSuperclass();
-		Class<D> daoClass = (Class<D>) superType.getActualTypeArguments()[0];
-		Name name = daoClass.getAnnotation(Name.class);
-		return name.value();
-	}
+    protected D getDao() {
+        return this.dao;
+    }
+
+    @SuppressWarnings(UNCHECKED)
+    protected String getDaoName() {
+        ParameterizedType superType = (ParameterizedType) getClass().getGenericSuperclass();
+        Class<D> daoClass = (Class<D>) superType.getActualTypeArguments()[0];
+        Name name = daoClass.getAnnotation(Name.class);
+        return name.value();
+    }
 }
