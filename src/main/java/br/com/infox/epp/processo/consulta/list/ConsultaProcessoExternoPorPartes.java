30ec583fa66972c974cdfa4275202fe14b02462e
Gabriel Morrison Lima Dantas <gabriel@infox.com.br>
2014-02-14 11:37:35 -0300

    Alteração nos EntityLists de grids de processo, para receber o fragmento de condição responsável por filtrar de acordo com o sigilo

--- src/main/java/br/com/infox/epp/processo/consulta/list/ConsultaProcessoExternoPorPartes.java
+++ src/main/java/br/com/infox/epp/processo/consulta/list/ConsultaProcessoExternoPorPartes.java
@@ -9,6 +9,7 @@ import org.jboss.seam.annotations.Scope;
 import br.com.infox.core.list.EntityList;
 import br.com.infox.core.list.SearchCriteria;
 import br.com.infox.epp.processo.entity.ProcessoEpa;
+import br.com.infox.epp.processo.sigilo.manager.SigiloProcessoPermissaoManager;
 
 @Name(ConsultaProcessoExternoPorPartes.NAME)
 @Scope(ScopeType.PAGE)
@@ -17,7 +18,8 @@ public class ConsultaProcessoExternoPorPartes extends EntityList<ProcessoEpa> {
     private static final long serialVersionUID = 1L;
     public static final String NAME = "consultaProcessoExternoPorPartes";
     
-    private static final String DEFAULT_EJBQL = "select o from ProcessoEpa o inner join o.naturezaCategoriaFluxo ncf inner join ncf.natureza n where n.hasPartes = true";
+    private static final String DEFAULT_EJBQL = "select o from ProcessoEpa o inner join o.naturezaCategoriaFluxo ncf inner join ncf.natureza n where n.hasPartes = true "
+    		+ "where " + SigiloProcessoPermissaoManager.getPermissaoConditionFragment();
     private static final String DEFAULT_ORDER = "o.dataInicio ASC";
     
     private String nomePartes;
