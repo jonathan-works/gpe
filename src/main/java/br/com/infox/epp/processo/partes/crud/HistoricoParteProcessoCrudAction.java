f92aff31271eee81ada9d9fbb5d879bf2819c955
jonasbezerra <jonasbezerra@infox.com.br>
2014-02-14 14:07:52 -0300

    Revert "[35083] Ajustes em HistoricoParteProcessoCrudAction"
    This reverts commit 9da84f5ba7e828e72e7d28a099fefe72898b516a.

--- src/main/java/br/com/infox/epp/processo/partes/crud/HistoricoParteProcessoCrudAction.java
+++ src/main/java/br/com/infox/epp/processo/partes/crud/HistoricoParteProcessoCrudAction.java
@@ -1,12 +1,9 @@
 package br.com.infox.epp.processo.partes.crud;
 
-import org.jboss.seam.annotations.Name;
-
 import br.com.infox.core.crud.AbstractCrudAction;
 import br.com.infox.epp.processo.partes.entity.HistoricoParteProcesso;
 import br.com.infox.epp.processo.partes.manager.HistoricoParteProcessoManager;
 
-@Name(HistoricoParteProcessoCrudAction.NAME)
 public class HistoricoParteProcessoCrudAction extends AbstractCrudAction<HistoricoParteProcesso, HistoricoParteProcessoManager> {
 
     private static final long serialVersionUID = 1L;
