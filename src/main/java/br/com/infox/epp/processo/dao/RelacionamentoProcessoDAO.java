017e57abc28df99af5be272073e1ad440d1d7587
erikliberal <erikliberal@infox.com.br>
2014-02-14 12:56:03 -0300

    Adição de annotation @AutoCreate

--- src/main/java/br/com/infox/epp/processo/dao/RelacionamentoProcessoDAO.java
+++ src/main/java/br/com/infox/epp/processo/dao/RelacionamentoProcessoDAO.java
@@ -1,10 +1,12 @@
 package br.com.infox.epp.processo.dao;
 
+import org.jboss.seam.annotations.AutoCreate;
 import org.jboss.seam.annotations.Name;
 
 import br.com.infox.core.dao.DAO;
 import br.com.infox.epp.processo.entity.RelacionamentoProcesso;
 
+@AutoCreate
 @Name(RelacionamentoProcessoDAO.NAME)
 public class RelacionamentoProcessoDAO extends DAO<RelacionamentoProcesso> {
 
