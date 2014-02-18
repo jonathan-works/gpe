c4dd5e41f596ae7a47517f556eb0af32b06cf16b
jonasbezerra <jonasbezerra@infox.com.br>
2014-02-18 10:46:58 -0300

    [32147] Formatação

--- src/main/java/br/com/itx/component/Template.java
+++ src/main/java/br/com/itx/component/Template.java
@@ -6,25 +6,25 @@ import java.util.Map;
 
 public class Template implements Serializable {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	private String id;
+    private String id;
 
-	private Map<String, Object> properties = new LinkedHashMap<String, Object>();
+    private Map<String, Object> properties = new LinkedHashMap<String, Object>();
 
-	public String getId() {
-		return id == null ? "default" : id;
-	}
+    public String getId() {
+        return id == null ? "default" : id;
+    }
 
-	public void setId(String id) {
-		this.id = id;
-	}
+    public void setId(String id) {
+        this.id = id;
+    }
 
-	public Map<String, Object> getProperties() {
-		return properties;
-	}
+    public Map<String, Object> getProperties() {
+        return properties;
+    }
 
-	public void setProperties(Map<String, Object> properties) {
-		this.properties = properties;
-	}
+    public void setProperties(Map<String, Object> properties) {
+        this.properties = properties;
+    }
 }
