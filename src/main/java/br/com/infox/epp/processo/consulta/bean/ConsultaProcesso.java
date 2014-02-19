5bd91d8ec22cd88f521a7df36d7f8245fc2456c6
jonasbezerra <jonasbezerra@infox.com.br>
2014-02-19 10:21:58 -0300

    [32147] Formtação

--- src/main/java/br/com/infox/epp/processo/consulta/bean/ConsultaProcesso.java
+++ src/main/java/br/com/infox/epp/processo/consulta/bean/ConsultaProcesso.java
@@ -8,76 +8,75 @@ import br.com.infox.epp.fluxo.entity.Natureza;
 
 public class ConsultaProcesso implements Serializable {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	private String numeroProcesso;
-	private Date dataInicio;
-	private Date dataFim;
-	private Boolean inPesquisa = false;
-	private String fluxo;
-	private Natureza natureza;
-	private Categoria categoria;
+    private String numeroProcesso;
+    private Date dataInicio;
+    private Date dataFim;
+    private Boolean inPesquisa = false;
+    private String fluxo;
+    private Natureza natureza;
+    private Categoria categoria;
 
-	public String getNumeroProcesso() {
-		return numeroProcesso;
-	}
+    public String getNumeroProcesso() {
+        return numeroProcesso;
+    }
 
-	public void setNumeroProcesso(String numeroProcesso) {
-		this.numeroProcesso = numeroProcesso;
-	}
+    public void setNumeroProcesso(String numeroProcesso) {
+        this.numeroProcesso = numeroProcesso;
+    }
 
-	public String getFluxo() {
-		return fluxo;
-	}
+    public String getFluxo() {
+        return fluxo;
+    }
 
-	public void setFluxo(String fluxo) {
-		this.fluxo = fluxo;
-	}
+    public void setFluxo(String fluxo) {
+        this.fluxo = fluxo;
+    }
 
-	public Date getDataInicio() {
-		return dataInicio;
-	}
+    public Date getDataInicio() {
+        return dataInicio;
+    }
 
-	public void setDataInicio(Date dataInicio) {
-		this.dataInicio = dataInicio;
-	}
+    public void setDataInicio(Date dataInicio) {
+        this.dataInicio = dataInicio;
+    }
 
-	public Date getDataFim() {
-		return dataFim;
-	}
+    public Date getDataFim() {
+        return dataFim;
+    }
 
-	public void setDataFim(Date dataFim) {
-		this.dataFim = br.com.infox.core.util.DateUtil.getEndOfDay(dataFim);
-	}
+    public void setDataFim(Date dataFim) {
+        this.dataFim = br.com.infox.core.util.DateUtil.getEndOfDay(dataFim);
+    }
 
+    public Boolean getInPesquisa() {
+        return inPesquisa;
+    }
 
-	public Boolean getInPesquisa() {
-		return inPesquisa;
-	}
+    public void setInPesquisa(Boolean inPesquisa) {
+        this.inPesquisa = inPesquisa;
+    }
 
-	public void setInPesquisa(Boolean inPesquisa) {
-		this.inPesquisa = inPesquisa;
-	}
+    @Override
+    public String toString() {
+        return numeroProcesso;
+    }
 
-	@Override
-	public String toString() {
-		return numeroProcesso;
-	}
+    public void setNatureza(Natureza natureza) {
+        this.natureza = natureza;
+    }
 
-	public void setNatureza(Natureza natureza) {
-		this.natureza = natureza;
-	}
+    public Natureza getNatureza() {
+        return natureza;
+    }
 
-	public Natureza getNatureza() {
-		return natureza;
-	}
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
 
-	public void setCategoria(Categoria categoria) {
-		this.categoria = categoria;
-	}
-
-	public Categoria getCategoria() {
-		return categoria;
-	}
+    public Categoria getCategoria() {
+        return categoria;
+    }
 
 }
