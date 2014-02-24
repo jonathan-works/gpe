68a16f292e1ff9f4c60ff3768c5ac8e9d1e17810
jonasbezerra <jonasbezerra@infox.com.br>
2014-02-24 14:43:54 -0300

    [32147] Formatação

--- src/main/java/br/com/itx/util/ConversationUtil.java
+++ src/main/java/br/com/itx/util/ConversationUtil.java
@@ -6,18 +6,19 @@ import org.jboss.seam.core.Conversation;
 @Name(ConversationUtil.NAME)
 public class ConversationUtil {
 
-	public static final String NAME = "conversationUtil";
+    public static final String NAME = "conversationUtil";
 
-	/**
-	 * Finaliza a conversação
-	 * @param toUrl
-	 * @return
-	 */
-	
-	//TODO Verificar como utilizar outcome em vez da url nos menus
-	public String endBeforeRedirect(String toUrl) {
-		Conversation.instance().root();
-		Conversation.instance().endBeforeRedirect();
-		return toUrl;
-	}
+    /**
+     * Finaliza a conversação
+     * 
+     * @param toUrl
+     * @return
+     */
+
+    // TODO Verificar como utilizar outcome em vez da url nos menus
+    public String endBeforeRedirect(String toUrl) {
+        Conversation.instance().root();
+        Conversation.instance().endBeforeRedirect();
+        return toUrl;
+    }
 }
