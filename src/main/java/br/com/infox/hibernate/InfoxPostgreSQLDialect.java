a1fd955c607153ef220a7dcb8635ade87d4c6b20
jonasbezerra <jonasbezerra@infox.com.br>
2014-02-24 15:33:22 -0300

    [32147] Formatação

--- src/main/java/br/com/infox/hibernate/InfoxPostgreSQLDialect.java
+++ src/main/java/br/com/infox/hibernate/InfoxPostgreSQLDialect.java
@@ -2,10 +2,11 @@ package br.com.infox.hibernate;
 
 import org.hibernate.dialect.PostgreSQL82Dialect;
 
-
 public class InfoxPostgreSQLDialect extends PostgreSQL82Dialect {
-	public InfoxPostgreSQLDialect() {
-		registerKeyword("true");
-		registerKeyword("false");
-	}
+
+    public InfoxPostgreSQLDialect() {
+        registerKeyword("true");
+        registerKeyword("false");
+    }
+
 }
