458cc0fe374a6db90d5d710939a778ea7d7ca4ae
jonasbezerra <jonasbezerra@infox.com.br>
2014-02-24 15:05:13 -0300

    [32147] Remoção de métodos não utilizados de XmlUtil

--- src/main/java/br/com/itx/util/XmlUtil.java
+++ src/main/java/br/com/itx/util/XmlUtil.java
@@ -1,11 +1,6 @@
 package br.com.itx.util;
 
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.InputStream;
-import java.io.OutputStreamWriter;
-import java.io.UnsupportedEncodingException;
-import java.io.Writer;
 import java.util.List;
 
 import org.jboss.seam.log.LogProvider;
@@ -14,29 +9,12 @@ import org.jdom2.Document;
 import org.jdom2.Element;
 import org.jdom2.input.SAXBuilder;
 import org.jdom2.input.sax.XMLReaders;
-import org.jdom2.output.Format;
-import org.jdom2.output.XMLOutputter;
 
 public final class XmlUtil {
-
-    private static final String ENCODING = "ISO-8859-1";
 
     private static final LogProvider LOG = Logging.getLogProvider(XmlUtil.class);
 
     private XmlUtil() {
-    }
-
-    public static Document readDocument(File file) {
-        Document doc = null;
-        try {
-            SAXBuilder builder = getSAXBuilder();
-            if (file != null && file.isFile()) {
-                doc = builder.build(file);
-            }
-        } catch (Exception ex) {
-            LOG.error(".readDocument()", ex);
-        }
-        return doc;
     }
 
     public static Document readDocument(InputStream file) {
@@ -58,36 +36,6 @@ public final class XmlUtil {
         builder.setFeature("http://apache.org/xml/features/"
                 + "nonvalidating/load-external-dtd", false);
         return builder;
-    }
-
-    public static void writeDocument(File file, Document doc) {
-        if (file != null && doc != null) {
-            try {
-                file.getParentFile().mkdirs();
-                FileOutputStream fout = new FileOutputStream(file);
-                try {
-                    Writer writer = new OutputStreamWriter(fout, ENCODING);
-                    XMLOutputter jdom = new XMLOutputter();
-                    Format format = jdom.getFormat();
-                    defineFormat(format);
-                    jdom.setFormat(format);
-                    jdom.output(doc, writer);
-                } catch (UnsupportedEncodingException err) {
-                    LOG.warn("writeDocument(file, Document)", err);
-                }
-                fout.close();
-            } catch (Exception ex) {
-                LOG.error(".writeDocument(file, Document)", ex);
-            }
-        }
-    }
-
-    public static void defineFormat(Format format) {
-        format.setOmitDeclaration(false);
-        format.setExpandEmptyElements(false);
-        format.setEncoding(ENCODING);
-        format.setIndent("    ");
-        format.setTextMode(Format.TextMode.TRIM_FULL_WHITE);
     }
 
     public static List<Element> getChildren(Element element, String name) {
