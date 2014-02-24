096fcdc7a1d9b224744a0afc1be399dded3c2e1f
jonasbezerra <jonasbezerra@infox.com.br>
2014-02-24 15:04:49 -0300

    [32147] Remoçõa de métodos não utilizados de StringUtil

--- src/main/java/br/com/itx/util/StringUtil.java
+++ src/main/java/br/com/itx/util/StringUtil.java
@@ -1,141 +1,12 @@
 package br.com.itx.util;
 
-import java.text.ParseException;
 import java.util.List;
 
-import org.jboss.seam.log.LogProvider;
-import org.jboss.seam.log.Logging;
 import org.jboss.seam.util.Strings;
 
 public final class StringUtil {
 	
-	private static final LogProvider LOG = Logging.getLogProvider(StringUtil.class);
-
     private StringUtil() {
-    }
-
-    private static final String FOREIGN_CHARS = "áÁéÉíÍóÓúÚàÀèÈìÌòÒùÙâÂêÊîÎôÔûÛäÄëËïÏöÖüÜãÃõÕçÇñÑ";
-
-    private static final String US_CHARS = "aAeEiIoOuUaAeEiIoOuUaAeEiIoOuUaAeEiIoOuUaAoOcCnN";
-
-    public static String changeChar(String text, char c1, String c2) {
-        StringBuilder aux = new StringBuilder();
-        for (int i = 0; i < text.length(); i++) {
-            char c = text.charAt(i);
-            if (c == c1) {
-                aux.append(c2);
-            } else {
-                aux.append(c);
-            }
-        }
-        return aux.toString();
-    }
-
-    public static String changeChars(String text, String oldChars,
-            String newChars) {
-        StringBuilder aux = new StringBuilder();
-        char let;
-        for (int i = 0; i < text.length(); i++) {
-            let = text.charAt(i);
-            int pos = oldChars.indexOf(let);
-            if (pos == -1) {
-                aux.append(let);
-            } else if (newChars.length() > pos) {
-                aux.append(newChars.charAt(pos));
-            }
-        }
-        return aux.toString();
-    }
-
-    public static String piece(String text, String delim, int p) {
-        return piece(text, delim, p, p);
-    }
-
-    public static String piece(String text, String delim, final int p1,
-            final int p2) {
-        int l1 = p1;
-        int l2 = p2;
-        if ((text == null) || (text.length() == 0)) {
-            return "";
-        }
-        if ((delim == null) || (delim.length() == 0)) {
-            return text;
-        }
-        if (l1 < 1) {
-            l1 = 1;
-        }
-        if (l2 < 0) {
-            l2 = 0;
-        }
-        if ((l2 != 0) && (l2 < l1)) {
-            return "";
-        }
-        int piece = 1;
-        int ini = 0;
-        int pos = 0;
-        int fim = text.length();
-        pos = text.indexOf(delim);
-        while (((piece <= l2) || (l2 == 0)) && (pos > -1)) {
-            if ((l2 > 0) && (piece == l2)) {
-                fim = pos;
-            }
-            piece++;
-            if (piece == l1) {
-                ini = pos + delim.length();
-            }
-            pos = text.indexOf(delim, pos + delim.length());
-        }
-        if (piece < l1) {
-            return "";
-        }
-        return text.substring(ini, fim);
-    }
-
-    public static String replace(String subject, String find, String replace) {
-        StringBuilder buf = new StringBuilder();
-        int lengthSubject = find.length();
-        int posAux = 0;
-        int posFind = subject.indexOf(find);
-
-        while (posFind != -1) {
-            buf.append(subject.substring(posAux, posFind));
-            buf.append(replace);
-            posAux = posFind + lengthSubject;
-            posFind = subject.indexOf(find, posAux);
-        }
-
-        buf.append(subject.substring(posAux));
-        return buf.toString();
-    }
-
-    /**
-     * Elimina acentuação do texto
-     * 
-     * @param text
-     * @return o texto sem os caracteres acentuados
-     */
-    public static String getUsAscii(String text) {
-        return changeChars(text, FOREIGN_CHARS, US_CHARS);
-    }
-
-    public static String retiraZerosEsquerda(String numero) {
-        if (Strings.isEmpty(numero) || !numero.startsWith("0")) {
-            return numero;
-        }
-        char[] charArray = numero.toCharArray();
-        int posUltimoZero = 0;
-        for (char c : charArray) {
-            if (c != '0') {
-                break;
-            }
-            posUltimoZero++;
-        }
-        return numero.substring(posUltimoZero);
-    }
-
-    public static String limparCharsNaoNumericos(String s) {
-        // TODO rever esse nome
-        return s.replaceAll("[^0-9]", "");
     }
 
     public static String replaceQuebraLinha(String texto) {
@@ -150,66 +21,6 @@ public final class StringUtil {
         }
     }
 
-    public static String removeNaoNumericos(String source) {
-        return source.replaceAll("\\D", "");
-    }
-
-    public static String removeNaoAlphaNumericos(String source) {
-        return source.replaceAll("\\d", "");
-    }
-
-    public static String capitalizeAllWords(String words) {
-        StringBuilder out = new StringBuilder();
-        if (!Strings.isEmpty(words)) {
-            String[] wordList = words.trim().split(" ");
-            for (int i = 0; i < wordList.length; i++) {
-                if (wordList[i].length() > 1) {
-                    wordList[i] = wordList[i].substring(0, 1).toUpperCase()
-                            + wordList[i].substring(1).toLowerCase();
-                } else {
-                    wordList[i] = wordList[i].toUpperCase();
-                }
-                out.append(wordList[i]);
-                out.append(" ");
-            }
-        }
-        return out.toString().trim();
-    }
-
-    public static String completaZeros(String numero, int tamanho) {
-        StringBuilder sb = new StringBuilder();
-        int zerosAdicionar = tamanho - numero.length();
-        while (sb.length() < zerosAdicionar) {
-            sb.append('0');
-        }
-        sb.append(numero);
-        return sb.toString();
-    }
-
-    public static String formatNumericString(String string, String mask) throws java.text.ParseException {
-        javax.swing.text.MaskFormatter mf = new javax.swing.text.MaskFormatter(mask);
-        mf.setValueContainsLiteralCharacters(false);
-        return mf.valueToString(string);
-    }
-
-    public static String formartCpf(String cpf) {
-        try {
-            return formatNumericString(cpf, "###.###.###-##");
-        } catch (ParseException e) {
-        	LOG.warn(".formartCpf(cpf)", e);
-            return null;
-        }
-    }
-
-    public static String formatCnpj(String cnpj) {
-        try {
-            return formatNumericString(cnpj, "##.###.###/####-##");
-        } catch (ParseException e) {
-            LOG.warn(".formartCnpj(cnpj)", e);
-            return null;
-        }
-    }
-
     public static String concatList(List<Object> list, String delimitador) {
         StringBuilder sb = new StringBuilder();
         for (Object object : list) {
@@ -219,24 +30,6 @@ public final class StringUtil {
             sb.append(object);
         }
         return sb.toString();
-    }
-
-    public static String toLowerCaseFirstChar(String string) {
-        if (string == null || string.length() == 0) {
-            return string;
-        } else {
-            char[] charArray = string.toCharArray();
-            charArray[0] = Character.toLowerCase(charArray[0]);
-            return String.valueOf(charArray);
-        }
-    }
-
-    public static String padRight(String s, int n) {
-        return String.format("%1$-" + n + "s", s);
-    }
-
-    public static String padLeft(String s, int n) {
-        return String.format("%1$#" + n + "s", s);
     }
 
 }
