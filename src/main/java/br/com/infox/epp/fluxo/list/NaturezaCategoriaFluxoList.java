da551c60afea997d763475cba72067b4a8fde7a7
jonasbezerra <jonasbezerra@infox.com.br>
2014-02-24 12:41:46 -0300

    [32147] Formtação

--- src/main/java/br/com/infox/epp/fluxo/list/NaturezaCategoriaFluxoList.java
+++ src/main/java/br/com/infox/epp/fluxo/list/NaturezaCategoriaFluxoList.java
@@ -15,33 +15,34 @@ import br.com.infox.epp.fluxo.entity.NaturezaCategoriaFluxo;
 @Name(NaturezaCategoriaFluxoList.NAME)
 @Scope(ScopeType.CONVERSATION)
 public class NaturezaCategoriaFluxoList extends EntityList<NaturezaCategoriaFluxo> {
-	private static final long	serialVersionUID	= 1L;
-	private static final String DEFAULT_EJBQL = "select o from NaturezaCategoriaFluxo o";
-	private static final String DEFAULT_ORDER = "fluxo";
-	public static final String NAME = "naturezaCategoriaFluxoList";
-	
-	@In private GenericManager genericManager;
+    private static final long serialVersionUID = 1L;
+    private static final String DEFAULT_EJBQL = "select o from NaturezaCategoriaFluxo o";
+    private static final String DEFAULT_ORDER = "fluxo";
+    public static final String NAME = "naturezaCategoriaFluxoList";
 
-	@Override
-	protected void addSearchFields() {
-		addSearchField("natureza", SearchCriteria.IGUAL);
-		addSearchField("categoria", SearchCriteria.IGUAL);
-		addSearchField("fluxo", SearchCriteria.IGUAL);
-	}
+    @In
+    private GenericManager genericManager;
 
-	@Override
-	protected String getDefaultEjbql() {
-		return NaturezaCategoriaFluxoList.DEFAULT_EJBQL;
-	}
+    @Override
+    protected void addSearchFields() {
+        addSearchField("natureza", SearchCriteria.IGUAL);
+        addSearchField("categoria", SearchCriteria.IGUAL);
+        addSearchField("fluxo", SearchCriteria.IGUAL);
+    }
 
-	@Override
-	protected String getDefaultOrder() {
-		return NaturezaCategoriaFluxoList.DEFAULT_ORDER;
-	}
+    @Override
+    protected String getDefaultEjbql() {
+        return NaturezaCategoriaFluxoList.DEFAULT_EJBQL;
+    }
 
-	@Override
-	protected Map<String, String> getCustomColumnsOrder() {
-		return null;
-	}
-	
+    @Override
+    protected String getDefaultOrder() {
+        return NaturezaCategoriaFluxoList.DEFAULT_ORDER;
+    }
+
+    @Override
+    protected Map<String, String> getCustomColumnsOrder() {
+        return null;
+    }
+
 }
