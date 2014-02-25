37efe38f12a6ca723dddcfaa3c2a2a881caa4ecc
jonasbezerra <jonasbezerra@infox.com.br>
2014-02-25 10:31:48 -0300

    [32147] Remoção de método não utilizado

--- src/main/java/br/com/infox/epp/fluxo/manager/CategoriaItemManager.java
+++ src/main/java/br/com/infox/epp/fluxo/manager/CategoriaItemManager.java
@@ -1,22 +1,12 @@
 package br.com.infox.epp.fluxo.manager;
 
-import static org.jboss.seam.faces.FacesMessages.instance;
-
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 
 import org.jboss.seam.annotations.AutoCreate;
 import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
-import org.jboss.seam.faces.FacesMessages;
-import org.jboss.seam.international.StatusMessage.Severity;
-import org.jboss.seam.log.Log;
-import org.jboss.seam.log.Logging;
 
 import br.com.infox.core.manager.Manager;
-import br.com.infox.core.persistence.DAOException;
-import br.com.infox.core.transaction.TransactionService;
 import br.com.infox.epp.fluxo.dao.CategoriaItemDAO;
 import br.com.infox.epp.fluxo.entity.Categoria;
 import br.com.infox.epp.fluxo.entity.CategoriaItem;
@@ -29,7 +19,6 @@ public class CategoriaItemManager extends Manager<CategoriaItemDAO, CategoriaIte
     private static final long serialVersionUID = -3580636874720809514L;
 
     public static final String NAME = "categoriaItemManager";
-    private static final Log LOG = Logging.getLog(CategoriaItemManager.class);
 
     @In
     private CategoriaItemDAO categoriaItemDAO;
@@ -44,59 +33,6 @@ public class CategoriaItemManager extends Manager<CategoriaItemDAO, CategoriaIte
 
     public boolean containsCategoriaItem(CategoriaItem categoriaItem) {
         return categoriaItemDAO.countByCategoriaItem(categoriaItem.getCategoria(), categoriaItem.getItem()) > 0;
-    }
-
-    public List<CategoriaItem> createCategoriaItemList(Categoria categoria,
-            Set<Item> itens) {
-        List<CategoriaItem> categoriaItemList = new ArrayList<CategoriaItem>();
-        if (itens != null) {
-            for (Item item : itens) {
-                if (item.getAtivo()) {
-                    persistCategoriaItem(categoria, item, categoriaItemList);
-                }
-            }
-        }
-
-        conclusionMessage("#{messages['entity_created']}", "Falha ao inserir", categoriaItemList.size() > 0);
-        return categoriaItemList;
-    }
-
-    private void conclusionMessage(String successMessage, String errorMessage,
-            boolean successful) {
-        FacesMessages fm = instance();
-        fm.clear();
-        if (successful) {
-            fm.add(successMessage);
-        } else {
-            fm.add(Severity.ERROR, errorMessage);
-        }
-    }
-
-    /**
-     * @param categoria Categoria a ser associada ao Item
-     * @param item Item a ser associado à categoria
-     * @param categoriaItemList Lista resultante
-     * 
-     *        Este método deve iniciar a transação antes de persistir e
-     *        finalizar após persistir devido à necessidade de serem inserções
-     *        atômicas, caso múltiplas entradas fossem permitidas em uma única
-     *        transação e uma destas causasse uma exceção, então todas as outras
-     *        falhariam
-     */
-    private void persistCategoriaItem(Categoria categoria, Item item,
-            List<CategoriaItem> categoriaItemList) {
-        TransactionService.beginTransaction();
-        CategoriaItem ci;
-        try {
-            ci = persist(new CategoriaItem(categoria, item));
-        } catch (DAOException e) {
-            LOG.error(null, e);
-            ci = null;
-        }
-        TransactionService.commitTransction();
-        if (ci != null) {
-            categoriaItemList.add(ci);
-        }
     }
 
 }
