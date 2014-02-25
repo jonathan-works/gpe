fa44a0fc8d72747935b98b63477380a136c5384e
jonasbezerra <jonasbezerra@infox.com.br>
2014-02-25 07:30:14 -0300

    [32147] Removidos métodos não utilizados de HibernateUtil

--- src/main/java/br/com/infox/hibernate/util/HibernateUtil.java
+++ src/main/java/br/com/infox/hibernate/util/HibernateUtil.java
@@ -1,54 +1,19 @@
 package br.com.infox.hibernate.util;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-
 import org.hibernate.Filter;
-import org.hibernate.Session;
-import org.hibernate.engine.spi.LoadQueryInfluencers;
 import org.hibernate.proxy.HibernateProxy;
-import org.jboss.seam.persistence.FullTextHibernateSessionProxy;
 
 import br.com.infox.hibernate.session.SessionAssistant;
 import br.com.itx.util.ComponentUtil;
 
-//TODO verificar se os métodos não utilizados dessa classe podem (devem) ser excluídos
 public final class HibernateUtil {
 
     private HibernateUtil() {
     }
 
-    public static void disableFilters(String... names) {
-        Session session = sessionAssistant().getSession();
-        for (String name : names) {
-            session.disableFilter(name);
-        }
-    }
-
-    public static void enableFilters(String... names) {
-        Session session = sessionAssistant().getSession();
-        for (String name : names) {
-            session.enableFilter(name);
-        }
-    }
-
-    public static void setFilterParameter(String filterName,
-            String parameterName, Object paremeterValue) {
+    public static void setFilterParameter(String filterName, String parameterName, Object paremeterValue) {
         Filter enabledFilter = getEnabledFilter(filterName);
         enabledFilter.setParameter(parameterName, paremeterValue);
-    }
-
-    public static void setFilterParameterList(String filterName,
-            String parameterName, Collection<?> paremeterValues) {
-        Filter enabledFilter = getEnabledFilter(filterName);
-        enabledFilter.setParameterList(parameterName, paremeterValues);
-    }
-
-    public static void setFilterParameterList(String filterName,
-            String parameterName, Object[] paremeterValues) {
-        Filter enabledFilter = getEnabledFilter(filterName);
-        enabledFilter.setParameterList(parameterName, paremeterValues);
     }
 
     public static Filter getEnabledFilter(String filterName) {
@@ -58,21 +23,6 @@ public final class HibernateUtil {
             enabledFilter = sessionAssistant().getSession().getEnabledFilter(filterName);
         }
         return enabledFilter;
-    }
-
-    public static void disableAllFilters() {
-        FullTextHibernateSessionProxy session = (FullTextHibernateSessionProxy) sessionAssistant().getSession();
-        LoadQueryInfluencers loadQueryInfluencers = session.getLoadQueryInfluencers();
-
-        // Dividido em dois fors para evitar o erro de acesso concorrente ao Map
-        // que contém os filtros ativos
-        List<String> filters = new ArrayList<String>();
-        for (String filter : loadQueryInfluencers.getEnabledFilters().keySet()) {
-            filters.add(filter);
-        }
-        for (String s : filters) {
-            session.disableFilter(s);
-        }
     }
 
     public static Object removeProxy(Object object) {
