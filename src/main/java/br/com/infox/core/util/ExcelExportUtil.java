a64dabdbf29f96ddf53e1fd38b56c575f1b22fcf
jonasbezerra <jonasbezerra@infox.com.br>
2014-02-26 15:30:11 -0300

    [32147] Movido FileUtil para core.util

--- src/main/java/br/com/infox/core/util/ExcelExportUtil.java
+++ src/main/java/br/com/infox/core/util/ExcelExportUtil.java
@@ -19,7 +19,6 @@ import org.jboss.seam.util.RandomStringUtils;
 import br.com.infox.core.exception.ExcelExportException;
 import br.com.infox.core.file.download.FileDownloader;
 import br.com.infox.jboss.path.PathResolver;
-import br.com.itx.util.FileUtil;
 
 //TODO Transformar essa classe em um Util de verdade ou em um componente
 public class ExcelExportUtil {
