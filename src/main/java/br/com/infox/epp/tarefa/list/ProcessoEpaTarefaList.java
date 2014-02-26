00b6893548e61193435606aa08d7e917c2f70031
jonasbezerra <jonasbezerra@infox.com.br>
2014-02-26 14:29:46 -0300

    [32147] Formatação

--- src/main/java/br/com/infox/epp/tarefa/list/ProcessoEpaTarefaList.java
+++ src/main/java/br/com/infox/epp/tarefa/list/ProcessoEpaTarefaList.java
@@ -13,56 +13,60 @@ import br.com.infox.epp.processo.manager.ProcessoEpaManager;
 import br.com.infox.epp.tarefa.entity.ProcessoEpaTarefa;
 import br.com.itx.util.StringUtil;
 
-
 /**
  * EntityList que consulta as tarefas de um determinado processo
+ * 
  * @author tassio
- *
+ * 
  */
 @Name(ProcessoEpaTarefaList.NAME)
 public class ProcessoEpaTarefaList extends EntityList<ProcessoEpaTarefa> {
-	
-	private static final int PORCENTAGEM = 100;
+
+    private static final int PORCENTAGEM = 100;
     private static final int LIMITE_PADRAO = 15;
     private static final long serialVersionUID = 1L;
-	public static final String NAME = "processoEpaTarefaList";
-	
-	private static final String DEFAULT_EJBQL = "select o from ProcessoEpaTarefa o";
-	private static final String DEFAULT_ORDER = "o.idProcessoTarefa";
-	
-	@In
-	private ProcessoEpaManager processoEpaManager;
-	
-	@Override
-	protected void addSearchFields() {
-		addSearchField("processoEpa", SearchCriteria.IGUAL);
-	}
-	@Override
-	protected String getDefaultEjbql() {
-		return DEFAULT_EJBQL;
-	}
-	@Override
-	protected String getDefaultOrder() {
-		return DEFAULT_ORDER;
-	}
-	@Override
-	protected Map<String, String> getCustomColumnsOrder() {
-		return null;
-	}
-	
-	public String rowClasses() {
-		List<Object> classes = new ArrayList<Object>();
-		for (ProcessoEpaTarefa row: list(LIMITE_PADRAO)) {
-			if (row.getPorcentagem() != null && row.getPorcentagem() > PORCENTAGEM) {
-				classes.add("red-back");
-			} else {
-				classes.add("white-back");
-			}
-		}
-		return StringUtil.concatList(classes, ",");
-	}
-	
-	public int getDiasDesdeInicioProcesso() {
-		return processoEpaManager.getDiasDesdeInicioProcesso(getEntity().getProcessoEpa());
-	}
+    public static final String NAME = "processoEpaTarefaList";
+
+    private static final String DEFAULT_EJBQL = "select o from ProcessoEpaTarefa o";
+    private static final String DEFAULT_ORDER = "o.idProcessoTarefa";
+
+    @In
+    private ProcessoEpaManager processoEpaManager;
+
+    @Override
+    protected void addSearchFields() {
+        addSearchField("processoEpa", SearchCriteria.IGUAL);
+    }
+
+    @Override
+    protected String getDefaultEjbql() {
+        return DEFAULT_EJBQL;
+    }
+
+    @Override
+    protected String getDefaultOrder() {
+        return DEFAULT_ORDER;
+    }
+
+    @Override
+    protected Map<String, String> getCustomColumnsOrder() {
+        return null;
+    }
+
+    public String rowClasses() {
+        List<Object> classes = new ArrayList<Object>();
+        for (ProcessoEpaTarefa row : list(LIMITE_PADRAO)) {
+            if (row.getPorcentagem() != null
+                    && row.getPorcentagem() > PORCENTAGEM) {
+                classes.add("red-back");
+            } else {
+                classes.add("white-back");
+            }
+        }
+        return StringUtil.concatList(classes, ",");
+    }
+
+    public int getDiasDesdeInicioProcesso() {
+        return processoEpaManager.getDiasDesdeInicioProcesso(getEntity().getProcessoEpa());
+    }
 }
