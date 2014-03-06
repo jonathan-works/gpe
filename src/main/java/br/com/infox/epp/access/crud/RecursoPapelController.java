16f381ab394f83dde74073baa2511383ece7ffaa
jonasbezerra <jonasbezerra@infox.com.br>
2014-03-06 10:15:00 -0300

    [37347] Criação de RecursoPapelController

---
+++ src/main/java/br/com/infox/epp/access/crud/RecursoPapelController.java
@@ -0,0 +1,53 @@
+package br.com.infox.epp.access.crud;
+
+import java.util.List;
+
+import org.jboss.seam.ScopeType;
+import org.jboss.seam.annotations.In;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Scope;
+
+import br.com.infox.epp.access.entity.Recurso;
+import br.com.infox.epp.access.manager.PapelManager;
+import br.com.infox.epp.access.manager.RecursoManager;
+
+@Name(RecursoPapelController.NAME)
+@Scope(ScopeType.CONVERSATION)
+public class RecursoPapelController {
+
+    public static final String NAME = "recursoPapelController";
+    
+    private Recurso recurso;
+    
+    private List<String> papeis;
+    private List<String> papeisDisponiveis;
+    
+    @In private RecursoManager recursoManager;
+    @In PapelManager papelManager;
+    
+    public Recurso getRecurso() {
+        return recurso;
+    }
+    public void setRecurso(Recurso recurso) {
+        this.recurso = recurso;
+        papeis = getPapeisAssociadosARecurso();
+        papeisDisponiveis = papelManager.getListaDeNomesDosPapeis();
+    }
+    public List<String> getPapeis() {
+        return papeis;
+    }
+    public void setPapeis(List<String> papeis) {
+        this.papeis = papeis;
+    }
+    public List<String> getPapeisDisponiveis() {
+        return papeisDisponiveis;
+    }
+    public void setPapeisDisponiveis(List<String> papeisDisponiveis) {
+        this.papeisDisponiveis = papeisDisponiveis;
+    }
+    
+    private List<String> getPapeisAssociadosARecurso(){
+        return papeis = recursoManager.getPapeisAssociadosARecurso(recurso);
+    }
+
+}
