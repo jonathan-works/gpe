bc68601530556826063e723f9743e3465c33e62f
Erik Liberal <erikliberal@infox.com.br>
2014-03-10 11:16:16 -0300

    Remoção de regras de criação de usuário da classe de fronteira

--- src/main/java/br/com/infox/epp/access/crud/UsuarioLoginCrudAction.java
+++ src/main/java/br/com/infox/epp/access/crud/UsuarioLoginCrudAction.java
@@ -1,15 +1,9 @@
 package br.com.infox.epp.access.crud;
 
-import javax.security.auth.login.LoginException;
-
 import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
-import org.jboss.seam.log.LogProvider;
-import org.jboss.seam.log.Logging;
 
 import br.com.infox.core.crud.AbstractCrudAction;
-import br.com.infox.core.exception.BusinessException;
-import br.com.infox.core.persistence.DAOException;
 import br.com.infox.epp.access.entity.UsuarioLogin;
 import br.com.infox.epp.access.manager.UsuarioLoginManager;
 import br.com.infox.epp.access.service.PasswordService;
@@ -18,13 +12,10 @@ import br.com.infox.epp.access.type.UsuarioEnum;
 @Name(UsuarioLoginCrudAction.NAME)
 public class UsuarioLoginCrudAction extends AbstractCrudAction<UsuarioLogin, UsuarioLoginManager> {
     private static final long serialVersionUID = 1L;
-    private static final LogProvider LOG = Logging.getLogProvider(UsuarioLoginCrudAction.class);
     public static final String NAME = "usuarioLoginCrudAction";
     
     @In private PasswordService passwordService;
 
-    private String password;
-    
     @Override
     public void newInstance() {
         super.newInstance();
@@ -33,50 +24,6 @@ public class UsuarioLoginCrudAction extends AbstractCrudAction<UsuarioLogin, Usu
         usuarioLogin.setProvisorio(Boolean.FALSE);
     }
 
-    @Override
-    protected boolean isInstanceValid() {
-        validarPermanencia();
-        return Boolean.TRUE;
-    }
-    
-    //TODO: Não é validação. definir outro nome e local para existir
-    private void validarPermanencia() {
-        final UsuarioLogin usuario = getInstance();
-        if (!usuario.getProvisorio()) {
-            usuario.setDataExpiracao(null);
-        }
-        if (!usuario.isHumano()){
-            usuario.setPessoaFisica(null);
-        }
-    }
-    
-    @Override
-    protected void afterSave(final String ret) {
-        final UsuarioLogin usuario = getInstance();
-        if (PERSISTED.equals(ret)) {
-            final String afterSaveExceptionMsg = "afterSave(ret)";
-            try {
-                passwordService.requisitarNovaSenha(Boolean.FALSE, usuario.getEmail());
-                getMessagesHandler().add("Senha gerada com sucesso.");
-            } catch (final BusinessException be){
-            	LOG.warn(afterSaveExceptionMsg, be);
-                getMessagesHandler().add(be.getLocalizedMessage());
-            } catch (final LoginException e) {
-                LOG.error(afterSaveExceptionMsg, e);
-            } catch (final DAOException e) {
-                LOG.error(afterSaveExceptionMsg, e);
-            }
-        }
-    }
-    
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(final String password) {
-        this.password = password;
-    }
-    
     public UsuarioEnum[] getTiposDeUsuario(){
         return UsuarioEnum.values();
     }
