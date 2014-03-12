202c82c98ecb37b626da27fd987b8c4f27254212
jonasbezerra <jonasbezerra@infox.com.br>
2014-03-12 11:23:34 -0300

    [32667] Empty statements should be removed

--- src/main/java/br/com/infox/epp/processo/documento/sigilo/action/SigiloDocumentoController.java
+++ src/main/java/br/com/infox/epp/processo/documento/sigilo/action/SigiloDocumentoController.java
@@ -13,30 +13,30 @@ import br.com.infox.epp.processo.documento.entity.ProcessoDocumento;
 @Scope(ScopeType.CONVERSATION)
 @AutoCreate
 public class SigiloDocumentoController implements Serializable {
-	private static final long serialVersionUID = 1L;
-	public static final String  NAME = "sigiloDocumentoController";
-	public static final String MSG_REGISTRO_ALTERADO = "#{messages['sigiloDocumento.registroAlterado']}";
+    private static final long serialVersionUID = 1L;
+    public static final String NAME = "sigiloDocumentoController";
+    public static final String MSG_REGISTRO_ALTERADO = "#{messages['sigiloDocumento.registroAlterado']}";
 
-	public static enum FragmentoSigilo {
-		MOTIVO_SIGILO, DETALHE_SIGILO, PERMISSOES_SIGILO
-	};
-	
-	private FragmentoSigilo fragmentoARenderizar;
-	private ProcessoDocumento documentoSelecionado;
-	
-	public FragmentoSigilo getFragmentoARenderizar() {
-		return fragmentoARenderizar;
-	}
-	
-	public void setFragmentoARenderizar(FragmentoSigilo fragmentoARenderizar) {
-		this.fragmentoARenderizar = fragmentoARenderizar;
-	}
-	
-	public ProcessoDocumento getDocumentoSelecionado() {
-		return documentoSelecionado;
-	}
-	
-	public void setDocumentoSelecionado(ProcessoDocumento documentoSelecionado) {
-		this.documentoSelecionado = documentoSelecionado;
-	}
+    public static enum FragmentoSigilo {
+        MOTIVO_SIGILO, DETALHE_SIGILO, PERMISSOES_SIGILO
+    }
+
+    private FragmentoSigilo fragmentoARenderizar;
+    private ProcessoDocumento documentoSelecionado;
+
+    public FragmentoSigilo getFragmentoARenderizar() {
+        return fragmentoARenderizar;
+    }
+
+    public void setFragmentoARenderizar(FragmentoSigilo fragmentoARenderizar) {
+        this.fragmentoARenderizar = fragmentoARenderizar;
+    }
+
+    public ProcessoDocumento getDocumentoSelecionado() {
+        return documentoSelecionado;
+    }
+
+    public void setDocumentoSelecionado(ProcessoDocumento documentoSelecionado) {
+        this.documentoSelecionado = documentoSelecionado;
+    }
 }
