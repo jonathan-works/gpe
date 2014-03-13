9ede8f5355795c16256c62b733d2a9592ca936de
jonasbezerra <jonasbezerra@infox.com.br>
2014-03-13 08:09:42 -0300

    [JAVADOC]

---
+++ src/main/java/br/com/infox/core/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Contém as classes básicas para a construção dos componentes da aplicação:
+ * CRUDs, Managers, Daos, EntityLists, Actions, Controllers, Handlers, Trees,
+ * Suggests
+ */
+package br.com.infox.core;
