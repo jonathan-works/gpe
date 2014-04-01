43776ea6da0bab537320696aa7a8c58a205631a9
Gabriel Morrison Lima Dantas <gabriel@infox.com.br>
2014-04-01 09:52:46 -0300

    [38614] Validação do domínio da variável do tipo Enumeração

---
+++ src/main/java/br/com/infox/ibpm/variable/validator/DominioVariavelTarefaValidator.java
@@ -0,0 +1,27 @@
+package br.com.infox.ibpm.variable.validator;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.FacesValidator;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+
+@FacesValidator("dominioVariavelTarefaValidator")
+public class DominioVariavelTarefaValidator implements Validator {
+
+    @Override
+    public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
+        if (value != null) {
+            String dominio = (String) value;
+            Pattern pattern = Pattern.compile("(.+?=.+?;)*");
+            Matcher matcher = pattern.matcher(dominio);
+            if (!matcher.matches()) {
+                throw new ValidatorException(new FacesMessage("Domínio inválido. Deve estar no formato label=valor, separado por ;"));
+            }
+        }
+    }
+}
