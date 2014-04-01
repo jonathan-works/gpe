d1281ff4f1b9bc3f8338a7c8da4f21125e04a255
Erik Liberal <erikliberal@infox.com.br>
2014-04-01 09:52:57 -0300

    Adição de variável contendo número de processo

--- src/main/java/br/com/infox/epp/system/variables/ExternalVariables.java
+++ src/main/java/br/com/infox/epp/system/variables/ExternalVariables.java
@@ -7,31 +7,38 @@ import java.io.Serializable;
 import java.text.DateFormat;
 import java.util.GregorianCalendar;
 
+import org.jboss.seam.Component;
 import org.jboss.seam.ScopeType;
 import org.jboss.seam.annotations.Factory;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Scope;
 
 import br.com.infox.epp.access.api.Authenticator;
+import br.com.infox.epp.processo.entity.Processo;
+import br.com.infox.epp.processo.home.ProcessoHome;
 
 /**
- * @author erikliberal
- * Classe criada para gerenciar variáveis que deverão ser
- * utilizadas pelo usuário final, a exemplo em modelos de
- * documento. Por motivos de segurança todos os valores
- * resultantes devem ser tipos primitivos ou encapsulamento de
- * valores primitivos
+ * @author erikliberal Classe criada para gerenciar variáveis que deverão ser
+ *         utilizadas pelo usuário final, a exemplo em modelos de documento. Por
+ *         motivos de segurança todos os valores resultantes devem ser tipos
+ *         primitivos ou encapsulamento de valores primitivos
  */
 @Name(ExternalVariables.NAME)
 @Scope(ScopeType.EVENT)
 public class ExternalVariables implements Serializable {
 
     private static final long serialVersionUID = 1L;
+
+    private static final String NULL_STRING = "-";
+    private static final int INT_ZERO_VALUE = 0;
+
+    private static final String NUMERO_PROCESSO_ATUAL = "numeroProcessoAtual";
     private static final String USUARIO_LOGADO = "usuarioLogado";
     private static final String LOCALIZACAO_USUARIO_LOGADO = "localizacaoUsuarioLogado";
     private static final String PERFIL_USUARIO_LOGADO = "perfilUsuarioLogado";
     private static final String DATA_ATUAL = "dataAtual";
     private static final String DATA_ATUAL_FORMATADA = "dataAtualFormatada";
+
     public static final String NAME = "externalVariables";
 
     @Factory(DATA_ATUAL_FORMATADA)
@@ -59,10 +66,23 @@ public class ExternalVariables implements Serializable {
         return extractObjectStringValue(getUsuarioLocalizacaoAtual());
     }
 
+    @Factory(NUMERO_PROCESSO_ATUAL)
+    public String getNumeroProcessoAtual() {
+        final Processo processo = ((ProcessoHome) Component
+                .getInstance(ProcessoHome.NAME)).getInstance();
+        String result;
+        if (processo == null) {
+            result = NULL_STRING;
+        } else {
+            result = processo.getNumeroProcesso();
+        }
+        return result;
+    }
+
     private String extractObjectStringValue(Object object) {
         String result;
         if (object == null) {
-            result = "-";
+            result = NULL_STRING;
         } else {
             result = object.toString();
         }
@@ -71,10 +91,10 @@ public class ExternalVariables implements Serializable {
 
     private String getCurrentDateFormattedValue(final int style) {
         final GregorianCalendar gregorianCalendar = new GregorianCalendar();
-        gregorianCalendar.set(GregorianCalendar.MILLISECOND, 0);
-        gregorianCalendar.set(GregorianCalendar.SECOND, 0);
-        gregorianCalendar.set(GregorianCalendar.MINUTE, 0);
-        gregorianCalendar.set(GregorianCalendar.HOUR_OF_DAY, 0);
+        gregorianCalendar.set(GregorianCalendar.MILLISECOND, INT_ZERO_VALUE);
+        gregorianCalendar.set(GregorianCalendar.SECOND, INT_ZERO_VALUE);
+        gregorianCalendar.set(GregorianCalendar.MINUTE, INT_ZERO_VALUE);
+        gregorianCalendar.set(GregorianCalendar.HOUR_OF_DAY, INT_ZERO_VALUE);
         return DateFormat.getDateInstance(style).format(
                 gregorianCalendar.getTime());
     }
