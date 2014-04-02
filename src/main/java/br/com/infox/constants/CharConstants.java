f633e3c62eb0dcf5842a0cda604e43410328d3cf
Gabriel Morrison Lima Dantas <gabriel@infox.com.br>
2014-04-02 10:22:26 -0300

    Adicionadas constantes que estavam faltando

--- src/main/java/br/com/infox/constants/CharConstants.java
+++ src/main/java/br/com/infox/constants/CharConstants.java
@@ -2,5 +2,5 @@ package br.com.infox.constants;
 
 
 public interface CharConstants {
-    String NODE_NAME_NOT_ALLOWED_CHARS_REGEX = "[^a-zA-Z0-9áàãéíóõú\\s]";
+    String NODE_NAME_NOT_ALLOWED_CHARS_REGEX = "[^a-zA-Z0-9ÁáÀàÃãÉéÍíÓóÕõÚúçÇ\\s]";
 }
