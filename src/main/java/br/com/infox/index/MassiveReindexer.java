4b304518ebe9ff6989f03ddb51c6a45d8c2118e9
jonasbezerra <jonasbezerra@infox.com.br>
2014-04-04 10:55:02 -0300

    Criado reindexador do epp

---
+++ src/main/java/br/com/infox/index/MassiveReindexer.java
@@ -0,0 +1,43 @@
+package br.com.infox.index;
+
+import org.hibernate.Session;
+import org.hibernate.search.FullTextSession;
+import org.hibernate.search.Search;
+import org.jboss.seam.ScopeType;
+import org.jboss.seam.annotations.Create;
+import org.jboss.seam.annotations.In;
+import org.jboss.seam.annotations.Install;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Scope;
+import org.jboss.seam.annotations.Startup;
+import org.jboss.seam.log.LogProvider;
+import org.jboss.seam.log.Logging;
+
+import br.com.infox.hibernate.session.SessionAssistant;
+
+@Scope(ScopeType.APPLICATION)
+@Name(MassiveReindexer.NAME)
+@Install
+@Startup
+public class MassiveReindexer {
+
+    public static final String NAME = "massiveReindexer";
+    private static final LogProvider LOG = Logging.getLogProvider(MassiveReindexer.class);
+    
+    @In private SessionAssistant sessionAssistant; 
+    
+    @Create
+    /**
+     * Reconstrói os índices do Lucene sempre que o epp é iniciado
+     * */
+    public void init() {
+        Session session = sessionAssistant.getSession();
+        FullTextSession fullTextSession = Search.getFullTextSession(session);
+        try {
+            fullTextSession.createIndexer().startAndWait();
+        } catch (InterruptedException e) {
+            LOG.error("Não possível realizar a indexação do lucene", e);
+        }
+    }
+    
+}
