2f9d52610292ebafc78a74cdd456c7bdd66ef4e3
Gabriel Morrison Lima Dantas <gabriel@infox.com.br>
2014-04-28 07:20:59 -0300

    Dialeto personalizado para que o Hibernate reconheça o tipo NCHAR do SQL Server 2012

---
+++ src/main/java/br/com/infox/hibernate/sqlserver/dialect/InfoxSQLServer2012Dialect.java
@@ -0,0 +1,12 @@
+package br.com.infox.hibernate.sqlserver.dialect;
+
+import java.sql.Types;
+
+import org.hibernate.dialect.SQLServer2012Dialect;
+import org.hibernate.type.StandardBasicTypes;
+
+public class InfoxSQLServer2012Dialect extends SQLServer2012Dialect {
+    public InfoxSQLServer2012Dialect() {
+        registerHibernateType(Types.NCHAR, StandardBasicTypes.STRING.getName());
+    }
+}
