6edac1a553949b4f751d0810beee7fbdbda42b7e
Gabriel Morrison Lima Dantas <gabriel@infox.com.br>
2014-04-28 09:06:01 -0300

    Exibição da versão do epp na tela de login

---
+++ src/main/java/br/com/infox/epp/git/GitRepository.java
@@ -0,0 +1,45 @@
+package br.com.infox.epp.git;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.Properties;
+
+import org.jboss.seam.ScopeType;
+import org.jboss.seam.annotations.AutoCreate;
+import org.jboss.seam.annotations.Create;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Scope;
+
+@Name(GitRepository.NAME)
+@AutoCreate
+@Scope(ScopeType.APPLICATION)
+public class GitRepository implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    public static final String NAME = "gitRepository";
+    
+    private String branch;
+    private String commitId;
+    private String buildTime;
+    
+    @Create
+    public void init() throws IOException {
+        Properties properties = new Properties();
+        properties.load(getClass().getClassLoader().getResourceAsStream("git.properties"));
+        this.branch = properties.getProperty("git.branch");
+        this.commitId = properties.getProperty("git.commit.id.abbrev");
+        this.buildTime = properties.getProperty("git.build.time");
+    }
+    
+    public String getBranch() {
+        return branch;
+    }
+    
+    public String getCommitId() {
+        return commitId;
+    }
+    
+    public String getBuildTime() {
+        return buildTime;
+    }
+}
