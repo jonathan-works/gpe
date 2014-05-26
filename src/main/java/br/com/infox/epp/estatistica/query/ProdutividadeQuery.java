5c37350ec17fa7e8b9ea0caeec513eb163daa30b
Jonas S. Bezerra <jonasbezerra@infox.com.br>
2014-05-26 11:32:04 -0300

    Ajuste da query de produtividade

--- src/main/java/br/com/infox/epp/estatistica/query/ProdutividadeQuery.java
+++ src/main/java/br/com/infox/epp/estatistica/query/ProdutividadeQuery.java
@@ -9,23 +9,23 @@ public interface ProdutividadeQuery {
     String PARAM_START = "start";
     String PARAM_COUNT = "count";
 
-    String BASE_QUERY = "SELECT t.nr_prazo, l.ds_localizacao, p.ds_nome, pes.nm_pessoa, "
+    String BASE_QUERY = "SELECT t.nr_prazo, l.ds_localizacao, p.ds_nome, ul.nm_usuario, "
             + "	t.ds_tarefa, AVG(pet.nr_tempo_gasto) AS media_tempo_gasto, MIN(pet.nr_tempo_gasto) AS min_tempo_gasto,"
             + " MAX(pet.nr_tempo_gasto) AS max_tempo_gasto, COUNT(pet.id_processo_epa_tarefa) AS quantidade_tarefas,"
             + " t.tp_prazo"
             + "	FROM tb_processo_epa_tarefa pet"
             + "	INNER JOIN tb_usuario_taskinstance ut ON (ut.id_taskinstance = pet.id_task_instance)"
             + "	INNER JOIN tb_papel p ON (p.id_papel = ut.id_papel)"
-            + "	INNER JOIN tb_pessoa pes ON (pes.id_pessoa = ut.id_usuario_login)"
+            + "	INNER JOIN tb_usuario_login ul ON (ul.id_usuario_login = ut.id_usuario_login)"
             + "	INNER JOIN tb_localizacao l ON (l.id_localizacao = ut.id_localizacao)"
             + "	INNER JOIN tb_tarefa t ON (t.id_tarefa = pet.id_tarefa)";
 
     String INNER_JOIN_FLUXO = " INNER JOIN tb_processo_epa pro ON (pro.id_processo = pet.id_processo)"
             + " INNER JOIN tb_natureza_categoria_fluxo ncf ON (ncf.id_natureza_categoria_fluxo = pro.id_natureza_categoria_fluxo)";
 
-    String GROUP_BY = " GROUP BY t.id_tarefa, t.nr_prazo, l.ds_localizacao, p.ds_nome, pes.nm_pessoa, t.ds_tarefa, t.tp_prazo";
+    String GROUP_BY = " GROUP BY t.id_tarefa, t.nr_prazo, l.ds_localizacao, p.ds_nome, ul.nm_usuario, t.ds_tarefa, t.tp_prazo";
 
-    String ORDER_BY = " ORDER BY pes.nm_pessoa";
+    String ORDER_BY = " ORDER BY ul.nm_usuario";
 
     String CONDICAO_FIXA = " WHERE 1=1";
 
