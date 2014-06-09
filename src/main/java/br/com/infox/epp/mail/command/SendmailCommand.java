e80a885ff8196c5a93bda52edb2c22d63de27655
Gabriel Morrison Lima Dantas <gabriel@infox.com.br>
2014-06-09 13:55:10 -0300

    [41544] Removida propriedade desnecessária que causava erro

--- src/main/java/br/com/infox/epp/mail/command/SendmailCommand.java
+++ src/main/java/br/com/infox/epp/mail/command/SendmailCommand.java
@@ -2,10 +2,6 @@ package br.com.infox.epp.mail.command;
 
 import static java.text.MessageFormat.format;
 
-import java.security.Security;
-
-import javax.net.ssl.SSLSocketFactory;
-
 import org.apache.commons.lang3.time.StopWatch;
 import org.jboss.seam.faces.FacesMessages;
 import org.jboss.seam.faces.Renderer;
@@ -22,20 +18,12 @@ public class SendmailCommand {
         sw.start();
         final FacesMessages messages = FacesMessages.instance();
         try {
-            setProperty(SSLSocketFactory.class.getName());
             Renderer.instance().render(templateFile);
             messages.add("Email enviado com sucesso.");
             LOG.info(format(SENDMAIL_LOG_PATTERN, sw.getTime()));
         } catch (Exception e) {
             messages.add("Erro ao enviar eMail", e);
             LOG.error(format(SENDMAIL_LOG_PATTERN, sw.getTime()), e);
-        } finally {
-            setProperty("");
         }
     }
-
-    private void setProperty(final String value) {
-        Security.setProperty("ssl.SocketFactory.provider", value);
-    }
-
 }
