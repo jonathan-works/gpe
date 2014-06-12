b5e66af3c53865eba3b3066615ae53dabbd61bea
Gabriel Morrison Lima Dantas <gabriel@infox.com.br>
2014-06-12 10:59:05 -0300

    Ajustes para desativar certas coisas quando em produção

--- src/main/java/br/com/infox/filter/MeeterPhaseListener.java
+++ src/main/java/br/com/infox/filter/MeeterPhaseListener.java
@@ -2,11 +2,13 @@ package br.com.infox.filter;
 
 import java.util.Date;
 
+import javax.annotation.PostConstruct;
 import javax.faces.event.PhaseEvent;
 
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Observer;
 import org.jboss.seam.annotations.intercept.BypassInterceptors;
+import org.jboss.seam.contexts.Contexts;
 
 /**
  * Classe para medição de tempo das fases do ciclos de vida JSF
@@ -21,18 +23,28 @@ import org.jboss.seam.annotations.intercept.BypassInterceptors;
 public class MeeterPhaseListener {
 
     private long time;
+    private boolean producao;
+    
+    @PostConstruct
+    public void init() {
+        producao = "true".equals(Contexts.getApplicationContext().get("producao"));
+    }
 
     @Observer("org.jboss.seam.beforePhase")
     public void beforePhase(PhaseEvent event) {
-        time = new Date().getTime();
-        System.out.println("Entrou: " + event.getPhaseId());
+        if (!producao) {
+            time = new Date().getTime();
+            System.out.println("Entrou: " + event.getPhaseId());
+        }
     }
 
     @Observer("org.jboss.seam.afterPhase")
     public void afterPhase(PhaseEvent event) {
-        System.out.println("Saiu: " + event.getPhaseId() + " - "
-                + (new Date().getTime() - time));
-        time = 0;
+        if (!producao) {
+            System.out.println("Saiu: " + event.getPhaseId() + " - "
+                    + (new Date().getTime() - time));
+            time = 0;
+        }
     }
 
 }
