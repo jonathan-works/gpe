1b59941ddfa2ec2f1eb4f2ce5b6fe268a12b9b19
Erik Liberal <erikliberal@infox.com.br>
2014-07-11 12:59:16 -0300

    Adição de método beforesave em perfilcrudaction para adicionar atuomaticamente o pai do setor como pai do setor cadastrado

--- src/main/java/br/com/infox/epp/access/crud/PerfilCrudAction.java
+++ src/main/java/br/com/infox/epp/access/crud/PerfilCrudAction.java
@@ -3,6 +3,7 @@ package br.com.infox.epp.access.crud;
 import org.jboss.seam.annotations.Name;
 
 import br.com.infox.core.crud.AbstractCrudAction;
+import br.com.infox.epp.access.entity.Localizacao;
 import br.com.infox.epp.access.entity.UsuarioLocalizacao;
 import br.com.infox.epp.access.manager.UsuarioLocalizacaoManager;
 
@@ -12,4 +13,19 @@ public class PerfilCrudAction extends AbstractCrudAction<UsuarioLocalizacao, Usu
     public static final String NAME = "perfilCrudAction";
     private static final long serialVersionUID = 1L;
 
+    private Localizacao estrutura;
+    
+    @Override
+    protected void beforeSave() {
+        getInstance().setEstrutura(estrutura);
+    }
+
+    public Localizacao getEstrutura() {
+        return estrutura;
+    }
+
+    public void setEstrutura(Localizacao estrutura) {
+        this.estrutura = estrutura;
+    }
+    
 }
