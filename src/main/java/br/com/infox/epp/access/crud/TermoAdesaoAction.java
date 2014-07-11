e4a593c792018902e6005a0520176d5aee4dbbef
Erik Liberal <erikliberal@infox.com.br>
2014-07-11 10:01:51 -0300

    Criação de action para recuperar termo de adesão a ser exibido em tela de login

---
+++ src/main/java/br/com/infox/epp/access/crud/TermoAdesaoAction.java
@@ -0,0 +1,46 @@
+package br.com.infox.epp.access.crud;
+
+import java.io.Serializable;
+
+import org.jboss.seam.ScopeType;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Scope;
+
+@Scope(ScopeType.CONVERSATION)
+@Name(value=TermoAdesaoAction.NAME)
+public class TermoAdesaoAction implements Serializable {
+    private static final long serialVersionUID = 1L;
+    public static final String NAME = "termoAdesaoAction";
+
+    public String getTermoAdesao() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("<div><div>");
+        sb.append("Cu vix saepe iudico, posse bonorum qualisque ut pri. Id eos clita legere mandamus, cibo insolens phaedrum qui id. Duis affert luptatum ei ius. Reque impedit expetenda vix et, has prodesset rationibus ea, an gubergren disputando intellegebat vim. Ornatus appareat an nec.");
+        sb.append("</div><div></div><div>");
+        sb.append("Ex vix aperiri feugiat maluisset, erat eripuit no eos, sed ullum decore vituperatoribus an. Noluisse reprimique his eu, vim officiis antiopam inciderint ut. Ferri labitur fabulas te vix. Pro ea everti reformidans. Has eius doming te, invenire constituam eum an. In his nemore feugiat, at cum discere aliquando, veritus nominavi ne eum. Scripta suscipiantur ei mea, ne nominati dignissim mea.");
+        sb.append("</div><div></div><div>");
+        sb.append("Dicta iudico an pri, erant quidam oblique eum in. Erant iuvaret cu mei. Nam cu offendit copiosae, an dicta exerci graece mel. Duo everti reprimique cu. Solum erant id pro.");
+        sb.append("</div><div></div><div>");
+        sb.append("Id modo efficiendi reprehendunt eam. Vel at iuvaret convenire intellegebat, autem ancillae ut nec, malis nulla commune qui in. Qui porro efficiantur cu. Vix idque theophrastus ad, noluisse abhorreant quo cu. Ut posse aeque eloquentiam vel.");
+        sb.append("</div><div></div><div>");
+        sb.append("Vix purto elit elitr at, no diceret assueverit sadipscing his, duo regione ancillae consequat ne. Eu usu soleat conceptam. Ea vis prima congue maiestatis, vis id malis pertinacia intellegebat. Mei propriae principes te. Quis paulo inimicus nam eu.");
+        sb.append("</div><div></div><div>");
+        sb.append("Id ornatus officiis duo. Ea porro erroribus liberavisse nam, in cetero detraxit definiebas pri. Reque explicari no has, alii admodum disputationi ex ius. Minim mnesarchum reprehendunt ad mei, cu ubique denique deserunt cum. Harum deleniti comprehensam ne his, sea in quidam scribentur. An vis tempor probatus referrentur, pro probo elitr ridens at. Sea ex quod utamur ullamcorper, pri an nostro virtute nostrud.");
+        sb.append("</div><div></div><div>");
+        sb.append("Odio omnis commune an pri. Sed an audiam similique maiestatis, reque nostrud indoctum quo ex, per no dolore legere repudiandae. Usu officiis tractatos prodesset ea, id labores docendi duo. Te quas facilisi nec, at augue iudico iuvaret nec. Suas aliquid platonem vix ut.");
+        sb.append("</div><div></div><div>");
+        sb.append("Vim ex invenire disputando, at agam graeci consulatu eum, usu ex causae sanctus. Ex cum movet inimicus. Omnium tacimates signiferumque qui ei. Vix an dicat moderatius philosophia, id ius intellegat consectetuer.");
+        sb.append("</div><div></div><div>");
+        sb.append("Case quas adhuc eu sed, ullum oratio labore cu qui, eam tota malorum eu. Ne prima aliquando vim. Tollit honestatis cum ex. In summo vivendum lucilius his, vim tempor ridens essent ei.");
+        sb.append("</div><div></div><div>");
+        sb.append("Duo ei dicant putant aliquid. Et alienum accommodare nam, vim id nonumy consetetur disputando, te ancillae liberavisse sed. Mel ex iudico blandit, id cetero epicurei pertinacia nec. Sed eu dicat ubique, ubique vocent concludaturque id mel.");
+        sb.append("</div></div>");
+        System.out.println(sb.toString());
+        return sb.toString();
+    }
+    
+    public void setTermoAdesao(String termoAdesao) {
+        
+    }
+    
+}
