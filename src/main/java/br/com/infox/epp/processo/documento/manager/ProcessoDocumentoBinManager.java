8a2a221b51bae1fe406a10b84477210abd38b2d3
Erik Liberal <erikliberal@infox.com.br>
2014-07-14 13:03:14 -0300

    Remoção de definição de atributo não mais necessário de ProcessoDocumentoBin

--- src/main/java/br/com/infox/epp/processo/documento/manager/ProcessoDocumentoBinManager.java
+++ src/main/java/br/com/infox/epp/processo/documento/manager/ProcessoDocumentoBinManager.java
@@ -6,7 +6,6 @@ import org.jboss.seam.annotations.Name;
 import br.com.infox.core.file.encode.MD5Encoder;
 import br.com.infox.core.manager.Manager;
 import br.com.infox.core.persistence.DAOException;
-import br.com.infox.epp.access.api.Authenticator;
 import br.com.infox.epp.processo.documento.dao.ProcessoDocumentoBinDAO;
 import br.com.infox.epp.processo.documento.entity.ProcessoDocumento;
 import br.com.infox.epp.processo.documento.entity.ProcessoDocumentoBin;
@@ -21,7 +20,6 @@ public class ProcessoDocumentoBinManager extends Manager<ProcessoDocumentoBinDAO
     public ProcessoDocumentoBin createProcessoDocumentoBin(
             ProcessoDocumento processoDocumento) throws DAOException {
         ProcessoDocumentoBin bin = processoDocumento.getProcessoDocumentoBin();
-        bin.setUsuario(Authenticator.getUsuarioLogado());
         if (bin.getMd5Documento() == null) {
             bin.setMd5Documento(MD5Encoder.encode(processoDocumento.getProcessoDocumentoBin().getModeloDocumento()));
         }
