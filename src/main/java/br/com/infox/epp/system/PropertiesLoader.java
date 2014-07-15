26b06902f97c46cda73413af9122e63306413603
Avner Pereira <avner@infox.com.br>
2014-07-15 11:46:24 -0300

    Leitura do arquivo de properties

--- src/main/java/br/com/infox/epp/system/PropertiesLoader.java
+++ src/main/java/br/com/infox/epp/system/PropertiesLoader.java
@@ -1,7 +1,12 @@
 package br.com.infox.epp.system;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.Properties;
 
@@ -23,10 +28,22 @@ public class PropertiesLoader extends Properties {
 	public void init() {
 		URL resource = getClass().getResource("/src/main/resources/custom_pages.properties");
 		if (resource != null) {
-			String path = resource.getPath();
+
+			InputStream is = getClass().getResourceAsStream("/src/main/resources/custom_pages.properties");
+			InputStreamReader reader = new InputStreamReader(is);
+			BufferedReader br = new BufferedReader(reader);
+		
+			try {
+				String line = null;
+				while ((line = br.readLine()) != null) {
+					System.out.println(line);
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} else {
 			
-			File file = new File(path);
-			file.exists();
 		}
 	}
 }
