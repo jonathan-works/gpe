716d2b66a58fa9f4d5a9bb25282d42632efb69a4
Erik Liberal <erikliberal@infox.com.br>
2014-07-16 11:38:54 -0300

    Adição de campos signature e certchain que ainda são utilizáveis

--- src/main/java/br/com/infox/epp/processo/documento/assinatura/DadosDocumentoAssinavel.java
+++ src/main/java/br/com/infox/epp/processo/documento/assinatura/DadosDocumentoAssinavel.java
@@ -5,6 +5,8 @@ import br.com.infox.epp.documento.entity.TipoProcessoDocumento;
 public class DadosDocumentoAssinavel {
     private TipoProcessoDocumento classificacao;
     private Integer idDocumento;
+    private String signature;
+    private String certChain;
     
     public TipoProcessoDocumento getClassificacao() {
         return classificacao;
@@ -21,4 +23,21 @@ public class DadosDocumentoAssinavel {
     public void setIdDocumento(Integer idDocumento) {
         this.idDocumento = idDocumento;
     }
+
+    public String getSignature() {
+        return signature;
+    }
+
+    public void setSignature(String signature) {
+        this.signature = signature;
+    }
+
+    public String getCertChain() {
+        return certChain;
+    }
+
+    public void setCertChain(String certChain) {
+        this.certChain = certChain;
+    }
+    
 }
