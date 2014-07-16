9b996e8b97e040aa6ad7c73c491b132af4b6cb26
Avner Pereira <avner@infox.com.br>
2014-07-16 11:23:32 -0300

    Carrega ou substitui paginas mapeadas em .properties para as paginas do epp

--- src/main/java/br/com/infox/epp/system/PropertiesLoader.java
+++ src/main/java/br/com/infox/epp/system/PropertiesLoader.java
@@ -1,13 +1,11 @@
 package br.com.infox.epp.system;
 
-import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.Enumeration;
 import java.util.Properties;
 
 import org.jboss.seam.ScopeType;
@@ -15,9 +13,10 @@ import org.jboss.seam.annotations.Create;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Scope;
 import org.jboss.seam.annotations.Startup;
+import org.jboss.seam.contexts.ServletLifecycle;
 
 @Name(PropertiesLoader.NAME)
-@Startup(depends={"poc-login.jar"})
+@Startup()
 @Scope(ScopeType.APPLICATION)
 public class PropertiesLoader extends Properties {
 
@@ -28,22 +27,44 @@ public class PropertiesLoader extends Properties {
 	public void init() {
 		URL resource = getClass().getResource("/src/main/resources/custom_pages.properties");
 		if (resource != null) {
-
 			InputStream is = getClass().getResourceAsStream("/src/main/resources/custom_pages.properties");
-			InputStreamReader reader = new InputStreamReader(is);
-			BufferedReader br = new BufferedReader(reader);
-		
+			
 			try {
-				String line = null;
-				while ((line = br.readLine()) != null) {
-					System.out.println(line);
+				load(is);
+				
+				Enumeration<Object> keys = keys();
+				while (keys.hasMoreElements()) {
+					String key = (keys.nextElement().toString());
+					String value = getProperty(key);
+					
+					File file = new File(ServletLifecycle.getServletContext().getRealPath(key));
+					
+					if (!file.isDirectory()) {
+						if (file.exists()) {
+							file.delete();
+						}
+						
+						InputStream newFileStream = getClass().getResourceAsStream(value);
+						
+						file.createNewFile();
+						FileOutputStream f = new FileOutputStream(file);
+	
+						int read = newFileStream.read();
+						while (read != -1) {
+							f.write(read);
+							read = newFileStream.read();
+						}
+						f.close();
+						newFileStream.close();
+					} else {
+						
+					}
 				}
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		} else {
-			
+			// Resource not found
 		}
 	}
 }
