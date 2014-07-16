4b6324eb03c944cdd0aab4cdfe73d60a65b0a49c
Jonas S. Bezerra <jonasbezerra@infox.com.br>
2014-07-16 13:33:02 -0300

    Crud básico de Perfil

--- src/main/java/br/com/infox/epp/access/crud/PerfilCrudAction.java
+++ src/main/java/br/com/infox/epp/access/crud/PerfilCrudAction.java
@@ -3,29 +3,13 @@ package br.com.infox.epp.access.crud;
 import org.jboss.seam.annotations.Name;
 
 import br.com.infox.core.crud.AbstractCrudAction;
-import br.com.infox.epp.access.entity.Localizacao;
-import br.com.infox.epp.access.entity.UsuarioLocalizacao;
-import br.com.infox.epp.access.manager.UsuarioLocalizacaoManager;
+import br.com.infox.epp.access.entity.Perfil;
+import br.com.infox.epp.access.manager.PerfilManager;
 
 @Name(PerfilCrudAction.NAME)
-public class PerfilCrudAction extends AbstractCrudAction<UsuarioLocalizacao, UsuarioLocalizacaoManager> {
+public class PerfilCrudAction extends AbstractCrudAction<Perfil, PerfilManager> {
 
     public static final String NAME = "perfilCrudAction";
     private static final long serialVersionUID = 1L;
 
-    private Localizacao estrutura;
-    
-    @Override
-    protected void beforeSave() {
-        getInstance().setEstrutura(estrutura);
-    }
-
-    public Localizacao getEstrutura() {
-        return estrutura;
-    }
-
-    public void setEstrutura(Localizacao estrutura) {
-        this.estrutura = estrutura;
-    }
-    
 }
