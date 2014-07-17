62bac948a911a84f012c28c7d966e0e5bf11e483
Jonas S. Bezerra <jonasbezerra@infox.com.br>
2014-07-17 11:40:25 -0300

    Criada aba de associação de perfil no menu de usuário

--- src/main/java/br/com/infox/epp/access/crud/UsuarioPerfilCrudAction.java
+++ src/main/java/br/com/infox/epp/access/crud/UsuarioPerfilCrudAction.java
@@ -5,6 +5,7 @@ import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Scope;
 
 import br.com.infox.core.crud.AbstractCrudAction;
+import br.com.infox.epp.access.entity.UsuarioLogin;
 import br.com.infox.epp.access.entity.UsuarioPerfil;
 import br.com.infox.epp.access.manager.UsuarioPerfilManager;
 
@@ -14,5 +15,23 @@ public class UsuarioPerfilCrudAction extends AbstractCrudAction<UsuarioPerfil, U
 
     private static final long serialVersionUID = 1L;
     public static final String NAME = "usuarioPerfilCrudAction";
+    
+    private UsuarioLogin usuarioLogin;
+
+    public void setUsuarioLogin(UsuarioLogin usuarioLogin) {
+        this.usuarioLogin = usuarioLogin;
+    }
+    
+    @Override
+    protected boolean isInstanceValid() {
+        getInstance().setUsuarioLogin(usuarioLogin);
+        return super.isInstanceValid();
+    }
+    
+    @Override
+    protected void afterSave(String ret) {
+        newInstance();
+        super.afterSave(ret);
+    }
 
 }
