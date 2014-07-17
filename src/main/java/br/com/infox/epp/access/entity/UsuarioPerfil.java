1bb5a09963aaa08171a9c3c3f7139bcbe0c0c004
Jonas S. Bezerra <jonasbezerra@infox.com.br>
2014-07-17 10:51:53 -0300

    Criação da entidade usuário perfil

---
+++ src/main/java/br/com/infox/epp/access/entity/UsuarioPerfil.java
@@ -0,0 +1,59 @@
+package br.com.infox.epp.access.entity;
+
+import static br.com.infox.core.persistence.ORConstants.GENERATOR;
+import static javax.persistence.FetchType.EAGER;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name="tb_usuario_perfil")
+public class UsuarioPerfil {
+
+    private Integer idUsuarioPerfil;
+    private Integer idUsuarioLogin;
+    private Integer idPerfil;
+    
+    @SequenceGenerator(allocationSize=1, initialValue=1, name = GENERATOR, sequenceName = "sq_tb_usuario_perfil")
+    @Id
+    @GeneratedValue(generator = GENERATOR, strategy = GenerationType.SEQUENCE)
+    @Column(name = "id_usuario_perfil", unique = true, nullable = false)
+    public Integer getIdUsuarioPerfil() {
+        return idUsuarioPerfil;
+    }
+    
+    public void setIdUsuarioPerfil(Integer idUsuarioPerfil) {
+        this.idUsuarioPerfil = idUsuarioPerfil;
+    }
+    
+    @ManyToOne(fetch = EAGER)
+    @JoinColumn(name = "id_usuario_login", nullable = false)
+    @NotNull
+    public Integer getIdUsuarioLogin() {
+        return idUsuarioLogin;
+    }
+    
+    public void setIdUsuarioLogin(Integer idUsuarioLogin) {
+        this.idUsuarioLogin = idUsuarioLogin;
+    }
+    
+    @ManyToOne(fetch = EAGER)
+    @JoinColumn(name = "id_perfil", nullable = false)
+    @NotNull
+    public Integer getIdPerfil() {
+        return idPerfil;
+    }
+    
+    public void setIdPerfil(Integer idPerfil) {
+        this.idPerfil = idPerfil;
+    }
+
+}
