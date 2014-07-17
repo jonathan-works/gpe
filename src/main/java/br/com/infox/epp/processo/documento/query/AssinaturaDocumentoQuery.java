e049bfa67e08bd7d61f2a6162a30018d176c2726
Erik Liberal <erikliberal@infox.com.br>
2014-07-17 11:59:14 -0300

    Adição de namedquery para entidade AssinaturaDocumento

--- src/main/java/br/com/infox/epp/processo/documento/query/AssinaturaDocumentoQuery.java
+++ src/main/java/br/com/infox/epp/processo/documento/query/AssinaturaDocumentoQuery.java
@@ -2,6 +2,7 @@ package br.com.infox.epp.processo.documento.query;
 
 public interface AssinaturaDocumentoQuery {
 
+    String PARAM_PROCESSO_DOCUMENTO = "processoDocumento";
     String TABLE_NAME = "tb_assinatura_documento";
     String SEQUENCE_NAME = "sq_" + TABLE_NAME;
     String COL_ID_ASSINATURA = "id_assinatura";
@@ -11,5 +12,12 @@ public interface AssinaturaDocumentoQuery {
     String COL_CERT_CHAIN = "ds_cert_chain";
     String COL_NOME_PAPEL = "nm_papel";
     String COL_NOME_LOCALIZACAO = "nm_localizacao";
+
+    String LIST_ASSINATURA_DOCUMENTO_BY_PROCESSO_DOCUMENTO = "listAssinaturaDocumentoByProcessoDocumento";
+    String LIST_ASSINATURA_DOCUMENTO_BY_PROCESSO_DOCUMENTO_QUERY = "select a "
+            + "from AssinaturaDocumento a "
+            + "inner join a.processoDocumentoBin pdBin " + "where :"
+            + PARAM_PROCESSO_DOCUMENTO
+            + " in elements(pdBin.processoDocumentoList)";
 
 }
