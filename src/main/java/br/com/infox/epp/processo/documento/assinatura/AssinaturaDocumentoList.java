5d0587d97bec6f56ef51252b92ef722a39ff602d
Erik Liberal <erikliberal@infox.com.br>
2014-07-23 07:45:08 -0300

    Adição de entidade para listar as assinaturas através de um entitylist

---
+++ src/main/java/br/com/infox/epp/processo/documento/assinatura/AssinaturaDocumentoList.java
@@ -0,0 +1,42 @@
+package br.com.infox.epp.processo.documento.assinatura;
+
+import java.util.Map;
+
+import org.jboss.seam.ScopeType;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Scope;
+
+import br.com.infox.core.list.EntityList;
+import br.com.infox.core.list.SearchCriteria;
+
+@Name(AssinaturaDocumentoList.NAME)
+@Scope(ScopeType.CONVERSATION)
+public class AssinaturaDocumentoList extends EntityList<AssinaturaDocumento> {
+
+    private static final String DEFAULT_ORDER = "dataAssinatura desc";
+    private static final String DEFAULT_EJBQL = "select o from AssinaturaDocumento";
+    private static final long serialVersionUID = 1L;
+    public static final String NAME = "assinaturaDocumentoList";
+    
+    @Override
+    protected void addSearchFields() {
+        addSearchField("usuario", SearchCriteria.IGUAL);
+        addSearchField("papel", SearchCriteria.IGUAL);
+    }
+
+    @Override
+    protected String getDefaultEjbql() {
+        return DEFAULT_EJBQL;
+    }
+
+    @Override
+    protected String getDefaultOrder() {
+        return DEFAULT_ORDER;
+    }
+
+    @Override
+    protected Map<String, String> getCustomColumnsOrder() {
+        return null;
+    }
+
+}
