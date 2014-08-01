a657f92814f725f58d6d47be096185cf5ec8f3fa
Jonas S. Bezerra <jonasbezerra@infox.com.br>
2014-08-01 07:45:19 -0300

    Ajuste nas trees do menu perfil

--- src/main/java/br/com/infox/epp/access/crud/PerfilTemplateCrudAction.java
+++ src/main/java/br/com/infox/epp/access/crud/PerfilTemplateCrudAction.java
@@ -1,10 +1,18 @@
 package br.com.infox.epp.access.crud;
 
+import static br.com.infox.constants.WarningConstants.UNCHECKED;
+
+import org.jboss.seam.Component;
 import org.jboss.seam.ScopeType;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Scope;
 
 import br.com.infox.core.crud.AbstractCrudAction;
+import br.com.infox.core.tree.TreeHandler;
+import br.com.infox.epp.access.component.tree.EstruturaLocalizacoesPerfilTreeHandler;
+import br.com.infox.epp.access.component.tree.PapelTreeHandler;
+import br.com.infox.epp.access.entity.Localizacao;
+import br.com.infox.epp.access.entity.Papel;
 import br.com.infox.epp.access.entity.PerfilTemplate;
 import br.com.infox.epp.access.manager.PerfilTemplateManager;
 
@@ -14,5 +22,23 @@ public class PerfilTemplateCrudAction extends AbstractCrudAction<PerfilTemplate,
 
     private static final long serialVersionUID = 1L;
     public static final String NAME = "perfilTemplateCrudAction";
+    
+    @Override
+    public void newInstance() {
+        super.newInstance();
+        clearTrees();
+    }
+
+    public void onClickLimpar() {
+        clearTrees();
+    }
+    
+    @SuppressWarnings(UNCHECKED)
+    private void clearTrees() {
+        ((TreeHandler<Papel>) Component.getInstance(PapelTreeHandler.NAME)).clearTree();
+        ((TreeHandler<Localizacao>) Component.getInstance(EstruturaLocalizacoesPerfilTreeHandler.NAME)).clearTree();
+    }
+    
+    
 
 }
