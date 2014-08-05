d4f427624da5fe5f0a2257eca7b306805f6b8194
Victor Pasqualino <victorpasqualino@infox.com.br>
2014-08-05 16:57:36 -0300

    Criado Tela de consulta do log do webservice

--- src/main/java/br/com/infox/epp/system/PropertiesLoader.java
+++ src/main/java/br/com/infox/epp/system/PropertiesLoader.java
@@ -4,8 +4,10 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URL;
+import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Enumeration;
+import java.util.List;
 import java.util.Properties;
 
 import org.jboss.seam.ScopeType;
@@ -18,6 +20,8 @@ import org.jboss.seam.international.Messages;
 import org.jboss.seam.log.LogProvider;
 import org.jboss.seam.log.Logging;
 
+import edu.emory.mathcs.backport.java.util.Collections;
+
 /**
  * Classe que carrega páginas customizadas pelo cliente e as insere no ePP
  * dinamicamente, ou substitui as páginas padrão do ePP, caso essas existam.
@@ -25,31 +29,36 @@ import org.jboss.seam.log.Logging;
  * @author avner
  * 
  */
+@Startup
 @Name(PropertiesLoader.NAME)
-@Startup()
 @Scope(ScopeType.APPLICATION)
-public class PropertiesLoader extends Properties {
+public class PropertiesLoader implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
 	private static final LogProvider LOG = Logging.getLogProvider(PropertiesLoader.class);
 	public static final String NAME = "propertiesLoader";
-	private static final long serialVersionUID = 1L;
+	
+	private static final String PAGE_PROPERTIES = "/custom_pages.properties";
+	private static final String MENU_PROPERTIES = "/menu.properties";
+	
+	private Properties pageProperties;
+	private Properties menuProperties;
+	private List<String> menuItems;
 	
 	@Create
 	public void init() {
-		URL resource = getClass().getResource("/src/main/resources/custom_pages.properties");
-		if (resource != null) {
-			InputStream is = getClass().getResourceAsStream("/src/main/resources/custom_pages.properties");
-			
+		InputStream is = getClass().getResourceAsStream(PAGE_PROPERTIES);
+		if (is != null) {
 			try {
-				load(is);
+				pageProperties = new Properties();
+				pageProperties.load(is);
 				
-				Enumeration<Object> keys = keys();
+				Enumeration<Object> keys = pageProperties.keys();
 				while (keys.hasMoreElements()) {
 					String key = (keys.nextElement().toString());
-					String value = getProperty(key);
+					String value = pageProperties.getProperty(key);
 
-					File file = new File(ServletLifecycle.getServletContext().getRealPath(key));
-
-					performLoad(file, value);
+					performLoad(key, value);
 				}
 			} catch (IOException e) {
 				LOG.error(Messages.instance().get("propertiesLoader.fail"), e);
@@ -59,9 +68,10 @@ public class PropertiesLoader extends Properties {
 		}
 	}
 	
-	private void performLoad(File file, String path) throws IOException {
-		InputStream newInputStream = getClass().getResourceAsStream(path);
+	private void performLoad(String key, String path) throws IOException {
+		InputStream newInputStream = getClass().getResourceAsStream(key);
 		if (newInputStream != null) {
+			File file = new File(ServletLifecycle.getServletContext().getRealPath(path));
 			if (file.exists()) {
 				file.delete();
 			} else {
@@ -79,4 +89,23 @@ public class PropertiesLoader extends Properties {
 			newOutputStream.close();
 		}
 	}
+	
+	@SuppressWarnings("unchecked")
+	public List<String> getMenuItems(){
+		if (menuProperties == null){
+			menuProperties = new Properties();
+			InputStream is = getClass().getResourceAsStream(MENU_PROPERTIES);
+			if (is != null) {
+				try {
+					menuProperties.load(is);
+					menuItems = Collections.unmodifiableList(new ArrayList<>(menuProperties.values()));
+				} catch (IOException e) {
+					menuItems = Collections.unmodifiableList(new ArrayList<>());
+					LOG.error(Messages.instance().get("propertiesLoader.fail"), e);
+				}
+			}
+		}
+		return menuItems;
+	}
+	
 }
