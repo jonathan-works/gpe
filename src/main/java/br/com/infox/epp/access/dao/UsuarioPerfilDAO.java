c531f1b24dc7873e2160f862f8d055b14fa786a8
Erik Liberal <erikliberal@infox.com.br>
2014-08-06 10:08:12 -0300

    Adição de métodos que fazem buscas parametrizadas de UsuarioPerfil

--- src/main/java/br/com/infox/epp/access/dao/UsuarioPerfilDAO.java
+++ src/main/java/br/com/infox/epp/access/dao/UsuarioPerfilDAO.java
@@ -10,9 +10,12 @@ import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Scope;
 
 import br.com.infox.core.dao.DAO;
+import br.com.infox.core.persistence.DAOException;
 import br.com.infox.epp.access.entity.Localizacao;
 import br.com.infox.epp.access.entity.PerfilTemplate;
+import br.com.infox.epp.access.entity.UsuarioLogin;
 import br.com.infox.epp.access.entity.UsuarioPerfil;
+import br.com.infox.epp.access.query.UsuarioPerfilQuery;
 
 @Name(UsuarioPerfilDAO.NAME)
 @Scope(ScopeType.EVENT)
@@ -32,6 +35,21 @@ public class UsuarioPerfilDAO extends DAO<UsuarioPerfil> {
             hql = "select o from PerfilTemplate o where o.localizacao is null";
         }
         return getResultList(hql, params);
+    }
+
+    public UsuarioPerfil getByUsuarioLoginPerfilTemplateLocalizacao(UsuarioLogin usuarioLogin, PerfilTemplate perfilTemplate,
+            Localizacao localizacao) {
+        final Map<String,Object> params = new HashMap<>();
+        params.put(UsuarioPerfilQuery.PARAM_USUARIO_LOGIN, usuarioLogin);
+        params.put(UsuarioPerfilQuery.PARAM_PERFIL_TEMPLATE, perfilTemplate);
+        params.put(UsuarioPerfilQuery.PARAM_LOCALIZACAO, localizacao);
+        return getNamedSingleResult(UsuarioPerfilQuery.GET_BY_USUARIO_LOGIN_PERFIL_TEMPLATE_LOCALIZACAO, params);
+    }
+    
+    public List<UsuarioPerfil> listByUsuarioLogin(UsuarioLogin usuarioLogin) {
+        final Map<String,Object> params = new HashMap<>();
+        params.put(UsuarioPerfilQuery.PARAM_USUARIO_LOGIN, usuarioLogin);
+        return getNamedResultList(UsuarioPerfilQuery.LIST_BY_USUARIO_LOGIN, params);
     }
 
 }
