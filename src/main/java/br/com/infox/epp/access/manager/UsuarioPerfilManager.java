5947c7c18631e3d3061d95fcf0e2379b535978bb
Erik Liberal <erikliberal@infox.com.br>
2014-08-06 10:08:51 -0300

    Adição de métodos que retornam UsuarioPerfil baseados em parâmetros e um método que realiza a exclusão baseada em parâmetros

--- src/main/java/br/com/infox/epp/access/manager/UsuarioPerfilManager.java
+++ src/main/java/br/com/infox/epp/access/manager/UsuarioPerfilManager.java
@@ -4,25 +4,55 @@ import java.util.List;
 
 import org.jboss.seam.ScopeType;
 import org.jboss.seam.annotations.AutoCreate;
+import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Scope;
 
 import br.com.infox.core.manager.Manager;
+import br.com.infox.core.persistence.DAOException;
 import br.com.infox.epp.access.dao.UsuarioPerfilDAO;
 import br.com.infox.epp.access.entity.Localizacao;
 import br.com.infox.epp.access.entity.PerfilTemplate;
+import br.com.infox.epp.access.entity.UsuarioLogin;
 import br.com.infox.epp.access.entity.UsuarioPerfil;
 
 @Name(UsuarioPerfilManager.NAME)
 @Scope(ScopeType.EVENT)
 @AutoCreate
-public class UsuarioPerfilManager extends Manager<UsuarioPerfilDAO, UsuarioPerfil> {
+public class UsuarioPerfilManager extends
+        Manager<UsuarioPerfilDAO, UsuarioPerfil> {
 
     private static final long serialVersionUID = 1L;
     public static final String NAME = "usuarioPerfilManager";
-    
+
+    @In
+    private UsuarioLoginManager usuarioLoginManager;
+
     public List<PerfilTemplate> getPerfisPermitidos(Localizacao localizacao) {
         return getDao().getPerfisPermitidos(localizacao);
     }
-    
+
+    public List<UsuarioPerfil> listByUsuarioLogin(UsuarioLogin usuarioLogin) {
+        return getDao().listByUsuarioLogin(usuarioLogin);
+    }
+
+    public UsuarioPerfil getByUsuarioLoginPerfilTemplateLocalizacao(
+            UsuarioLogin usuarioLogin, PerfilTemplate perfilTemplate,
+            Localizacao localizacao) {
+        return getDao().getByUsuarioLoginPerfilTemplateLocalizacao(
+                usuarioLogin, perfilTemplate, localizacao);
+    }
+
+    public void removeByUsuarioPerfilTemplateLocalizacao(
+            UsuarioLogin usuarioLogin, PerfilTemplate perfilTemplate,
+            Localizacao localizacao) throws DAOException {
+        UsuarioPerfil usuarioPerfil = getDao().getByUsuarioLoginPerfilTemplateLocalizacao(
+                usuarioLogin, perfilTemplate, localizacao);
+        remove(usuarioPerfil);
+        if (listByUsuarioLogin(usuarioLogin).isEmpty()) {
+            usuarioLogin.setAtivo(Boolean.FALSE);
+            usuarioLoginManager.update(usuarioLogin);
+        }
+    }
+
 }
