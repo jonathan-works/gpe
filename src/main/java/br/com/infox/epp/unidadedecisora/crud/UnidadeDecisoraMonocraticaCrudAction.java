0ce7dce2012e012ebc17d4947a7e4ef5f3a7eae7
Victor Pasqualino <victorpasqualino@infox.com.br>
2014-08-06 08:31:59 -0300

    Correcao para limpar a tree de unidade decisora monocratica

--- src/main/java/br/com/infox/epp/unidadedecisora/crud/UnidadeDecisoraMonocraticaCrudAction.java
+++ src/main/java/br/com/infox/epp/unidadedecisora/crud/UnidadeDecisoraMonocraticaCrudAction.java
@@ -3,15 +3,24 @@ package br.com.infox.epp.unidadedecisora.crud;
 import org.jboss.seam.annotations.Name;
 
 import br.com.infox.core.crud.AbstractCrudAction;
+import br.com.infox.epp.access.component.tree.LocalizacaoTreeHandler;
 import br.com.infox.epp.access.entity.Localizacao;
 import br.com.infox.epp.unidadedecisora.entity.UnidadeDecisoraMonocratica;
 import br.com.infox.epp.unidadedecisora.manager.UnidadeDecisoraMonocraticaManager;
+import br.com.infox.seam.util.ComponentUtil;
 
 @Name(UnidadeDecisoraMonocraticaCrudAction.NAME)
 public class UnidadeDecisoraMonocraticaCrudAction extends AbstractCrudAction<UnidadeDecisoraMonocratica, UnidadeDecisoraMonocraticaManager>{
 
 	private static final long serialVersionUID = 1L;
 	public static final String NAME = "unidadeDecisoraMonocraticaCrudAction";
+	
+	@Override
+	public void newInstance() {
+		super.newInstance();
+		LocalizacaoTreeHandler tree = ComponentUtil.getComponent(LocalizacaoTreeHandler.NAME);
+		tree.clearTree();
+	}
 	
 	public Localizacao getLocalizacao() {
 	    return getInstance().getLocalizacao();
