6d0fec7bcc4e686fe2c02b0e7f2d5328aded8159
Gabriel Morrison Lima Dantas <gabriel@infox.com.br>
2014-08-14 07:58:21 -0300

    Validações de associação

--- src/main/java/br/com/infox/epp/tce/prestacaocontas/modelo/manager/ModeloPrestacaoContasManager.java
+++ src/main/java/br/com/infox/epp/tce/prestacaocontas/modelo/manager/ModeloPrestacaoContasManager.java
@@ -4,6 +4,7 @@ import org.jboss.seam.annotations.AutoCreate;
 import org.jboss.seam.annotations.Name;
 
 import br.com.infox.core.manager.Manager;
+import br.com.infox.core.persistence.DAOException;
 import br.com.infox.epp.tce.prestacaocontas.modelo.dao.ModeloPrestacaoContasDAO;
 import br.com.infox.epp.tce.prestacaocontas.modelo.entity.ModeloPrestacaoContas;
 
@@ -13,4 +14,26 @@ public class ModeloPrestacaoContasManager extends Manager<ModeloPrestacaoContasD
 
     private static final long serialVersionUID = 1L;
     public static final String NAME = "modeloPrestacaoContasManager";
+    
+    @Override
+    public ModeloPrestacaoContas persist(ModeloPrestacaoContas o) throws DAOException {
+        validateAssociations(o);
+        return super.persist(o);
+    }
+
+    
+    @Override
+    public ModeloPrestacaoContas update(ModeloPrestacaoContas o) throws DAOException {
+        validateAssociations(o);
+        return super.update(o);
+    }
+    
+    private void validateAssociations(ModeloPrestacaoContas o) throws DAOException {
+        if (o.getClassificacoesDocumento().isEmpty()) {
+            throw new DAOException("O modelo não possui documentos associados");
+        }
+        if (o.getResponsaveis().isEmpty()) {
+            throw new DAOException("O modelo não possui responsáveis associados");
+        }
+    }
 }
