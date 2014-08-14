3e08cf5a69a39dc82ac072d0bc05745c2681010b
Gabriel Morrison Lima Dantas <gabriel@infox.com.br>
2014-08-14 07:57:57 -0300

    toString()

--- src/main/java/br/com/infox/epp/tce/prestacaocontas/modelo/entity/TipoParte.java
+++ src/main/java/br/com/infox/epp/tce/prestacaocontas/modelo/entity/TipoParte.java
@@ -89,4 +89,9 @@ public class TipoParte implements Serializable {
             return false;
         return true;
     }
+    
+    @Override
+    public String toString() {
+        return descricao;
+    }
 }
