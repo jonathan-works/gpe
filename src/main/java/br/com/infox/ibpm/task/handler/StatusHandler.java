3799b0fedfe160f8397521279265335761a8bd1e
Avner Pereira <avner@infox.com.br>
2014-08-15 11:41:20 -0300

    Implementa metodo para mudanca de status do processo ao movimentar

--- src/main/java/br/com/infox/ibpm/task/handler/StatusHandler.java
+++ src/main/java/br/com/infox/ibpm/task/handler/StatusHandler.java
@@ -5,6 +5,8 @@ import static java.text.MessageFormat.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.jboss.seam.log.LogProvider;
+import org.jboss.seam.log.Logging;
 import org.jbpm.context.exe.ContextInstance;
 import org.jbpm.graph.def.ActionHandler;
 import org.jbpm.graph.exe.ExecutionContext;
@@ -14,16 +16,17 @@ import br.com.infox.epp.processo.entity.ProcessoEpa;
 import br.com.infox.epp.processo.manager.ProcessoEpaManager;
 import br.com.infox.epp.processo.status.entity.StatusProcesso;
 import br.com.infox.epp.processo.status.manager.StatusProcessoManager;
-import br.com.infox.epp.tarefa.entity.ProcessoEpaTarefa;
-import br.com.infox.epp.tarefa.manager.ProcessoEpaTarefaManager;
 import br.com.infox.ibpm.process.definition.variable.VariableType;
 import br.com.infox.seam.util.ComponentUtil;
 
 public class StatusHandler implements ActionHandler {
 
+	private static final LogProvider LOG = Logging.getLogProvider(StatusHandler.class);
 	private static final long serialVersionUID = 1L;
 	private Integer statusProcesso;
 
+	public StatusHandler() {}
+	
 	public StatusHandler(String s) {
 		Pattern pattern = Pattern.compile("<statusProcesso>(\\d+)</statusProcesso>");
 		Matcher matcher = pattern.matcher(s);
@@ -35,27 +38,28 @@ public class StatusHandler implements ActionHandler {
 	
 	@Override
 	public void execute(ExecutionContext executionContext) throws Exception {
-		
-		ProcessoEpaTarefaManager processoEpaTarefaManager = ComponentUtil.getComponent(ProcessoEpaTarefaManager.NAME);
 		ProcessoEpaManager processoEpaManager = ComponentUtil.getComponent(ProcessoEpaManager.NAME);
 		StatusProcessoManager statusProcessoManager = ComponentUtil.getComponent(StatusProcessoManager.NAME);
 		
-		ProcessoEpaTarefa processoEpaTarefa = processoEpaTarefaManager.getByTaskInstance(executionContext.getTaskInstance().getId());
-		
-		StatusProcesso status = statusProcessoManager.find(statusProcesso);
-		ProcessoEpa processoEpa = processoEpaTarefa.getProcessoEpa();
-		processoEpa.setStatusProcesso(status);
-		
-		processoEpaManager.update(processoEpa);
-		String varName = format("{0}:{1}", VariableType.STRING, "statusProcesso");
-		
-		ContextInstance contextInstance = executionContext.getProcessInstance().getContextInstance();
-		Token token = executionContext.getToken();
-		String nome = status.getNome();
-		if (contextInstance.hasVariable(varName, token)) {
-			contextInstance.setVariable(varName, nome, token);
-		} else {
-			contextInstance.createVariable(varName, nome, token);
+		try {
+			StatusProcesso status = statusProcessoManager.find(statusProcesso);
+			ProcessoEpa processoEpa = processoEpaManager.find(Integer.parseInt(executionContext.getContextInstance().getVariable("processo").toString(), 10));
+			processoEpa.setStatusProcesso(status);
+			
+			processoEpaManager.update(processoEpa);
+			String varName = format("{0}:{1}", VariableType.STRING, "statusProcesso");
+			
+			ContextInstance contextInstance = executionContext.getProcessInstance().getContextInstance();
+			Token token = executionContext.getToken();
+			String nome = status.getNome();
+			if (contextInstance.hasVariable(varName, token)) {
+				contextInstance.setVariable(varName, nome, token);
+			} else {
+				contextInstance.createVariable(varName, nome, token);
+			}
+			processoEpaManager.flush();
+		} catch (Exception e) {
+			LOG.error("Falha na atribuição de status do processo", e);
 		}
 	}
 }
