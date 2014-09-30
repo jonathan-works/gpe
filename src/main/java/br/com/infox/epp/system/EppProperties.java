4004bc974b529edc61fa8ea2e10ff5c9417477d0
Gabriel Morrison Lima Dantas <gabriel@infox.com.br>
2014-09-30 14:01:55 -0300

    Ajuste temporário para saber qual o banco em tempo de execução

---
+++ src/main/java/br/com/infox/epp/system/EppProperties.java
@@ -0,0 +1,34 @@
+package br.com.infox.epp.system;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.Properties;
+
+import org.jboss.seam.ScopeType;
+import org.jboss.seam.annotations.AutoCreate;
+import org.jboss.seam.annotations.Create;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Scope;
+
+@Name(EppProperties.NAME)
+@Scope(ScopeType.APPLICATION)
+@AutoCreate
+public class EppProperties implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    public static final String NAME = "eppProperties";
+    
+    private Properties properties;
+    
+    @Create
+    public void init() throws IOException {
+        properties = new Properties();
+        properties.load(getClass().getResourceAsStream("/epp.properties"));
+    }
+    
+    public String getProperty(String property) {
+        return properties.getProperty(property);
+    }
+    
+    public static final String PROPERTY_TIPO_BANCO_DADOS = "tipoBancoDados";
+}
