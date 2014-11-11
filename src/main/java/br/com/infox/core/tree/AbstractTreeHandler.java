c9d27e751c1fc72e58dccdba83047a8070fa96b8
Gabriel Morrison Lima Dantas <gabriel@infox.com.br>
2014-11-11 11:56:26 -0300

    Evitando erro aleatório na tree

--- src/main/java/br/com/infox/core/tree/AbstractTreeHandler.java
+++ src/main/java/br/com/infox/core/tree/AbstractTreeHandler.java
@@ -4,6 +4,7 @@ import static br.com.infox.constants.WarningConstants.UNCHECKED;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import javax.faces.component.UIComponent;
@@ -119,7 +120,11 @@ public abstract class AbstractTreeHandler<E> implements TreeHandler<E>, Serializ
     @Override
     public void processTreeSelectionChange(TreeSelectionChangeEvent ev) {
         // Considerando single selection
-        Object selectionKey = new ArrayList<Object>(ev.getNewSelection()).get(0);
+    	Iterator<Object> it = ev.getNewSelection().iterator();
+    	if (!it.hasNext()) {
+    		return;
+    	}
+        Object selectionKey = it.next();
         UITree tree = (UITree) ev.getSource();
         treeId = ":" + tree.getClientId();
 
