e4182d0f71728a03317e0b4a630e6d5e8854d780
Erik Liberal <erikliberal@infox.com.br>
2014-11-26 16:30:31 -0200

    Atualização nos métodos da classe de internacionalização de mensagens

--- src/main/java/br/com/infox/core/messages/Messages.java
+++ src/main/java/br/com/infox/core/messages/Messages.java
@@ -9,33 +9,37 @@ import br.com.infox.epp.system.EppMessagesContextLoader;
 
 public final class Messages {
 
-    private static final Messages messages;
+    private static final Messages MESSAGES;
     static{
-        messages = new Messages();
+        MESSAGES = new Messages();
     }
     
     private Messages() {
     }
     
     public static final Messages getInstance(){
-        return messages;
+        return MESSAGES;
     }
-
+    
     /**
      * Busca o valor de uma propriedade no arquivo de internacionalização
      * @param property a propriedade a ser avaliada
      * @return a tradução do propriedade
      * */
-    public String getMessage(String property) {
-        String result = getMessageMap().get(property);
+    public final String getMessage(String property) {
+        String result = getMessages().get(property);
         if (result==null || result.trim().isEmpty()) {
             result = property;
         }
         return property;
     }
     
+    public final static String resolveMessage(String property){
+        return MESSAGES.getMessage(property);
+    }
+    
     @SuppressWarnings("unchecked")
-    private Map<String,String> getMessageMap(){
+    public final Map<String,String> getMessages(){
         Map<String,String> result;
         if (Contexts.isApplicationContextActive()){
             result = (Map<String, String>) Contexts.getApplicationContext().get(EppMessagesContextLoader.EPP_MESSAGES);
