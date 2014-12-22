5ee44249e1dec37ffe6e85a9943ccbe9dcea0aaa
Erik Liberal <erikliberal@infox.com.br>
2014-12-22 09:34:13 -0200

    Atualização em método de envio de e-mail para que utilize a api de mail da infox

--- src/main/java/br/com/infox/epp/mail/service/AccessMailService.java
+++ src/main/java/br/com/infox/epp/mail/service/AccessMailService.java
@@ -1,8 +1,13 @@
 package br.com.infox.epp.mail.service;
 
-import static java.text.MessageFormat.format;
-
+import java.io.UnsupportedEncodingException;
+import java.text.MessageFormat;
 import java.util.Map;
+
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 
 import org.jboss.seam.ScopeType;
 import org.jboss.seam.annotations.AutoCreate;
@@ -10,14 +15,17 @@ import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Scope;
 
+import br.com.infox.core.mail.Contact;
+import br.com.infox.core.mail.EMailBean;
+import br.com.infox.core.mail.MailSender;
 import br.com.infox.core.messages.Messages;
 import br.com.infox.epp.access.entity.UsuarioLogin;
 import br.com.infox.epp.documento.entity.ModeloDocumento;
 import br.com.infox.epp.documento.manager.ModeloDocumentoManager;
-import br.com.infox.epp.mail.command.SendmailCommand;
 import br.com.infox.epp.mail.entity.EMailData;
 import br.com.infox.epp.system.entity.Parametro;
 import br.com.infox.epp.system.manager.ParametroManager;
+import br.com.infox.seam.exception.ApplicationException;
 import br.com.infox.seam.exception.BusinessException;
 
 @Name(AccessMailService.NAME)
@@ -45,73 +53,107 @@ public class AccessMailService {
     private ParametroManager parametroManager;
     @In(create = true)
     private EMailData emailData;
+    @In
+    private String nomeSistema;
+    @In
+    private String emailSistema;
 
-    private String resolveTipoDeEmail(String parametro) {
+    private String resolveTipoDeEmail(final String parametro) {
         String nomeParam = null;
-        if (LOGIN.equals(parametro)) {
-            nomeParam = MODELO_SEM_LOGIN;
-        } else if (EMAIL.equals(parametro)) {
-            nomeParam = MODELO_COM_LOGIN;
+        if (AccessMailService.LOGIN.equals(parametro)) {
+            nomeParam = AccessMailService.MODELO_SEM_LOGIN;
+        } else if (AccessMailService.EMAIL.equals(parametro)) {
+            nomeParam = AccessMailService.MODELO_COM_LOGIN;
         }
         return nomeParam;
     }
 
     /**
-     * Envia um e-mail informando a nova senha para o usuário que solicitou a mudança
-     * 
-     * @param parametro o tipo de modelo a ser utilizado (com/sem login)
-     * @param usuario o usuário que solicitou a mudança
-     * @param password a senha gerada
-     * 
-     * @throws BusinessException caso não seja possível enviar o e-mail
+     * Envia um e-mail informando a nova senha para o usuário que solicitou a
+     * mudança
+     *
+     * @param parametro
+     *            o tipo de modelo a ser utilizado (com/sem login)
+     * @param usuario
+     *            o usuário que solicitou a mudança
+     * @param password
+     *            a senha gerada
+     * @throws MessagingException
+     * @throws UnsupportedEncodingException
+     *
+     * @throws BusinessException
+     *             caso não seja possível enviar o e-mail
      * */
-    public void enviarEmailDeMudancaDeSenha(final String parametro,
-            final UsuarioLogin usuario, final String password) {
+    public void enviarEmailDeMudancaDeSenha(final String parametro, final UsuarioLogin usuario, final String password) {
         final String nomeParametro = resolveTipoDeEmail(parametro);
         final ModeloDocumento modelo = findModelo(nomeParametro);
         enviarEmailModelo(modelo, usuario, password);
     }
 
-    private ModeloDocumento findModelo(String nomeParametro) {
-        final Parametro parametro = parametroManager.getParametro(nomeParametro);
+    private ModeloDocumento findModelo(final String nomeParametro) {
+        final Parametro parametro = this.parametroManager.getParametro(nomeParametro);
         ModeloDocumento result = null;
         if (parametro != null) {
             final String nomeModelo = parametro.getValorVariavel();
-            if (nomeModelo != null && !"false".equals(nomeModelo)) {
-                result = modeloDocumentoManager.getModeloDocumentoByTitulo(nomeModelo);
+            if ((nomeModelo != null) && !"false".equals(nomeModelo)) {
+                result = this.modeloDocumentoManager.getModeloDocumentoByTitulo(nomeModelo);
             }
         }
         if (result == null) {
             result = new ModeloDocumento();
-            StringBuilder defaultEmail = new StringBuilder();
-            Map<String, String> localeMsgs = Messages.getInstance().getMessages();
-            defaultEmail.append(format(DEFAULT_EMAIL_LINE, localeMsgs.get(USUARIO_MAIL_DEFAULT_FIELD_NOME), CAMPO_USUARIO));
-            defaultEmail.append(format(DEFAULT_EMAIL_LINE, localeMsgs.get(USUARIO_MAIL_DEFAULT_FIELD_SENHA), CAMPO_SENHA));
+            final StringBuilder defaultEmail = new StringBuilder();
+            final Map<String, String> localeMsgs = Messages.getInstance().getMessages();
+            defaultEmail
+            .append(MessageFormat.format(AccessMailService.DEFAULT_EMAIL_LINE,
+                    localeMsgs.get(AccessMailService.USUARIO_MAIL_DEFAULT_FIELD_NOME),
+                    AccessMailService.CAMPO_USUARIO));
+            defaultEmail.append(MessageFormat.format(AccessMailService.DEFAULT_EMAIL_LINE,
+                    localeMsgs.get(AccessMailService.USUARIO_MAIL_DEFAULT_FIELD_SENHA), AccessMailService.CAMPO_SENHA));
             result.setModeloDocumento(defaultEmail.toString());
         }
         return result;
     }
 
-    private void enviarEmailModelo(ModeloDocumento modelo,
-            UsuarioLogin usuario, String password) {
-        String conteudo = resolverConteudo(modelo, usuario, password);
-
-        emailData.setUseHtmlBody(true);
-        emailData.setBody(conteudo);
-        emailData.getRecipientList().clear();
-        emailData.getRecipientList().add(usuario);
-        emailData.setSubject(Messages.resolveMessage("usuario.senha.generated.subject"));
-        new SendmailCommand().execute("/WEB-INF/email/emailTemplate.xhtml");
+    private Session getEmailSession() {
+        Session session = null;
+        try {
+            final InitialContext context = new InitialContext();
+            session = (Session) context.lookup("java:jboss/mail/epp");
+        } catch (final NamingException e) {
+            throw new IllegalArgumentException(e);
+        }
+        return session;
     }
 
-    private String resolverConteudo(ModeloDocumento modelo,
-            UsuarioLogin usuario, String password) {
+    private void enviarEmailModelo(final ModeloDocumento modelo, final UsuarioLogin usuario, final String password) {
+        final String conteudo = resolverConteudo(modelo, usuario, password);
+
+        final EMailBean mail = new EMailBean();
+        mail.setUseHtmlBody(true);
+        mail.setBody(conteudo);
+        mail.getReceivers().clear();
+        mail.getReceivers().add(new Contact(usuario.getNomeUsuario(), usuario.getEmail()));
+        mail.setSubject("[otherMail] " + Messages.resolveMessage("usuario.senha.generated.subject"));
+        mail.setSender(new Contact(this.nomeSistema, this.emailSistema));
+        try {
+            MailSender.sendMail(mail, getEmailSession());
+        } catch (UnsupportedEncodingException | MessagingException e) {
+            throw new ApplicationException(Messages.resolveMessage("mail.send.fail"), e);
+        }
+    }
+
+    private String resolverConteudo(final ModeloDocumento modelo, final UsuarioLogin usuario, final String password) {
         String modeloDocumento = modelo.getModeloDocumento();
 
-        modeloDocumento = modeloDocumentoManager.evaluateModeloDocumento(modelo);
-        modeloDocumento = modeloDocumento.replace(format(EXPRESSION_PATTERN, CAMPO_USUARIO), usuario.getNomeUsuario());
-        modeloDocumento = modeloDocumento.replace(format(EXPRESSION_PATTERN, CAMPO_LOGIN), usuario.getLogin());
-        modeloDocumento = modeloDocumento.replace(format(EXPRESSION_PATTERN, CAMPO_SENHA), password);
+        modeloDocumento = this.modeloDocumentoManager.evaluateModeloDocumento(modelo);
+        modeloDocumento = modeloDocumento.replace(
+                MessageFormat.format(AccessMailService.EXPRESSION_PATTERN, AccessMailService.CAMPO_USUARIO),
+                usuario.getNomeUsuario());
+        modeloDocumento = modeloDocumento.replace(
+                MessageFormat.format(AccessMailService.EXPRESSION_PATTERN, AccessMailService.CAMPO_LOGIN),
+                usuario.getLogin());
+        modeloDocumento = modeloDocumento.replace(
+                MessageFormat.format(AccessMailService.EXPRESSION_PATTERN, AccessMailService.CAMPO_SENHA), password);
         return modeloDocumento;
     }
 
