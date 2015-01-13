f73f46d32a1e7a48c8f47a264eca9f8a24292ba0
Avner Pereira <avner@infox.com.br>
2015-01-13 09:55:47 -0300

    Tela de Impressão: adiciona filtro de documentos impressos, corrige filtro de data de assinatura e mostra dataTable apenas ao pesquisar

--- src/main/java/br/com/infox/epp/processo/comunicacao/impressao/ImpressaoComunicacaoList.java
+++ src/main/java/br/com/infox/epp/processo/comunicacao/impressao/ImpressaoComunicacaoList.java
@@ -12,7 +12,6 @@ import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Scope;
 
 import br.com.infox.core.list.EntityList;
-import br.com.infox.core.list.SearchCriteria;
 import br.com.infox.epp.processo.comunicacao.ComunicacaoMetadadoProvider;
 import br.com.infox.epp.processo.comunicacao.MeioExpedicao;
 import br.com.infox.epp.processo.comunicacao.service.ComunicacaoService;
@@ -40,24 +39,48 @@ public class ImpressaoComunicacaoList extends EntityList<Processo> {
 	
 	private static final String DEFAULT_ORDER = "idProcesso";
 	
-	private static final String R1 = "exists (select 1 from MetadadoProcesso mp where mp.processo = o " +
-									 "			and mp.metadadoType = '" + ComunicacaoService.MEIO_EXPEDICAO + "' " +
-									 "			and mp.valor = #{impressaoComunicacaoList.meioExpedicao.name()} ) ";
+	private static final String CONDICAO_MEIO_IMPRESSAO =
+	        "and exists (select 1 from MetadadoProcesso mp where "
+	                + "mp.metadadoType = '" + ComunicacaoService.MEIO_EXPEDICAO + "' "
+                    + "and mp.valor = #{impressaoComunicacaoList.meioExpedicao.name()} "
+                    + "and mp.processo = o) ";
 	
-	private static final String R2 = "exists (select 1 from DestinatarioModeloComunicacao dmc " +
-									 " 		  inner join dmc.comunicacao c " +
-									 "		  inner join c.assinaturas a " +	
-									 " 	      where cast(a.dataAssinatura as date) = cast(#{impressaoComunicacaoList.dataAssinatura} as date) " +
-									 "		  and dmc.id = (select cast(mp.valor as integer) from MetadadoProcesso mp " + 
-									 "						where mp.metadadoType = '" + ComunicacaoMetadadoProvider.DESTINATARIO.getMetadadoType() + "' " +
-									 "						and mp.processo = o ) )";	
+	private static final String CONDICAO_DATA_ASSINATURA_PREFIX =
+	        "and exists (select 1 from DestinatarioModeloComunicacao dmc "
+	        + "inner join dmc.comunicacao c "
+	        + "inner join c.assinaturas a "
+	        + "where a.dataAssinatura is not null ";
+	private static final String CONDICAO_DATA_ASSINATURA_SUFIX =
+	        "and dmc.id = (select cast(mp.valor as integer) from MetadadoProcesso mp "
+	                + "where mp.metadadoType = '" + ComunicacaoMetadadoProvider.DESTINATARIO.getMetadadoType() + "' "
+	                + "and mp.processo = o))";
+	private static final String CONDICAO_DATA_INICIO = "and cast(a.dataAssinatura as date) >= cast(#{impressaoComunicacaoList.dataInicio} as date) ";
+	private static final String CONDICAO_DATA_FIM = "and cast(a.dataAssinatura as date) <= cast(#{impressaoComunicacaoList.dataFim} as date) ";
+	private static final String CONDICAO_IMPRESSO =
+	        "and exists (select 1 from MetadadoProcesso mp where "
+	                + "mp.metadadoType = '" + ComunicacaoMetadadoProvider.IMPRESSA.getMetadadoType() + "' "
+	                + "and cast(mp.valor as boolean) = #{impressaoComunicacaoList.impresso} "
+	                + "and mp.processo = o)";
+	private static final String CONDICAO_NAO_IMPRESSO = "and "
+	        + "("
+                + "not exists (select 1 from MetadadoProcesso mp where "
+                    + "mp.metadadoType = '" + ComunicacaoMetadadoProvider.IMPRESSA.getMetadadoType() + "' "
+                    + "and mp.processo = o) "
+                + "or exists (select 1 from MetadadoProcesso mp where "
+                    + "mp.metadadoType = '" + ComunicacaoMetadadoProvider.IMPRESSA.getMetadadoType() + "' "
+                    + "and cast(mp.valor as boolean) = #{impressaoComunicacaoList.impresso} "
+                    + "and mp.processo = o)"
+            + ")";
 	
 	@In
 	private ImpressaoComunicacaoService impressaoComunicacaoService;
 	
 	private List<MeioExpedicao> meiosExpedicao;
 	private MeioExpedicao meioExpedicao;
-	private Date dataAssinatura;
+	private Boolean showDataTable = false;
+	private Date dataInicio;
+	private Date dataFim;
+	private Boolean impresso = false;
 	
 	{
 		meioExpedicao = MeioExpedicao.IM;
@@ -77,27 +100,61 @@ public class ImpressaoComunicacaoList extends EntityList<Processo> {
 			return compare == 0 ? o2DataAssinatura.compareTo(o1DataAssinatura): compare;
 		}
 	};
-	
+
+	private String getEjbqlRestrictedByFilters() {
+        StringBuilder sb = new StringBuilder(DEFAULT_EJBQL);
+        if (meioExpedicao != null) {
+            sb.append(CONDICAO_MEIO_IMPRESSAO);
+        }
+        if (impresso != null) {
+            sb.append(impresso ? CONDICAO_IMPRESSO : CONDICAO_NAO_IMPRESSO);
+        }
+        if (dataInicio == null && dataFim == null) {
+            return sb.toString();
+        } else {
+            sb.append(CONDICAO_DATA_ASSINATURA_PREFIX);
+            if (dataInicio != null) {
+                sb.append(CONDICAO_DATA_INICIO);
+            }
+            if (dataFim != null) {
+                sb.append(CONDICAO_DATA_FIM);
+            }
+            sb.append(CONDICAO_DATA_ASSINATURA_SUFIX);
+        }
+        return sb.toString();
+    }
+
 	@Override
 	public List<Processo> getResultList() {
+	    setEjbql(getEjbqlRestrictedByFilters());
 		List<Processo> resultList = super.getResultList();
 		Collections.sort(resultList, comparator);
+		this.showDataTable = true;
 		return resultList;
 	}
 	
-	@Override
-	public void newInstance() {
-		super.newInstance();
-		this.meioExpedicao = MeioExpedicao.IM;
-	}
+    @Override
+    public void newInstance() {
+        super.newInstance();
+        this.meioExpedicao = MeioExpedicao.IM;
+        this.dataInicio = null;
+        this.dataFim = null;
+        this.impresso = false;
+    }
 
 	@Override
 	protected void addSearchFields() {
-		addSearchField("meioExpedicao", SearchCriteria.IGUAL, R1);
-		addSearchField("dataAssinatura", SearchCriteria.DATA_IGUAL, R2);
 	}
-	
-	public List<MeioExpedicao> getMeiosExpedicao() {
+
+    public void showDataTable() {
+        setShowDataTable(true);
+    }
+
+    public void hideDataTable() {
+        setShowDataTable(false);
+    }
+
+    public List<MeioExpedicao> getMeiosExpedicao() {
 		return meiosExpedicao;
 	}
 
@@ -124,12 +181,36 @@ public class ImpressaoComunicacaoList extends EntityList<Processo> {
 		this.meioExpedicao = meioExpedicao;
 	}
 
-	public Date getDataAssinatura() {
-		return dataAssinatura;
-	}
+    public Boolean getShowDataTable() {
+        return showDataTable;
+    }
 
-	public void setDataAssinatura(Date dataAssinatura) {
-		this.dataAssinatura = dataAssinatura;
-	}
+    public void setShowDataTable(Boolean showDataTable) {
+        this.showDataTable = showDataTable;
+    }
+
+    public Date getDataInicio() {
+        return dataInicio;
+    }
+
+    public void setDataInicio(Date dataInicio) {
+        this.dataInicio = dataInicio;
+    }
+
+    public Date getDataFim() {
+        return dataFim;
+    }
+
+    public void setDataFim(Date dataFim) {
+        this.dataFim = dataFim;
+    }
+
+    public Boolean getImpresso() {
+        return impresso;
+    }
+
+    public void setImpresso(Boolean impresso) {
+        this.impresso = impresso;
+    }
 	
 }
