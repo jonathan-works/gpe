6c9f8b7a1278d1f572cbf920ddd9bb146a27caee
Avner Pereira <avner@infox.com.br>
2015-01-14 17:01:06 -0300

    Cria DateRangeValidator e edita inputDate para validar intervalor arbitrários de datas

---
+++ src/main/java/br/com/infox/jsf/validator/DateRangeValidator.java
@@ -0,0 +1,59 @@
+package br.com.infox.jsf.validator;
+
+import java.util.Date;
+import java.util.Map;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.intercept.BypassInterceptors;
+
+import br.com.infox.core.messages.Messages;
+import br.com.infox.core.util.DateUtil;
+
+@org.jboss.seam.annotations.faces.Validator(id = DateRangeValidator.NAME)
+@Name(DateRangeValidator.NAME)
+@BypassInterceptors
+public class DateRangeValidator implements Validator {
+
+    public static final String NAME = "dateRangeValidator";
+    private static final String DEFAULT_ERROR_MESSAGE = Messages.resolveMessage("validator.Date.Range");
+
+    public void validate(FacesContext context, UIComponent component, Object value) {
+        Date startDate = null, endDate = null;
+        String errorMessage = null;
+        
+        Map<String, Object> attr = component.getAttributes();
+        startDate = (Date) attr.get("startDate");
+        endDate = (Date) attr.get("endDate");
+        errorMessage = (String) attr.get("errorMessage");
+        if (errorMessage == null) {
+            errorMessage = DEFAULT_ERROR_MESSAGE;
+        }
+        
+        if (value == null) {
+            return;
+        }
+        Date date = (Date) value;
+        if (endDate == null && startDate == null) {
+            return;
+        }
+        if (endDate != null) {
+            endDate = DateUtil.getEndOfDay(endDate);
+            if (date != null && date.after(endDate)) {
+                throw new ValidatorException(new FacesMessage(errorMessage));
+            }
+        }
+        if (startDate != null) {
+            startDate = DateUtil.getBeginningOfDay(startDate);
+            if (date != null && date.before(startDate)) {
+                throw new ValidatorException(new FacesMessage(errorMessage));
+            }
+        }
+    }
+
+}
