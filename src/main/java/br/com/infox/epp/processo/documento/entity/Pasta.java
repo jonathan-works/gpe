2e0ba855251e53b525c44e63f88c5dd4edc16965
Jonas S. Bezerra <jonasbezerra@infox.com.br>
2015-02-18 14:22:03 -0300

    Na anexação de documento, permitir escolher pastas, vindo a pasta da Instrução marcada por default refs #53911

--- src/main/java/br/com/infox/epp/processo/documento/entity/Pasta.java
+++ src/main/java/br/com/infox/epp/processo/documento/entity/Pasta.java
@@ -2,8 +2,6 @@ package br.com.infox.epp.processo.documento.entity;
 
 import static br.com.infox.epp.processo.documento.query.PastaQuery.GET_BY_PROCESSO;
 import static br.com.infox.epp.processo.documento.query.PastaQuery.GET_BY_PROCESSO_QUERY;
-import static br.com.infox.epp.processo.documento.query.PastaQuery.GET_DEFAULT_BY_PROCESSO;
-import static br.com.infox.epp.processo.documento.query.PastaQuery.GET_DEFAULT_BY_PROCESSO_QUERY;
 import static br.com.infox.epp.processo.documento.query.PastaQuery.TOTAL_DOCUMENTOS_PASTA;
 import static br.com.infox.epp.processo.documento.query.PastaQuery.TOTAL_DOCUMENTOS_PASTA_QUERY;
 
@@ -33,7 +31,6 @@ import br.com.infox.epp.processo.entity.Processo;
 @Table(name = Pasta.TABLE_NAME)
 @NamedQueries({
     @NamedQuery(name = GET_BY_PROCESSO, query = GET_BY_PROCESSO_QUERY),
-    @NamedQuery(name = GET_DEFAULT_BY_PROCESSO, query = GET_DEFAULT_BY_PROCESSO_QUERY),
     @NamedQuery(name = TOTAL_DOCUMENTOS_PASTA, query = TOTAL_DOCUMENTOS_PASTA_QUERY)
 })
 public class Pasta implements Serializable, Cloneable {
