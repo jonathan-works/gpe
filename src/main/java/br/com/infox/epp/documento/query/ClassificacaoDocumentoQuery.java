7af158cc66e78df50e75daffa21d0b72558a9db4
Victor Pasqualino <victorpasqualino@infox.com.br>
2015-03-12 18:16:22 -0300

    Ajuste ao trazer as classificacoes documento diferentes

--- src/main/java/br/com/infox/epp/documento/query/ClassificacaoDocumentoQuery.java
+++ src/main/java/br/com/infox/epp/documento/query/ClassificacaoDocumentoQuery.java
@@ -24,7 +24,8 @@ public interface ClassificacaoDocumentoQuery {
     
     String FIND_CLASSIFICACAO_DOCUMENTO_BY_DESCRICAO = "findClassificacaoDocumentoByDescricao";
     String FIND_CLASSIFICACAO_DOCUMENTO_BY_DESCRICAO_QUERY = "select o from ClassificacaoDocumento o where o.descricao like :" + PARAM_DESCRICAO;
+    
     String LIST_CLASSIFICACAO_DOCUMENTO_BY_PROCESSO = "listClassificacaoDocumentoByProcesso";
-    String LIST_CLASSIFICACAO_DOCUMENTO_BY_PROCESSO_QUERY = "select o from ClassificacaoDocumento o inner join o.documentoList dl"
+    String LIST_CLASSIFICACAO_DOCUMENTO_BY_PROCESSO_QUERY = "select distinct(o) from ClassificacaoDocumento o inner join o.documentoList dl"
     		+ " where dl.processo = :" + PARAM_PROCESSO + " order by o.descricao";
 }
