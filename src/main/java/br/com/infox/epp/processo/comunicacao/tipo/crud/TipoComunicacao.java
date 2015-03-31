7bec5e1ee28ce7851fc879d3c2e9ed1993754629
Maria Luísa Souza Matos <marialuisa@infox.com.br>
2015-03-31 11:42:00 -0300

    Ajustes para verificação do Tipo de Comunicação refs #56291

--- src/main/java/br/com/infox/epp/processo/comunicacao/tipo/crud/TipoComunicacao.java
+++ src/main/java/br/com/infox/epp/processo/comunicacao/tipo/crud/TipoComunicacao.java
@@ -38,8 +38,8 @@ public class TipoComunicacao implements Serializable {
     @NotNull
     @Column(name = "ds_tipo_comunicacao", nullable = false, length = LengthConstants.DESCRICAO_MEDIA, unique = true)
     private String descricao;
-    
-    @Min(0)
+
+	@Min(0)
     @NotNull
     @Column(name = "nr_dias_prazo_ciencia", nullable = false)
     private Integer quantidadeDiasCiencia;
@@ -108,4 +108,36 @@ public class TipoComunicacao implements Serializable {
     public String toString() {
     	return descricao;
     }
+    
+    @Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((descricao == null) ? 0 : descricao.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (!(obj instanceof TipoComunicacao))
+			return false;
+		TipoComunicacao other = (TipoComunicacao) obj;
+		if (descricao == null) {
+			if (other.descricao != null)
+				return false;
+		} else if (!descricao.equals(other.descricao))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
 }
