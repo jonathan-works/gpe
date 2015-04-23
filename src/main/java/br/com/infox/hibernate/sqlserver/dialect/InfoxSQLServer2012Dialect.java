b84114cc74654888296d2c0f71b1d606579af59f
Avner Pereira <avner@infox.com.br>
2015-04-23 17:34:30 -0300

    Cria função de banco para retornar o número do processo root e poder filtrar no painel refs #57876

--- src/main/java/br/com/infox/hibernate/sqlserver/dialect/InfoxSQLServer2012Dialect.java
+++ src/main/java/br/com/infox/hibernate/sqlserver/dialect/InfoxSQLServer2012Dialect.java
@@ -6,10 +6,12 @@ import org.hibernate.dialect.SQLServer2012Dialect;
 import org.hibernate.type.StandardBasicTypes;
 
 import br.com.infox.hibernate.function.DocumentoSuficientementeAssinado;
+import br.com.infox.hibernate.function.NumeroProcessoRoot;
 
 public class InfoxSQLServer2012Dialect extends SQLServer2012Dialect {
     public InfoxSQLServer2012Dialect() {
         registerHibernateType(Types.NCHAR, StandardBasicTypes.STRING.getName());
         registerFunction("DocumentoSuficientementeAssinado", new DocumentoSuficientementeAssinado());
+        registerFunction("NumeroProcessoRoot", new NumeroProcessoRoot());
     }
 }
