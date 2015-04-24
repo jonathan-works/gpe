92605ab8f0a22f9a6b939245a1a356574813f15a
Maria Luísa Souza Matos <marialuisa@infox.com.br>
2015-04-24 10:13:07 -0300

    Ajustes nas funções de assinatura para desconsiderar o perfil inativo de usuários refs #57531

--- src/main/java/br/com/infox/certificado/ValidaDocumentoAction.java
+++ src/main/java/br/com/infox/certificado/ValidaDocumentoAction.java
@@ -118,7 +118,7 @@ public class ValidaDocumentoAction implements Serializable {
 
 	public boolean podeAssinar() {
 		UsuarioPerfil usuarioPerfil = Authenticator.getUsuarioPerfilAtual();
-		if (documento == null)
+		if (documento == null || !usuarioPerfil.getAtivo())
 			return false;
 
 		boolean isAssinavel = documento.isDocumentoAssinavel(usuarioPerfil.getPerfilTemplate().getPapel());
@@ -157,7 +157,7 @@ public class ValidaDocumentoAction implements Serializable {
 	}
 
 	public void assinaDocumento(UsuarioPerfil usuarioPerfil) {
-		if (this.documentoBin != null && !isAssinadoPor(usuarioPerfil)) {
+		if (this.documentoBin != null && usuarioPerfil.getAtivo() && !isAssinadoPor(usuarioPerfil)) {
 			try {
 				CertificateSignatureBundleBean bundle = getSignature();
 				for (CertificateSignatureBean certificateSignatureBean : bundle.getSignatureBeanList()) {
