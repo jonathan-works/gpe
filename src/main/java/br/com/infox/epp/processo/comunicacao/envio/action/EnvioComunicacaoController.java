dc8e26dadeedf1bb6ca8bfcbc99bf22bdbfc292f
Maria Luísa Souza Matos <marialuisa@infox.com.br>
2015-04-27 10:22:53 -0300

    Ajustes para utilização do popup para download da Comunicação na página de tarefa de Enviar Comunicação refs #58027

--- src/main/java/br/com/infox/epp/processo/comunicacao/envio/action/EnvioComunicacaoController.java
+++ src/main/java/br/com/infox/epp/processo/comunicacao/envio/action/EnvioComunicacaoController.java
@@ -24,7 +24,6 @@ import br.com.infox.certificado.bean.CertificateSignatureBundleBean;
 import br.com.infox.certificado.bean.CertificateSignatureBundleStatus;
 import br.com.infox.certificado.exception.CertificadoException;
 import br.com.infox.core.action.ActionMessagesService;
-import br.com.infox.core.file.download.FileDownloader;
 import br.com.infox.core.messages.InfoxMessages;
 import br.com.infox.core.persistence.DAOException;
 import br.com.infox.epp.access.api.Authenticator;
@@ -283,16 +282,6 @@ public class EnvioComunicacaoController implements Serializable {
 		return signatureBean;
 	}
 
-	public void downloadComunicacaoCompleta(DestinatarioModeloComunicacao destinatario) {
-		try {
-			byte[] pdf = comunicacaoService.gerarPdfCompleto(modeloComunicacao, destinatario);
-			FileDownloader.download(pdf, "application/pdf", "Comunicação.pdf");
-		} catch (DAOException e) {
-			LOG.error("", e);
-			actionMessagesService.handleDAOException(e);
-		}
-	}
-	
 	public List<TipoComunicacao> getTiposComunicacao() {
 		if (tiposComunicacao == null) {
 			tiposComunicacao = tipoComunicacaoManager.listTiposComunicacaoAtivos();
