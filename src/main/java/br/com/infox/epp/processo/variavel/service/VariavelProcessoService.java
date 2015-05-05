8d036bf326933c8aede3e12db61a369fd9a387c7
Erik Liberal <erik.liberal@gmail.com>
2015-05-05 11:00:14 -0300

    Adição de interpretação de expressões refs #58263

--- src/main/java/br/com/infox/epp/processo/variavel/service/VariavelProcessoService.java
+++ src/main/java/br/com/infox/epp/processo/variavel/service/VariavelProcessoService.java
@@ -13,8 +13,9 @@ import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Scope;
 import org.jboss.seam.annotations.Transactional;
 import org.jboss.seam.bpm.ManagedJbpmContext;
-import org.jboss.seam.contexts.Contexts;
 import org.jboss.seam.core.Expressions;
+import org.jboss.seam.core.Expressions.MethodExpression;
+import org.jboss.seam.core.Expressions.ValueExpression;
 import org.jbpm.context.exe.ContextInstance;
 import org.jbpm.graph.exe.ProcessInstance;
 
@@ -86,31 +87,41 @@ public class VariavelProcessoService {
         Object variable = processInstance.getContextInstance().getVariable(definicao.getNome());
         VariavelProcesso variavelProcesso = inicializaVariavelProcesso(processInstance, definicao);
 		if (variable != null){
-        	variavelProcesso= setValor(variavelProcesso, variable);
+			variavelProcesso.setValor(variable.toString());
         } else {
         	List<MetadadoProcesso> metadados = metadadoProcessoManager.getMetadadoProcessoByType(processo, definicao.getNome());
         	if (metadados != null && metadados.size()>0){
-        		variavelProcesso = setValor(processo, variavelProcesso, metadados);
-        	} else if (definicao.getValorPadrao() != null){
-        		Contexts.getEventContext().set("processo", processo);
-        		variavelProcesso = setValor(definicao, variavelProcesso);
-    		} else {
-    			variavelProcesso = null;
-    		}
+        		setValor(processo, metadados, variavelProcesso);
+        	} else {
+				final String valorPadrao = definicao.getValorPadrao();
+				if (valorPadrao != null){
+					setValor(valorPadrao, processo, variavelProcesso);
+				} else {
+					variavelProcesso = null;
+				}
+			}
         }
         return variavelProcesso;
 	}
 
 	/**
 	 * Adiciona valor baseado em expressão, de valor padrão configurada na definição do fluxo, ao container de variável de processo
-	 * @param definicao
+	 * @param valorPadrao
+	 * @param processo
 	 * @param variavelProcesso
 	 * @return
 	 */
-	private VariavelProcesso setValor(DefinicaoVariavelProcesso definicao, VariavelProcesso variavelProcesso) {
-		Object result = Expressions.instance().createValueExpression(definicao.getValorPadrao()).getValue();
-		variavelProcesso.setValor(result != null ? result.toString() : "");
-		return variavelProcesso;
+	private void setValor(String valorPadrao, Processo processo, VariavelProcesso variavelProcesso) {
+		Object value = null;
+		Expressions expressions = Expressions.instance();
+		try {
+			MethodExpression<Object> methodExpression = expressions.createMethodExpression(valorPadrao, Object.class, Processo.class);
+			value = methodExpression.invoke(processo);
+		} catch (Exception e) {
+			ValueExpression<Object> valueExpression = expressions.createValueExpression(valorPadrao);
+			value = valueExpression.getValue();
+		}
+		variavelProcesso.setValor(value != null ? value.toString() : "");
 	}
 
 	/**
@@ -120,7 +131,7 @@ public class VariavelProcessoService {
 	 * @param metadados
 	 * @return
 	 */
-	private VariavelProcesso setValor(Processo processo, VariavelProcesso variavelProcesso, List<MetadadoProcesso> metadados) {
+	private void setValor(Processo processo, List<MetadadoProcesso> metadados, VariavelProcesso variavelProcesso) {
 		StringBuilder sb = new StringBuilder();
 		boolean firstValue = true;
 		for (MetadadoProcesso metadadoProcesso : metadados) {
@@ -131,18 +142,6 @@ public class VariavelProcessoService {
 			firstValue = false;
 		}
 		variavelProcesso.setValor(sb.toString());
-		return variavelProcesso;
-	}
-
-	/**
-	 *  Adiciona valor armazenado no contexto do jbpm ao container de variável de processo
-	 * @param variavelProcesso
-	 * @param variable
-	 * @return
-	 */
-	private VariavelProcesso setValor(VariavelProcesso variavelProcesso, Object variable) {
-		variavelProcesso.setValor(variable.toString());
-		return variavelProcesso;
 	}
 
 	private VariavelProcesso inicializaVariavelProcesso(ProcessInstance processInstance,
