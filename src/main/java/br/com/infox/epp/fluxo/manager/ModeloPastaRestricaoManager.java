55335aeaaec17686bcb59d242957cad59fbfaffe
Alan Costa <alancosta@infox.com.br>
2015-05-08 11:39:33 -0300

    Implementação da chamada de DAO para listagem e remoção de todas as restrições de determinado modelo de pasta refs #58402
    exibição da div de restrições ao selecionar modelo de pasta refs #58402

--- src/main/java/br/com/infox/epp/fluxo/manager/ModeloPastaRestricaoManager.java
+++ src/main/java/br/com/infox/epp/fluxo/manager/ModeloPastaRestricaoManager.java
@@ -1,17 +1,30 @@
 package br.com.infox.epp.fluxo.manager;
 
+import java.util.List;
+
 import org.jboss.seam.annotations.AutoCreate;
 import org.jboss.seam.annotations.Name;
 
 import br.com.infox.core.manager.Manager;
+import br.com.infox.core.persistence.DAOException;
 import br.com.infox.epp.fluxo.dao.ModeloPastaDAO;
+import br.com.infox.epp.fluxo.dao.ModeloPastaRestricaoDAO;
 import br.com.infox.epp.fluxo.entity.ModeloPasta;
+import br.com.infox.epp.fluxo.entity.ModeloPastaRestricao;
 
 @Name(ModeloPastaRestricaoManager.NAME)
 @AutoCreate
-public class ModeloPastaRestricaoManager extends Manager<ModeloPastaDAO, ModeloPasta>{
+public class ModeloPastaRestricaoManager extends Manager<ModeloPastaRestricaoDAO, ModeloPastaRestricao>{
 
 	private static final long serialVersionUID = 1L;
 	static final String NAME = "modeloPastaRestricaoManager";
+	
+	public List<ModeloPastaRestricao> getByModeloPasta(ModeloPasta modeloPasta) {
+		return getDao().getByModeloPasta(modeloPasta);
+	}
+	
+	public void deleteByModeloPasta(ModeloPasta modelo) throws DAOException{
+		getDao().deleteByModeloPasta(modelo);
+	}
 
 }
