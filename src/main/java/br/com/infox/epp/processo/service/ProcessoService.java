71c77a553f7ba57ba466abe8752662537ce5ec66
Maria Luísa Souza Matos <marialuisa@infox.com.br>
2015-05-27 16:07:19 -0300

    Correção na verificação de tipo do processo para considerar processos que não possuem tipo refs #59837

--- src/main/java/br/com/infox/epp/processo/service/ProcessoService.java
+++ src/main/java/br/com/infox/epp/processo/service/ProcessoService.java
@@ -7,6 +7,7 @@ import javax.ejb.TransactionAttribute;
 import javax.ejb.TransactionAttributeType;
 
 import br.com.infox.epp.processo.entity.Processo;
+import br.com.infox.epp.processo.metadado.entity.MetadadoProcesso;
 import br.com.infox.epp.processo.metadado.type.EppMetadadoProvider;
 import br.com.infox.epp.processo.type.TipoProcesso;
 
@@ -23,8 +24,12 @@ public class ProcessoService implements Serializable{
 	
 	public boolean isTipoProcesso(String tipoProcesso, Processo processo){
 		TipoProcesso byName = TipoProcesso.getByName(tipoProcesso);
-		TipoProcesso tpProcesso = processo.getMetadado(EppMetadadoProvider.TIPO_PROCESSO).getValue();
-		return byName.equals(tpProcesso);
+		MetadadoProcesso metadadoTipoProcesso = processo.getMetadado(EppMetadadoProvider.TIPO_PROCESSO);
+		if(metadadoTipoProcesso != null){
+		    TipoProcesso tpProcesso = metadadoTipoProcesso.getValue();
+		    return byName.equals(tpProcesso);
+		}
+		return false; 
 	}
 
 }
