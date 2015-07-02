ff38944cc8d282d8d142a49e74afe784a3b17128
Maria Luísa Souza Matos <marialuisa@infox.com.br>
2015-07-02 16:13:01 -0300

    Testes para geração do pdf sem necessidade de contexto jsp (para el usadas no fluxo)

--- src/main/java/br/com/infox/epp/fluxo/crud/NaturezaCrudAction.java
+++ src/main/java/br/com/infox/epp/fluxo/crud/NaturezaCrudAction.java
@@ -1,8 +1,20 @@
 package br.com.infox.epp.fluxo.crud;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpMethod;
+import org.apache.commons.httpclient.methods.GetMethod;
 import org.jboss.seam.annotations.Name;
+import org.jboss.seam.document.DocumentData;
 
 import br.com.infox.core.crud.AbstractCrudAction;
+import br.com.infox.core.report.AutomaticReportController;
+import br.com.infox.epp.cdi.config.BeanManager;
 import br.com.infox.epp.fluxo.entity.Natureza;
 import br.com.infox.epp.fluxo.manager.NaturezaManager;
 import br.com.infox.epp.processo.partes.type.ParteProcessoEnum;
@@ -12,6 +24,8 @@ public class NaturezaCrudAction extends AbstractCrudAction<Natureza, NaturezaMan
 
     private static final long serialVersionUID = 1L;
     public static final String NAME = "naturezaCrudAction";
+    
+    private DocumentData data;
     
     @Override
     public void newInstance() {
@@ -63,4 +77,50 @@ public class NaturezaCrudAction extends AbstractCrudAction<Natureza, NaturezaMan
         return ParteProcessoEnum.values();
     }
 
+    public void teste() throws IOException {
+		ExecutorService executor = Executors.newFixedThreadPool(20);
+		class A implements Runnable {
+			private int id;
+			public A(int id) {
+				this.id = id;
+			}
+			
+			@Override
+			public void run() {
+				try {
+					HttpClient client = new HttpClient();
+		    		HttpMethod method = new GetMethod("http://localhost:8080/epp-tcepe/RelacaoJulgamento/PautaDOE/pautadoe.seam?id=" + id);
+		    		AutomaticReportController a = BeanManager.INSTANCE.getReference(AutomaticReportController.class);
+		    		if (id % 2 == 0) {
+		    			method.addRequestHeader(AutomaticReportController.KEY_HEADER_NAME, a.getKey().toString());
+		    		}
+		    		client.executeMethod(method);
+					FileOutputStream fos = new FileOutputStream("/tmp/a/" + id + ".pdf");
+					byte[] bytes = method.getResponseBody();
+					fos.write(bytes, 0, bytes.length);
+					fos.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		
+		for (int i = 0; i < 100; i++) {
+			executor.execute(new A(i));
+		}
+		executor.shutdown();
+		try {
+			executor.awaitTermination(999999, TimeUnit.DAYS);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+    }
+    
+    public DocumentData getData() {
+		return data;
+	}
+    
+    public void setData(DocumentData data) {
+		this.data = data;
+	}
 }
