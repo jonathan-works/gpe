3ca67add12eba40c08fefe26d02502dfa14b1ecd
Avner Pereira <avner@infox.com.br>
2015-08-21 08:27:41 -0300

    Ajuste na query que retorna o nome da pasta com contador de documentos para considerar os excluídos quando for o caso

--- src/main/java/br/com/infox/epp/processo/documento/action/PastaAction.java
+++ src/main/java/br/com/infox/epp/processo/documento/action/PastaAction.java
@@ -172,7 +172,7 @@ public class PastaAction implements Serializable {
     }
 
     public String getNomePasta(Pasta pasta) {
-		return pastaManager.getNomePasta(pasta, documentoProcessoAction.getDocumentoFilter(), !canLogicDelete(pasta));
+		return pastaManager.getNomePasta(pasta, documentoProcessoAction.getDocumentoFilter(), !documentoProcessoAction.podeUsuarioVerHistorico());
     }
 
     public Map<Integer, PastaRestricaoBean> getRestricoes() {
