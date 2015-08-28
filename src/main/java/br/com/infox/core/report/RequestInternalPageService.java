93d10de54b845d5b57642efb2b7081a0cd3c0847
Jovan Fernandes <jovan.andrade.fernandes@gmail.com>
2015-08-28 11:43:57 -0300

    Comentarios sobre a variavel do jboss utilizada

--- src/main/java/br/com/infox/core/report/RequestInternalPageService.java
+++ src/main/java/br/com/infox/core/report/RequestInternalPageService.java
@@ -36,7 +36,12 @@ public class RequestInternalPageService implements Serializable {
 
 	public static final LogProvider LOG = Logging
 			.getLogProvider(RequestInternalPageService.class);
-
+	
+	/**
+	 * TODO: como está implementada a classe depende do jboss pois é necessario sabe o ip e a porta do servidor que está rodando.
+	 * Uma alternativa é passar o <ip>:<port> como parâmetro de inicialização ou colocar na configuração do servidor de aplicação um jndi 
+	 * que informe o parâmetro   
+	 */
 	private static final String JBOSS_HTTP_SOCKET_BINDING = "jboss.as:socket-binding-group=standard-sockets,socket-binding=http";
 	private static final String JBOSS_HTTPS_SOCKET_BINDING = "jboss.as:socket-binding-group=standard-sockets,socket-binding=https";
 	private static final long serialVersionUID = 1L;
@@ -94,6 +99,7 @@ public class RequestInternalPageService implements Serializable {
 
 	private Integer getServerListeningPort() {
 		try {
+			//em alguns AS servers a propriedade é "port" ou inver e "boundPort"
 			return (Integer) mBeanServer.getAttribute(socketBindingMBean,
 					"boundPort");
 		} catch (AttributeNotFoundException | InstanceNotFoundException
@@ -111,6 +117,7 @@ public class RequestInternalPageService implements Serializable {
 			mBeanServer = ManagementFactory.getPlatformMBeanServer();
 
 			socketBindingMBean = new ObjectName(JBOSS_HTTPS_SOCKET_BINDING);
+			//em alguns AS servers a propriedade é inet-address ao invés de boundAddress
 			String  boundAddress = (String) mBeanServer.getAttribute(socketBindingMBean, "boundAddress");
 			if (boundAddress == null) {
 				socketBindingMBean = new ObjectName(JBOSS_HTTP_SOCKET_BINDING);
