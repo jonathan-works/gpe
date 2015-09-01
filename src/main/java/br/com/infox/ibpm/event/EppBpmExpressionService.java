392dced95b303ffdac605c4d8a779af3c1c87391
Erik Liberal <erikliberal@infox.com.br>
2015-09-01 12:10:56 -0300

    Ajuste em classe de expressões para utilizar mensagens internacionalizadas e incluir método que retorna lista de métodos externos refs #64410

--- src/main/java/br/com/infox/ibpm/event/EppBpmExpressionService.java
+++ src/main/java/br/com/infox/ibpm/event/EppBpmExpressionService.java
@@ -3,6 +3,7 @@ package br.com.infox.ibpm.event;
 import static br.com.infox.epp.processo.service.VariaveisJbpmProcessosGerais.PROCESSO;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.ejb.Stateless;
 import javax.inject.Named;
@@ -15,36 +16,41 @@ import com.google.inject.Inject;
 
 @Stateless
 @Named(BpmExpressionService.NAME)
-public class EppBpmExpressionService extends AbstractBpmExpressionService implements Serializable, BpmExpressionService {
+public class EppBpmExpressionService implements Serializable, BpmExpressionService {
 
     private static final long serialVersionUID = 1L;
 
     @Inject private ContabilizadorPrazo contabilizadorPrazo;
     @Inject private PastaManager pastaManager;
 
-    @External(tooltip="???Verificar???")
+    @External(tooltip = "process.events.expression.atribuirCiencia.tooltip")
     public void atribuirCiencia() {
         contabilizadorPrazo.atribuirCiencia();
     }
 
-    @External(tooltip="???Verificar???")
+    @External(tooltip = "process.events.expression.atribuirCumprimento.tooltip")
     public void atribuirCumprimento() {
         contabilizadorPrazo.atribuirCumprimento();
     }
 
-    @External(value={
-            @Parameter(label = "Nome da pasta", tooltip = "Nome da pasta conforme definido em campo Descrição", selectable = true),
-            @Parameter(defaultValue = PROCESSO) }, tooltip = "Expressão utilizada para tornar pasta disponível para leitura para participantes do processo")
+    @External(value = {
+            @Parameter(defaultValue = "'Nome da pasta'", label = "process.events.expression.param.nomePasta.label", tooltip = "process.events.expression.param.nomePasta.tooltip", selectable = true),
+            @Parameter(defaultValue = PROCESSO, label = "process.events.expression.param.processo.label", tooltip = "process.events.expression.param.processo.tooltip") }, tooltip = "process.events.expression.disponibilizarPastaParaParticipantesProcesso.tooltip")
     public void disponibilizarPastaParaParticipantesProcesso(String descricaoPasta, Long idProcesso)
             throws DAOException {
         pastaManager.disponibilizarPastaParaParticipantesProcesso(descricaoPasta, idProcesso);
     }
-    
-    @External(value={
-        @Parameter(label = "Nome da pasta", tooltip = "Nome da pasta conforme definido em campo Descrição", selectable = true),
-        @Parameter(defaultValue = PROCESSO) }, tooltip = "Expressão utilizada para tornar pasta disponível para leitura para não participantes e consulta pública")
-    public void tornarPastaPublica(String nomePasta, Long processo) throws DAOException{
+
+    @External(value = {
+            @Parameter(defaultValue = "'Nome da pasta'", label = "process.events.expression.param.nomePasta.label", tooltip = "process.events.expression.param.nomePasta.tooltip", selectable = true),
+            @Parameter(defaultValue = PROCESSO, label = "process.events.expression.param.processo.label", tooltip = "process.events.expression.param.processo.tooltip") }, tooltip = "process.events.expression.tornarPastaPublica.tooltip")
+    public void tornarPastaPublica(String nomePasta, Long processo) throws DAOException {
         pastaManager.tornarPastaPublica(nomePasta, processo);
+    }
+
+    @Override
+    public List<ExternalMethod> getExternalMethods() {
+        return BpmExpressionServiceConsumer.instance().getExternalMethods(this);
     }
 
 }
