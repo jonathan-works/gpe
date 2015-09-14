ff85e9363dd208d8a62776e3ecb47c99728eb55c
Paulo J. R. Braga Filho <paulofilho@infox.com.br>
2015-09-14 16:08:45 -0300

    Remocao de arquivos nao utilizados

--- src/main/java/br/com/infox/epp/estado/EstadoQuery.java
+++
@@ -1,8 +0,0 @@
-package br.com.infox.epp.estado;
-
-public interface EstadoQuery {
-    String PARAM_SIGLA = "sigla";
-    
-    String ESTADO_BY_SIGLA = "Estado.estadoBySigla";
-    String ESTADO_BY_SIGLA_QUERY = "select o from Estado o where o.sigla = :" + PARAM_SIGLA;
-}
