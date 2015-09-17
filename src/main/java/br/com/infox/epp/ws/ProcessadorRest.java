e5847c491e0410506b2717ac82c61996037cc0e5
Paulo J. R. Braga Filho <paulofilho@infox.com.br>
2015-09-17 15:45:56 -0300

    ProcessadorRest utilizado apenas no pacote dos serviços de perfil

--- src/main/java/br/com/infox/epp/ws/ProcessadorRest.java
+++ src/main/java/br/com/infox/epp/ws/ProcessadorRest.java
@@ -1,5 +1,6 @@
 package br.com.infox.epp.ws;
 
+import java.lang.reflect.Method;
 import java.util.List;
 
 import javax.inject.Inject;
@@ -12,54 +13,72 @@ import org.jboss.resteasy.core.ResourceMethod;
 import org.jboss.resteasy.core.ServerResponse;
 import org.jboss.resteasy.spi.Failure;
 import org.jboss.resteasy.spi.HttpRequest;
+import org.jboss.resteasy.spi.interception.AcceptedByMethod;
 import org.jboss.resteasy.spi.interception.PreProcessInterceptor;
 
 import br.com.infox.epp.system.manager.ParametroManager;
 import br.com.infox.epp.ws.messages.WSMessages;
 
 /**
- * Define um processador padrão para serviços REST, responsável por gerenciar a autenticação por meio de um token
+ * Define um processador padrão para serviços REST, responsável por gerenciar a
+ * autenticação por meio de um token
+ * 
  * @author paulo
  *
  */
 @Provider
 @ServerInterceptor
-public class ProcessadorRest implements PreProcessInterceptor {
-	
+public class ProcessadorRest implements PreProcessInterceptor, AcceptedByMethod {
+
+	/**
+	 * Pacote onde esse interceptador será aplicado
+	 */
+	private static final String PACOTE_INTERCEPTADOR2 = UsuarioRest.class.getPackage().getName();
+
 	private static final String TOKEN_NAME = "webserviceToken";
 	public static final String NOME_TOKEN_HEADER_HTTP = "token";
 
 	@Inject
 	private ParametroManager parametroManager;
-	
-	
+
 	private ServerResponse getRespostaForbidden() throws ValidationException {
 		throw new ValidationException(WSMessages.ME_TOKEN_INVALIDO.codigo());
 	}
-	
+
 	private void validarToken(String token) throws ValidationException {
-		String tokenParametro = parametroManager.getValorParametro(TOKEN_NAME); 
-		if (tokenParametro == null || !tokenParametro.equals(token)){
+		String tokenParametro = parametroManager.getValorParametro(TOKEN_NAME);
+		if (tokenParametro == null || !tokenParametro.equals(token)) {
 			throw new ValidationException(WSMessages.ME_TOKEN_INVALIDO.codigo());
 		}
 	}
-	
+
 	@Override
-	public ServerResponse preProcess(HttpRequest request, ResourceMethod method) throws Failure, WebApplicationException {
-		List<String> valoresToken = request.getHttpHeaders().getRequestHeader(NOME_TOKEN_HEADER_HTTP); 
-		if(valoresToken == null || valoresToken.size() == 0)
-		{
+	public ServerResponse preProcess(HttpRequest request, ResourceMethod method)
+			throws Failure, WebApplicationException {
+		List<String> valoresToken = request.getHttpHeaders().getRequestHeader(NOME_TOKEN_HEADER_HTTP);
+		if (valoresToken == null || valoresToken.size() == 0) {
 			return getRespostaForbidden();
 		}
 		String valorToken = valoresToken.get(0);
-		try
-		{
-		validarToken(valorToken);
-		}
-		catch(ValidationException e) {
+		try {
+			validarToken(valorToken);
+		} catch (ValidationException e) { //
+
 			return getRespostaForbidden();
 		}
 		return null;
+	}
+
+	/**
+	 * Aplica esse interceptador apenas no pacote definido em
+	 * {@link #PACOTE_INTERCEPTADOR2}
+	 */
+	@Override
+	public boolean accept(@SuppressWarnings("rawtypes") Class classe, Method metodo) {
+		if (classe.getPackage().getName().startsWith(PACOTE_INTERCEPTADOR2)) {
+			return true;
+		}
+		return false;
 	}
 
 }
