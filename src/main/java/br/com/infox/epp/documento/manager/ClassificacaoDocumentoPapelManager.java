11431aaa54daa8ad88745b9d85931245584c0de0
Maria Luísa Souza Matos <marialuisa@infox.com.br>
2015-09-28 11:49:34 -0300

    Adição de consulta para validar se a assinatura de um papel pode tornar o documento suficientemente assinado refs #65481

--- src/main/java/br/com/infox/epp/documento/manager/ClassificacaoDocumentoPapelManager.java
+++ src/main/java/br/com/infox/epp/documento/manager/ClassificacaoDocumentoPapelManager.java
@@ -2,6 +2,11 @@ package br.com.infox.epp.documento.manager;
 
 import java.util.List;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import javax.persistence.criteria.Subquery;
+
 import org.jboss.seam.annotations.AutoCreate;
 import org.jboss.seam.annotations.Name;
 
@@ -10,6 +15,8 @@ import br.com.infox.epp.access.entity.Papel;
 import br.com.infox.epp.documento.dao.ClassificacaoDocumentoPapelDAO;
 import br.com.infox.epp.documento.entity.ClassificacaoDocumento;
 import br.com.infox.epp.documento.entity.ClassificacaoDocumentoPapel;
+import br.com.infox.epp.documento.entity.ClassificacaoDocumentoPapel_;
+import br.com.infox.epp.documento.type.TipoAssinaturaEnum;
 
 @AutoCreate
 @Name(ClassificacaoDocumentoPapelManager.NAME)
@@ -28,5 +35,34 @@ public class ClassificacaoDocumentoPapelManager extends Manager<ClassificacaoDoc
 
 	public List<ClassificacaoDocumentoPapel> getByClassificacaoDocumento(ClassificacaoDocumento classificacaoDocumento) {
 		return getDao().getByClassificacaoDocumento(classificacaoDocumento);
+	}
+	
+	public boolean papelPodeTornarSuficientementeAssinado(Papel papel, ClassificacaoDocumento classificacaoDocumento) {
+		CriteriaBuilder cb = getDao().getEntityManager().getCriteriaBuilder();
+		CriteriaQuery<Integer> query = cb.createQuery(Integer.class);
+		Root<ClassificacaoDocumentoPapel> cdp = query.from(ClassificacaoDocumentoPapel.class);
+		
+		Subquery<Long> subquery1 = query.subquery(Long.class);
+		Root<ClassificacaoDocumentoPapel> cdp2 =  subquery1.from(ClassificacaoDocumentoPapel.class);
+		subquery1.select(cb.count(cdp2.get(ClassificacaoDocumentoPapel_.id)));
+		subquery1.where(cb.equal(cdp2.get(ClassificacaoDocumentoPapel_.tipoAssinatura), TipoAssinaturaEnum.O),
+					cb.equal(cdp2.get(ClassificacaoDocumentoPapel_.classificacaoDocumento), cdp.get(ClassificacaoDocumentoPapel_.classificacaoDocumento)));
+		
+		Subquery<Integer> subquery2 = query.subquery(Integer.class);
+		cdp2 = subquery2.from(ClassificacaoDocumentoPapel.class);
+		subquery2.select(cb.literal(1));
+		subquery2.where(cb.equal(cdp2.get(ClassificacaoDocumentoPapel_.papel), cdp.get(ClassificacaoDocumentoPapel_.papel)),
+					cb.equal(cdp2.get(ClassificacaoDocumentoPapel_.classificacaoDocumento), cdp.get(ClassificacaoDocumentoPapel_.classificacaoDocumento)),
+					cdp2.get(ClassificacaoDocumentoPapel_.tipoAssinatura).in(TipoAssinaturaEnum.O, TipoAssinaturaEnum.S));
+		
+		query.where(cb.equal(cdp.get(ClassificacaoDocumentoPapel_.classificacaoDocumento), classificacaoDocumento),
+					cb.equal(cdp.get(ClassificacaoDocumentoPapel_.papel), papel),
+					cb.lt(subquery1, 2),
+					cb.exists(subquery2)
+				);
+
+		query.select(cb.literal(1));
+		List<Integer> result = getDao().getEntityManager().createQuery(query).setMaxResults(1).getResultList();
+		return result != null && !result.isEmpty();
 	}
 }
