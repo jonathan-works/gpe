21319b8de88aac97965bde02d3a924e84aa9d3e0
Erik Liberal <erikliberal@infox.com.br>
2015-09-29 15:25:10 -0300

    Adição de system exception ancestral a EppSystemException para exceções que não necessitam da geração de um protocolo e adição de classe factory para criar ambos os tipos de exceção refs #65475

--- src/main/java/br/com/infox/core/exception/EppSystemException.java
+++ src/main/java/br/com/infox/core/exception/EppSystemException.java
@@ -1,38 +1,23 @@
 package br.com.infox.core.exception;
 
-import static java.text.MessageFormat.format;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Map.Entry;
 import java.util.UUID;
 
-import br.com.infox.core.messages.InfoxMessages;
-
-public class EppSystemException extends RuntimeException {
+public class EppSystemException extends SystemException {
 
     private static final long serialVersionUID = 1L;
 
-    private final ErrorCode errorCode;
-    private final Map<String, Object> parameters;
     private final String protocol;
-    
+
     public EppSystemException(ErrorCode errorCode) {
         this(errorCode, null, UUID.randomUUID());
     }
 
-    private EppSystemException(ErrorCode errorCode, Throwable cause, UUID protocol) {
-        super(getLocaleMessage(errorCode, protocol), cause);
-        this.errorCode = errorCode;
-        this.parameters = new HashMap<>();
+    EppSystemException(ErrorCode errorCode, Throwable cause, UUID protocol) {
+        super(errorCode, SystemException.getLocaleMessage(errorCode, protocol.toString()), cause);
         this.protocol = protocol.toString();
     }
 
-    private static String getLocaleMessage(ErrorCode errorCode, UUID protocol) {
-        String messageKey = format("{0}_{1}", errorCode.getClass().getName(), errorCode.getErrorCode());
-        return format(InfoxMessages.getInstance().get(messageKey), protocol.toString());
-    }
-
+    @Deprecated
     public static EppSystemException create(ErrorCode errorCode, Throwable cause) {
         if (cause instanceof EppSystemException) {
             return (EppSystemException) cause;
@@ -40,33 +25,12 @@ public class EppSystemException extends RuntimeException {
         return new EppSystemException(errorCode, cause, UUID.randomUUID());
     }
 
-    public ErrorCode getErrorCode() {
-        return errorCode;
-    }
-
-    public EppSystemException set(String field, Object value) {
-        parameters.put(field, value);
-        return this;
-    }
-
-    public Object get(String field) {
-        return parameters.get(field);
-    }
-    
-    private String getLogErrorMessage() {
-        StringBuilder sb = new StringBuilder();
-        sb.append("\r\nProtocol ").append(protocol).append("\r\n");
-        sb.append(errorCode.getClass().getName()).append(": ").append(errorCode.getErrorCode()).append("\r\n");
-        sb.append("Parameters");
-        for (Entry<String, Object> entry : parameters.entrySet()) {
-            sb.append("   ").append(entry.getKey()).append(" : ").append(entry.getValue()).append("\r\n");
-        }
-        return sb.toString();
-    }
-     
     @Override
     public String toString() {
-        return getLogErrorMessage();
+        StringBuilder sb = new StringBuilder();
+        sb.append("\r\nProtocol ").append(protocol).append("\r\n");
+        sb.append(super.toString());
+        return sb.toString();
     }
-    
+
 }
