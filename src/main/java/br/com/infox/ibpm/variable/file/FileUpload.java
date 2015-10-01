0c59c5e17abfdb849c52ed19516840fe000c8c49
Gabriel Morrison Lima Dantas <gabriel@infox.com.br>
2015-10-01 13:20:12 -0300

    Removendo documento ao alterar classificação de documento na variável do tipo upload refs #65724

--- src/main/java/br/com/infox/ibpm/variable/file/FileUpload.java
+++ src/main/java/br/com/infox/ibpm/variable/file/FileUpload.java
@@ -1,109 +1,45 @@
 package br.com.infox.ibpm.variable.file;
 
-import java.util.Date;
-
 import javax.faces.component.UIComponent;
-import javax.faces.event.AbortProcessingException;
-import javax.transaction.SystemException;
+import javax.inject.Inject;
 
-import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
-import org.jboss.seam.annotations.Transactional;
-import org.jboss.seam.faces.FacesMessages;
-import org.jboss.seam.transaction.Transaction;
 import org.richfaces.event.FileUploadEvent;
 import org.richfaces.event.FileUploadListener;
 import org.richfaces.model.UploadedFile;
 
-import br.com.infox.core.manager.GenericManager;
+import br.com.infox.core.action.ActionMessagesService;
 import br.com.infox.core.persistence.DAOException;
-import br.com.infox.core.util.FileUtil;
-import br.com.infox.epp.access.api.Authenticator;
-import br.com.infox.epp.documento.manager.ClassificacaoDocumentoManager;
-import br.com.infox.epp.processo.documento.entity.Documento;
-import br.com.infox.epp.processo.documento.entity.DocumentoBin;
-import br.com.infox.epp.processo.documento.manager.DocumentoBinManager;
-import br.com.infox.epp.processo.documento.manager.DocumentoBinarioManager;
-import br.com.infox.epp.processo.documento.manager.DocumentoManager;
-import br.com.infox.epp.processo.home.ProcessoEpaHome;
-import br.com.infox.ibpm.task.home.TaskInstanceHome;
+import br.com.infox.epp.cdi.seam.ContextDependency;
 import br.com.infox.log.LogProvider;
 import br.com.infox.log.Logging;
-import br.com.infox.seam.exception.BusinessException;
+import br.com.infox.seam.exception.BusinessRollbackException;
 
 @Name(FileUpload.NAME)
+@ContextDependency
 public class FileUpload implements FileUploadListener {
 
     public static final String NAME = "fileUpload";
     private static final LogProvider LOG = Logging.getLogProvider(FileUpload.class);
     
-    @In
-    private DocumentoManager documentoManager;
-    @In
-    private GenericManager genericManager;
-    @In
-    private DocumentoBinManager documentoBinManager;
-    @In
-    private DocumentoBinarioManager documentoBinarioManager;
-    @In
-    private ClassificacaoDocumentoManager classificacaoDocumentoManager;
-    @In
-    private ProcessoEpaHome processoEpaHome;
+    @Inject
+    private FileVariableHandler fileVariableHandler;
+    @Inject
+    private ActionMessagesService actionMessagesService;
     
     @Override
-    @Transactional
     public void processFileUpload(FileUploadEvent event) {
         UploadedFile file = event.getUploadedFile();
         UIComponent uploadFile = event.getComponent();
-        Integer idDocumentoExistente = (Integer) TaskInstanceHome.instance().getValueOfVariableFromTaskInstance(TaskInstanceHome.instance().getVariableName(uploadFile.getId()));
-        if (idDocumentoExistente != null) {
-            try {
-                Documento doc = documentoManager.find(idDocumentoExistente);
-                documentoManager.remove(doc);
-                documentoBinManager.remove(doc.getDocumentoBin());
-                documentoBinarioManager.remove(doc.getDocumentoBin().getId());
-            } catch (DAOException e) {
-            	try {
-					Transaction.instance().setRollbackOnly();
-				} catch (IllegalStateException | SystemException e1) {
-					throw new AbortProcessingException(e1);
-				}
-                LOG.error("Erro ao remover o documento existente, com id: " + idDocumentoExistente, e);
-                String message = e.getDatabaseErrorCode() != null ? e.getLocalizedMessage() : e.getMessage();
-                FacesMessages.instance().add("Erro ao substituir o documento: " + message);
-                throw new AbortProcessingException(e);
-            }
-        }
-        Documento documento = createDocumento(file, uploadFile.getId());
         try {
-            documentoManager.gravarDocumentoNoProcesso(processoEpaHome.getInstance().getProcessoRoot(), documento);
-            TaskInstanceHome.instance().getInstance().put(uploadFile.getId(), documento.getId());
-        } catch (DAOException e) {
-            LOG.error("Não foi possível gravar o documento " + file.getName() + "no processo " + processoEpaHome.getInstance().getIdProcesso(), e);
-        } catch (BusinessException e) {
-        	FacesMessages.instance().add(e.getMessage());
+        	fileVariableHandler.gravarDocumento(file, uploadFile.getId());
+        } catch (BusinessRollbackException e) {
+        	 LOG.error("Erro ao remover o documento existente", e);
+        	 if (e.getCause() instanceof DAOException) {
+        		 actionMessagesService.handleDAOException((DAOException) e.getCause());
+        	 } else {
+        		 actionMessagesService.handleException("Erro ao substituir o documento", e);
+        	 }
         }
-        TaskInstanceHome.instance().update();
-    }
-    
-    private Documento createDocumento(final UploadedFile file, final String id) {
-        Documento pd = new Documento();
-        pd.setDescricao(file.getName());
-        pd.setAnexo(true);
-        pd.setDocumentoBin(createDocumentoBin(file));
-        pd.setClassificacaoDocumento(TaskInstanceHome.instance().getVariaveisDocumento().get(id).getClassificacaoDocumento());
-        pd.setUsuarioInclusao(Authenticator.getUsuarioLogado());
-        pd.setLocalizacao(Authenticator.getLocalizacaoAtual());
-        return pd;
-    }
-
-    private DocumentoBin createDocumentoBin(final UploadedFile file) {
-        DocumentoBin pdb = new DocumentoBin();
-        pdb.setNomeArquivo(file.getName());
-        pdb.setExtensao(FileUtil.getFileType(file.getName()));
-        pdb.setSize(Long.valueOf(file.getSize()).intValue());
-        pdb.setProcessoDocumento(file.getData());
-        pdb.setDataInclusao(new Date());
-        return pdb;
     }
 }
---
+++ src/main/java/br/com/infox/ibpm/variable/file/FileVariableHandler.java
@@ -0,0 +1,87 @@
+package br.com.infox.ibpm.variable.file;
+
+import java.util.Date;
+
+import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.inject.Inject;
+
+import org.richfaces.model.UploadedFile;
+
+import br.com.infox.core.persistence.DAOException;
+import br.com.infox.core.util.FileUtil;
+import br.com.infox.epp.access.api.Authenticator;
+import br.com.infox.epp.processo.documento.entity.Documento;
+import br.com.infox.epp.processo.documento.entity.DocumentoBin;
+import br.com.infox.epp.processo.documento.manager.DocumentoBinManager;
+import br.com.infox.epp.processo.documento.manager.DocumentoBinarioManager;
+import br.com.infox.epp.processo.documento.manager.DocumentoManager;
+import br.com.infox.epp.processo.home.ProcessoEpaHome;
+import br.com.infox.ibpm.task.home.TaskInstanceHome;
+import br.com.infox.seam.exception.BusinessException;
+import br.com.infox.seam.exception.BusinessRollbackException;
+import br.com.infox.seam.util.ComponentUtil;
+
+@Stateless
+@TransactionAttribute(TransactionAttributeType.SUPPORTS)
+public class FileVariableHandler {
+	@Inject
+	private DocumentoBinManager documentoBinManager;
+	@Inject
+	private DocumentoManager documentoManager;
+	
+	@TransactionAttribute(TransactionAttributeType.REQUIRED)
+	public void gravarDocumento(UploadedFile file, String variableFieldName) {
+		TaskInstanceHome taskInstanceHome = TaskInstanceHome.instance();
+		ProcessoEpaHome processoEpaHome = ComponentUtil.getComponent(ProcessoEpaHome.NAME);
+		Integer idDocumentoExistente = (Integer) taskInstanceHome.getValueOfVariableFromTaskInstance(taskInstanceHome.getVariableName(variableFieldName));
+        if (idDocumentoExistente != null) {
+            try {
+                removeDocumento(documentoManager.find(idDocumentoExistente), variableFieldName);
+            } catch (DAOException e) {
+                throw new BusinessRollbackException(e);
+            }
+        }
+        Documento documento = createDocumento(file, variableFieldName);
+        try {
+            documentoManager.gravarDocumentoNoProcesso(processoEpaHome.getInstance().getProcessoRoot(), documento);
+            taskInstanceHome.getInstance().put(variableFieldName, documento.getId());
+        } catch (DAOException | BusinessException e) {
+            throw new BusinessRollbackException(e);
+        }
+        taskInstanceHome.update();
+	}
+	
+	@TransactionAttribute(TransactionAttributeType.REQUIRED)
+	public void removeDocumento(Documento documento, String variableFieldName) throws DAOException {
+		DocumentoBinarioManager documentoBinarioManager = ComponentUtil.getComponent(DocumentoBinarioManager.NAME);
+		TaskInstanceHome taskInstanceHome = TaskInstanceHome.instance();
+		documentoManager.remove(documento);
+        documentoBinManager.remove(documento.getDocumentoBin());
+        documentoBinarioManager.remove(documento.getDocumentoBin().getId());
+        taskInstanceHome.getInstance().put(variableFieldName, null);
+        taskInstanceHome.update();
+	}
+	
+	private Documento createDocumento(final UploadedFile file, final String id) {
+        Documento pd = new Documento();
+        pd.setDescricao(file.getName());
+        pd.setAnexo(true);
+        pd.setDocumentoBin(createDocumentoBin(file));
+        pd.setClassificacaoDocumento(TaskInstanceHome.instance().getVariaveisDocumento().get(id).getClassificacaoDocumento());
+        pd.setUsuarioInclusao(Authenticator.getUsuarioLogado());
+        pd.setLocalizacao(Authenticator.getLocalizacaoAtual());
+        return pd;
+    }
+
+    private DocumentoBin createDocumentoBin(final UploadedFile file) {
+        DocumentoBin pdb = new DocumentoBin();
+        pdb.setNomeArquivo(file.getName());
+        pdb.setExtensao(FileUtil.getFileType(file.getName()));
+        pdb.setSize(Long.valueOf(file.getSize()).intValue());
+        pdb.setProcessoDocumento(file.getData());
+        pdb.setDataInclusao(new Date());
+        return pdb;
+    }
+}
