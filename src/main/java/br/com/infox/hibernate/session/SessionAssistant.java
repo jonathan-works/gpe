43dc68e9fcc62f45fb08c2e29f8d6119ed05b5fb
Avner Pereira <avner@infox.com.br>
2015-10-30 09:54:35 -0300

    Correção no merge

--- src/main/java/br/com/infox/hibernate/session/SessionAssistant.java
+++ src/main/java/br/com/infox/hibernate/session/SessionAssistant.java
@@ -4,15 +4,15 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 import javax.naming.NamingException;
-import javax.persistence.EntityManager;
 
 import org.hibernate.Session;
 import org.hibernate.internal.SessionFactoryImpl;
 import org.hibernate.internal.SessionImpl;
 import org.hibernate.service.jdbc.connections.internal.DatasourceConnectionProviderImpl;
 import org.jboss.seam.annotations.AutoCreate;
-import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
+
+import br.com.infox.cdi.producer.EntityManagerProducer;
 
 @Name(SessionAssistant.NAME)
 @AutoCreate
@@ -20,15 +20,12 @@ public class SessionAssistant {
 
     public static final String NAME = "sessionAssistant";
 
-    @In
-    private transient EntityManager entityManager;
-
     public Session getSession() {
-        return (Session) entityManager.getDelegate();
+        return (Session) EntityManagerProducer.getEntityManager().getDelegate();
     }
 
     public Connection getConnection() throws NamingException, SQLException {
-        SessionImpl sessionImpl = (SessionImpl) entityManager.unwrap(Session.class);
+        SessionImpl sessionImpl = (SessionImpl) EntityManagerProducer.getEntityManager().unwrap(Session.class);
         SessionFactoryImpl sessionFactoryImpl = (SessionFactoryImpl) sessionImpl.getSessionFactory();
         DatasourceConnectionProviderImpl provider = (DatasourceConnectionProviderImpl) sessionFactoryImpl.getConnectionProvider();
         return provider.getDataSource().getConnection();
