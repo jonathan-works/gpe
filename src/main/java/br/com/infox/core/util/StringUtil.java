ff54bf80659d266e48ccb120819e73543bb6bc20
Victor Pasqualino <victorpasqualino@infox.com.br>
2015-11-04 08:24:25 -0300

    Ajustes de melhoria

--- src/main/java/br/com/infox/core/util/StringUtil.java
+++ src/main/java/br/com/infox/core/util/StringUtil.java
@@ -44,4 +44,13 @@ public final class StringUtil {
     	return value == null || value.trim().length() == 0;
     }
     
+    public static void replaceAll(StringBuilder builder, String from, String to) {
+        int index = builder.indexOf(from);
+        while (index != -1) {
+            builder.replace(index, index + from.length(), to);
+            index += to.length();
+            index = builder.indexOf(from, index);
+        }
+    }
+    
 }
