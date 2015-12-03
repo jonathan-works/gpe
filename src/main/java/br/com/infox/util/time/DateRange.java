a93b97eb75cd027279a595734e3ff2275adb4157
Erik Liberal <erikliberal@infox.com.br>
2015-12-03 13:47:00 -0300

    Atualização de entidade DateRange para atender requisitos de cálculos de prazos refs #66972

--- src/main/java/br/com/infox/util/time/DateRange.java
+++ src/main/java/br/com/infox/util/time/DateRange.java
@@ -1,10 +1,15 @@
 package br.com.infox.util.time;
 
-import java.util.Date;
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.joda.time.DateTime;
+import org.joda.time.Interval;
+import org.joda.time.LocalDate;
 
 /**
  * @author erik
- *
  */
 public class DateRange {
     private long start;
@@ -23,7 +28,27 @@ public class DateRange {
         this.end = 0;
     }
 
-    public DateRange(final Date date1, final Date date2) {
+    public DateRange(DateTime date1, DateTime date2) {
+        if (date1.isBefore(date2)) {
+            this.start = date1.getMillis();
+            this.end = date2.getMillis();
+        } else {
+            this.start = date2.getMillis();
+            this.end = date1.getMillis();
+        }
+    }
+
+    public DateRange(LocalDate date1, LocalDate date2) {
+        if (date1.isBefore(date2)) {
+            setStart(date1);
+            setEnd(date2);
+        } else {
+            setStart(date2);
+            setEnd(date1);
+        }
+    }
+
+    public DateRange(final java.util.Date date1, final java.util.Date date2) {
         final long _d1 = date1.getTime();
         final long _d2 = date2.getTime();
 
@@ -36,7 +61,12 @@ public class DateRange {
         }
     }
 
-    public boolean contains(final Date date) {
+    private DateRange(Interval interval) {
+        this.start = interval.getStartMillis();
+        this.end = interval.getEndMillis();
+    }
+
+    public boolean contains(final java.util.Date date) {
         final long _date = date.getTime();
         return (_date >= this.start) && (_date <= this.end);
     }
@@ -45,21 +75,25 @@ public class DateRange {
         return (this.start <= range.start) && (this.end >= range.end);
     }
 
+    public Long getDays() {
+        return get(DAYS);
+    }
+
     public Long get(final int intervalFormat) {
         long divisor = 1;
         switch (intervalFormat) {
-            case DAYS:
-                divisor *= 24;
-            case HOURS:
-                divisor *= 60;
-            case MINUTES:
-                divisor *= 60;
-            case SECONDS:
-                divisor *= 1000;
-                break;
-            default:
-                divisor = 1;
-                break;
+        case DAYS:
+            divisor *= 24;
+        case HOURS:
+            divisor *= 60;
+        case MINUTES:
+            divisor *= 60;
+        case SECONDS:
+            divisor *= 1000;
+            break;
+        default:
+            divisor = 1;
+            break;
         }
         return new Long((this.end - this.start) / divisor);
     }
@@ -72,50 +106,145 @@ public class DateRange {
         return new Date(this.start);
     }
 
+    public boolean intersectsAny(DateRange... ranges){
+        for (DateRange range : ranges) {
+            if (intersects(range)){
+                return true;
+            }
+        }
+        return false;
+    }
+    
     public boolean intersects(final DateRange range) {
+        if (range == null) {
+            return false;
+        }
         return ((range.start >= this.start) && (range.start <= this.end))
                 || ((range.end >= this.start) && (range.end <= this.end));
     }
 
-    public void setEnd(final Date date) {
-        final long _end = date.getTime();
+    private void setEnd(LocalDate end){
+        setEnd(end.toDateTimeAtStartOfDay().withTime(23, 59, 59, 999).getMillis());
+    }
+    
+    private void setEnd(long _end){
         if (_end >= this.start) {
             this.end = _end;
         }
     }
-
-    public void setStart(final Date date) {
-        final long _start = date.getTime();
-        if (_start <= this.end) {
-            this.start = _start;
-        }
+    
+    public void setEnd(final java.util.Date date) {
+        setEnd(LocalDate.fromDateFields(date));
     }
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + (int) (end ^ (end >>> 32));
-		result = prime * result + (int) (start ^ (start >>> 32));
-		return result;
-	}
+    private void setStart(final LocalDate date){
+        setStart(date.toDateTimeAtStartOfDay().getMillis());
+    }
+    
+    public void setStart(long start){
+        if (start <= this.end) {
+            this.start = start;
+        }
+    }
+    
+    public void setStart(final java.util.Date date) {
+        setStart(LocalDate.fromDateFields(date));
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		DateRange other = (DateRange) obj;
-		if (end != other.end)
-			return false;
-		if (start != other.start)
-			return false;
-		return true;
-	}
-    
-    
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (int) (end ^ (end >>> 32));
+        result = prime * result + (int) (start ^ (start >>> 32));
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        DateRange other = (DateRange) obj;
+        if (end != other.end)
+            return false;
+        if (start != other.start)
+            return false;
+        return true;
+    }
+
+    private Interval toInterval() {
+        return new Interval(LocalDate.fromDateFields(getStart()).toDateTimeAtStartOfDay(), LocalDate
+                .fromDateFields(getEnd()).toDateTimeAtStartOfDay().toDateTime().withTime(23, 59, 59, 999));
+    }
+
+    public DateRange union(DateRange other) {
+        Interval interval1 = toInterval();
+        Interval interval2 = other.toInterval();
+        if (!interval1.abuts(interval2) && !interval1.overlaps(interval2)) {
+            throw new IllegalStateException("Can't unite intervals that can't connect");
+        }
+        DateTime start = interval1.getStart().isBefore(interval2.getStart()) ? interval1.getStart() : interval2
+                .getStart();
+        DateTime end = interval1.getEnd().isAfter(interval2.getEnd()) ? interval1.getEnd() : interval2.getEnd();
+        return new DateRange(start, end);
+    }
+
+    public DateRange intersection(DateRange other) {
+        Interval overlap = toInterval().overlap(other.toInterval());
+        return overlap == null ? null : new DateRange(overlap);
+    }
+
+    public DateRange connection(DateRange range) {
+        if (range == null){
+            return null;
+        }
+        DateRange result = null;
+        if (abuts(range)) {
+            result = range;
+        } else if (intersects(range)) {
+            if (end <= range.end) {
+                result = range;
+            } else {
+                result = intersection(range);
+            }
+        }
+        return result;
+    }
+
+    public Collection<? extends DateRange> connections(Collection<? extends DateRange> ranges) {
+        Collection<DateRange> result = new ArrayList<>();
+        for (DateRange dateRange : ranges) {
+            DateRange connection = connection(dateRange);
+            if (connection != null) {
+                result.add(connection);
+            }
+        }
+        return result;
+    }
+
+    public boolean abuts(DateRange other) {
+        return toInterval().abuts(other.toInterval());
+    }
+
+    /**
+     * Incrementa o período a partir do início utilizando objeto daterange como duração
+     * 
+     * @param dateRange
+     * @return
+     */
+    public DateRange incrementStartByDuration(DateRange dateRange) {
+        this.end = this.end + dateRange.end - dateRange.start;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return MessageFormat.format("{0,date,dd/MM/yyyy kk:mm:ss} to {1,date,dd/MM/yyyy kk:mm:ss} ({2} days)",
+                getStart(), getEnd(), String.valueOf(getDays()));
+    }
     
 }
