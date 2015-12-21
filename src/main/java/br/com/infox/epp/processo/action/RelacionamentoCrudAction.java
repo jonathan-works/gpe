2c477b6bbe47710bebab0113d4aff81ebb60c2db
Paulo José Rolim Braga Filho <paulofilho@infox.com.br>
2015-12-21 10:02:47 -0300

    Criados Relacionamrntos para processos internos e externos

--- src/main/java/br/com/infox/epp/processo/action/RelacionamentoCrudAction.java
+++ src/main/java/br/com/infox/epp/processo/action/RelacionamentoCrudAction.java
@@ -1,7 +1,5 @@
 package br.com.infox.epp.processo.action;
 
-import static java.text.MessageFormat.format;
-
 import java.lang.reflect.InvocationTargetException;
 import java.util.Date;
 import java.util.List;
@@ -11,19 +9,18 @@ import org.jboss.seam.annotations.AutoCreate;
 import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Scope;
+import org.jboss.seam.faces.FacesMessages;
+import org.jboss.seam.international.StatusMessage.Severity;
 
 import br.com.infox.core.action.AbstractAction;
 import br.com.infox.core.crud.AbstractCrudAction;
-import br.com.infox.core.exception.EppSystemException;
-import br.com.infox.core.exception.StandardErrorCode;
 import br.com.infox.core.messages.InfoxMessages;
-import br.com.infox.core.persistence.DAOException;
 import br.com.infox.core.util.EntityUtil;
 import br.com.infox.epp.access.api.Authenticator;
+import br.com.infox.epp.processo.entity.Processo;
 import br.com.infox.epp.processo.entity.Relacionamento;
-import br.com.infox.epp.processo.entity.RelacionamentoProcesso;
+import br.com.infox.epp.processo.entity.RelacionamentoProcessoInterno;
 import br.com.infox.epp.processo.entity.TipoRelacionamentoProcesso;
-import br.com.infox.epp.processo.entity.RelacionamentoProcesso.TipoProcesso;
 import br.com.infox.epp.processo.manager.ProcessoManager;
 import br.com.infox.epp.processo.manager.RelacionamentoManager;
 import br.com.infox.epp.processo.manager.RelacionamentoProcessoManager;
@@ -33,112 +30,114 @@ import br.com.infox.hibernate.util.HibernateUtil;
 @Name(RelacionamentoCrudAction.NAME)
 @Scope(ScopeType.CONVERSATION)
 @AutoCreate
-public class RelacionamentoCrudAction extends
-        AbstractCrudAction<Relacionamento, RelacionamentoManager> {
+public class RelacionamentoCrudAction extends AbstractCrudAction<Relacionamento, RelacionamentoManager> {
 
-    public static final String NAME = "relacionamentoCrudAction";
-    public static final String PROCESSO_RELAC_MSG = "Processo já está relacionado.";
-    private static final long serialVersionUID = 1L;
+	public static final String NAME = "relacionamentoCrudAction";
+	public static final String PROCESSO_RELAC_MSG = "Processo já está relacionado.";
+	private static final long serialVersionUID = 1L;
 
-    @In
-    private RelacionamentoProcessoManager relacionamentoProcessoManager;
-    @In
-    private TipoRelacionamentoProcessoManager tipoRelacionamentoProcessoManager;
-    @In
-    private ProcessoManager processoManager;
-    @In
-    private InfoxMessages infoxMessages;
+	@In
+	private RelacionamentoProcessoManager relacionamentoProcessoManager;
+	@In
+	private TipoRelacionamentoProcessoManager tipoRelacionamentoProcessoManager;
+	@In
+	private ProcessoManager processoManager;
+	@In
+	private InfoxMessages infoxMessages;
 
-    private String processo;
-    private String processoRelacionado;
-    private List<TipoRelacionamentoProcesso> tipoRelacionamentoProcessoList;
+	private String processo;
+	private String processoRelacionado;
+	private List<TipoRelacionamentoProcesso> tipoRelacionamentoProcessoList;
 
-    public String getProcesso() {
-        return processo;
-    }
+	RelacionamentoProcessoInterno rp1;
+	RelacionamentoProcessoInterno rp2;
 
-    public void setEditMode(String numeroProcesso, Object id) {
-        setId(id);
-        this.processoRelacionado = numeroProcesso;
-    }
+	public String getProcesso() {
+		return processo;
+	}
 
-    @Override
-    public String inactive(Relacionamento t) {
-        return super.inactive((Relacionamento) HibernateUtil.removeProxy(t));
-    }
+	public void setEditMode(String numeroProcesso, Object id) {
+		setId(id);
+		this.processoRelacionado = numeroProcesso;
+	}
 
-    @Override
-    protected boolean isInstanceValid() {
-        boolean result = true;
-    	if (getInstance().getMotivo().trim().length() > 0 && !isManaged()){
-    		if (relacionamentoProcessoManager.existeRelacionamento(processo, TipoProcesso.ELE, processoRelacionado, TipoProcesso.FIS)){
-    			getMessagesHandler().add(PROCESSO_RELAC_MSG);
-    			return result = false;
-    		}
-    	}
-    	return result;
-    }
+	@Override
+	public String inactive(Relacionamento t) {
+		return super.inactive((Relacionamento) HibernateUtil.removeProxy(t));
+	}
 
-    @Override
-    protected void beforeSave() {
-        final Relacionamento relacionamento = getInstance();
-        if (relacionamento.getAtivo() == null) {
-            relacionamento.setAtivo(Boolean.TRUE);
-        }
-        relacionamento.setDataRelacionamento(new Date());
-        relacionamento.setNomeUsuario(Authenticator.getUsuarioLogado()
-                .getNomeUsuario());
-    }
+	@Override
+	protected boolean isInstanceValid() {
+		boolean result = true;
+		if (getInstance().getMotivo().trim().length() > 0 && !isManaged()) {
+			Relacionamento relacionamento;
+			try {
+				relacionamento = getManager().find(EntityUtil.getIdValue(getInstance()));
+				Processo p1 = processoManager.getProcessoEpaByNumeroProcesso(processo);
+				Processo p2 = processoManager.getProcessoEpaByNumeroProcesso(processoRelacionado);
+				if (p2 == null) {
+					FacesMessages.instance().add(Severity.ERROR,
+					        "Processo eletrônico com número " + processoRelacionado + " não encontrado");
+				}
+				rp1 = new RelacionamentoProcessoInterno(relacionamento, p1);
+				rp2 = new RelacionamentoProcessoInterno(relacionamento, p2);
+				if (relacionamentoProcessoManager.existeRelacionamento(rp1, rp2)) {
+					getMessagesHandler().add(PROCESSO_RELAC_MSG);
+					return result = false;
+				}
+			} catch (IllegalAccessException | InvocationTargetException e) {
+				throw new RuntimeException(e);
+			}
+		}
+		return result;
+	}
 
-    @Override
-    protected void afterSave(String ret) {
-        if (AbstractAction.PERSISTED.equals(ret)) {
-            try {
-                Relacionamento relacionamento = getManager().find(
-                        EntityUtil.getIdValue(getInstance()));
-                relacionamentoProcessoManager
-                        .persist(new RelacionamentoProcesso(relacionamento,
-                                processo, TipoProcesso.ELE));
-                relacionamentoProcessoManager
-                        .persist(new RelacionamentoProcesso(relacionamento,
-                                processoRelacionado, TipoProcesso.FIS));
-            } catch (IllegalAccessException | IllegalArgumentException
-                    | InvocationTargetException | DAOException e) {
-                throw EppSystemException.create(StandardErrorCode.UNKNOWN, e);
-            }
-            newInstance();
-        }
-    }
+	@Override
+	protected void beforeSave() {
+		final Relacionamento relacionamento = getInstance();
+		if (relacionamento.getAtivo() == null) {
+			relacionamento.setAtivo(Boolean.TRUE);
+		}
+		relacionamento.setDataRelacionamento(new Date());
+		relacionamento.setNomeUsuario(Authenticator.getUsuarioLogado().getNomeUsuario());
+	}
 
-    @Override
-    public void newInstance() {
-        super.newInstance();
-        processoRelacionado = "";
-    }
+	@Override
+	protected void afterSave(String ret) {
+		if (AbstractAction.PERSISTED.equals(ret)) {
+			relacionamentoProcessoManager.persist(rp1);
+			relacionamentoProcessoManager.persist(rp2);
+			newInstance();
+		}
+	}
 
-    public void setProcesso(final String processo) {
-        this.processo = processo;
-    }
+	@Override
+	public void newInstance() {
+		super.newInstance();
+		processoRelacionado = "";
+	}
 
-    public List<TipoRelacionamentoProcesso> getTipoRelacionamentoProcessoList() {
-        if (tipoRelacionamentoProcessoList == null) {
-            tipoRelacionamentoProcessoList = tipoRelacionamentoProcessoManager
-                    .findAll();
-        }
-        return tipoRelacionamentoProcessoList;
-    }
+	public void setProcesso(final String processo) {
+		this.processo = processo;
+	}
 
-    public void setTipoRelacionamentoProcessoList(
-            List<TipoRelacionamentoProcesso> tipoRelacionamentoProcessoList) {
-        this.tipoRelacionamentoProcessoList = tipoRelacionamentoProcessoList;
-    }
+	public List<TipoRelacionamentoProcesso> getTipoRelacionamentoProcessoList() {
+		if (tipoRelacionamentoProcessoList == null) {
+			tipoRelacionamentoProcessoList = tipoRelacionamentoProcessoManager.findAll();
+		}
+		return tipoRelacionamentoProcessoList;
+	}
 
-    public String getProcessoRelacionado() {
-        return processoRelacionado;
-    }
+	public void setTipoRelacionamentoProcessoList(List<TipoRelacionamentoProcesso> tipoRelacionamentoProcessoList) {
+		this.tipoRelacionamentoProcessoList = tipoRelacionamentoProcessoList;
+	}
 
-    public void setProcessoRelacionado(String processoRelacionado) {
-        this.processoRelacionado = processoRelacionado;
-    }
+	public String getProcessoRelacionado() {
+		return processoRelacionado;
+	}
+
+	public void setProcessoRelacionado(String processoRelacionado) {
+		this.processoRelacionado = processoRelacionado;
+	}
 
 }
--- src/main/java/br/com/infox/epp/processo/entity/RelacionamentoProcesso.java
+++ src/main/java/br/com/infox/epp/processo/entity/RelacionamentoProcesso.java
@@ -1,11 +1,7 @@
 package br.com.infox.epp.processo.entity;
 
-import static br.com.infox.constants.LengthConstants.FLAG;
-import static br.com.infox.constants.LengthConstants.NUMERACAO_PROCESSO;
 import static br.com.infox.core.persistence.ORConstants.GENERATOR;
-import static br.com.infox.epp.processo.query.ProcessoQuery.ID_PROCESSO;
 import static br.com.infox.epp.processo.query.RelacionamentoProcessoQuery.ID_RELACIONAMENTO_PROCESSO;
-import static br.com.infox.epp.processo.query.RelacionamentoProcessoQuery.NUMERO_PROCESSO;
 import static br.com.infox.epp.processo.query.RelacionamentoProcessoQuery.RELACIONAMENTO_BY_PROCESSO;
 import static br.com.infox.epp.processo.query.RelacionamentoProcessoQuery.RELACIONAMENTO_BY_PROCESSO_QUERY;
 import static br.com.infox.epp.processo.query.RelacionamentoProcessoQuery.SEQUENCE_NAME;
@@ -16,9 +12,9 @@ import static javax.persistence.FetchType.LAZY;
 import java.io.Serializable;
 
 import javax.persistence.Column;
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.DiscriminatorType;
 import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -28,57 +24,28 @@ import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
-
-import br.com.infox.core.type.Displayable;
 
 @Entity
-@Table(name = TABLE_NAME, uniqueConstraints = { @UniqueConstraint(columnNames = {
-        NUMERO_PROCESSO, ID_RELACIONAMENTO }) })
+@Table(name = TABLE_NAME)
 @NamedQueries(value = { @NamedQuery(name = RELACIONAMENTO_BY_PROCESSO, query = RELACIONAMENTO_BY_PROCESSO_QUERY) })
-public class RelacionamentoProcesso implements Serializable {
+@DiscriminatorColumn(name="tp_processo", discriminatorType = DiscriminatorType.STRING)
+public abstract class RelacionamentoProcesso implements Serializable {
     private static final long serialVersionUID = 1L;
-
-    private Integer idRelacionamentoProcesso;
-    private Processo processo;
-    private Relacionamento relacionamento;
-    private String numeroProcesso;
-    private TipoProcesso tipoProcesso;
     
-    
-
-    public enum TipoProcesso implements Displayable {
-        FIS("Físico"), ELE("Eletrônico");
-
-        private String label;
-
-        TipoProcesso(String label) {
-            this.label = label;
-        }
-
-        @Override
-        public String getLabel() {
-            return label;
-        }
-    	
-    }
-    
-    public RelacionamentoProcesso() {
-    }
-
-    public RelacionamentoProcesso(Relacionamento relacionamento,
-            String numeroProcesso, TipoProcesso tipoProcesso) {
-        this.relacionamento = relacionamento;
-        this.numeroProcesso = numeroProcesso;
-        this.tipoProcesso = tipoProcesso;
-    }
+    public static final String DISCRIMINATOR_RELACIONAMENTOS_EXTERNOS = "EXT";
+    public static final String DISCRIMINATOR_RELACIONAMENTOS_INTERNOS = "INT";
 
     @Id
     @GeneratedValue(generator = GENERATOR, strategy = GenerationType.SEQUENCE)
     @Column(name = ID_RELACIONAMENTO_PROCESSO, unique = true, nullable = false)
     @SequenceGenerator(allocationSize=1, initialValue=1, name = GENERATOR, sequenceName = SEQUENCE_NAME)
+    protected Integer idRelacionamentoProcesso;
+    @NotNull
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = ID_RELACIONAMENTO, nullable = false)
+    protected Relacionamento relacionamento;
+    
     public Integer getIdRelacionamentoProcesso() {
         return idRelacionamentoProcesso;
     }
@@ -88,30 +55,6 @@ public class RelacionamentoProcesso implements Serializable {
         this.idRelacionamentoProcesso = idRelacionamentoProcesso;
     }
 
-    @ManyToOne(fetch = LAZY)
-    @JoinColumn(name = ID_PROCESSO, nullable = true, unique = true)
-    public Processo getProcesso() {
-        return processo;
-    }
-
-    public void setProcesso(Processo processo) {
-        this.processo = processo;
-    }
-
-    @NotNull
-    @Size(min = FLAG, max = NUMERACAO_PROCESSO)
-    @Column(name = NUMERO_PROCESSO, length = NUMERACAO_PROCESSO, nullable = false, unique = true)
-    public String getNumeroProcesso() {
-        return numeroProcesso;
-    }
-
-    public void setNumeroProcesso(String numeroProcesso) {
-        this.numeroProcesso = numeroProcesso;
-    }
-
-    @NotNull
-    @ManyToOne(fetch = LAZY)
-    @JoinColumn(name = ID_RELACIONAMENTO, nullable = false)
     public Relacionamento getRelacionamento() {
         return relacionamento;
     }
@@ -120,50 +63,6 @@ public class RelacionamentoProcesso implements Serializable {
         this.relacionamento = relacionamento;
     }
 
-    @NotNull
-    @Column(name = "tp_processo", nullable = false)
-    @Enumerated(EnumType.STRING)
-	public TipoProcesso getTipoProcesso() {
-		return tipoProcesso;
-	}
-
-	public void setTipoProcesso(TipoProcesso tipoProcesso) {
-		this.tipoProcesso = tipoProcesso;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((numeroProcesso == null) ? 0 : numeroProcesso.hashCode());
-		result = prime * result + ((relacionamento == null) ? 0 : relacionamento.hashCode());
-		result = prime * result + ((tipoProcesso == null) ? 0 : tipoProcesso.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		RelacionamentoProcesso other = (RelacionamentoProcesso) obj;
-		if (numeroProcesso == null) {
-			if (other.numeroProcesso != null)
-				return false;
-		} else if (!numeroProcesso.equals(other.numeroProcesso))
-			return false;
-		if (relacionamento == null) {
-			if (other.relacionamento != null)
-				return false;
-		} else if (!relacionamento.equals(other.relacionamento))
-			return false;
-		if (tipoProcesso != other.tipoProcesso)
-			return false;
-		return true;
-	}
 	
 	
 
