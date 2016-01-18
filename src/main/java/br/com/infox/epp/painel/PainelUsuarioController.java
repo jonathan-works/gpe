1982160d70b46983d0c51aef90c037727811057f
Victor Pasqualino <victorpasqualino@infox.com.br>
2016-01-18 16:23:38 -0300

    Remocao de import nao utilizado

--- src/main/java/br/com/infox/epp/painel/PainelUsuarioController.java
+++ src/main/java/br/com/infox/epp/painel/PainelUsuarioController.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Logger;
 
 import javax.annotation.PostConstruct;
 import javax.faces.context.FacesContext;
