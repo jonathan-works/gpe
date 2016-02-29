a0e34acc5b30cb5801cd1bbe4bd2382cc9f9d6a3
Victor Pasqualino <victorpasqualino@infox.com.br>
2016-02-29 18:16:59 -0300

    Utilizando ejbSyncronizations

--- src/main/java/br/com/infox/jta/JtaSyncronizations.java
+++ src/main/java/br/com/infox/jta/JtaSyncronizations.java
@@ -2,19 +2,10 @@ package br.com.infox.jta;
 
 import javax.transaction.Synchronization;
 
-import org.jboss.seam.ScopeType;
-import org.jboss.seam.annotations.Install;
-import org.jboss.seam.annotations.Name;
-import org.jboss.seam.annotations.Scope;
-import org.jboss.seam.annotations.intercept.BypassInterceptors;
 import org.jboss.seam.transaction.Synchronizations;
 
 import br.com.infox.core.transaction.TransactionSyncronizationsUtil;
 
-@Name("org.jboss.seam.transaction.synchronizations")
-@Scope(ScopeType.EVENT)
-@Install(precedence=Install.DEPLOYMENT)
-@BypassInterceptors
 public class JtaSyncronizations implements Synchronizations {
     
     @Override
