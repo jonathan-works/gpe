a04867ee16aa48780020bcb9d03efecca79dfc51
Avner Pereira <avner@infox.com.br>
2016-03-01 15:51:22 -0300

    Ajustes no tratamento de erros lançados via webservices refs #69059

--- src/main/java/br/com/infox/epp/ws/MapeadorExcecoes.java
+++ src/main/java/br/com/infox/epp/ws/MapeadorExcecoes.java
@@ -4,13 +4,21 @@ import static br.com.infox.epp.ws.services.MensagensErroService.CODIGO_ERRO_INDE
 
 import javax.ejb.EJBException;
 import javax.inject.Inject;
+import javax.validation.ValidationException;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import br.com.infox.epp.rest.RestException;
 import br.com.infox.epp.ws.exception.ExcecaoServico;
 import br.com.infox.epp.ws.exception.ExcecaoServico.ErroServico;
+import br.com.infox.epp.ws.messages.WSMessages;
 import br.com.infox.epp.ws.services.MensagensErroService;
 
 /**
@@ -38,13 +46,28 @@ public class MapeadorExcecoes implements ExceptionMapper<Throwable> {
 
 	@Override
 	public Response toResponse(Throwable e) {
-		ErroServico erro = mensagensService.getErro(e);
-		String mensagemResposta = erro.getCodigo();
-		if (CODIGO_ERRO_INDEFINIDO.equals(mensagemResposta)) {
-			mensagemResposta = erro.getMensagem();
-		}
+		if (e instanceof WebApplicationException) {
+			WebApplicationException exception = (WebApplicationException) e;
+			return exception.getResponse();
+		} else if (e instanceof ValidationException) {
+			WebApplicationException wae = buildWebApplicationException((ValidationException) e);
+			return wae.getResponse();
+		} else {
+			ErroServico erro = mensagensService.getErro(e);
+			String mensagemResposta = erro.getCodigo();
+			if (CODIGO_ERRO_INDEFINIDO.equals(mensagemResposta)) {
+				mensagemResposta = erro.getMensagem();
+			}
 
-		int status = getStatus(e);
-		return Response.status(status).entity(mensagemResposta).build();
+			int status = getStatus(e);
+			return Response.status(status).entity(mensagemResposta).build();
+		}
+	}
+
+	private WebApplicationException buildWebApplicationException(ValidationException ve) {
+		RestException re = new RestException(WSMessages.ME_ATRIBUTO_INVALIDO.codigo(), ve.getMessage());
+		Gson gson = new GsonBuilder().create();
+		Response response = Response.status(Status.BAD_REQUEST.getStatusCode()).entity(gson.toJson(re)).type(MediaType.APPLICATION_JSON).build();
+		return new WebApplicationException(response);
 	}
 }
