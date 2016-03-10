4e7c822f3d0a60ba86a44504f95526d641251f90
Paulo José Rolim Braga Filho <paulofilho@infox.com.br>
2016-03-10 15:45:06 -0300

    AutenticadorTokenPadrao também pode ser utilizado como cliente

--- src/main/java/br/com/infox/epp/ws/autenticacao/AutenticadorTokenPadrao.java
+++ src/main/java/br/com/infox/epp/ws/autenticacao/AutenticadorTokenPadrao.java
@@ -1,5 +1,7 @@
 package br.com.infox.epp.ws.autenticacao;
 
+import java.net.URLConnection;
+
 import javax.enterprise.inject.Default;
 import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
@@ -7,14 +9,21 @@ import javax.servlet.http.HttpServletRequest;
 import br.com.infox.epp.system.manager.ParametroManager;
 import br.com.infox.epp.ws.exception.UnauthorizedException;
 import br.com.infox.epp.ws.messages.WSMessages;
+import br.com.infox.ws.request.TokenInjector;
 
+/**
+ * Autenticador padrão de serviços REST que utiliza o valor de um token definido por um parâmetro de sistema em um header da requisição HTTP 
+ * 
+ * @author paulo
+ */
 @Default
-public class AutenticadorTokenPadrao implements AutenticadorToken {
+public class AutenticadorTokenPadrao implements AutenticadorToken, TokenInjector {
 
 	public static final String NOME_TOKEN_HEADER_PADRAO = "token";
 	private static final String NOME_PARAMETRO_TOKEN = "webserviceToken";
 	
 	private String nomeHeader = NOME_TOKEN_HEADER_PADRAO;
+	private String nomeParametro = NOME_PARAMETRO_TOKEN;
 	
 
 	@Inject
@@ -23,15 +32,20 @@ public class AutenticadorTokenPadrao implements AutenticadorToken {
 	public AutenticadorTokenPadrao() {
 	}
 	
-	public AutenticadorTokenPadrao(String nomeHeader) {
+	public AutenticadorTokenPadrao(String nomeParametro, String nomeHeader) {
 		super();
+		this.nomeParametro = nomeParametro;
 		this.nomeHeader = nomeHeader;
+	}
+	
+	private String getTokenParametro() {
+		return parametroManager.getValorParametro(nomeParametro);
 	}
 	
 	@Override
 	public void validarToken(HttpServletRequest request) throws UnauthorizedException {
 		String tokenRequest = getValorToken(request);
-		String tokenParametro = parametroManager.getValorParametro(NOME_PARAMETRO_TOKEN);
+		String tokenParametro = getTokenParametro();
 		if (tokenParametro == null || !tokenParametro.equals(tokenRequest)) {
 			throw new UnauthorizedException(WSMessages.ME_TOKEN_INVALIDO.codigo(),
 					WSMessages.ME_TOKEN_INVALIDO.label());				
@@ -41,6 +55,11 @@ public class AutenticadorTokenPadrao implements AutenticadorToken {
 	@Override
 	public String getValorToken(HttpServletRequest request) {
 		return ((HttpServletRequest) request).getHeader(nomeHeader);
+	}
+
+	@Override
+	public void injectAuthenticationToken(URLConnection urlConnection) {
+		urlConnection.setRequestProperty(nomeHeader, getTokenParametro());
 	}
 
 }
