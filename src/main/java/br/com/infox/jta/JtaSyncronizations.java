1937147013efd400faef034c715c4dc65c4f934b
Victor Pasqualino <victorpasqualino@infox.com.br>
2016-03-11 12:34:40 -0300

    Ajuste para utilizar o JtaSyncronizations

--- src/main/java/br/com/infox/jta/JtaSyncronizations.java
+++ src/main/java/br/com/infox/jta/JtaSyncronizations.java
@@ -1,11 +1,23 @@
 package br.com.infox.jta;
 
+import javax.transaction.RollbackException;
 import javax.transaction.Synchronization;
+import javax.transaction.SystemException;
+import javax.transaction.TransactionManager;
 
+import org.jboss.seam.ScopeType;
+import org.jboss.seam.annotations.Install;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Scope;
+import org.jboss.seam.annotations.intercept.BypassInterceptors;
 import org.jboss.seam.transaction.Synchronizations;
 
-import br.com.infox.core.transaction.TransactionSyncronizationsUtil;
+import br.com.infox.epp.cdi.util.JNDI;
 
+@Name("org.jboss.seam.transaction.synchronizations")
+@Scope(ScopeType.EVENT)
+@Install(precedence=Install.DEPLOYMENT, dependencies="org.jboss.seam.transaction.ejbTransaction")
+@BypassInterceptors
 public class JtaSyncronizations implements Synchronizations {
     
     @Override
@@ -38,7 +50,16 @@ public class JtaSyncronizations implements Synchronizations {
 
     @Override
     public void registerSynchronization(Synchronization sync) {
-        TransactionSyncronizationsUtil.getTransactionSynchronizationRegistry().registerInterposedSynchronization(sync);
+        TransactionManager transactionManager = JNDI.lookup("java:jboss/TransactionManager"); // JBOSS
+        if (transactionManager == null) {
+            transactionManager = JNDI.lookup("java:comp/TransactionManager"); // TOMCAT
+        }
+        try {
+            if (transactionManager.getTransaction() != null) {
+                transactionManager.getTransaction().registerSynchronization(sync);
+            }
+        } catch (IllegalStateException | SystemException | RollbackException e) {
+        }
     }
 
     @Override
