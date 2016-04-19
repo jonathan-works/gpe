5543a103bd16c2a8bf9e4febb51c94959df17ff3
Paulo José Rolim Braga Filho <paulofilho@infox.com.br>
2016-04-19 17:48:40 -0300

    Cada captcha resolvido permite visualizar 5 processos na consulta externa refs #70868

--- src/main/java/br/com/infox/epp/processo/consulta/action/ConsultaExternaController.java
+++ src/main/java/br/com/infox/epp/processo/consulta/action/ConsultaExternaController.java
@@ -2,12 +2,16 @@ package br.com.infox.epp.processo.consulta.action;
 
 import java.util.List;
 
+import javax.inject.Inject;
+
 import org.jboss.seam.ScopeType;
 import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Scope;
 
 import br.com.infox.core.controller.AbstractController;
+import br.com.infox.epp.captcha.ServicoCaptchaSessao;
+import br.com.infox.epp.cdi.seam.ContextDependency;
 import br.com.infox.epp.processo.documento.entity.Documento;
 import br.com.infox.epp.processo.documento.manager.DocumentoManager;
 import br.com.infox.epp.processo.entity.Processo;
@@ -15,18 +19,22 @@ import br.com.infox.epp.tarefa.entity.ProcessoTarefa;
 
 @Scope(ScopeType.CONVERSATION)
 @Name(ConsultaExternaController.NAME)
+@ContextDependency
 public class ConsultaExternaController extends AbstractController {
 
     private static final long serialVersionUID = 1L;
     public static final String NAME = "consultaExternaController";
     public static final String TAB_VIEW = "processoExternoView";
 
-    private boolean mostrarCaptcha = true;
-    
     @In
     private DocumentoManager documentoManager;
+    
+    @Inject
+    private ServicoCaptchaSessao servicoCaptcha;
 
     private Processo processo;
+    
+    private boolean mostrarCaptcha = true;
     
     public Processo getProcesso() {
 		return processo;
@@ -37,7 +45,10 @@ public class ConsultaExternaController extends AbstractController {
 	}
 
 	public void selectProcesso(Processo processo) {
-        mostrarCaptcha = true;
+        mostrarCaptcha = servicoCaptcha.isMostrarCaptcha();
+        if(!mostrarCaptcha) {
+    		servicoCaptcha.telaMostrada();        	
+        }
         setTab(TAB_VIEW);
         setProcesso(processo);
     }
@@ -53,12 +64,14 @@ public class ConsultaExternaController extends AbstractController {
     public void onClickSearchParteTab() {
         setProcesso(null);
     }
-
+    
 	public boolean isMostrarCaptcha() {
 		return mostrarCaptcha;
 	}
 	
 	public void validateCaptcha() {
+		servicoCaptcha.captchaResolvido();
+		servicoCaptcha.telaMostrada();
 		mostrarCaptcha = false;
 	}
 }
