792cf74c1a17e22cda287f68f6f91237ffb3bf96
Victor Pasqualino <victorpasqualino@infox.com.br>
2016-05-10 11:54:20 -0300

    Ajsutes na configuração das multiplas instancias

--- src/main/java/br/com/infox/ibpm/node/handler/ActivityNodeType.java
+++ src/main/java/br/com/infox/ibpm/node/handler/ActivityNodeType.java
@@ -1,7 +1,12 @@
 package br.com.infox.ibpm.node.handler;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang3.tuple.Pair;
 import org.jbpm.activity.exe.ActivityBehavior;
 import org.jbpm.activity.exe.LoopActivityBehavior;
+import org.jbpm.activity.exe.MultiInstanceActivityBehavior;
 import org.jbpm.activity.exe.ParallelMultiInstanceActivityBehavior;
 import org.jbpm.activity.exe.SequentialMultiInstanceActivityBehavior;
 
@@ -12,28 +17,50 @@ public enum ActivityNodeType implements Displayable {
     
     NONE(null), 
     LOOP(LoopActivityBehavior.class), 
-    SEQMULTIINSTANCE(SequentialMultiInstanceActivityBehavior.class), 
-    PARALLELMULTIINSTANCE(ParallelMultiInstanceActivityBehavior.class);
+    SEQUENTIAL_MULTIINSTANCE(SequentialMultiInstanceActivityBehavior.class), 
+    PARALLEL_MULTIINSTANCE(ParallelMultiInstanceActivityBehavior.class);
     
     private Class<? extends ActivityBehavior> activityClass;
+    private List<Pair<String, String>> variables;
     
     private ActivityNodeType(Class<? extends ActivityBehavior> clazz) {
         this.activityClass = clazz;
+        if (activityClass != null) {
+            this.variables = loadVariables(activityClass);
+        }
     }
     
+    private static List<Pair<String, String>> loadVariables(Class<? extends ActivityBehavior> clazz) {
+        List<Pair<String, String>> variables = new ArrayList<>();
+        if (LoopActivityBehavior.class.equals(clazz)) {
+            variables.add(Pair.of(LoopActivityBehavior.LOOP_COUNTER, "Variável que indica a contagem atual."));
+        } else if (MultiInstanceActivityBehavior.class.isAssignableFrom(clazz)) {
+            variables.add(Pair.of(MultiInstanceActivityBehavior.LOOP_COUNTER, "Indica a contagem atual."));
+            variables.add(Pair.of(MultiInstanceActivityBehavior.NUMBER_OF_INSTANCES, "Indica o total de instâncias criadas ou que serão criadas."));
+            variables.add(Pair.of(MultiInstanceActivityBehavior.NUMBER_OF_ACTIVE_INSTANCES, "Indica o número de instâncias ativas."));
+            variables.add(Pair.of(MultiInstanceActivityBehavior.NUMBER_OF_COMPLETED_INSTANCES, "Indica o número de instâncias que finalizaram pelo usuário."));
+            variables.add(Pair.of(MultiInstanceActivityBehavior.NUMBER_OF_TERMINATED_INSTANCES, "Indica o número de instâncias que foram terminadas, foram cancelada."));
+        }
+        return variables;
+    }
+
     public Class<? extends ActivityBehavior> getActivityClass() {
         return activityClass;
     }
     
+    public List<Pair<String, String>> getVariables() {
+        return variables;
+    }
+
     public static ActivityNodeType fromActivity(ActivityBehavior activityBehavior) {
         if (activityBehavior == null) {
             return NONE;
         } else if (LOOP.getActivityClass().isAssignableFrom(activityBehavior.getClass())) {
             return LOOP;
-        } else if (SEQMULTIINSTANCE.getActivityClass().isAssignableFrom(activityBehavior.getClass())) {
-            return SEQMULTIINSTANCE;
-        } else if (PARALLELMULTIINSTANCE.getActivityClass().isAssignableFrom(activityBehavior.getClass())) {
-            return PARALLELMULTIINSTANCE;
+        } else if (SEQUENTIAL_MULTIINSTANCE.getActivityClass().isAssignableFrom(activityBehavior.getClass())) {
+            return SEQUENTIAL_MULTIINSTANCE;
+        } else if (PARALLEL_MULTIINSTANCE.getActivityClass().isAssignableFrom(activityBehavior.getClass())) {
+            return PARALLEL_MULTIINSTANCE;
         } else {
             return null;
         }
@@ -58,7 +85,7 @@ public enum ActivityNodeType implements Displayable {
     }
     
     public boolean isMultiInstance() {
-        return this == SEQMULTIINSTANCE || this == PARALLELMULTIINSTANCE;
+        return this == SEQUENTIAL_MULTIINSTANCE || this == PARALLEL_MULTIINSTANCE;
     }
     
     public boolean isLoop() {
