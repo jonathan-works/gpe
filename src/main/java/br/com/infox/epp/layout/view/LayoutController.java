7f05a382e01cba6c74eda5d38a862001f3086862
Erik Liberal <erikliberal@infox.com.br>
2016-05-17 12:44:44 -0300

    Adição de função para importar ícones do material design refs #71669

--- src/main/java/br/com/infox/epp/layout/view/LayoutController.java
+++ src/main/java/br/com/infox/epp/layout/view/LayoutController.java
@@ -3,6 +3,7 @@ package br.com.infox.epp.layout.view;
 import java.io.Serializable;
 
 import javax.enterprise.context.SessionScoped;
+import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
 
@@ -32,6 +33,11 @@ public class LayoutController implements Serializable {
 	public String getResourceUrlByPath(String path) {
 		String skin = skinSessaoManager.getSkin();
 		return layoutManager.getResourceUrlByPath(skin, path);
+	}
+	
+	public String getMaterialDesignIconUrl(String dpir, String cor, String res, String nome){
+	    String urlBase=FacesContext.getCurrentInstance().getExternalContext().getRequestContextPath();
+	    return String.format("%s/resources/styleSkinInfox/all/%s_web/ic_%s_%s_%s.png", urlBase, dpir, nome, cor, res); 
 	}
 
 }
