e5ed5892a5c336a2acae627e767a64016aceafa7
Avner Pereira <avner@infox.com.br>
2016-05-30 16:58:19 -0300

    Cria entidades referentes ao Checklist de entregas refs #72387

---
+++ src/main/java/br/com/infox/epp/entrega/checklist/Checklist.java
@@ -0,0 +1,79 @@
+package br.com.infox.epp.entrega.checklist;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import br.com.infox.epp.access.entity.UsuarioLogin;
+
+@Entity
+@Table(name = "tb_checklist")
+public class Checklist implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private static final String GENERATOR_NAME = "checklistGenerator";
+    private static final String SEQUENCE_NAME = "sq_checklist";
+
+    @Id
+    @SequenceGenerator(allocationSize = 1, initialValue = 1, name = GENERATOR_NAME, sequenceName = SEQUENCE_NAME)
+    @GeneratedValue(generator = GENERATOR_NAME, strategy = GenerationType.SEQUENCE)
+    @Column(name = "id_checklist", nullable = false, updatable = false, unique = true)
+    private Integer id;
+
+    // TODO falta pegar integração das tarefas de Gabriel e Paulo aonde a entidade Entrega estará no core
+    // private Entrega entrega;
+
+    @NotNull
+    @Column(name = "dt_criacao", nullable = false)
+    private Date dataCriacao;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_usuario_criacao", nullable = false)
+    private UsuarioLogin usuarioCriacao;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    // TODO descomentar depois
+//    public Entrega getEntrega() {
+//        return entrega;
+//    }
+//
+//    public void setEntrega(Entrega entrega) {
+//        this.entrega = entrega;
+//    }
+
+    public Date getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(Date dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+    public UsuarioLogin getUsuarioCriacao() {
+        return usuarioCriacao;
+    }
+
+    public void setUsuarioCriacao(UsuarioLogin usuarioCriacao) {
+        this.usuarioCriacao = usuarioCriacao;
+    }
+
+}
