e21be349163fe5ca8dd788bb92d1835a710f60e1
Jovan Andrade <jovan@infox.com.br>
2016-05-31 18:47:17 -0300

    Adiciona metodo que verifica se existe entrega por localizacao no EstregaSearch

---
+++ src/main/java/br/com/infox/epp/entrega/documentos/EntregaSearch.java
@@ -0,0 +1,76 @@
+package br.com.infox.epp.entrega.documentos;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Join;
+import javax.persistence.criteria.JoinType;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import javax.persistence.criteria.Subquery;
+
+import br.com.infox.core.persistence.PersistenceController;
+import br.com.infox.epp.access.entity.Localizacao;
+import br.com.infox.epp.documento.entity.ClassificacaoDocumento;
+import br.com.infox.epp.entrega.modelo.ClassificacaoDocumentoEntrega;
+import br.com.infox.epp.entrega.modelo.ClassificacaoDocumentoEntrega_;
+import br.com.infox.epp.entrega.modelo.ModeloEntrega;
+import br.com.infox.epp.processo.documento.entity.Documento;
+import br.com.infox.epp.processo.documento.entity.Documento_;
+
+@Stateless
+@TransactionAttribute(TransactionAttributeType.SUPPORTS)
+public class EntregaSearch extends PersistenceController {
+
+	public List<ClassificacaoDocumentoEntrega> getClassificacoesDisponiveis(Entrega entrega, boolean obrigatorias) {
+		EntityManager entityManager = getEntityManager();
+		CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+		CriteriaQuery<ClassificacaoDocumentoEntrega> query = cb.createQuery(ClassificacaoDocumentoEntrega.class);
+		Root<ClassificacaoDocumentoEntrega> classificacaoEntrega = query.from(ClassificacaoDocumentoEntrega.class);
+		Join<ClassificacaoDocumentoEntrega, ClassificacaoDocumento> classificacao = classificacaoEntrega.join(ClassificacaoDocumentoEntrega_.classificacaoDocumento, JoinType.INNER);
+		
+		Subquery<Integer> subquery = query.subquery(Integer.class);
+		subquery.select(cb.literal(1));
+		Root<Documento> documento = subquery.from(Documento.class);
+		subquery.where(
+			cb.equal(documento.get(Documento_.pasta), entrega.getPasta()),
+			cb.equal(documento.get(Documento_.classificacaoDocumento), classificacao)
+		);
+
+		Predicate predicateObrigatorias = obrigatorias ? cb.isTrue(classificacaoEntrega.get(ClassificacaoDocumentoEntrega_.obrigatorio)) 
+				: cb.isFalse(classificacaoEntrega.get(ClassificacaoDocumentoEntrega_.obrigatorio));
+		
+		query.where(
+			cb.equal(classificacaoEntrega.get(ClassificacaoDocumentoEntrega_.modeloEntrega), entrega.getModeloEntrega()),
+			predicateObrigatorias,
+			cb.or(
+				cb.isTrue(classificacaoEntrega.get(ClassificacaoDocumentoEntrega_.multiplosDocumentos)),
+				cb.exists(subquery).not()
+			)
+		);
+		
+		return entityManager.createQuery(query).getResultList();
+	}
+	
+	public boolean existeEntregaModeloLocalizacao(ModeloEntrega modeloEntrega, Localizacao localizacao){
+		EntityManager entityManager = getEntityManager();
+		CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+		CriteriaQuery<Integer> query =  cb.createQuery(Integer.class);
+		query.select(cb.literal(1));
+		Root<Entrega> entrega = query.from(Entrega.class);
+		query.where(cb.equal(entrega.get(Entrega_.localizacao), localizacao),cb.equal(entrega.get(Entrega_.modeloEntrega), modeloEntrega));
+		try {
+            Integer result = getEntityManager().createQuery(query).getSingleResult();
+            return result == 1;
+        } catch (NoResultException nre) {
+            return false;
+        }
+	}
+	
+}
