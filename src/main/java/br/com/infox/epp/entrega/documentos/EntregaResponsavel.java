9f3ea68cd06d5f140214cbd8a0d73e57f0ad2abe
Avner Pereira <avner@infox.com.br>
2016-06-08 08:28:43 -0300

    Adiciona hashCode e equals para EntregaResponsavel refs #72678

--- src/main/java/br/com/infox/epp/entrega/documentos/EntregaResponsavel.java
+++ src/main/java/br/com/infox/epp/entrega/documentos/EntregaResponsavel.java
@@ -125,4 +125,29 @@ public class EntregaResponsavel implements Serializable {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((getId() == null) ? 0 : getId().hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        EntregaResponsavel other = (EntregaResponsavel) obj;
+        if (getId() == null) {
+            if (other.getId() != null)
+                return false;
+        } else if (!getId().equals(other.getId()))
+            return false;
+        return true;
+    }
 }
