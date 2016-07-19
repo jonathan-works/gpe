7cd8f150b744c6c9ffca21da9594a153a8a4955a
Gabriel Morrison Lima Dantas <gabriel@infox.com.br>
2016-07-19 12:55:18 -0300

    Adaptador de erros para o Oracle refs #72394

---
+++ src/main/java/br/com/infox/core/persistence/OracleErrorAdaptor.java
@@ -0,0 +1,30 @@
+package br.com.infox.core.persistence;
+
+import java.sql.SQLException;
+
+import br.com.infox.hibernate.oracle.error.OracleErrorCode;
+
+public class OracleErrorAdaptor {
+
+    public GenericDatabaseErrorCode resolve(SQLException sqlException) {
+        int code = sqlException.getErrorCode();
+        for (OracleErrorCode errorCode : OracleErrorCode.values()) {
+        	if (errorCode.getCode() == code) {
+        		switch (errorCode) {
+				case UNIQUE_VIOLATION:
+					return GenericDatabaseErrorCode.UNIQUE_VIOLATION;
+				case CHECK_CONSTRAINT_VIOLATION:
+					return GenericDatabaseErrorCode.CHECK_VIOLATION;
+				case FOREIGN_KEY_VIOLATION_HAS_CHILDREN:
+					return GenericDatabaseErrorCode.FOREIGN_KEY_VIOLATION;
+				case NOT_NULL_VIOLATION:
+					return GenericDatabaseErrorCode.NOT_NULL_VIOLATION;
+				default:
+					break;
+				}
+        	}
+        }
+        return null;
+    }
+    
+}
