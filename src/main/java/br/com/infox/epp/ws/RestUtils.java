2415d16141c0bbe11e5c12f218b4493d3735daab
Maria Luisa Souza Matos <marialuisa@infox.com.br>
2016-07-22 12:49:07 -0300

    Adiciona metodo para geração da URL de location retornada nos serviços de inclusão

--- src/main/java/br/com/infox/epp/ws/RestUtils.java
+++ src/main/java/br/com/infox/epp/ws/RestUtils.java
@@ -3,6 +3,8 @@ package br.com.infox.epp.ws;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.ws.rs.core.UriInfo;
+
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 
@@ -21,6 +23,13 @@ public class RestUtils {
 		Gson gson = new Gson();
 		error.add("properties", gson.toJsonTree(properties));
 		return gson.toJson(error);
+	}
+	
+	public static String generateLocationURL(UriInfo uriInfo, String codigo) {
+		StringBuilder sb = new StringBuilder(uriInfo.getAbsolutePath().toString());
+		sb.append("/");
+		sb.append(codigo);
+		return sb.toString();
 	}
 	
 }
