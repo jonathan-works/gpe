c9e4fe4241d01d5200a82c1c3993bad2104aa87d
Jovan Andrade <jovan@infox.com.br>
2016-07-27 18:19:12 -0300

    Corrigi problema de marcadores na tela de reabrir comunicação

--- src/main/java/br/com/infox/epp/processo/comunicacao/ModeloComunicacao.java
+++ src/main/java/br/com/infox/epp/processo/comunicacao/ModeloComunicacao.java
@@ -107,7 +107,7 @@ public class ModeloComunicacao implements Serializable, Cloneable {
     private String taskKey;
 	
 	@Column(name = "cd_marcadores", nullable = true)
-	private String codigosMarcadoresString;
+	private String codigosMarcadoresString = "";
 	
 	@OneToMany(fetch = FetchType.LAZY, mappedBy = "modeloComunicacao")
 	private List<DestinatarioModeloComunicacao> destinatarios = new ArrayList<>(0);
@@ -260,6 +260,8 @@ public class ModeloComunicacao implements Serializable, Cloneable {
 	
     public void setCodigosMarcadores(List<String> codigosMarcadores) {
         this.codigosMarcadores = new ArrayList<>();
+        if(codigosMarcadores == null)
+        	return ;
         for (String codigo : codigosMarcadores) {
 			this.codigosMarcadores.add(codigo.toUpperCase());
 		}
