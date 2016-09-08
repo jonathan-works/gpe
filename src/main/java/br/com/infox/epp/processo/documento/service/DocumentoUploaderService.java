4f0a7dac54464c1f889de757d896605718a71d35
Avner Pereira <avner@infox.com.br>
2016-09-08 09:36:14 -0300

    Unifica métodos getFileType que estavam repetidos refs #84497

--- src/main/java/br/com/infox/epp/processo/documento/service/DocumentoUploaderService.java
+++ src/main/java/br/com/infox/epp/processo/documento/service/DocumentoUploaderService.java
@@ -25,6 +25,7 @@ import com.lowagie.text.pdf.PdfReader;
 
 import br.com.infox.core.messages.InfoxMessages;
 import br.com.infox.core.persistence.DAOException;
+import br.com.infox.core.util.FileUtil;
 import br.com.infox.epp.documento.entity.ClassificacaoDocumento;
 import br.com.infox.epp.documento.entity.ExtensaoArquivo;
 import br.com.infox.epp.documento.manager.ExtensaoArquivoManager;
@@ -54,7 +55,7 @@ public class DocumentoUploaderService implements Serializable {
 	
 	public DocumentoBin createProcessoDocumentoBin(UploadedFile uploadedFile) throws Exception {
 		DocumentoBin documentoBin = new DocumentoBin();
-		documentoBin.setExtensao(getFileType(uploadedFile.getName()));
+		documentoBin.setExtensao(FileUtil.getFileType(uploadedFile.getName()));
 		documentoBin.setNomeArquivo(uploadedFile.getName());
 		documentoBin.setSize(Long.valueOf(uploadedFile.getSize()).intValue());
 		documentoBin.setProcessoDocumento(uploadedFile.getData());
@@ -64,26 +65,18 @@ public class DocumentoUploaderService implements Serializable {
 	
 	public DocumentoBin createDocumentoBin(UploadedFile uploadedFile) {
         DocumentoBin documentoBin = new DocumentoBin();
-        documentoBin.setExtensao(getFileType(uploadedFile.getName()));
+        documentoBin.setExtensao(FileUtil.getFileType(uploadedFile.getName()));
         documentoBin.setNomeArquivo(uploadedFile.getName());
         documentoBin.setSize(Long.valueOf(uploadedFile.getSize()).intValue());
         documentoBin.setModeloDocumento(null);
         return documentoBin;
     }
 	
-	private String getFileType(String nomeArquivo) {
-        String ret = "";
-        if (nomeArquivo != null) {
-            ret = nomeArquivo.substring(nomeArquivo.lastIndexOf('.') + 1);
-        }
-        return ret.toLowerCase();
-    }
-
 	public void validaDocumento(UploadedFile uploadFile, ClassificacaoDocumento classificacaoDocumento, byte[] dataStream) throws Exception {
         if (uploadFile == null) {
         	throw new ValidationException(InfoxMessages.getInstance().get("documentoUploader.error.noFile"));
         }
-        String extensao = getFileType(uploadFile.getName());
+        String extensao = FileUtil.getFileType(uploadFile.getName());
         ExtensaoArquivo extensaoArquivo = extensaoArquivoManager.getTamanhoMaximo(classificacaoDocumento, extensao);
         if (extensaoArquivo == null) {
         	throw new ValidationException("Arquivo: "+ uploadFile.getName() + " - " + InfoxMessages.getInstance().get("documentoUploader.error.invalidExtension"));
