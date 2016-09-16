38f082edb8bdb4b33cbafe17a01776341fdff0bd
Erik Liberal <erikliberal@infox.com.br>
2016-09-16 14:28:05 -0300

    Ajuste em controlador do novo termo de adesão para que as responsabilidades de assinatura sejam da classe de serviços refs #84786

--- src/main/java/br/com/infox/epp/access/TermoAdesaoViewController.java
+++ src/main/java/br/com/infox/epp/access/TermoAdesaoViewController.java
@@ -1,26 +1,31 @@
 package br.com.infox.epp.access;
 
+import java.io.IOException;
 import java.io.Serializable;
-import java.util.Map;
 
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-import br.com.infox.core.exception.SystemExceptionFactory;
+import org.jboss.seam.faces.FacesMessages;
+import org.jboss.seam.faces.Redirect;
+import org.jboss.seam.international.StatusMessage.Severity;
+
+import br.com.infox.core.action.ActionMessagesService;
+import br.com.infox.core.messages.InfoxMessages;
+import br.com.infox.epp.access.api.Authenticator;
+import br.com.infox.epp.access.entity.UsuarioLogin;
+import br.com.infox.epp.access.service.AuthenticatorService;
 import br.com.infox.epp.cdi.ViewScoped;
+import br.com.infox.epp.cdi.config.BeanManager;
+import br.com.infox.epp.cdi.exception.ExceptionHandled;
 import br.com.infox.epp.pessoa.entity.PessoaFisica;
-import br.com.infox.epp.pessoaFisica.PessoaFisicaSearch;
 import br.com.infox.epp.processo.documento.entity.DocumentoBin;
-import br.com.infox.epp.system.Parametros;
-import br.com.infox.epp.system.manager.ParametroManager;
-import br.com.infox.jwt.JWT;
-import br.com.infox.jwt.claims.InfoxPrivateClaims;
-import br.com.infox.jwt.verifiers.Verifiers;
-import br.com.infox.security.rsa.RSAErrorCodes;
-import br.com.infox.security.rsa.RSAUtil;
+import br.com.infox.seam.exception.BusinessException;
 
 @Named
 @ViewScoped
@@ -28,34 +33,107 @@ public class TermoAdesaoViewController implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    @Inject private TermoAdesaoService termoAdesaoManager;
-    @Inject private ParametroManager parametroManager;
-    @Inject private PessoaFisicaSearch pessoaFisicaSearch;
+    @Inject private TermoAdesaoService termoAdesaoService;
+    @Inject private AuthenticatorService authenticatorService;
+    @Inject private InfoxMessages infoxMessages;
 
     private PessoaFisica pessoaFisica;
     private String urlTermoAdesao;
     private DocumentoBin termoAdesao;
 
+    private String digestDocumento;
+    private String token;
+
+    private String jwt;
+    private boolean viaLoginUsuario = false;
+
     @PostConstruct
     protected void init() {
-        String jwt = getRequestParameter("jwt");
-        if (jwt != null){
-            this.pessoaFisica = retrievePessoaFisica(jwt, getKey());
-            this.termoAdesao = retrieveTermoAdesao(pessoaFisica);
-            urlTermoAdesao=termoAdesaoManager.buildUrlDownload(getContextPath(), jwt, this.termoAdesao.getUuid().toString());
+        UsuarioLogin usuarioLogado = Authenticator.getUsuarioLogado();
+        if (usuarioLogado != null) {
+            Authenticator.instance().unAuthenticate();
+
+            pessoaFisica = usuarioLogado.getPessoaFisica();
+            viaLoginUsuario = true;
+        } else if ((this.jwt = getRequestParameter("jwt")) == null) {
+            try {
+                HttpServletResponse response = (HttpServletResponse) FacesContext.getCurrentInstance()
+                        .getExternalContext().getResponse();
+                response.sendError(404);
+                return;
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 
-    private DocumentoBin retrieveTermoAdesao(PessoaFisica pessoaFisica) {
-        if (!isTermoAdesaoAssinado(pessoaFisica)){
-            return termoAdesaoManager.createTermoAdesaoFor(pessoaFisica);
+    @ExceptionHandled
+    public void onRenderView() {
+        if (!viaLoginUsuario) {
+            if (jwt != null) {
+                try {
+                    this.pessoaFisica = termoAdesaoService.retrievePessoaFisica(jwt.trim());
+                } catch (BusinessException e){
+                    FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, e.getMessage(), null));
+                }
+            }
+        }
+
+        this.termoAdesao = retrieveTermoAdesao();
+        this.setDigestDocumento(this.termoAdesao.getMd5Documento());
+
+        this.urlTermoAdesao = termoAdesaoService.buildUrlDownload(getContextPath(), jwt,
+                this.termoAdesao.getUuid().toString());
+    }
+
+    @ExceptionHandled
+    public boolean isTermoAdesaoAssinado() {
+        return pessoaFisica != null && termoAdesaoService.isTermoAdesaoAssinado(pessoaFisica.getCpf());
+    }
+
+    public String getUrlTermoAdesao() {
+        return urlTermoAdesao;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getDigestDocumento() {
+        return digestDocumento;
+    }
+
+    public void setDigestDocumento(String digestDocumento) {
+        this.digestDocumento = digestDocumento;
+    }
+
+    @ExceptionHandled
+    public void assinarTermoAdesao() {
+        this.termoAdesaoService.assinarTermoAdesao(token, pessoaFisica, pessoaFisica.getTermoAdesao().getUuid());
+        if (viaLoginUsuario) {
+            authenticatorService.loginWithoutPassword(pessoaFisica.getUsuarioLogin());
+            Redirect redirect = Redirect.instance();
+            redirect.getParameters().clear();
+            redirect.setViewId(Authenticator.instance().getCaminhoPainel());
+            redirect.execute();
+        }
+        FacesMessages.instance().add(Severity.INFO, infoxMessages.get("termoAdesao.sign.success"));
+    }
+
+    private String getRequestParameter(String name) {
+        return getHttpServletRequest().getParameter(name);
+    }
+
+    private DocumentoBin retrieveTermoAdesao() {
+        if (!isTermoAdesaoAssinado()) {
+            return termoAdesaoService.createTermoAdesaoFor(pessoaFisica);
         } else {
             return pessoaFisica.getTermoAdesao();
         }
-    }
-    
-    public boolean isTermoAdesaoAssinado(PessoaFisica pessoaFisica){
-        return pessoaFisica != null && termoAdesaoManager.isTermoAdesaoAssinado(pessoaFisica.getCpf());
     }
 
     private String getContextPath() {
@@ -64,36 +142,6 @@ public class TermoAdesaoViewController implements Serializable {
 
     private HttpServletRequest getHttpServletRequest() {
         return (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
-    }
-    
-    public String getUrlTermoAdesao(){
-        return urlTermoAdesao;
-    }
-
-    private String getRequestParameter(String name) {
-        return getHttpServletRequest().getParameter(name);
-    }
-
-    public PessoaFisica retrievePessoaFisica(String jwt, byte[] secret) {
-        Map<String, Object> decodedPayload = JWT.parser().setKey(secret).parse(jwt);
-
-        Verifiers.anyOf(InfoxPrivateClaims.CPF).verify(decodedPayload);
-
-        String cpf = (String) decodedPayload.get(InfoxPrivateClaims.CPF.getClaim());
-        if (cpf != null) {
-            return pessoaFisicaSearch.getByCpf(cpf);
-        }
-
-        return null;
-    }
-
-    private byte[] getKey() {
-        String base64RsaKey = parametroManager.getValorParametro(Parametros.EPP_API_RSA_PUBLIC_KEY.getLabel());
-        if (base64RsaKey == null || base64RsaKey.isEmpty()) {
-            throw SystemExceptionFactory.create(RSAErrorCodes.INVALID_PRIVATE_KEY_STRUCTURE)
-                    .set(Parametros.EPP_API_RSA_PUBLIC_KEY.getLabel(), base64RsaKey);
-        }
-        return RSAUtil.getPublicKeyFromBase64(base64RsaKey).getEncoded();
     }
 
 }
