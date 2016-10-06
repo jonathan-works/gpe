151e14839e07a693e1aa1bdd655586ec4740cd2b
Jovan Andrade <jovan@infox.com.br>
2016-10-06 12:26:56 -0300

    Ajuste na verificacao se documento é binario refs #85630

--- src/main/java/br/com/infox/epp/processo/documento/entity/DocumentoBin.java
+++ src/main/java/br/com/infox/epp/processo/documento/entity/DocumentoBin.java
@@ -367,7 +367,7 @@ public class DocumentoBin implements Serializable {
 
     @Transient
     public boolean isBinario() {
-        return !Strings.isEmpty(getExtensao());
+        return !Strings.isEmpty(getExtensao()) && (size != null &&  size.intValue() > 0 ) ;
     }
 
     @Transient
