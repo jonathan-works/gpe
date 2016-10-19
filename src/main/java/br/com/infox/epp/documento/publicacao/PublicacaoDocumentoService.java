7df557afa14156dae9ad7f03f4f7529231f80502
Paulo José Rolim Braga Filho <paulofilho@infox.com.br>
2016-10-19 18:15:47 -0300

    Implementada visualização de publicações nos documentos refs #85586

--- src/main/java/br/com/infox/epp/documento/publicacao/PublicacaoDocumentoService.java
+++ src/main/java/br/com/infox/epp/documento/publicacao/PublicacaoDocumentoService.java
@@ -1,10 +1,13 @@
 package br.com.infox.epp.documento.publicacao;
 
+import java.util.List;
+
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 
 import br.com.infox.cdi.dao.Dao;
 import br.com.infox.cdi.qualifier.GenericDao;
+import br.com.infox.epp.processo.documento.entity.Documento;
 
 @Stateless
 public class PublicacaoDocumentoService {
@@ -12,9 +15,15 @@ public class PublicacaoDocumentoService {
 	@Inject
 	@GenericDao
 	private Dao<PublicacaoDocumento, Long> dao;
+	@Inject
+	private PublicacaoDocumentoSearch publicacaoDocumentoSearch;
 		
 	public void publicarDocumento(PublicacaoDocumento publicacao) {
 		dao.persist(publicacao);
+	}
+	
+	public List<PublicacaoDocumento> getByDocumento(Documento documento) {
+		return publicacaoDocumentoSearch.getByDocumento(documento);
 	}
 	
 }
