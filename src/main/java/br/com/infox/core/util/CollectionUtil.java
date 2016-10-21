a06d22d865fc2ea923db731ce7a52181255f17f3
Erik Fernando Siqueira Liberal <erikliberal@infox.com.br>
2016-10-21 12:16:26 -0300

    Adição de novos métodos utilitários de coleções refs #85968

--- src/main/java/br/com/infox/core/util/CollectionUtil.java
+++ src/main/java/br/com/infox/core/util/CollectionUtil.java
@@ -2,6 +2,7 @@ package br.com.infox.core.util;
 
 import java.util.Collection;
 
+
 public final class CollectionUtil {
     private CollectionUtil(){
     }
@@ -9,4 +10,19 @@ public final class CollectionUtil {
     public static boolean isEmpty(Collection<?> collection){
         return collection == null || collection.isEmpty();
     }
+
+    public static <T> T firstOrNull(Collection<T> collection) {
+        return isEmpty(collection) ? null : collection.iterator().next();
+    }
+    
+    public static boolean hasAtLeast(Collection<?> collection, int ammount){
+        if (ammount < 0) 
+            return false;
+        if (ammount == 0){
+            return isEmpty(collection) || hasAtLeast(collection, 1);
+        }
+        return collection != null && collection.size() >= ammount;
+    }
+    
 }
+
