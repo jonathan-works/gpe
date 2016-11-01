d3df53b2b14edd736c7acc687904903fb3aa8e56
Erik Fernando Siqueira Liberal <erikliberal@infox.com.br>
2016-11-01 09:35:45 -0300

    Introdução de método que retorna o último elemento de uma coleção, ou null se a mesma estiver vazia

--- src/main/java/br/com/infox/core/util/CollectionUtil.java
+++ src/main/java/br/com/infox/core/util/CollectionUtil.java
@@ -1,6 +1,7 @@
 package br.com.infox.core.util;
 
 import java.util.Collection;
+import java.util.Iterator;
 
 
 public final class CollectionUtil {
@@ -14,6 +15,16 @@ public final class CollectionUtil {
     public static <T> T firstOrNull(Collection<T> collection) {
         return isEmpty(collection) ? null : collection.iterator().next();
     }
+    public static <T> T lastOrNull(Collection<T> collection) {
+        return isEmpty(collection) ? null : lastOfCollection(collection);
+    }
+    private static <T> T lastOfCollection(Collection<T> collection){
+        T result = null;
+        for(Iterator<T> iterator=collection.iterator();iterator.hasNext();)
+            result = iterator.next();
+        
+        return result;
+    }
     
     public static boolean hasAtLeast(Collection<?> collection, int ammount){
         if (ammount < 0) 
@@ -23,6 +34,6 @@ public final class CollectionUtil {
         }
         return collection != null && collection.size() >= ammount;
     }
-    
+
 }
 
