dd55321acbae27607e92fad7c793e894df5ef8d0
Gabriel Morrison Lima Dantas <gabriel@infox.com.br>
2016-12-23 07:51:44 -0300

    Ajusta posição dos pontos das transições ao importar bpmn refs #84664

--- src/main/java/br/com/infox/epp/modeler/BpmnDiagramAdapter.java
+++ src/main/java/br/com/infox/epp/modeler/BpmnDiagramAdapter.java
@@ -1,5 +1,13 @@
 package br.com.infox.epp.modeler;
 
+import java.awt.geom.Line2D;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 import org.camunda.bpm.model.bpmn.BpmnModelInstance;
 import org.camunda.bpm.model.bpmn.instance.Activity;
 import org.camunda.bpm.model.bpmn.instance.BaseElement;
@@ -15,10 +23,24 @@ import org.camunda.bpm.model.bpmn.instance.di.Waypoint;
 class BpmnDiagramAdapter implements BpmnAdapter {
 
 	private BpmnModelInstance bpmnModel;
+	private Map<String, Set<SequenceFlow>> sequenceFlowsByNodeIds;
 	
 	@Override
 	public BpmnModelInstance checkAndConvert(BpmnModelInstance bpmnModel) {
 		this.bpmnModel = bpmnModel;
+		this.sequenceFlowsByNodeIds = new HashMap<>();
+		for (SequenceFlow sequenceFlow : bpmnModel.getModelElementsByType(SequenceFlow.class)) {
+		    if (!sequenceFlowsByNodeIds.containsKey(sequenceFlow.getSource().getId())) {
+		        sequenceFlowsByNodeIds.put(sequenceFlow.getSource().getId(), new HashSet<SequenceFlow>());
+		    }
+		    if (!sequenceFlowsByNodeIds.containsKey(sequenceFlow.getTarget().getId())) {
+                sequenceFlowsByNodeIds.put(sequenceFlow.getTarget().getId(), new HashSet<SequenceFlow>());
+            }
+		    
+		    sequenceFlowsByNodeIds.get(sequenceFlow.getSource().getId()).add(sequenceFlow);
+		    sequenceFlowsByNodeIds.get(sequenceFlow.getTarget().getId()).add(sequenceFlow);
+		}
+		
 		adjustLaneSizes();
 		normalizeNodeSizes();
 		normalizeSequenceFlowSizes();
@@ -31,9 +53,8 @@ class BpmnDiagramAdapter implements BpmnAdapter {
 			BpmnShape shape = (BpmnShape) flowNode.getDiagramElement();
 			if (!isDefinedBounds(shape.getBounds())) {
 				shape.removeChildElement(shape.getBounds());
-			} else {
-				shape.getBounds().setWidth(getDefaultWidth(flowNode));
-				shape.getBounds().setHeight(getDefaultHeight(flowNode));
+			} else if (sequenceFlowsByNodeIds.containsKey(flowNode.getId())) {
+			    adjustEdgePoints(shape);
 			}
 			if (shape.getBpmnLabel() != null && !isDefinedBounds(shape.getBpmnLabel().getBounds())) {
 				shape.removeChildElement(shape.getBpmnLabel());
@@ -41,7 +62,53 @@ class BpmnDiagramAdapter implements BpmnAdapter {
 		}
 	}
 	
-	private void normalizeSequenceFlowSizes() {
+	private void adjustEdgePoints(BpmnShape flowNodeShape) {
+	    FlowNode flowNode = (FlowNode) flowNodeShape.getBpmnElement();
+	    
+	    double originalWidth = flowNodeShape.getBounds().getWidth();
+        double originalHeight = flowNodeShape.getBounds().getHeight();
+        flowNodeShape.getBounds().setWidth(getDefaultWidth(flowNode));
+        flowNodeShape.getBounds().setHeight(getDefaultHeight(flowNode));
+        
+        double x1 = flowNodeShape.getBounds().getX();
+        double y1 = flowNodeShape.getBounds().getY();
+        double x2 = x1 + flowNodeShape.getBounds().getWidth();
+        double y2 = y1 + flowNodeShape.getBounds().getHeight();
+        double x2Original = x1 + originalWidth;
+        double y2Original = y1 + originalHeight;
+        
+        Line2D topLine = new Line2D.Double(x1, y1, x2Original, y1);
+        Line2D bottomLine = new Line2D.Double(x1, y2Original, x2Original, y2Original);
+        Line2D leftLine = new Line2D.Double(x1, y1, x1, y2Original);
+        Line2D rightLine = new Line2D.Double(x2Original, y1, x2Original, y2Original);
+        
+        for (SequenceFlow sequenceFlow : sequenceFlowsByNodeIds.get(flowNode.getId())) {
+            BpmnEdge edge = sequenceFlow.getDiagramElement();
+            Waypoint waypoint1, waypoint2;
+            List<Waypoint> waypoints = new ArrayList<>(edge.getWaypoints());
+            if (flowNode.equals(sequenceFlow.getSource())) {
+                waypoint1 = waypoints.get(0);
+                waypoint2 = waypoints.get(1);
+            } else {
+                waypoint1 = waypoints.get(waypoints.size() - 1);
+                waypoint2 = waypoints.get(waypoints.size() - 2);
+            }
+            
+            Line2D waypointLine = new Line2D.Double(waypoint1.getX(), waypoint1.getY(), waypoint2.getX(), waypoint2.getY());
+            
+            if (waypointLine.intersectsLine(topLine)) {
+                waypoint1.setY(y1);
+            } else if (waypointLine.intersectsLine(bottomLine)) {
+                waypoint1.setY(y2);
+            } else if (waypointLine.intersectsLine(rightLine)) {
+                waypoint1.setX(x2);
+            } else if (waypointLine.intersectsLine(leftLine)) {
+                waypoint1.setX(x1);
+            }
+        }
+    }
+
+    private void normalizeSequenceFlowSizes() {
 		for (SequenceFlow sequenceFlow : bpmnModel.getModelElementsByType(SequenceFlow.class)) {
 			BpmnEdge edge = (BpmnEdge) sequenceFlow.getDiagramElement();
 			if (edge.getBpmnLabel() != null && !isDefinedBounds(edge.getBpmnLabel().getBounds())) {
