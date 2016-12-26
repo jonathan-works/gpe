892ca25ab9d75c6a6402f888b4b04ece6ce9bf5e
Erik Fernando Siqueira Liberal <erikliberal@infox.com.br>
2016-12-26 15:07:41 -0300

    Ajuste em construtor de assinavel provider para previnir NPE refs #88229

--- src/main/java/br/com/infox/epp/assinador/assinavel/AssinavelGenericoProvider.java
+++ src/main/java/br/com/infox/epp/assinador/assinavel/AssinavelGenericoProvider.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.apache.commons.lang3.ObjectUtils;
+
 public class AssinavelGenericoProvider implements AssinavelProvider {
 	
 	private List<AssinavelSource> assinaveis;
@@ -20,7 +22,7 @@ public class AssinavelGenericoProvider implements AssinavelProvider {
 	
 	public AssinavelGenericoProvider(List<String> textos) {
 		dataList = new ArrayList<>();
-		for(String texto : textos) {
+		for(String texto : ObjectUtils.defaultIfNull(textos, new ArrayList<String>())) {
 			dataList.add(texto.getBytes(StandardCharsets.UTF_8));
 		}
 	}	
