f2655cd7878a97324945f7b044f95c58fe2e16e9
Fabio Paes <fabiopaes@infox.com.br>
2017-01-02 17:22:44 -0300

    EL para copiar participantes dos processos relacionados refs #88643

--- src/main/java/br/com/infox/epp/processo/partes/dao/ParticipanteProcessoService.java
+++ src/main/java/br/com/infox/epp/processo/partes/dao/ParticipanteProcessoService.java
@@ -1,12 +1,14 @@
 package br.com.infox.epp.processo.partes.dao;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 
+import br.com.infox.core.util.DateUtil;
 import br.com.infox.epp.processo.entity.Processo;
 import br.com.infox.epp.processo.entity.RelacionamentoProcessoInterno;
 import br.com.infox.epp.processo.partes.entity.ParticipanteProcesso;
@@ -16,58 +18,101 @@ import br.com.infox.epp.relacionamentoprocessos.RelacionamentoProcessoDAO;
 @Stateless
 public class ParticipanteProcessoService {
 
-    @Inject
-    private RelacionamentoProcessoDAO relacionamentoProcessoDAO;
-    @Inject
-    private ParticipanteProcessoManager participanteProcessoManager;
+	@Inject
+	private RelacionamentoProcessoDAO relacionamentoProcessoDAO;
+	@Inject
+	private ParticipanteProcessoManager participanteProcessoManager;
 
-    public void adicionaParticipantesProcessoRelacionado(Processo processo) throws CloneNotSupportedException {
-        List<RelacionamentoProcessoInterno> relacionados = relacionamentoProcessoDAO.getListProcessosEletronicosRelacionados(processo);
-        Iterator<RelacionamentoProcessoInterno> it = relacionados.iterator();
-        while (it.hasNext()) {
-            Processo relacionado = it.next().getProcesso();
-            List<ParticipanteProcesso> participantesRaizProcesso = participanteProcessoManager.getParticipantesProcessoRaiz(relacionado);
-            for (ParticipanteProcesso raiz : participantesRaizProcesso) {
-                adicionaParticipantesFilhos(processo, raiz, relacionado);
-            }
-        }
-    }
+	public void adicionaParticipantesProcessoRelacionado(Processo processo) {
+		List<RelacionamentoProcessoInterno> relacionados = relacionamentoProcessoDAO.getListProcessosEletronicosRelacionados(processo);
+		Iterator<RelacionamentoProcessoInterno> it = relacionados.iterator();
+		while (it.hasNext()) {
+			Processo relacionado = it.next().getProcesso();
+			List<ParticipanteProcesso> participantesRaizProcesso = participanteProcessoManager.getParticipantesProcessoRaiz(relacionado);
+			for (ParticipanteProcesso raiz : participantesRaizProcesso) {
+				try {
+					adicionaParticipantesFilhos(processo, raiz, relacionado);
 
-    private void adicionaParticipantesFilhos(Processo processo, ParticipanteProcesso parte, Processo relacionado)
-            throws CloneNotSupportedException {
+				} catch (CloneNotSupportedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
 
-        List<ParticipanteProcesso> listaPartes = new ArrayList<>();
+	private void adicionaParticipantesFilhos(Processo processo, ParticipanteProcesso parte, Processo relacionado) throws CloneNotSupportedException {
 
-        ParticipanteProcesso participanteRaizCopy = parte.copiarParticipanteMantendoFilhos();
-        participanteRaizCopy.setProcesso(processo);
-        participanteRaizCopy.setParticipantesFilhos(getCopyParticipantesFilhos(participanteRaizCopy, processo));
-        listaPartes.add(participanteRaizCopy);
-        preencherParticipantesFilhos(processo, listaPartes, participanteRaizCopy.getParticipantesFilhos());
+		List<ParticipanteProcesso> listaPartes = new ArrayList<>();
+		boolean insereParticipante = false;
 
-        participanteProcessoManager.gravarListaParticipantes(listaPartes);
+		ParticipanteProcesso existente = participanteProcessoManager.getParticipanteProcessoByPessoaProcesso(parte.getPessoa(), processo);
+		if (existente != null) {
 
-    }
+			boolean mesmoTipo = existente.getTipoParte().getId() == parte.getTipoParte().getId();
 
-    private List<ParticipanteProcesso> getCopyParticipantesFilhos(ParticipanteProcesso participanteAtual, Processo processo)
-            throws CloneNotSupportedException {
+			boolean mesmoSuperior = false;
+			if ((existente.getParticipantePai() != null && parte.getParticipantePai() != null
+					&& existente.getParticipantePai().getId() == parte.getParticipantePai().getId())
+					|| (existente.getParticipantePai() == null && parte.getParticipantePai() == null)
+			)
+				mesmoSuperior = true;
 
-        List<ParticipanteProcesso> participantesFilhos = new ArrayList<>();
-        for (ParticipanteProcesso participanteProcesso : participanteAtual.getParticipantesFilhos()) {
-            ParticipanteProcesso parteFilho = participanteProcesso.copiarParticipanteMantendoFilhos();
-            parteFilho.setProcesso(processo);
-            parteFilho.setParticipantePai(participanteAtual);
-            participantesFilhos.add(parteFilho);
-        }
-        return participantesFilhos;
-    }
+			boolean mesmoPeriodo = false;
+			Date ptIni = parte.getDataInicio();
+			Date ptFim = parte.getDataFim();
+			Date exIni = existente.getDataInicio();
+			Date exFim = existente.getDataFim();
+			
+			if (ptIni.before(exIni) && (ptFim == null || (ptFim != null && ptFim.after(exIni)))) {
+				mesmoPeriodo = true;
+				existente.setDataInicio(ptIni);
+			}
 
-    private void preencherParticipantesFilhos(Processo processo, List<ParticipanteProcesso> participantesParaPersistir,
-            List<ParticipanteProcesso> participantesFilhos) throws CloneNotSupportedException {
+			if (ptFim != null && exFim != null && ptFim.after(exFim)) {
+				mesmoPeriodo = true;
+				existente.setDataFim(ptFim);
+			}
+			
+			if (mesmoTipo && mesmoSuperior && mesmoPeriodo) {
+				participanteProcessoManager.update(existente);
+			} else {
+				insereParticipante = true;
+			}
+		} else {
+			insereParticipante = true;
+		}
 
-        for (ParticipanteProcesso filho : participantesFilhos) {
-            filho.setParticipantesFilhos(getCopyParticipantesFilhos(filho, processo));
-            participantesParaPersistir.add(filho);
-            preencherParticipantesFilhos(processo, participantesParaPersistir, filho.getParticipantesFilhos());
-        }
-    }
+		if (insereParticipante) {
+			ParticipanteProcesso participanteRaizCopy = parte.copiarParticipanteMantendoFilhos();
+			participanteRaizCopy.setProcesso(processo);
+			participanteRaizCopy.setParticipantesFilhos(getCopyParticipantesFilhos(participanteRaizCopy, processo));
+			listaPartes.add(participanteRaizCopy);
+			preencherParticipantesFilhos(processo, listaPartes, participanteRaizCopy.getParticipantesFilhos());
+			participanteProcessoManager.gravarListaParticipantes(listaPartes);
+		}
+
+	}
+
+	private List<ParticipanteProcesso> getCopyParticipantesFilhos(ParticipanteProcesso participanteAtual, Processo processo)
+			throws CloneNotSupportedException {
+
+		List<ParticipanteProcesso> participantesFilhos = new ArrayList<>();
+		for (ParticipanteProcesso participanteProcesso : participanteAtual.getParticipantesFilhos()) {
+			ParticipanteProcesso parteFilho = participanteProcesso.copiarParticipanteMantendoFilhos();
+			parteFilho.setProcesso(processo);
+			parteFilho.setParticipantePai(participanteAtual);
+			participantesFilhos.add(parteFilho);
+		}
+		return participantesFilhos;
+	}
+
+	private void preencherParticipantesFilhos(Processo processo, List<ParticipanteProcesso> participantesParaPersistir,
+			List<ParticipanteProcesso> participantesFilhos) throws CloneNotSupportedException {
+
+		for (ParticipanteProcesso filho : participantesFilhos) {
+			filho.setParticipantesFilhos(getCopyParticipantesFilhos(filho, processo));
+			participantesParaPersistir.add(filho);
+			preencherParticipantesFilhos(processo, participantesParaPersistir, filho.getParticipantesFilhos());
+		}
+	}
 }
