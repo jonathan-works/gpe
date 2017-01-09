81f1b6034e19327447ed8a1f6b632f13926098b7
Erik Fernando Siqueira Liberal <erikliberal@infox.com.br>
2017-01-09 12:08:37 -0300

    Adição de novos métodos utilitários ao ObjectComparator

--- src/main/java/br/com/infox/core/util/ObjectComparator.java
+++ src/main/java/br/com/infox/core/util/ObjectComparator.java
@@ -1,12 +1,18 @@
 package br.com.infox.core.util;
 
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.Map;
 
 public interface ObjectComparator<T> {
 
     @SuppressWarnings("unchecked")
     boolean in(T... args);
     boolean in (Collection<T> args);
+
+    boolean empty();
+
+    boolean notEmpty();
 }
 
 class ObjectComparatorImpl<T> implements ObjectComparator<T> {
@@ -34,4 +40,26 @@ class ObjectComparatorImpl<T> implements ObjectComparator<T> {
         return args.contains(obj);
     }
 
+    public boolean empty() {
+        if (obj == null)
+            return true;
+
+        if (obj instanceof String)
+            return ((String) obj).trim().isEmpty();
+
+        if (obj instanceof Collection)
+            return ((Collection<?>) obj).isEmpty();
+
+        if (obj instanceof Map)
+            return ((Map<?, ?>) obj).isEmpty();
+
+        if (obj.getClass().isArray())
+            return Arrays.asList(obj).isEmpty();
+
+        return false;
+    }
+
+    public boolean notEmpty() {
+        return !empty();
+    }
 }
