511bbecfbbb16eb79f1227419cb709b89640a15a
Victor Pasqualino <victorpasqualino@infox.com.br>
2017-03-03 17:37:55 -0300

    Ajuste no converter do autocomplete

--- src/main/java/br/com/infox/jsf/converter/AutoCompleteItemConverter.java
+++ src/main/java/br/com/infox/jsf/converter/AutoCompleteItemConverter.java
@@ -1,13 +1,13 @@
 package br.com.infox.jsf.converter;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
 import javax.faces.convert.FacesConverter;
-
-import org.primefaces.component.autocomplete.AutoComplete;
 
 @FacesConverter("br.com.infox.jsf.converter.autocomplete")
 public class AutoCompleteItemConverter implements Converter {
@@ -16,7 +16,7 @@ public class AutoCompleteItemConverter implements Converter {
     
     @Override
     public Object getAsObject(FacesContext context, UIComponent component, String value) {
-        list = loadSuggestionsFromViewMap(context, component);
+        if ( list == null ) list = loadSuggestionsFromViewMap(context, component);
         try {
             return list.get(Integer.valueOf(value));
         } catch ( NumberFormatException e) {
@@ -26,17 +26,29 @@ public class AutoCompleteItemConverter implements Converter {
 
     @Override
     public String getAsString(FacesContext context, UIComponent component, Object value) {
-        if ( list == null ) {
-            list = setSuggestionsToViewMap(context, component);
+        if (isNewQuery(context, component) && list == null) {
+            clearSuggestionsViewMap(context, component);
         }
+        addSuggestionToViewMap(context, component, value);
         return String.valueOf(list.indexOf(value));
     }
 
-    @SuppressWarnings("unchecked")
-    public List<Object> setSuggestionsToViewMap(FacesContext context, UIComponent component) {
-        List<Object> list = ((AutoComplete) component).getSuggestions();
-        context.getViewRoot().getViewMap().put(component.getClientId() + "_suggestions", list);
-        return list;
+    private boolean isNewQuery(FacesContext context, UIComponent component) {
+        Map<String, String> params = context.getExternalContext().getRequestParameterMap();
+        String query = params.get(component.getClientId(context) + "_query");
+        return query != null;
+    }
+
+    public void clearSuggestionsViewMap(FacesContext context, UIComponent component) {
+        context.getViewRoot().getViewMap().put(component.getClientId() + "_suggestions", null);
+    }
+    
+    private void addSuggestionToViewMap(FacesContext context, UIComponent component, Object value) {
+        if ( list == null ) { 
+            list = new ArrayList<Object>();
+            context.getViewRoot().getViewMap().put(component.getClientId() + "_suggestions", list);
+        }
+        list.add(value);
     }
     
     @SuppressWarnings("unchecked")
