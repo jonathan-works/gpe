c7cd58af51b6f4b3308dc06aa5523b5d38be1330
Jovan Andrade <jovan@infox.com.br>
2017-04-12 16:44:12 -0300

    Remove a verficacao do certchain do usuário.

--- src/main/java/br/com/infox/epp/assinador/ValidadorAssinaturaPadrao.java
+++ src/main/java/br/com/infox/epp/assinador/ValidadorAssinaturaPadrao.java
@@ -70,10 +70,6 @@ public class ValidadorAssinaturaPadrao implements ValidadorAssinatura, Validador
                 if (!pessoaFisica.equals(pessoaFisicaCertificado)) {
                     throw new AssinaturaException(Motivo.CPF_CERTIFICADO_DIFERENTE_USUARIO);
                 }
-                
-                if (pessoaFisicaCertificado == null || !pessoaFisicaCertificado.checkCertChain(certChainBase64Encoded)) {
-                	throw new AssinaturaException(Motivo.CERTIFICADO_USUARIO_DIFERENTE_CADASTRO);
-                }
             }
 		}
 		catch(CertificadoException e) {
