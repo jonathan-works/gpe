acf126621da4363e9bd992aab62fdda712867a8f
Victor Pasqualino <victorpasqualino@infox.com.br>
2017-04-13 14:39:00 -0300

    Criaçao de AbstractLazyData

--- src/main/java/br/com/infox/core/util/ObjectUtil.java
+++ src/main/java/br/com/infox/core/util/ObjectUtil.java
@@ -1,5 +1,9 @@
 package br.com.infox.core.util;
 
+import java.lang.reflect.Array;
+import java.util.Collection;
+import java.util.Map;
+
 public class ObjectUtil {
     
     public static boolean equals(Object obj1, Object obj2) {
@@ -8,6 +12,39 @@ public class ObjectUtil {
     
     public static <T> ObjectComparator<T> is(T obj){
         return new ObjectComparatorImpl<T>(obj);
+    }
+    
+    public static boolean isEmpty(Object value) {
+        if (value == null) {
+            return true;
+        }
+        else if (value instanceof String) {
+            return isEmpty((String) value);
+        }
+        else if (value instanceof Collection) {
+            return isEmpty((Collection<?>) value);
+        }
+        else if (value instanceof Map) {
+            return isEmpty((Map<?, ?>) value);
+        }
+        else if (value.getClass().isArray()) {
+            return Array.getLength(value) == 0;
+        }
+        else {
+            return value.toString() == null || value.toString().isEmpty();
+        }
+    }
+    
+    public static boolean isEmpty(String string) {
+        return string == null || string.isEmpty();
+    }
+
+    public static boolean isEmpty(Collection<?> collection) {
+        return collection == null || collection.isEmpty();
+    }
+
+    public static boolean isEmpty(Map<?, ?> map) {
+        return map == null || map.isEmpty();
     }
 
 }
