3d83ae11bd37e3f666baf0e13c53b3b9ad37aa8e
Maria Luisa Souza Matos <marialuisa@infox.com.br>
2017-05-16 16:03:38 -0300

    Ajustes no crud variavel refs #94658

--- src/main/java/br/com/infox/epp/documento/list/associative/AssociativeTipoModeloDocumentoList.java
+++ src/main/java/br/com/infox/epp/documento/list/associative/AssociativeTipoModeloDocumentoList.java
@@ -2,20 +2,18 @@ package br.com.infox.epp.documento.list.associative;
 
 import java.util.Map;
 
-import org.jboss.seam.ScopeType;
-import org.jboss.seam.annotations.Name;
-import org.jboss.seam.annotations.Scope;
+import javax.inject.Named;
 
 import br.com.infox.core.list.EntityList;
 import br.com.infox.core.list.SearchCriteria;
+import br.com.infox.epp.cdi.ViewScoped;
 import br.com.infox.epp.documento.entity.TipoModeloDocumento;
 
-@Name(AssociativeTipoModeloDocumentoList.NAME)
-@Scope(ScopeType.PAGE)
+@Named
+@ViewScoped
 public class AssociativeTipoModeloDocumentoList extends EntityList<TipoModeloDocumento> {
 
     private static final long serialVersionUID = 1L;
-    public static final String NAME = "associativeTipoModeloDocumentoList";
 
     private static final String DEFAULT_EJBQL = "select o from TipoModeloDocumento o where o not in "
             + "(select vti.tipoModeloDocumento from VariavelTipoModelo vti inner join vti.variavel v where v.variavel = #{associativeTipoModeloDocumentoList.variavelToIgnore})";
