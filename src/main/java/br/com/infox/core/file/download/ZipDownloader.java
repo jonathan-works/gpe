4331d65584188d30c2bd36795e5610f332958700
Paulo José Rolim Braga Filho <paulofilho@infox.com.br>
2017-05-17 18:22:22 -0300

    Exportador de ZIP agora usa lista de idDocumento refs #93757

--- src/main/java/br/com/infox/core/file/download/ZipDownloader.java
+++ src/main/java/br/com/infox/core/file/download/ZipDownloader.java
@@ -28,8 +28,8 @@ import br.com.infox.cdi.producer.EntityManagerProducer;
 import br.com.infox.core.file.download.FileDownloader.Exporter;
 import br.com.infox.epp.processo.documento.entity.Documento;
 import br.com.infox.epp.processo.documento.entity.DocumentoBin;
-import br.com.infox.epp.processo.documento.entity.DocumentoBin_;
-import br.com.infox.epp.processo.documento.manager.DocumentoBinManager;
+import br.com.infox.epp.processo.documento.entity.Documento_;
+import br.com.infox.epp.processo.documento.manager.DocumentoManager;
 import br.com.infox.epp.processo.entity.Processo;
 import br.com.infox.epp.system.entity.Parametro;
 import br.com.infox.epp.system.manager.ParametroManager;
@@ -48,7 +48,7 @@ public class ZipDownloader {
 	@Inject
 	protected FileDownloader fileDownloader;
 	@Inject
-	protected DocumentoBinManager documentoBinManager;
+	protected DocumentoManager documentoManager;
 	
 	protected static String nomeSistema;
 	
@@ -107,11 +107,11 @@ public class ZipDownloader {
 		return nomeArquivo;
 	}
 	
-	protected String getNomeArquivo(Integer idDocumentoBin, Set<String> nomesUtilizados) {
+	protected String getNomeArquivo(Integer idDocumento, Set<String> nomesUtilizados) {
 		final Pattern pattern = Pattern.compile("^(.+)\\.([^\\.]+)$");
 		
-		DocumentoBin documentoBin = documentoBinManager.find(idDocumentoBin);
-		Documento documento = documentoBin.getDocumentoList().iterator().next();
+		Documento documento = documentoManager.find(idDocumento);
+		DocumentoBin documentoBin = documento.getDocumentoBin();
 		Integer numero = documento.getNumeroDocumento();
 		String nomeArquivoOriginal = documentoBin.getNomeArquivo();
 
@@ -135,38 +135,38 @@ public class ZipDownloader {
 		return retorno;
 	}
 	
-	protected void exportDocumento(Integer idDocumentoBin, OutputStream outputStream, ZipOutputStream zos, String zipEntryName) throws IOException {
-		DocumentoBin documentoBin = documentoBinManager.find(idDocumentoBin);
+	protected void exportDocumento(Integer idDocumento, OutputStream outputStream, ZipOutputStream zos, String zipEntryName) throws IOException {
+		Documento documento = documentoManager.find(idDocumento);
 		zos.putNextEntry(new ZipEntry(zipEntryName));
-		fileDownloader.export(documentoBin, outputStream);
+		fileDownloader.export(documento.getDocumentoBin(), outputStream);
 		outputStream.flush();
 		zos.closeEntry();
 	}
 	
-	protected void exportDocumentos(List<Integer> idsDocumentoBin, OutputStream outputStream, ZipOutputStream zos) throws IOException {
+	protected void exportDocumentos(List<Integer> idsDocumentos, OutputStream outputStream, ZipOutputStream zos) throws IOException {
 		Set<String> nomesUtilizados = new HashSet<>();
 		
 		EntityManager em = EntityManagerProducer.getEntityManager();
 		CriteriaBuilder cb = em.getCriteriaBuilder();
-		CriteriaQuery<DocumentoBin> cq = cb.createQuery(DocumentoBin.class);
-		Root<DocumentoBin> documentoBin = cq.from(DocumentoBin.class);
-		documentoBin.fetch(DocumentoBin_.documentoList);
-		cq.where(documentoBin.get(DocumentoBin_.id).in(idsDocumentoBin));
+		CriteriaQuery<Documento> cq = cb.createQuery(Documento.class);
+		Root<Documento> doc = cq.from(Documento.class);
+		doc.fetch(Documento_.documentoBin);
+		cq.where(doc.get(Documento_.id).in(idsDocumentos));
 		
-		for(Integer idDocumentoBin : idsDocumentoBin) {
-			String nomeArquivo = getNomeArquivo(idDocumentoBin, nomesUtilizados);
-			exportDocumento(idDocumentoBin, outputStream, zos, nomeArquivo);
+		for(Integer idDocumento : idsDocumentos) {
+			String nomeArquivo = getNomeArquivo(idDocumento, nomesUtilizados);
+			exportDocumento(idDocumento, outputStream, zos, nomeArquivo);
 			nomesUtilizados.add(nomeArquivo);
 		}		
 	}
 	
-	public void exportZipDocumentos(List<Integer> idsDocumentoBin, OutputStream outputStream) throws IOException {
+	public void exportZipDocumentos(List<Integer> idsDocumentos, OutputStream outputStream) throws IOException {
 		try(ZipOutputStream zos = new ZipOutputStream(outputStream, Charset.forName(ENCODING))) {
 			zos.setLevel(compressionLevel);
 			BufferedOutputStream bos = new BufferedOutputStream(zos);
 			ProgressOutputStream pos = new ProgressOutputStream(bos, null);
 
-			exportDocumentos(idsDocumentoBin, pos, zos);
+			exportDocumentos(idsDocumentos, pos, zos);
 			
 			outputStream.flush();
 		}
@@ -174,21 +174,21 @@ public class ZipDownloader {
 	
 	public class ZipDocumentosExporter implements Exporter {
 
-		private List<Integer> idsDocumentoBin;
+		private List<Integer> idsDocumentos;
 		
-		public ZipDocumentosExporter(List<Integer> idsDocumentoBin) {
+		public ZipDocumentosExporter(List<Integer> idsDocumentos) {
 			super();
-			this.idsDocumentoBin = idsDocumentoBin;
+			this.idsDocumentos = idsDocumentos;
 		}
 
 		@Override
 		public void export(OutputStream outputStream) throws IOException {
-			exportZipDocumentos(idsDocumentoBin, outputStream);
+			exportZipDocumentos(idsDocumentos, outputStream);
 		}
 	}
 	
-	public void downloadZipDocumentos(List<Integer> idsDocumentoBin, String filename) throws IOException {
-		Exporter exporter = new ZipDocumentosExporter(idsDocumentoBin);
+	public void downloadZipDocumentos(List<Integer> idsDocumentos, String filename) throws IOException {
+		Exporter exporter = new ZipDocumentosExporter(idsDocumentos);
 		fileDownloader.downloadDocumento(exporter, MediaType.ZIP.toString(), filename);
 	}
 	
@@ -203,31 +203,31 @@ public class ZipDownloader {
 		return String.format("%s_%s_%s.zip", nomeSistema, numeroProcesso, sufixoData);
 	}
 	
-	protected Processo getProcesso(Integer idDocumentoBin) {
-		DocumentoBin documentoBin = documentoBinManager.find(idDocumentoBin);
-		return documentoBin.getDocumentoList().get(0).getPasta().getProcesso();
+	protected Processo getProcesso(Integer idDocumento) {
+		Documento documento = documentoManager.find(idDocumento);
+		return documento.getPasta().getProcesso();
 	}
 	
-	protected String getNumeroProcesso(List<Integer> idsDocumentoBin) {
-		if(idsDocumentoBin.isEmpty()) {
+	protected String getNumeroProcesso(List<Integer> idsDocumentos) {
+		if(idsDocumentos.isEmpty()) {
 			return null;
 		}
-		return getProcesso(idsDocumentoBin.iterator().next()).getNumeroProcesso();
+		return getProcesso(idsDocumentos.iterator().next()).getNumeroProcesso();
 	}
 	
-	protected String getNomeArquivoPadrao(List<Integer> idsDocumentoBin) {
-		String numeroProcesso = getNumeroProcesso(idsDocumentoBin);
+	protected String getNomeArquivoPadrao(List<Integer> idsDocumentos) {
+		String numeroProcesso = getNumeroProcesso(idsDocumentos);
 		return getNomeArquivoPadrao(numeroProcesso);		
 	}
 	
-	public void downloadZipDocumentos(List<Integer> idsDocumentoBin) throws IOException {
-		if(idsDocumentoBin.isEmpty()) {
+	public void downloadZipDocumentos(List<Integer> idsDocumentos) throws IOException {
+		if(idsDocumentos.isEmpty()) {
 			return;
 		}
-		String numeroProcesso = getNumeroProcesso(idsDocumentoBin);
+		String numeroProcesso = getNumeroProcesso(idsDocumentos);
 		String nomeArquivo = getNomeArquivoPadrao(numeroProcesso);
 		
-		downloadZipDocumentos(idsDocumentoBin, nomeArquivo);
+		downloadZipDocumentos(idsDocumentos, nomeArquivo);
 	}
 	
 
