8759c6491ad0e9cb6181dc8ca5277cb2da79e1b8
Maria Luisa Souza Matos <marialuisa@infox.com.br>
2017-05-22 16:42:59 -0300

    Ajustes no crud do tipo de comunicação refs #94658

--- src/main/java/br/com/infox/epp/processo/comunicacao/tipo/crud/TipoComunicacaoList.java
+++ src/main/java/br/com/infox/epp/processo/comunicacao/tipo/crud/TipoComunicacaoList.java
@@ -2,20 +2,16 @@ package br.com.infox.epp.processo.comunicacao.tipo.crud;
 
 import java.util.Map;
 
-import org.jboss.seam.ScopeType;
-import org.jboss.seam.annotations.AutoCreate;
-import org.jboss.seam.annotations.Name;
-import org.jboss.seam.annotations.Scope;
+import javax.inject.Named;
 
 import br.com.infox.core.list.EntityList;
 import br.com.infox.core.list.SearchCriteria;
+import br.com.infox.epp.cdi.ViewScoped;
 
-@Name(TipoComunicacaoList.NAME)
-@Scope(ScopeType.PAGE)
-@AutoCreate
+@Named
+@ViewScoped
 public class TipoComunicacaoList extends EntityList<TipoComunicacao> {
     private static final long serialVersionUID = 1L;
-    public static final String NAME = "tipoComunicacaoList";
 
     private static final String DEFAULT_EJBQL = "select o from TipoComunicacao o";
     private static final String DEFAULT_ORDER = "descricao";
