84fe69bb7092b65a579422c569563b2b91d2ac71
jonathamoreira <jonatha@infox.com.br>
2019-12-19 14:04:30 -0300

    Ajustes para corração dos campos de telfone refs #124527

--- epp/jsf-gui/src/main/java/br/com/infox/epp/loglab/eturmalina/service/ETurmalinaService.java
+++ epp/jsf-gui/src/main/java/br/com/infox/epp/loglab/eturmalina/service/ETurmalinaService.java
@@ -13,6 +13,8 @@ import javax.ejb.TransactionAttributeType;
 import com.google.gson.Gson;
 import com.google.gson.JsonSyntaxException;
 
+import br.com.infox.core.exception.EppConfigurationException;
+import br.com.infox.core.util.StringUtil;
 import br.com.infox.epp.loglab.eturmalina.bean.DadosServidorBean;
 import br.com.infox.epp.loglab.eturmalina.bean.DadosServidorResponseBean;
 import br.com.infox.epp.loglab.eturmalina.ws.WSIntegracaoRH;
@@ -28,8 +30,9 @@ public class ETurmalinaService implements Serializable{
 	private static final long serialVersionUID = 1L;
 
     public List<DadosServidorResponseBean> getDadosServidor(DadosServidorBean dadosServidor) {
-        WSIntegracaoRHGETDADOSSERVIDOR request = criarDadosServidor(dadosServidor);
+    	validarParametros();
 
+        WSIntegracaoRHGETDADOSSERVIDOR request = criarDadosServidor(dadosServidor);
         try {
             URL url = new URL(Parametros.DS_URL_SERVICO_ETURMALINA.getValue());
             WSIntegracaoRH wsIntegracao = new WSIntegracaoRH(url);
@@ -46,7 +49,6 @@ public class ETurmalinaService implements Serializable{
 
     private List<DadosServidorResponseBean> getServidoresEmExercicio(WSIntegracaoRHGETDADOSSERVIDORResponse retornoWs) {
         List<DadosServidorResponseBean> servidoresEmExercicioList = new ArrayList<DadosServidorResponseBean>();
-
         try {
             if(retornoWs.getRetorno() != null){
                 Gson gson = new Gson();
@@ -67,7 +69,6 @@ public class ETurmalinaService implements Serializable{
 
 	private WSIntegracaoRHGETDADOSSERVIDOR criarDadosServidor(DadosServidorBean dadosServidor) {
 	    WSIntegracaoRHGETDADOSSERVIDOR request = new WSIntegracaoRHGETDADOSSERVIDOR();
-
 	    request.setUsuario(Parametros.DS_LOGIN_USUARIO_ETURMALINA.getValue());
 	    request.setSenha(Parametros.DS_SENHA_USUARIO_ETURMALINA.getValue());
 	    request.setCpf(dadosServidor.getCpf());
@@ -75,6 +76,21 @@ public class ETurmalinaService implements Serializable{
         request.setDatainicio("");
         request.setDatafim("");
         return request;
+	}
+
+	private void validarParametros( ) {
+		String msg = "O parâmetro '%s' não foi preenchido.";
+    	if(StringUtil.isEmpty(Parametros.DS_LOGIN_USUARIO_ETURMALINA.getValue())) {
+    		throw new EppConfigurationException(String.format(msg, Parametros.DS_LOGIN_USUARIO_ETURMALINA.getParametroDefinition().getNome()));
+    	}
+
+    	if(StringUtil.isEmpty(Parametros.DS_SENHA_USUARIO_ETURMALINA.getValue())) {
+    		throw new EppConfigurationException(String.format(msg, Parametros.DS_SENHA_USUARIO_ETURMALINA.getParametroDefinition().getNome()));
+    	}
+
+    	if(StringUtil.isEmpty(Parametros.DS_URL_SERVICO_ETURMALINA.getValue())) {
+    		throw new EppConfigurationException(String.format(msg, Parametros.DS_URL_SERVICO_ETURMALINA.getParametroDefinition().getNome()));
+    	}
 	}
 
 }
