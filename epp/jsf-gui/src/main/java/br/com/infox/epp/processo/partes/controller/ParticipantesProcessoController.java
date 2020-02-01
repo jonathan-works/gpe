713fdf42acd69fdfff9f274f3397d94ae3096dbe
Helio Lima <helio@infox.com.br>
2020-02-01 16:05:29 -0300

    Adiciona métodos de pesquisa e eventos de mudança de tipo de parte em aba de participantes ref #127494,#127495

--- epp/jsf-gui/src/main/java/br/com/infox/epp/processo/partes/controller/ParticipantesProcessoController.java
+++ epp/jsf-gui/src/main/java/br/com/infox/epp/processo/partes/controller/ParticipantesProcessoController.java
@@ -7,11 +7,20 @@ import javax.inject.Inject;
 import javax.inject.Named;
 import javax.xml.ws.Holder;
 
+import org.jboss.seam.faces.FacesMessages;
+
+import br.com.infox.core.util.StringUtil;
 import br.com.infox.epp.access.component.tree.ParticipanteProcessoTreeHandler;
 import br.com.infox.epp.access.entity.UsuarioLogin;
 import br.com.infox.epp.access.manager.UsuarioLoginManager;
 import br.com.infox.epp.cdi.ViewScoped;
 import br.com.infox.epp.fluxo.entity.Natureza;
+import br.com.infox.epp.loglab.contribuinte.type.TipoParticipanteEnum;
+import br.com.infox.epp.loglab.search.EmpresaSearch;
+import br.com.infox.epp.loglab.search.ServidorContribuinteSearch;
+import br.com.infox.epp.loglab.vo.EmpresaVO;
+import br.com.infox.epp.loglab.vo.PesquisaParticipanteVO;
+import br.com.infox.epp.loglab.vo.ServidorContribuinteVO;
 import br.com.infox.epp.pessoa.entity.PessoaFisica;
 import br.com.infox.epp.pessoa.type.TipoPessoaEnum;
 import br.com.infox.epp.processo.entity.Processo;
@@ -20,7 +29,10 @@ import br.com.infox.epp.processo.partes.entity.ParticipanteProcesso;
 import br.com.infox.epp.processo.partes.entity.TipoParte;
 import br.com.infox.epp.processo.partes.manager.TipoParteManager;
 import br.com.infox.epp.processo.partes.type.ParteProcessoEnum;
+import br.com.infox.jsf.util.JsfUtil;
 import br.com.infox.seam.security.SecurityUtil;
+import lombok.Getter;
+import lombok.Setter;
 
 @Named
 @ViewScoped
@@ -39,8 +51,30 @@ public class ParticipantesProcessoController extends AbstractParticipantesContro
     private ParticipanteProcessoTreeHandler participanteProcessoTree;
     @Inject
     private ParticipanteProcessoService participanteProcessoService;
+    @Inject
+    private ServidorContribuinteSearch servidorContribuinteSearch;
+    @Inject
+    private EmpresaSearch empresaSearch;
 
     protected List<TipoParte> tipoPartes;
+
+    @Getter
+    @Setter
+    private EmpresaVO empresaVO;
+
+    @Getter
+    private List<EmpresaVO> empresaList;
+
+    @Getter
+    @Setter
+    private ServidorContribuinteVO servidorContribuinteVO;
+
+    @Getter
+    private List<ServidorContribuinteVO> servidorContribuinteList;
+
+    @Getter
+    @Setter
+    private PesquisaParticipanteVO pesquisaParticipanteVO;
 
     @Override
     public void init(Holder<Processo> processoHolder) {
@@ -49,12 +83,41 @@ public class ParticipantesProcessoController extends AbstractParticipantesContro
         if (!podeAdicionarPartesFisicas() && podeAdicionarPartesJuridicas()) {
             setTipoPessoa(TipoPessoaEnum.J);
         }
+        limparDadosParticipante();
+    }
+
+    public void limparDadosParticipante() {
+        empresaVO = null;
+        empresaList = null;
+        pesquisaParticipanteVO = new PesquisaParticipanteVO();
+        pesquisaParticipanteVO.setTipoParticipante(TipoParticipanteEnum.CO);
+        limparServidorContribuinte();
+    }
+
+    public void limparServidorContribuinte() {
+        servidorContribuinteVO = null;
+        servidorContribuinteList = null;
+        limparCamposPesquisa();
+    }
+
+    public void limparCamposPesquisa() {
+        pesquisaParticipanteVO.setCpf(null);
+        pesquisaParticipanteVO.setMatricula(null);
+        pesquisaParticipanteVO.setNomeCompleto(null);
+        pesquisaParticipanteVO.setNomeFantasia(null);
+        pesquisaParticipanteVO.setRazaoSocial(null);
+        pesquisaParticipanteVO.setCnpj(null);
+    }
+
+    public void onChangeTipoPessoa() {
+        limparDadosParticipante();
     }
 
     @Override
     protected void clearParticipanteProcesso() {
         super.clearParticipanteProcesso();
         participanteProcessoTree.clearTree();
+        limparDadosParticipante();
     }
 
     @Override
@@ -179,4 +242,68 @@ public class ParticipantesProcessoController extends AbstractParticipantesContro
         }
     }
 
+    public void buscarServidorContribuinte() {
+        if (StringUtil.isEmpty(pesquisaParticipanteVO.getCpf())
+                && StringUtil.isEmpty(pesquisaParticipanteVO.getNomeCompleto())
+                && StringUtil.isEmpty(pesquisaParticipanteVO.getMatricula())) {
+            FacesMessages.instance().add("Por favor, preencha pelo menos um campo de busca.");
+        } else {
+            servidorContribuinteList = servidorContribuinteSearch.pesquisaServidorContribuinte(pesquisaParticipanteVO);
+
+            if(servidorContribuinteList != null && servidorContribuinteList.size() > 0) {
+                if(servidorContribuinteList.size() == 1) {
+                    servidorContribuinteVO = servidorContribuinteList.get(0);
+//                    onChangeParticipanteCpf();
+                    servidorContribuinteList = null;
+                } else {
+                    JsfUtil.instance().execute("PF('servidorContribuinteDialog').show();");
+                }
+            } else if(pesquisaParticipanteVO.getTipoParticipante().equals(TipoParticipanteEnum.CO)) {
+                servidorContribuinteVO = new ServidorContribuinteVO();
+                servidorContribuinteVO.setTipoParticipante(pesquisaParticipanteVO.getTipoParticipante());
+                servidorContribuinteVO.setCpf(pesquisaParticipanteVO.getCpf());
+                FacesMessages.instance().add("Contribuinte não encontrado. Preencha os dados para adicionar um novo.");
+            } else {
+                FacesMessages.instance().add("Nenhum registro foi encontrado com os dados da busca.");
+            }
+        }
+    }
+
+    public void selecionarServidorContribuinte(ServidorContribuinteVO row) {
+        servidorContribuinteVO = row;
+//        onChangeParticipanteCpf();
+        servidorContribuinteList = null;
+        JsfUtil.instance().execute("PF('servidorContribuinteDialog').hide();");
+    }
+
+    public void buscarEmpresa() {
+        if (StringUtil.isEmpty(pesquisaParticipanteVO.getCnpj())
+                && StringUtil.isEmpty(pesquisaParticipanteVO.getNomeFantasia())
+                && StringUtil.isEmpty(pesquisaParticipanteVO.getRazaoSocial())) {
+            FacesMessages.instance().add("Por favor, preencha pelo menos um campo de busca.");
+        } else {
+            empresaList = empresaSearch.pesquisaEmpresaVO(pesquisaParticipanteVO);
+
+            if(empresaList != null && empresaList.size() > 0) {
+                if(empresaList.size() == 1) {
+                    empresaVO = empresaList.get(0);
+//                    onChangeParticipanteCnpj();
+                    empresaList = null;
+                } else {
+                    JsfUtil.instance().execute("PF('empresaDialog').show();");
+                }
+            } else {
+                empresaVO = new EmpresaVO();
+                empresaVO.setCnpj(pesquisaParticipanteVO.getCnpj());
+                FacesMessages.instance().add("Registro não encontrado. Preencha os dados para adicionar um novo.");
+            }
+        }
+    }
+
+    public void selecionarEmpresa(EmpresaVO row) {
+        empresaVO = row;
+//        onChangeParticipanteCnpj();
+        empresaList = null;
+        JsfUtil.instance().execute("PF('empresaDialog').hide();");
+    }
 }
