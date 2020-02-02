075e0725cee7a6e866265a1822ab331d938b67b4
Helio Lima <helio@infox.com.br>
2020-02-01 22:24:34 -0300

    Aprimora ParticipanteProcessoVO refs #127494,#127495

--- epp/jsf-gui/src/main/java/br/com/infox/epp/loglab/vo/ParticipanteProcessoVO.java
+++ epp/jsf-gui/src/main/java/br/com/infox/epp/loglab/vo/ParticipanteProcessoVO.java
@@ -16,10 +16,35 @@ public class ParticipanteProcessoVO {
 
     private TipoPessoaEnum tipoPessoa = TipoPessoaEnum.F;
     private Integer idProcesso;
+    private Integer idParticipantePai;
     private String cdTipoParte;
     private Date dataInicio;
     private Date dataFim;
     private EmpresaVO empresaVO;
     private ServidorContribuinteVO servidorContribuinteVO;
+
+    public Integer getIdPessoa() {
+        if(tipoPessoa.equals(TipoPessoaEnum.F)) {
+            return servidorContribuinteVO != null ? servidorContribuinteVO.getIdPessoaFisica() : null;
+        } else {
+            return empresaVO != null ? empresaVO.getIdPessoaJuridica() : null;
+        }
+    }
+
+    public String getCodigoPessoa() {
+        if(tipoPessoa.equals(TipoPessoaEnum.F)) {
+            return servidorContribuinteVO != null ? servidorContribuinteVO.getCpf() : null;
+        } else {
+            return empresaVO != null ? empresaVO.getCnpj() : null;
+        }
+    }
+
+    public String getNome() {
+        if(tipoPessoa.equals(TipoPessoaEnum.F)) {
+            return servidorContribuinteVO != null ? servidorContribuinteVO.getNomeCompleto() : null;
+        } else {
+            return empresaVO != null ? empresaVO.getNomeFantasia() : null;
+        }
+    }
 
 }
