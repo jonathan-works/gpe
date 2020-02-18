6693d05428d733c96b3fad4e1804a176600d6714
Helio Lima <helio@infox.com.br>
2020-02-18 09:30:40 -0300

    Exibe lista de certificados gerados na página Certificado Eletrônico podendo reemitir para cada pessoa física refs #127875

--- epp/jsf-gui/src/main/java/br/com/infox/epp/certificadoeletronico/view/CertificadoEletronicoView.java
+++ epp/jsf-gui/src/main/java/br/com/infox/epp/certificadoeletronico/view/CertificadoEletronicoView.java
@@ -2,6 +2,7 @@ package br.com.infox.epp.certificadoeletronico.view;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
@@ -9,8 +10,10 @@ import javax.inject.Named;
 
 import br.com.infox.epp.cdi.ViewScoped;
 import br.com.infox.epp.cdi.exception.ExceptionHandled;
+import br.com.infox.epp.certificadoeletronico.CertificadoEletronicoSearch;
 import br.com.infox.epp.certificadoeletronico.CertificadoEletronicoService;
 import br.com.infox.epp.certificadoeletronico.entity.CertificadoEletronico;
+import br.com.infox.seam.exception.BusinessException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -27,8 +30,12 @@ public class CertificadoEletronicoView implements Serializable {
     private Date dataFim;
     @Getter
     private boolean exibeBotaoGerarCertificado;
+    @Getter
+    private List<CertificadoEletronicoVO> certificadosGerados;
     @Inject
     private CertificadoEletronicoService certificadoEletronicoService;
+    @Inject
+    private CertificadoEletronicoSearch certificadoEletronicoSearch;
 
     @PostConstruct
     private void init() {
@@ -41,6 +48,7 @@ public class CertificadoEletronicoView implements Serializable {
             this.dataInicio = certificadoEletronicoRaiz.getDataInicio();
             this.dataFim = certificadoEletronicoRaiz.getDataFim();
             this.exibeBotaoGerarCertificado = false;
+            this.certificadosGerados = certificadoEletronicoSearch.getListaCertificadoEletronicoVO();
         } else {
             this.exibeBotaoGerarCertificado = true;
         }
@@ -50,6 +58,16 @@ public class CertificadoEletronicoView implements Serializable {
     public void gerarCertificadoRaiz() {
         certificadoEletronicoService.gerarCertificadoRaiz();
         refreshInitValues();
+    }
+
+    @ExceptionHandled(successMessage = "Certificado reemitido com sucesso")
+    public void reemitirCertificado(CertificadoEletronicoVO certificadoEletronicoVO) {
+        if(certificadoEletronicoVO != null) {
+            certificadoEletronicoService.reemitirCertificadoEletronico(certificadoEletronicoVO.getIdPessoa());
+            refreshInitValues();
+        } else {
+            throw new BusinessException("Houve um erro ao tentar reemitir");
+        }
     }
 
 }
