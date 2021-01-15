bdb104dfc53de3b9909a9ef20d151fe866490b33
Gabriel Morrison Lima Dantas <gabriel@infox.com.br>
2021-01-15 15:41:10 -0300

    Acesso aos Processos Legados refs #133492

--- epp/jsf-gui/src/main/java/br/com/infox/jwt/claims/InfoxPrivateClaims.java
+++ epp/jsf-gui/src/main/java/br/com/infox/jwt/claims/InfoxPrivateClaims.java
@@ -4,16 +4,17 @@ import br.com.infox.core.exception.SystemExceptionFactory;
 import br.com.infox.jwt.JWTErrorCodes;
 public enum InfoxPrivateClaims implements JWTClaim {
     LOGIN("login",BasicJWTValidators.STRING),
-    CPF("cpf",InfoxJWTValidators.CPF);
+    CPF("cpf",InfoxJWTValidators.CPF),
+    NOME_USUARIO("nomeUsuario", BasicJWTValidators.STRING);
     public static final String NAMESPACE="http://www.infox.com.br";
     private final String key;
     private final JWTValidator validator;
 
     private InfoxPrivateClaims(String key, JWTValidator validator) {
         this.key = key;
-        this.validator = validator; 
+        this.validator = validator;
     }
-    
+
     @Override
     public String getClaim(){
         return String.format("%s/%s", NAMESPACE,key);
@@ -23,7 +24,7 @@ public enum InfoxPrivateClaims implements JWTClaim {
     public JWTValidator validator() {
         return this.validator;
     }
-    
+
 }
 
 class InfoxJWTValidators {
@@ -32,11 +33,11 @@ class InfoxJWTValidators {
         public void validate(Object value) {
             if (value == null || String.valueOf(value).trim().isEmpty())
                 throw SystemExceptionFactory.create(JWTErrorCodes.INVALID_CLAIM).set("value", value)
-                    .set("expected","valid cpf");
+                .set("expected","valid cpf");
         }
-        
+
     };
-    
+
     private InfoxJWTValidators(){
     }
 }
