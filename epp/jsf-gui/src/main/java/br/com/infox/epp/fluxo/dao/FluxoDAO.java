d50076c656dadc1fb591ec389c4f7c013778bd05
Gustavo <gustavomarcone@gmail.com>
2021-05-03 16:41:09 -0300

    Ordenar tarefas por nome - refs #135126

--- epp/jsf-gui/src/main/java/br/com/infox/epp/fluxo/dao/FluxoDAO.java
+++ epp/jsf-gui/src/main/java/br/com/infox/epp/fluxo/dao/FluxoDAO.java
@@ -30,6 +30,7 @@ import javax.persistence.criteria.Subquery;
 import org.jboss.seam.annotations.AutoCreate;
 import org.jboss.seam.annotations.Name;
 import org.jbpm.taskmgmt.def.Task;
+
 import br.com.infox.core.dao.DAO;
 import br.com.infox.epp.access.entity.Localizacao;
 import br.com.infox.epp.access.entity.Localizacao_;
@@ -178,15 +179,17 @@ public class FluxoDAO extends DAO<Fluxo> {
                 cb.isNull(p.get(Processo_.dataFim)));
         return getEntityManager().createQuery(cq).getSingleResult();
     }
-    
+
     public List<Task> getListaTaskByFluxo(Fluxo fluxo){
     	CriteriaBuilder cb = getEntityManager().getCriteriaBuilder();
         CriteriaQuery<Task> cq = cb.createQuery(Task.class);
         Root<Task> taskRoot = cq.from(Task.class);
         cq.select(taskRoot);
-        
+
+        cq.orderBy(cb.asc(taskRoot.get("name")));
+
         List<Task> listaTarefa = getEntityManager().createQuery(cq).getResultList();
-        Iterator<Task> iteratorTask = listaTarefa.iterator(); 
+        Iterator<Task> iteratorTask = listaTarefa.iterator();
         while(iteratorTask.hasNext()) {
         	Task taskIt = iteratorTask.next();
         	if(!taskIt.getProcessDefinition().getName().equals(fluxo.getFluxo())) {
@@ -200,8 +203,8 @@ public class FluxoDAO extends DAO<Fluxo> {
         		}
         	}
         }
-        
-        
+
+
         return listaTarefa;
     }
 }
