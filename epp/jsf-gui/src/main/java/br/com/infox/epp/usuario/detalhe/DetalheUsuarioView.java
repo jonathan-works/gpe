d67e4d75b3a06ef6dde6465df345880669a36b91
Helio Lima <helio@infox.com.br>
2021-09-23 17:21:47 -0300

    Ajusta página de detalhes do usuário para permitir cadastrar imagem de assinatura eletrônica refs #136607

--- epp/jsf-gui/src/main/java/br/com/infox/epp/usuario/detalhe/DetalheUsuarioView.java
+++ epp/jsf-gui/src/main/java/br/com/infox/epp/usuario/detalhe/DetalheUsuarioView.java
@@ -1,5 +1,6 @@
 package br.com.infox.epp.usuario.detalhe;
 
+import java.io.IOException;
 import java.io.Serializable;
 import java.util.Date;
 
@@ -9,12 +10,22 @@ import javax.inject.Inject;
 import javax.inject.Named;
 import javax.servlet.http.HttpServletRequest;
 
+import org.richfaces.event.FileUploadEvent;
+import org.richfaces.model.UploadedFile;
+
+import br.com.infox.core.util.ArrayUtil;
 import br.com.infox.epp.access.TermoAdesaoService;
 import br.com.infox.epp.access.api.Authenticator;
 import br.com.infox.epp.access.entity.UsuarioLogin;
+import br.com.infox.epp.assinaturaeletronica.AssinaturaEletronicaDTO;
+import br.com.infox.epp.assinaturaeletronica.AssinaturaEletronicaSearch;
+import br.com.infox.epp.assinaturaeletronica.AssinaturaEletronicaService;
+import br.com.infox.epp.assinaturaeletronica.AssinaturaEletronicaServlet;
 import br.com.infox.epp.cdi.ViewScoped;
 import br.com.infox.epp.cdi.exception.ExceptionHandled;
 import br.com.infox.epp.certificadoeletronico.CertificadoEletronicoService;
+import br.com.infox.log.LogProvider;
+import br.com.infox.log.Logging;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -22,6 +33,8 @@ import lombok.Setter;
 @ViewScoped
 public class DetalheUsuarioView implements Serializable {
     private static final long serialVersionUID = 1L;
+
+    private static final LogProvider LOG = Logging.getLogProvider(DetalheUsuarioView.class);
 
     @Getter
     @Setter
@@ -47,6 +60,17 @@ public class DetalheUsuarioView implements Serializable {
     @Getter
     private boolean podeExibirTermoAdesao = false;
 
+    private UploadedFile uploadedFile;
+
+    @Getter
+    private AssinaturaEletronicaDTO assinaturaEletronicaAtualDTO = null;
+    @Getter
+    private AssinaturaEletronicaDTO assinaturaEletronicaNovoDTO = null;
+    @Inject
+    private AssinaturaEletronicaSearch assinaturaEletronicaSearch;
+    @Inject
+    private AssinaturaEletronicaService assinaturaEletronicaService;
+
     @PostConstruct
     private void init() {
         possuiCertificadoEmitido = false;
@@ -61,6 +85,7 @@ public class DetalheUsuarioView implements Serializable {
         this.podeExibirTermoAdesao = usuario.getPessoaFisica() != null
                 && usuario.getPessoaFisica().getTermoAdesao() != null;
         if (usuario.getPessoaFisica() != null) {
+            this.assinaturaEletronicaAtualDTO = assinaturaEletronicaSearch.getAssinaturaEletronicaDTOByIdPessoaFisica(usuario.getPessoaFisica().getIdPessoa());
             this.possuiCertificadoEmitido = usuario.getPessoaFisica().getCertificadoEletronico() != null;
             this.podeEmitirCertificado = !this.possuiCertificadoEmitido
                     || !usuario.getPessoaFisica().getCertificadoEletronico().isAtivo();
@@ -85,6 +110,65 @@ public class DetalheUsuarioView implements Serializable {
 
     private HttpServletRequest getHttpServletRequest() {
         return (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
+    }
+
+    @ExceptionHandled
+    public void uploadAssinaturaEletronica(FileUploadEvent fileUploadEvent) {
+        uploadedFile = fileUploadEvent.getUploadedFile();
+
+        try {
+            assinaturaEletronicaNovoDTO = new AssinaturaEletronicaDTO();
+            assinaturaEletronicaNovoDTO.setIdPessoa(getIdPessoaFisicaLogada());
+            assinaturaEletronicaNovoDTO.setExtensao(uploadedFile.getFileExtension());
+            assinaturaEletronicaNovoDTO.setNomeArquivo(uploadedFile.getName());
+            assinaturaEletronicaNovoDTO.setContentType(uploadedFile.getContentType());
+            assinaturaEletronicaNovoDTO.setImagem(ArrayUtil.copyOf(uploadedFile.getData()));
+        } finally {
+            limpaUploadAssinaturaEletronica();
+        }
+    }
+
+    @ExceptionHandled
+    public void limpaUploadAssinaturaEletronica() {
+        if(uploadedFile != null) {
+            try {
+                uploadedFile.delete();
+            } catch (IOException e) {
+                LOG.error(".limpaUploadAssinaturaEletronica", e);
+            }
+        }
+    }
+
+    @ExceptionHandled(successMessage = "#{infoxMessages['assinaturaEletronica.assinaturaSalva']}")
+    public void salvarAssinaturaEletronica() {
+        assinaturaEletronicaAtualDTO = assinaturaEletronicaService.salvar(assinaturaEletronicaNovoDTO);
+        assinaturaEletronicaNovoDTO = null;
+    }
+
+    @ExceptionHandled(successMessage = "#{infoxMessages['assinaturaEletronica.assinaturaRemovida']}")
+    public void removerAssinaturaEletronica() {
+        assinaturaEletronicaService.remove(assinaturaEletronicaAtualDTO);
+        assinaturaEletronicaAtualDTO = null;
+    }
+
+    private Integer getIdPessoaFisicaLogada() {
+        UsuarioLogin usuario = Authenticator.getUsuarioLogado();
+        return usuario.getPessoaFisica().getIdPessoa();
+    }
+
+    public String getLinkAssinaturaEletronica() {
+        String link = "";
+
+        if(assinaturaEletronicaAtualDTO != null) {
+            link = String.format("%s%s%s", FacesContext.getCurrentInstance().getExternalContext().getRequestContextPath(),
+                    AssinaturaEletronicaServlet.SERVLET_BASE_URL, assinaturaEletronicaAtualDTO.getUuid().toString());
+        }
+        return link;
+    }
+
+    public boolean podeConfigurarAssinaturaEletronica() {
+        UsuarioLogin usuario = Authenticator.getUsuarioLogado();
+        return usuario.getPessoaFisica() != null;
     }
 
 }
