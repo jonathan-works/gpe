e8f9d7c3acdb6b7be1e8981fbb0a23a07d1da74f
Eliton Santos <eliton@infox.com.br>
2021-10-19 16:12:19 -0300

    Incluido esqueleto da pagina de relatorio analitico de processos e filtros refs #136734

---
+++ epp/jsf-gui/src/main/java/br/com/infox/epp/relatorio/AcumuladoSinteticoProcessosView.java
@@ -0,0 +1,51 @@
+package br.com.infox.epp.relatorio;
+
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import br.com.infox.core.util.DateUtil;
+import br.com.infox.epp.cdi.ViewScoped;
+import br.com.infox.epp.fluxo.dao.FluxoDAO;
+import br.com.infox.epp.fluxo.entity.Fluxo;
+import lombok.Getter;
+import lombok.Setter;
+
+@Named
+@ViewScoped
+public class AcumuladoSinteticoProcessosView implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Inject
+	private FluxoDAO fluxoDAO;
+	
+	@Getter @Setter
+	private List<Fluxo> listaAssunto;
+	@Getter @Setter
+	private List<String> listaStatus;
+	@Getter @Setter
+	private List<String> listaMes;
+	
+	@Getter @Setter
+	private List<Fluxo> listaAssuntoSelecionado;
+	@Getter @Setter
+	private List<String> listaStatusSelecionado;
+	@Getter @Setter
+	private List<String> listaMesSelecionado;
+	@Getter @Setter
+	private Integer ano;
+	
+	
+	@PostConstruct
+	private void init() {
+		listaAssunto = fluxoDAO.getFluxosPrimariosAtivos();
+		listaStatus = Arrays.asList("Em andamento", "Arquivados/Finalizados");
+		listaMes = DateUtil.getListaTodosMeses();
+	}
+
+}
